9c4324eb8426255220b62b05f5220558d208ef10
David Hedbor <david@hedbor.org>
1995-08-18 19:39:39 +0200

    werg
    Rev: src/modules/spider/conf.h:1.1
    Rev: src/modules/spider/sdebug.c:1.1

---
+++ src/modules/spider/conf.h
@@ -0,0 +1,9 @@
+extern void f__dump_string_table(INT32 args);
+extern void f__string_debug(INT32 args);
+extern void f__dump_obj_table(INT32 args);
+extern void f__num_objects(INT32 args);
+extern void f__num_mappings(INT32 args);
+extern void f__num_arrays(INT32 args);
+extern void f__num_dest_objects(INT32 args);
+extern void f_discdate(INT32 argc);
+extern void f_stardate (INT32 args);
---
+++ src/modules/spider/sdebug.c
@@ -0,0 +1,113 @@
+#include "stralloc.h"
+#include "global.h"
+#include "types.h"
+#include "macros.h"
+#include "object.h"
+#include "add_efun.h"
+#include "interpret.h"
+#include "svalue.h"
+#include "mapping.h"
+#include "array.h"
+#include "builtin_efuns.h"
+#include "spider.h"
+#include "config.h"
+#include "program.h"
+
+extern void dump_stralloc_strings(void);
+void f__dump_string_table(INT32 args)
+{
+  pop_n_elems(args);
+  dump_stralloc_strings();
+  push_int(0);
+}
+
+void f__string_debug(INT32 args)
+{
+  pop_n_elems(args);
+  push_string(add_string_status(args));
+}
+
+static char *program_name(struct program *p)
+{
+  char *f;
+  f=(char *)(p->linenumbers+1);
+  if(!p->linenumbers || !strlen(f))
+  {
+    p->refs++;
+    push_program(p);
+    APPLY_MASTER("program_name", 1);
+    if(sp[-1].type == T_STRING)
+      return 0;
+    pop_stack();
+    f="Unknown program";
+  }
+  return f;
+}
+
+void f__dump_obj_table(INT32 args)
+{
+  struct object *o;
+  int n=0;
+  pop_n_elems(args);
+  o=first_object;
+  while(o) 
+  { 
+    if(o->prog)
+    {
+      char *f;
+      f=program_name(o->prog);
+      if(f)
+	push_string(make_shared_string(f));
+    } else {
+      push_string(make_shared_binary_string("No program (Destructed?)",24));
+    }
+    push_int(o->refs);
+    f_aggregate(2);
+    ++n;
+    o=o->next; 
+  }
+  f_aggregate(n);
+}
+
+void f__num_objects(INT32 args)
+{
+  struct object *o;
+  int n=0;
+  pop_n_elems(args);
+  o=first_object;
+  while(o) { ++n; o=o->next; }
+  push_int(n);
+}
+
+void f__num_mappings(INT32 args)
+{
+  extern struct mapping *first_mapping;
+  struct mapping *o;
+  int n=0;
+  pop_n_elems(args);
+  o=first_mapping;
+  while(o) { ++n; o=o->next; }
+  push_int(n);
+}
+
+void f__num_arrays(INT32 args)
+{
+  extern struct array empty_array;
+  struct array *o;
+  int n=0;
+  pop_n_elems(args);
+  o=(&empty_array)->next;
+  while(o && o != &empty_array) { ++n; o=o->next; }
+  push_int(n+1);
+}
+
+void f__num_dest_objects(INT32 args)
+{
+  struct object *o;
+  int n=0;
+  pop_n_elems(args);
+  o=first_object;
+  while(o) { if(!o->prog) n++; o=o->next; }
+  push_int(n);
+}
+
