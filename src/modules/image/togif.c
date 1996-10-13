5a5a44f21ee2dbeeb1356a11ed55510c42d4f3be
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1996-10-13 19:36:45 +0200

    togif.c: transparency cancelled when color not used
    Rev: src/modules/image/togif.c:1.5

--- src/modules/image/togif.c
+++ src/modules/image/togif.c
@@ -167,14 +167,20 @@ struct pike_string *
 
    if (transparent)
    {
-      low_my_putchar( '!', &buf );  /* extras */
-      low_my_putchar( 0xf9, &buf ); /* transparency */
-      low_my_putchar( 4, &buf );
-      low_my_putchar( 1, &buf );
-      low_my_putchar( 0, &buf );
-      low_my_putchar( 0, &buf );
-      low_my_putchar( colortable_rgb(ct,*transparent), &buf );
-      low_my_putchar( 0, &buf );
+      i=colortable_rgb(ct,*transparent);
+      if (ct->clut[i].r==transparent->r 
+	  && ct->clut[i].g==transparent->g 
+	  && ct->clut[i].b==transparent->b)
+      {
+	 low_my_putchar( '!', &buf );  /* extras */
+	 low_my_putchar( 0xf9, &buf ); /* transparency */
+	 low_my_putchar( 4, &buf );
+	 low_my_putchar( 1, &buf );
+	 low_my_putchar( 0, &buf );
+	 low_my_putchar( 0, &buf );
+	 low_my_putchar( i, &buf );
+	 low_my_putchar( 0, &buf );
+      }
    }
 
 
