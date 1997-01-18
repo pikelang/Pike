8dee0d7ef95997c516afa7ee3345fc06c9b494cf
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1997-01-18 13:30:58 -0800

    Now creates a file called 'dependencies' which is then included by autoconf.
    Rev: bin/fixdepends.sh:1.2
    Rev: src/modules/regexp/Makefile.in:1.3
    Rev: src/modules/spider/Makefile.in:1.5
    Rev: src/modules/sprintf/Makefile.in:1.3
    Rev: src/modules/ssleay/Makefile.in:1.1
    Rev: src/modules/zlibmod/Makefile.in:1.1

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-cd $1
-
-(
-  cat Makefile.src
-  echo "# Depencies begin here"
-  sed 's@[-/a-zA-Z0-9.,_]*/\([-a-zA-Z0-9.,_]*\)@\1@g'
-) > Makefile.in
+sed 's@[-/a-zA-Z0-9.,_]*/\([-a-zA-Z0-9.,_]*\)@\1@g' > $1/dependencies
 
