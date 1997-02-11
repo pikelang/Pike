471ee64c235260aac229c8893dd276adcd4621af
Henrik Grubbström (Grubba) <grubba@grubba.org>
1997-02-11 19:46:57 +0100

    Added
    Rev: src/modules/Regexp/acconfig.h:1.1

---
+++ src/modules/Regexp/acconfig.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: acconfig.h,v 1.1 1997/02/11 18:46:57 grubba Exp $
+ */
+
+#ifndef PIKE_MODULES_REGEXP_H
+#define PIKE_MODULES_REGEXP_H
+
+@TOP@
+
+/* Define if you want to use the systems regexp's */
+#undef USE_SYSTEM_REGEXP
+
+@BOTTOM@
+
+#endif /* PIKE_MODULES_REGEXP_H */
