6e79af0f01248e37f3c3ae927fbc262639487be3
Niels Möller <nisse@lysator.liu.se>
1997-02-12 07:12:21 +0100

    New file, defining INT8, INT16 and INT32, even if there's no pike.
    Rev: src/modules/_Crypto/include/crypto_types.h:1.1

---
+++ src/modules/_Crypto/include/crypto_types.h
@@ -0,0 +1,16 @@
+/* crypto_types.h
+ *
+ * Defines the types INT32 and INT8 */
+
+#ifndef CRYPTO_TYPES_H_INCLUDED
+#define CRYPTO_TYPES_H_INCLUDED
+
+#ifdef PIKE
+#include <types.h>
+#else /* !PIKE */
+#define INT32 long
+#define INT16 short
+#define INT8 char
+#endif
+
+#endif /* CRYPTO_TYPES_H_INCLUDED */
