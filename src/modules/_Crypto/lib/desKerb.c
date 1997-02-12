ceed78e26f849209dc375311de61bd22047b33d0
Niels Möller <nisse@lysator.liu.se>
1997-02-12 07:25:17 +0100

    From the desCore package
    Rev: src/modules/_Crypto/lib/desCode.h:1.1
    Rev: src/modules/_Crypto/lib/desKerb.c:1.1
    Rev: src/modules/_Crypto/lib/desQuick.c:1.1
    Rev: src/modules/_Crypto/lib/desTest.c:1.1
    Rev: src/modules/_Crypto/lib/desUtil.c:1.1
    Rev: src/modules/_Crypto/lib/descore.README:1.1
    Rev: src/modules/_Crypto/lib/desdata.c:1.1
    Rev: src/modules/_Crypto/lib/desinfo.h:1.1

---
+++ src/modules/_Crypto/lib/desKerb.c
@@ -0,0 +1,31 @@
+/*
+ *	des - fast & portable DES encryption & decryption.
+ *	Copyright (C) 1992  Dana L. How
+ *	Please see the file `README' for the complete copyright notice.
+ */
+
+#include "des.h"
+
+#include "RCSID.h"
+RCSID(desKerb_cRcs, "$Id: desKerb.c,v 1.1 1997/02/12 06:24:32 nisse Exp $");
+
+/* permit the default style of des functions to be changed */
+
+DesFunc *DesCryptFuncs[2] = { DesSmallFipsDecrypt, DesSmallFipsEncrypt };
+
+/* kerberos-compatible key schedule function */
+
+int
+des_key_sched(INT8 *k, INT32 *s)
+{
+	return DesMethod(s, k);
+}
+
+/* kerberos-compatible des coding function */
+
+int
+des_ecb_encrypt(INT8 *s, INT8 *d, INT32 *r, int e)
+{
+	(*DesCryptFuncs[e])(d, r, s);
+	return 0;
+}
---
+++ src/modules/_Crypto/lib/desQuick.c
@@ -0,0 +1,54 @@
+/*
+ *	des - fast & portable DES encryption & decryption.
+ *	Copyright (C) 1992  Dana L. How
+ *	Please see the file `README' for the complete copyright notice.
+ *
+ * Slightly edited by Niels Möller, 1997
+ */
+
+#include "des.h"
+
+#include "RCSID.h"
+RCSID(desQuick_cRcs, "$Id: desQuick.c,v 1.1 1997/02/12 06:24:41 nisse Exp $");
+
+extern unsigned INT32 des_keymap[];
+
+
+/* static information */
+
+static depth = 0;		/* keep track of the request depth */
+unsigned INT32 des_bigmap[0x4000];	/* big lookup table */
+
+/* fill in the 64k table used by the `quick' option */
+
+void
+DesQuickInit()
+{
+	int s1, s3, x;
+	unsigned INT32 * t0, * t1, * t2, * t3;
+
+	if ( depth++ )
+		return;
+
+	t0 = des_bigmap;
+	t1 = t0 + 64;
+	t2 = t1 + 64;
+	t3 = t2 + 64;
+
+	for ( s3 = 63; s3 >= 0; s3-- ) {
+		for ( s1 = 63; s1 >= 0; s1-- ) {
+			x = (s3 << 8) | s1;
+			t0[x] = des_keymap[s3+128] ^ des_keymap[s1+192];
+			t1[x] = des_keymap[s3    ] ^ des_keymap[s1+ 64];
+			t2[x] = des_keymap[s3+384] ^ des_keymap[s1+448];
+			t3[x] = des_keymap[s3+256] ^ des_keymap[s1+320];
+		}
+	}
+}
+
+/* free the 64k table, if necessary */
+
+void
+DesQuickDone()
+{
+}
