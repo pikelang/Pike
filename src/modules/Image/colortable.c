45138380ceb9ba3712d01ccfaf08adb2abcd526f
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1997-05-01 14:38:41 +0200

    colortable, take 3; an object
    Rev: src/modules/Image/colortable.c:1.1

---
+++ src/modules/Image/colortable.c
@@ -0,0 +1,104 @@
+#include <config.h>
+
+/* $Id: colortable.c,v 1.1 1997/05/01 12:38:41 mirar Exp $ */
+
+/*
+**! module Image
+**! class colortable
+**!
+**!	This object keeps colortable information,
+**!	mostly for image re-coloring (quantization).
+**!
+**! see also: Image, Image.image, Image.font
+*/
+
+
+#include "global.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <netinet/in.h>
+#include <errno.h>
+
+#include "config.h"
+
+#include "stralloc.h"
+#include "types.h"
+#include "pike_macros.h"
+#include "object.h"
+#include "constants.h"
+#include "interpret.h"
+#include "svalue.h"
+#include "array.h"
+#include "threads.h"
+#include "builtin_functions.h"
+
+#include "image.h"
+
+
+struct program *colortable_program;
+
+#define THIS (*(struct colortable **)(fp->current_storage))
+#define THISOBJ (fp->current_object)
+
+#if 0
+
+/***************** init & exit *********************************/
+
+static inline void free_colortable_struct(struct colortable *colortable)
+{
+   if (colortable)
+   {
+      if (colortable->mem)
+      {
+#ifdef HAVE_MMAP
+	 munmap(colortable->mem,colortable->mmaped_size);
+#else
+	 free(colortable->mem);
+#endif
+      }
+      free(colortable);
+   }
+}
+
+static void init_colortable_struct(struct object *o)
+{
+  THIS=NULL;
+}
+
+static void exit_colortable_struct(struct object *obj)
+{
+   free_colortable_struct(THIS);
+   THIS=NULL;
+}
+
+/***************** global init etc *****************************/
+
+/*
+
+*/
+
+void init_colortable_programs(void)
+{
+   start_new_program();
+   add_storage(sizeof(struct colortable*));
+
+   set_init_callback(init_colortable_struct);
+   set_exit_callback(exit_colortable_struct);
+  
+   colortable_program=end_program();
+   add_program_constant("colortable",colortable_program, 0);
+}
+
+void exit_colortable(void) 
+{
+  if(colortable_program)
+  {
+    free_program(colortable_program);
+    colortable_program=0;
+  }
+}
+
+
+#endif
