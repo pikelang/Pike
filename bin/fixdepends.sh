19397023c583de0b791fdf7b9a392062e05131a9
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1997-05-19 02:02:32 -0700

    Bugfix for language.yacc dependencies
    Rev: bin/fixdepends.sh:1.3.2.1

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sed "s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
+sed -e "s@/./@/@g
+s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
 
 # sed "s@[-/a-zA-Z0-9.,_][-/a-zA-Z0-9.,_]*/\([-a-zA-Z0-9.,_]*\)@\1@g" > $1/dependencies
 
