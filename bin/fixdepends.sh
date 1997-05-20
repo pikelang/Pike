71b9c10bdeb54cfc36bdc38e0b053d669ac6deab
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1997-05-19 20:37:46 -0700

    bugfix
    Rev: bin/fixdepends.sh:1.5
    Rev: src/modules/files/configure.in:1.16

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sed -e "s@/./@/@g
-s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
+sed -e "s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
+
+#sed -e "s@/./@/@g
+#s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
 
 # sed "s@[-/a-zA-Z0-9.,_][-/a-zA-Z0-9.,_]*/\([-a-zA-Z0-9.,_]*\)@\1@g" > $1/dependencies
 
