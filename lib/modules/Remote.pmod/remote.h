22ff158cb21aa37ab7db3f08c7feb7c2fd458be1
David Kågedal <david@kagedal.org>
1997-09-23 19:36:48 +0200

    Abstraherade kodningen med lite defines.
    Rev: lib/modules/Remote.pmod/remote.h:1.2

--- lib/modules/Remote.pmod/remote.h
+++ lib/modules/Remote.pmod/remote.h
@@ -16,3 +16,14 @@ program Context = ((program)"context");
 program Connection = ((program)"connection");
 program Call = ((program)"call");
 program Obj = ((program)"obj");
+
+#define CTX_ERROR      0
+#define CTX_OTHER      1
+#define CTX_OBJECT     2
+#define CTX_FUNCTION   3
+#define CTX_PROGRAM    3
+#define CTX_CALL_SYNC  4
+#define CTX_ARRAY      5
+#define CTX_RETURN     6
+#define CTX_MAPPING    7
+#define CTX_CALL_ASYNC 8
