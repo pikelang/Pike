1b2eafe1bfcf914835b9e6ea699188782eccca9c
Henrik Grubbström (Grubba) <grubba@grubba.org>
1997-10-14 15:33:41 +0200

    Renamed h_errno to gh_errno to avoid problems if h_errno is a #define.
    Rev: src/modules/system/system.c:1.29

--- src/modules/system/system.c
+++ src/modules/system/system.c
@@ -1,5 +1,5 @@
 /*
- * $Id: system.c,v 1.28 1997/09/17 10:31:52 hubbe Exp $
+ * $Id: system.c,v 1.29 1997/10/14 13:33:41 grubba Exp $
  *
  * System-call module for Pike
  *
@@ -14,7 +14,7 @@
 #include "system.h"
 
 #include <global.h>
-RCSID("$Id: system.c,v 1.28 1997/09/17 10:31:52 hubbe Exp $");
+RCSID("$Id: system.c,v 1.29 1997/10/14 13:33:41 grubba Exp $");
 #include <module_support.h>
 #include <las.h>
 #include <interpret.h>
@@ -562,16 +562,16 @@ int my_isipnr(char *s)
     struct hostent *ret; \
     struct hostent result; \
     char data[2048]; \
-    int h_errno
+    int gh_errno
 
 #define CALL_GETHOSTBYNAME(X) \
     THREADS_ALLOW(); \
-    ret=gethostbyname_r((X), &result, data, sizeof(data), &h_errno); \
+    ret=gethostbyname_r((X), &result, data, sizeof(data), &gh_errno); \
     THREADS_DISALLOW()
 
 #define CALL_GETHOSTBYADDR(X,Y,Z) \
     THREADS_ALLOW(); \
-    ret=gethostbyaddr_r((X),(Y),(Z), &result, data, sizeof(data), &h_errno); \
+    ret=gethostbyaddr_r((X),(Y),(Z), &result, data, sizeof(data), &gh_errno); \
     THREADS_DISALLOW()
 
 #else /* HAVE_SOLARIS_GETHOSTBYNAME_R */
@@ -842,7 +842,7 @@ void pike_module_init(void)
            OPT_TRY_OPTIMIZE);
   add_efun("gethostbyaddr", f_gethostbyaddr, "function(string:array)",
            OPT_TRY_OPTIMIZE);
-#endif /* HAVE_GETHOSTBYNAME */
+#endif /* GETHOST_DECLARE */
 
   /*
    * From syslog.c:
