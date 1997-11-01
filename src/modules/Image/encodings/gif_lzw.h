4779483564351645de98f740db70f1826bd8561a
Fredrik Noring <noring@nocrew.org>
1997-11-01 15:30:20 +0100

    dummy.
    Rev: src/modules/Image/encodings/gif_lzw.h:1.3

--- src/modules/Image/encodings/gif_lzw.h
+++ src/modules/Image/encodings/gif_lzw.h
@@ -1,11 +1,12 @@
 /*
 **! module Image
 **! note
-**!	$Id: gif_lzw.h,v 1.2 1997/10/27 22:41:37 mirar Exp $
+**!	$Id: gif_lzw.h,v 1.3 1997/11/01 14:30:20 noring Exp $
 */
 
 struct gif_lzw
 {
+  int very_dummy;
 };
 
 /* returns number of strings written on stack */
