de3a80dba1e8d29c21ebf59e3dd577500e0820cc
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1998-01-15 04:02:07 +0100

    ...
    Rev: src/modules/_Image_JPEG/.cvsignore:1.1
    Rev: src/modules/_Image_JPEG/configure.in:1.2
    Rev: src/modules/_Image_JPEG/image_jpeg.c:1.2

--- src/modules/_Image_JPEG/image_jpeg.c
+++ src/modules/_Image_JPEG/image_jpeg.c
@@ -9,13 +9,71 @@ RCSID("$id: $");
 
 #ifdef HAVE_JPEGLIB_H
 
+#include "jpeglib.h"
 
+#include "pike_macros.h"
+#include "object.h"
+#include "constants.h"
+#include "interpret.h"
+#include "svalue.h"
+#include "threads.h"
+#include "array.h"
+#include "error.h"
+#include "threads.h"
+
+#include "../Image/image.h"
+
+/*
+**! module Image
+**! submodule JPEG
+*/
+
+void my_exit_error(struct jpeg_common_struct *cinfo)
+{
+}
+
+void my_emit_message(struct jpeg_common_struct *cinfo,int msg_level)
+{
+}
+
+void my_output_message(struct jpeg_common_struct *cinfo)
+{
+}
+
+void my_format_message(struct jpeg_common_struct *cinfo,char *buffer)
+{
+}
+
+void my_reset_error_mgr(struct jpeg_common_struct *cinfo)
+{
+}
+
+/*
+**! method object decode
+*/
+
+static void image_jpeg_decode(INT32 args)
+{
+   struct jpeg_decompress_struct cinfo;
+   struct jpeg_error_mgr jerr;
+   
+   struct jpeg_error_mgr my_error_mgr=
+   { my_exit_error,
+     my_emit_message,
+     my_output_message,
+     my_format_message,
+     my_reset_error_mgr
+   };
+   
+}
 
 #endif /* HAVE_JPEGLIB_H */
 
 
 void pike_module_exit(void)
 {
+   add_function("decode",image_jpeg_decode,
+		"function(string:object)",0);
 }
 
 void pike_module_init(void)
