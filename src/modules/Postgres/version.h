0935f05c50b14a920c04fad9fd7439ee3db109b8
Francesco Chemolli <li@kinkie.it>
1998-02-02 19:32:16 +0100

    Updated to 1.0.2.
    Rev: src/modules/Postgres/ChangeLog:1.4
    Rev: src/modules/Postgres/configure.in:1.5
    Rev: src/modules/Postgres/version.h:1.3

--- src/modules/Postgres/version.h
+++ src/modules/Postgres/version.h
@@ -1,8 +1,8 @@
 /*
- * $Id: version.h,v 1.2 1997/12/16 15:55:21 grubba Exp $
+ * $Id: version.h,v 1.3 1998/02/02 18:32:16 grubba Exp $
  *
  * This file is provided for CVS's sake, and to define a simple string to be
  * used as the module's release version
  */
 
-#define PGSQL_VERSION "Postgres/1.0.1"
+#define PGSQL_VERSION "Postgres/1.0.2"
