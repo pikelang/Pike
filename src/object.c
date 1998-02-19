6ca25beaf0b0bba16021ed870bcd383a77d03d11
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1998-02-18 19:22:30 -0800

    bugfix
    Rev: src/object.c:1.40

--- src/object.c
+++ src/object.c
@@ -4,7 +4,7 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 #include "global.h"
-RCSID("$Id: object.c,v 1.39 1998/02/19 02:58:37 hubbe Exp $");
+RCSID("$Id: object.c,v 1.40 1998/02/19 03:22:30 hubbe Exp $");
 #include "object.h"
 #include "dynamic_buffer.h"
 #include "interpret.h"
@@ -922,6 +922,8 @@ void gc_mark_object_as_referenced(struct object *o)
       if(frame.context.prog->gc_marked)
 	frame.context.prog->gc_marked(o);
 
+      if(frame.context.parent)
+	gc_mark_object_as_referenced(frame.context.parent);
 
       for(d=0;d<(int)frame.context.prog->num_identifiers;d++)
       {
@@ -971,6 +973,10 @@ void gc_check_all_objects(void)
       {
 	struct inherit in=o->prog->inherits[e];
 	char *base=o->storage + in.storage_offset;
+
+	if(in.parent)
+	  gc_check(in.parent);
+
 	for(d=0;d<in.prog->num_identifiers;d++)
 	{
 	  struct identifier *i=in.prog->identifiers+d;
