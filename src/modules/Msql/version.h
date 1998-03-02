ec10ebce3ed96e2f05378df098a8c425144ab864
Francesco Chemolli <li@kinkie.it>
1998-03-02 19:52:49 +0100

    Bumped to 1.1.1.
    Rev: src/modules/Msql/version.h:1.2

--- src/modules/Msql/version.h
+++ src/modules/Msql/version.h
@@ -1,8 +1,8 @@
 /*
- * $Id: version.h,v 1.1 1997/11/24 21:32:16 grubba Exp $
+ * $Id: version.h,v 1.2 1998/03/02 18:52:49 grubba Exp $
  *
  * This file is provided for CVS's sake, and to define a simple string to be
  * used as the module's release version
  */
 
-#define MSQLMOD_VERSION "mSQL/1.1"
+#define MSQLMOD_VERSION "mSQL/1.1.1"
