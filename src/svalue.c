1eef29dd2c3b358cfe5266ff5ab18bc9719f1875
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-04-06 19:05:10 +0200

    Fixed bug.
    Rev: src/svalue.c:1.24

--- src/svalue.c
+++ src/svalue.c
@@ -20,7 +20,7 @@
 #include "pike_macros.h"
 #include <ctype.h>
 
-RCSID("$Id: svalue.c,v 1.23 1998/04/06 17:03:23 grubba Exp $");
+RCSID("$Id: svalue.c,v 1.24 1998/04/06 17:05:10 grubba Exp $");
 
 struct svalue dest_ob_zero = { T_INT, 0 };
 
@@ -770,7 +770,7 @@ void describe_svalue(struct svalue *s,int indent,struct processing *p)
 
             default:
 	      if(is8bitalnum(((unsigned char *)s->u.string->str)[i]) ||
-		 isprint(((unsigned char *)s->u.string->str[i])))
+		 isprint(((unsigned char *)s->u.string->str)[i]))
 	      {
 		my_putchar(s->u.string->str[i]);
 		break;
