d99d8b987c61f2626bf2cc63f0ad5bde75d5c3f6
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-06-08 19:59:23 +0200

    Pike release bumped to 47 by bump_version.
    Rev: src/version.c:1.64

--- src/version.c
+++ src/version.c
@@ -9,10 +9,10 @@
 #include "interpret.h"
 #include "stralloc.h"
 
-RCSID("$Id: version.c,v 1.63 1998/06/01 16:05:58 grubba Exp $");
+RCSID("$Id: version.c,v 1.64 1998/06/08 17:59:23 grubba Exp $");
 
 void f_version(INT32 args)
 {
   pop_n_elems(args);
-  push_text("Pike v0.6 release 46");
+  push_text("Pike v0.6 release 47");
 }
