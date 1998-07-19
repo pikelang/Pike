c4d5f817f5a6aea8c3a99c0f1307167911c26629
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-07-20 00:49:08 +0200

    Minor cosmetic change.
    Rev: src/builtin_functions.c:1.117

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -4,7 +4,7 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 #include "global.h"
-RCSID("$Id: builtin_functions.c,v 1.116 1998/07/17 06:19:50 neotron Exp $");
+RCSID("$Id: builtin_functions.c,v 1.117 1998/07/19 22:49:08 grubba Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "pike_macros.h"
@@ -1058,14 +1058,14 @@ static struct pike_string * replace_many(struct pike_string *str,
   for(e=0;e<from->size;e++)
   {
     if(ITEM(from)[e].type != T_STRING)
-      error("Replace: from array not string *\n");
+      error("Replace: from array is not array(string)\n");
     v[e].ind=ITEM(from)[e].u.string;
   }
 
   for(e=0;e<to->size;e++)
   {
     if(ITEM(to)[e].type != T_STRING)
-      error("Replace: to array not string *\n");
+      error("Replace: to array is not array(string)\n");
     v[e].val=ITEM(to)[e].u.string;
   }
 
