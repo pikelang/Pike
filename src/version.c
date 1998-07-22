efb5e3bfc430c7b4ca38face8ddbb9b649567793
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1998-07-22 16:47:08 -0700

    release number bumped by export.pike
    Rev: src/version.c:1.92

--- src/version.c
+++ src/version.c
@@ -9,10 +9,10 @@
 #include "interpret.h"
 #include "stralloc.h"
 
-RCSID("$Id: version.c,v 1.91 1998/07/22 23:46:05 hubbe Exp $");
+RCSID("$Id: version.c,v 1.92 1998/07/22 23:47:08 hubbe Exp $");
 
 void f_version(INT32 args)
 {
   pop_n_elems(args);
-  push_text("Pike v0.6 release 74");
+  push_text("Pike v0.6 release 75");
 }
