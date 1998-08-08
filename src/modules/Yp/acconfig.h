d6b96b0e7da44a079e6672f59e57a81d885a0151
Henrik Grubbström (Grubba) <grubba@grubba.org>
1998-08-08 14:17:51 +0200

    Added YP_ORDER_TYPE.
    Rev: src/modules/Yp/acconfig.h:1.3

--- src/modules/Yp/acconfig.h
+++ src/modules/Yp/acconfig.h
@@ -1,10 +1,13 @@
 /*
- * $Id: acconfig.h,v 1.2 1997/05/22 16:30:39 grubba Exp $
+ * $Id: acconfig.h,v 1.3 1998/08/08 12:17:51 grubba Exp $
  */
 #ifndef YP_MACHINE_H
 #define YP_MACHINE_H
 
 @TOP@
 @BOTTOM@
+
+/* The last argument to yp_order() is a YP_ORDER_TYPE * */
+#define YP_ORDER_TYPE	unsigned
 
 #endif
