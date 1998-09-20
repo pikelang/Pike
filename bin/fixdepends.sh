fdbf1e28f65a83d974b7978c7bf02a6a7627140d
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1998-09-20 01:25:35 -0700

    minor improvement
    Rev: bin/fixdepends.sh:1.6

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -e "s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
+sed -e "s@ $1/\([-a-zA-Z0-9.,_]*\)@ \$(SRCDIR)/\1@g" >$1/dependencies
 
 #sed -e "s@/./@/@g
 #s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
