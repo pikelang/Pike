53a575957853434962a7b49b7f9669e3069c1f41
Peter Bortas <zino@lysator.liu.se>
1998-10-16 21:06:22 +0200

    Pike release bumped to 98 by bump_version.
    Rev: src/version.c:1.115

--- src/version.c
+++ src/version.c
@@ -9,10 +9,10 @@
 #include "interpret.h"
 #include "stralloc.h"
 
-RCSID("$Id: version.c,v 1.114 1998/10/10 07:02:25 peter Exp $");
+RCSID("$Id: version.c,v 1.115 1998/10/16 19:06:22 peter Exp $");
 
 void f_version(INT32 args)
 {
   pop_n_elems(args);
-  push_text("Pike v0.6 release 97");
+  push_text("Pike v0.6 release 98");
 }
