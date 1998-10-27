79319d44635bdaaf8aa518cabe2ac6868011ccb5
Johan Schön <js@opera.com>
1998-10-27 01:45:58 +0100

    Modifiers in wrong order
    Rev: src/stralloc.h:1.25

--- src/stralloc.h
+++ src/stralloc.h
@@ -5,7 +5,7 @@
 \*/
 
 /*
- * $Id: stralloc.h,v 1.24 1998/10/22 00:33:34 hubbe Exp $
+ * $Id: stralloc.h,v 1.25 1998/10/27 00:45:58 js Exp $
  */
 #ifndef STRALLOC_H
 #define STRALLOC_H
@@ -151,7 +151,7 @@ INLINE INT32 PIKE_CONCAT4(compare_,FROM,_to_,TO)(const PIKE_CONCAT(p_wchar,TO) *
 /* Prototypes begin here */
 INLINE unsigned INT32 index_shared_string(struct pike_string *s, int pos);
 INLINE void low_set_index(struct pike_string *s, int pos, int value);
-struct INLINE pike_string *debug_check_size_shift(struct pike_string *a,int shift);
+INLINE struct pike_string *debug_check_size_shift(struct pike_string *a,int shift);
 CONVERT(0,1)
 CONVERT(0,2)
 CONVERT(1,0)
