2a406a2b7ae2e9eea19818e8e15f57375a55958e
Mirar (Pontus Hagland) <pike@sort.mirar.org>
1999-04-21 18:45:02 +0200

    `/ added on Image.Image, operator supports Image.Color
    Rev: src/modules/Image/image.c:1.132
    Rev: src/modules/Image/image.h:1.28
    Rev: src/modules/Image/operator.c:1.21

--- src/modules/Image/operator.c
+++ src/modules/Image/operator.c
@@ -1,9 +1,9 @@
-/* $Id: operator.c,v 1.20 1999/04/13 12:32:27 mirar Exp $ */
+/* $Id: operator.c,v 1.21 1999/04/21 16:45:02 mirar Exp $ */
 
 /*
 **! module Image
 **! note
-**!	$Id: operator.c,v 1.20 1999/04/13 12:32:27 mirar Exp $
+**!	$Id: operator.c,v 1.21 1999/04/21 16:45:02 mirar Exp $
 **! class Image
 */
 
@@ -37,77 +37,62 @@ extern struct program *image_program;
 
 #define testrange(x) ((COLORTYPE)MAXIMUM(MINIMUM(((int)x),255),0))
 
-#define STANDARD_OPERATOR_HEADER(what) \
-   struct object *o;			   			\
-   struct image *img,*oper=NULL;	   			\
-   rgb_group *s1,*s2,*d;		   			\
-   rgbl_group rgb;                                              \
-   INT32 i;				   			\
-					   			\
-   if (!THIS->img) error("no image\n");	   			\
-   		   			   			\
-   if (args && sp[-args].type==T_INT)				\
-   {								\
-      rgb.r=sp[-args].u.integer;				\
-      rgb.g=sp[-args].u.integer;				\
-      rgb.b=sp[-args].u.integer;				\
-      oper=NULL;						\
-   }								\
-   else if (args && sp[-args].type==T_FLOAT)			\
-   {								\
-      rgb.r=(long)(255*sp[-args].u.float_number);		\
-      rgb.g=(long)(255*sp[-args].u.float_number);		\
-      rgb.b=(long)(255*sp[-args].u.float_number);		\
-      oper=NULL;						\
-   }								\
-   else if (args && sp[-args].type==T_ARRAY			\
-       && sp[-args].u.array->size>=3				\
-       && sp[-args].u.array->item[0].type==T_INT		\
-       && sp[-args].u.array->item[1].type==T_INT		\
-       && sp[-args].u.array->item[2].type==T_INT)		\
-   {								\
-      rgb.r=sp[-args].u.array->item[0].u.integer;		\
-      rgb.g=sp[-args].u.array->item[1].u.integer;		\
-      rgb.b=sp[-args].u.array->item[2].u.integer;		\
-      oper=NULL;						\
-   }								\
-   else if (args && sp[-args].type==T_ARRAY                     \
-       && sp[-args].u.array->size>=3                            \
-       && sp[-args].u.array->item[0].type==T_FLOAT              \
-       && sp[-args].u.array->item[1].type==T_FLOAT              \
-       && sp[-args].u.array->item[2].type==T_FLOAT)             \
-   {                                                            \
-      rgb.r=(long)(sp[-args].u.array->item[0].u.float_number*255); \
-      rgb.g=(long)(sp[-args].u.array->item[1].u.float_number*255); \
-      rgb.b=(long)(sp[-args].u.array->item[2].u.float_number*255); \
-      oper=NULL;                                                \
-   }                                                            \
-   else                                                         \
-   {                                                            \
-      if (args<1 || sp[-args].type!=T_OBJECT			\
-       || !sp[-args].u.object					\
-       || sp[-args].u.object->prog!=image_program)		\
-      error("illegal arguments to image->"what"()\n");		\
-   		   		   				\
-      oper=(struct image*)sp[-args].u.object->storage;		\
-      if (!oper->img) error("no image (operand)\n");		\
-      if (oper->xsize!=THIS->xsize 		   		\
-          || oper->ysize!=THIS->ysize) 		   		\
-         error("operands differ in size (image->"what")");	\
-   }  	   		   		   		        \
-		   		   		   		\
-   push_int(THIS->xsize);		   			\
-   push_int(THIS->ysize);		   			\
-   o=clone_object(image_program,2);				\
-   img=(struct image*)o->storage;		   		\
-   if (!img->img) { free_object(o); error("out of memory\n"); }	\
-		   		   		   		\
-   s1=THIS->img;		   		   		\
-   if (oper) s2=oper->img; else s2=NULL;   		   	\
-   d=img->img;		   		   			\
-		   		   		   		\
-   i=img->xsize*img->ysize;			   		\
-   THREADS_ALLOW();                                             \
+#define STANDARD_OPERATOR_HEADER(what)					\
+   struct object *o;							\
+   struct image *img,*oper=NULL;					\
+   rgb_group *s1,*s2,*d;						\
+   rgbl_group rgb;							\
+   INT32 i;								\
+									\
+   if (!THIS->img) error("no image\n");					\
+									\
+   if (args && sp[-args].type==T_INT)					\
+   {									\
+      rgb.r=sp[-args].u.integer;					\
+      rgb.g=sp[-args].u.integer;					\
+      rgb.b=sp[-args].u.integer;					\
+      oper=NULL;							\
+   }									\
+   else if (args && sp[-args].type==T_FLOAT)				\
+   {									\
+      rgb.r=(long)(255*sp[-args].u.float_number);			\
+      rgb.g=(long)(255*sp[-args].u.float_number);			\
+      rgb.b=(long)(255*sp[-args].u.float_number);			\
+      oper=NULL;							\
+   }									\
+   else if (args && (sp[-args].type==T_ARRAY ||				\
+		     sp[-args].type==T_OBJECT ||			\
+		     sp[-args].type==T_STRING) &&			\
+            image_color_arg(-args,&rgb))				\
+   {									\
+      oper=NULL;							\
+   }									\
+   else									\
+   {									\
+      if (args<1 || sp[-args].type!=T_OBJECT				\
+       || !sp[-args].u.object						\
+       || sp[-args].u.object->prog!=image_program)			\
+      error("illegal arguments to image->"what"()\n");			\
+									\
+      oper=(struct image*)sp[-args].u.object->storage;			\
+      if (!oper->img) error("no image (operand)\n");			\
+      if (oper->xsize!=THIS->xsize					\
+          || oper->ysize!=THIS->ysize)					\
+         error("operands differ in size (image->"what")");		\
+   }									\
+									\
+   push_int(THIS->xsize);						\
+   push_int(THIS->ysize);						\
+   o=clone_object(image_program,2);					\
+   img=(struct image*)o->storage;					\
+   if (!img->img) { free_object(o); error("out of memory\n"); }		\
+									\
+   s1=THIS->img;							\
+   if (oper) s2=oper->img; else s2=NULL;				\
+   d=img->img;								\
+									\
+   i=img->xsize*img->ysize;						\
+   THREADS_ALLOW();							\
    if (s2)
 
 
@@ -233,6 +218,54 @@ STANDARD_OPERATOR_HEADER("`*")
       d->r=testrange(floor(s1->r*q*rgb.r+0.5));
       d->g=testrange(floor(s1->g*q*rgb.g+0.5));
       d->b=testrange(floor(s1->b*q*rgb.b+0.5));
+      s1++; d++; 
+   }
+   THREADS_DISALLOW();
+   pop_n_elems(args);		   		   		
+   push_object(o);		   		   		
+}
+
+/*
+**! method object `/(object operand)
+**! method object `/(array(int) color)
+**! method object `/(int value)
+**!	Multiplies pixel values and creates a new image.
+**! returns the new image object
+**!
+**!	This can be useful to lower the values of an image,
+**!	making it greyer, for instance:
+**!
+**!	<pre>image=image/128+64;</pre>
+**!
+**! arg object operand
+**!	the other image to multiply with;
+**!	the images must have the same size.
+**! arg array(int) color
+**!	an array in format ({r,g,b}), this is equal
+**!	to using an uniform-colored image.
+**! arg int value
+**!	equal to ({value,value,value}).
+**!
+**! see also: `-, `+, `|, `&, add_layers
+*/
+
+void image_operator_divide(INT32 args)
+{
+   double q=1/255.0;
+STANDARD_OPERATOR_HEADER("`*")
+   while (i--)
+   {
+      d->r=floor(s1->r/(q*(s2->r+1))+0.5);
+      d->g=floor(s1->g/(q*(s2->g+1))+0.5);
+      d->b=floor(s1->b/(q*(s2->b+1))+0.5);
+      s1++; s2++; d++; 
+   }
+   else
+   while (i--)
+   {
+      d->r=testrange(floor(s1->r/(q*(rgb.r+1))+0.5));
+      d->g=testrange(floor(s1->g/(q*(rgb.g+1))+0.5));
+      d->b=testrange(floor(s1->b/(q*(rgb.b+1))+0.5));
       s1++; d++; 
    }
    THREADS_DISALLOW();
