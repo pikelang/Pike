915d092119e25735ad661ff132d0e33bd92362f4
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-04-23 01:16:02 +0200

    Fixed bug in the USE_WAIT_THREAD case.
    Rev: src/signal_handler.c:1.125

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -25,7 +25,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.124 1999/04/12 05:27:48 hubbe Exp $");
+RCSID("$Id: signal_handler.c,v 1.125 1999/04/22 23:16:02 grubba Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -857,6 +857,10 @@ static void *wait_thread(void *data)
     WAITSTATUSTYPE status;
     int pid;
 
+#ifdef PROC_DEBUG
+    fprintf(stderr, "wait_thread: getting the lock.\n");
+#endif
+
     mt_lock(&wait_thread_mutex);
     pid=MY_WAIT_ANY(&status, WNOHANG);
     
@@ -872,6 +876,10 @@ static void *wait_thread(void *data)
 #endif
     }
 
+#ifdef PROC_DEBUG
+    fprintf(stderr, "wait_thread: releasing the lock.\n");
+#endif
+
     mt_unlock(&wait_thread_mutex);
 
     if(pid <= 0) pid=MY_WAIT_ANY(&status, 0);
@@ -882,7 +890,15 @@ static void *wait_thread(void *data)
     
     if(pid>0)
     {
+#ifdef PROC_DEBUG
+      fprintf(stderr, "wait thread: locking interpreter\n",pid,errno);
+#endif
+
       mt_lock(&interpreter_lock);
+
+#ifdef PROC_DEBUG
+      fprintf(stderr, "wait thread: reporting the event!\n");
+#endif
       report_child(pid, status);
       co_broadcast(& process_status_change);
 
@@ -2784,7 +2800,13 @@ void init_signals(void)
     co_init(& process_status_change);
     co_init(& start_wait_thread);
     mt_init(& wait_thread_mutex);
-    th_create_small(&foo,wait_thread,0);
+
+    if (th_create_small(&foo,wait_thread,0)) {
+      fatal("wait thread: Failed to create thread!\n"
+	    "errno: %d\n", errno);
+    }
+    num_threads++;    /* We use the interpreter lock */
+
     my_signal(SIGCHLD, SIG_DFL);
   }
 #endif
