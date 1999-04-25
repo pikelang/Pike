b411f7461aa207e33498bac20736560784644c41
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-04-25 22:34:00 +0200

    Removed C++-style comment.
    Rev: src/modules/Image/colors.c:1.14

--- src/modules/Image/colors.c
+++ src/modules/Image/colors.c
@@ -1,7 +1,7 @@
 /*
 **! module Image
 **! note
-**!	$Id: colors.c,v 1.13 1999/04/18 22:05:16 mirar Exp $
+**!	$Id: colors.c,v 1.14 1999/04/25 20:34:00 grubba Exp $
 **! submodule Color
 **!
 **!	This module keeps names and easy handling 
@@ -97,7 +97,7 @@
 #include "global.h"
 #include <config.h>
 
-RCSID("$Id: colors.c,v 1.13 1999/04/18 22:05:16 mirar Exp $");
+RCSID("$Id: colors.c,v 1.14 1999/04/25 20:34:00 grubba Exp $");
 
 #include "config.h"
 
@@ -400,7 +400,7 @@ static void image_color_hsvf(INT32 args)
    if(r==max) h = (g-b)/delta;
    else if(g==max) h = 2+(b-r)/delta;
    else /*if(b==max)*/ h = 4+(r-g)/delta;
-   h *= 60; // now in degrees.
+   h *= 60; /* now in degrees. */
    if(h<0) h+=360;
 
    push_float(h);
