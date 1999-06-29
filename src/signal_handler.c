9a4cf93293a8e9e1cbdad0b4506f697c067a602a
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1999-06-28 17:00:27 -0700

    EAGAIN no longer automatically handled by create_process..
    Rev: src/signal_handler.c:1.143

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -25,7 +25,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.142 1999/06/25 21:34:22 hubbe Exp $");
+RCSID("$Id: signal_handler.c,v 1.143 1999/06/29 00:00:27 hubbe Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -2328,8 +2328,7 @@ void f_create_process(INT32 args)
 #else
       pid=fork();
 #endif
-
-    }while(pid == -1 && (errno==EAGAIN || errno==EINTR));
+    }while(pid==-1 && errno==EINTR)
 
     UNSET_ONERROR(err);
 
