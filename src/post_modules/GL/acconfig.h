c96612520750574acd1e685a6dea561d15d767b8
Marcus Comstedt <marcus@mc.pp.se>
1999-07-18 17:05:13 +0200

    Now possible to select OpenGL or Mesa.
    Rev: src/post_modules/GL/Makefile.in:1.3
    Rev: src/post_modules/GL/acconfig.h:1.2
    Rev: src/post_modules/GL/configure.in:1.3

--- src/post_modules/GL/acconfig.h
+++ src/post_modules/GL/acconfig.h
@@ -1,1 +1,8 @@
-/* $Id: acconfig.h,v 1.1 1999/07/16 20:01:12 marcus Exp $ */
+/* $Id: acconfig.h,v 1.2 1999/07/18 15:05:12 marcus Exp $ */
+
+/* Define if you have libGL */
+#undef HAVE_LIBGL
+
+/* Define if you have libMesaGL */
+#undef HAVE_LIBMESAGL
+
