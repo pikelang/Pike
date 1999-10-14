2eb54b057b93f8ca33e157ead7fbd77c54db1501
Peter Bortas <zino@lysator.liu.se>
1999-10-14 15:02:56 +0200

    release number bumped to 45 by export.pike
    Rev: src/version.h:1.47

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.46 1999/10/14 12:39:31 peter Exp $ */
+/* $Id: version.h,v 1.47 1999/10/14 13:02:56 peter Exp $ */
 
 #define PIKE_MAJOR_VERSION 0
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 44
+#define PIKE_BUILD_VERSION 45
 
 /* Prototypes begin here */
 void f_version(INT32 args);
