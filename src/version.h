f383252adec6883c97d20d96b909de571e99cbc7
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
1999-10-30 16:24:11 -0700

    --with-bignums is now the default (bumped version too)
    Rev: src/version.h:1.63

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.62 1999/10/25 19:53:02 peter Exp $ */
+/* $Id: version.h,v 1.63 1999/10/30 23:24:11 hubbe Exp $ */
 
 #define PIKE_MAJOR_VERSION 0
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 60
+#define PIKE_BUILD_VERSION 61
 
 /* Prototypes begin here */
 void f_version(INT32 args);
