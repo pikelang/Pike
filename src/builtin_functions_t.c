41b13dcf46665b14aac40c44c4ad36f2150393de
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-08 21:49:57 +0100

    First version
    Rev: src/builtin_functions_t.c:1.1
    Rev: src/las_t.c:1.1
    Rev: src/peep_t.c:1.1

---
+++ src/builtin_functions_t.c
@@ -0,0 +1,12 @@
+/*
+ * $Id: builtin_functions_t.c,v 1.1 1999/11/08 20:49:57 grubba Exp $
+ *
+ * Used to strap pike.
+ *
+ * upper_case() & lower_case() are castrated in this file.
+ *
+ * Henrik Grubbström 1999-11-08
+ */
+
+#define IN_TPIKE
+#include "builtin_functions.c"
---
+++ src/peep_t.c
@@ -0,0 +1,12 @@
+/*
+ * $Id: peep_t.c,v 1.1 1999/11/08 20:49:57 grubba Exp $
+ *
+ * Used to strap pike.
+ *
+ * Optimizer is castrated in this file.
+ *
+ * Henrik Grubbström 1999-11-08
+ */
+
+#define IN_TPIKE
+#include "peep.c"
