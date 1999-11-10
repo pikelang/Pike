a7705b1456ed86f303d04eed48ba2176c7e39d8c
Build system <distmaker@roxen.com>
1999-11-10 10:04:05 +0100

    release number bumped to 67 by export.pike
    Rev: src/version.h:1.69

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.68 1999/11/10 09:02:23 build Exp $ */
+/* $Id: version.h,v 1.69 1999/11/10 09:04:05 build Exp $ */
 
 #define PIKE_MAJOR_VERSION 0
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 66
+#define PIKE_BUILD_VERSION 67
 
 /* Prototypes begin here */
 void f_version(INT32 args);
