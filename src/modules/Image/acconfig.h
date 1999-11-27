5839471222497e3bfc0b084ffd5d54fa39b084e5
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-11-27 01:33:42 +0100

    Added HAVE_LIBM.
    Rev: src/modules/Image/acconfig.h:1.3

--- src/modules/Image/acconfig.h
+++ src/modules/Image/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.2 1999/05/30 20:11:51 mirar Exp $
+ * $Id: acconfig.h,v 1.3 1999/11/27 00:33:42 grubba Exp $
  */
 
 #ifndef IMAGE_MACHINE_H
@@ -7,6 +7,9 @@
 
 /* define if you want lzw code to generate only rle packing */
 #undef GIF_LZW_RLE
+
+/* Define if you have the m library (-lm).  */
+#undef HAVE_LIBM
 
 @TOP@
 @BOTTOM@
