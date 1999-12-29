2ea6b7193678e57e7b6537c3b965919071da8196
Henrik Grubbström (Grubba) <grubba@grubba.org>
1999-12-29 18:54:03 +0100

    Added OPT_WEAK_TYPE.
    Rev: src/las.h:1.33

--- src/las.h
+++ src/las.h
@@ -5,7 +5,7 @@
 \*/
 
 /*
- * $Id: las.h,v 1.32 1999/12/26 18:55:35 grubba Exp $
+ * $Id: las.h,v 1.33 1999/12/29 17:54:03 grubba Exp $
  */
 #ifndef LAS_H
 #define LAS_H
@@ -81,6 +81,7 @@ extern struct node_hash_table node_hash;
 #define OPT_BREAK           0x100  /* contains break(s) */
 #define OPT_RETURN          0x200  /* contains return(s) */
 #define OPT_TYPE_NOT_FIXED  0x400  /* type-field might be wrong */
+#define OPT_WEAK_TYPE	    0x800  /* don't warn even if strict types */
 
 #define OPT_DEFROSTED	    0x4000 /* Node may be a duplicate */
 #define OPT_NOT_SHARED	    0x8000 /* Node is not to be shared */
