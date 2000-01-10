df4312f63f7eb1376f9d49826bd2ef760fbd63e2
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-01-09 16:44:57 -0800

    more debug..
    Rev: src/bignum.c:1.16

--- src/bignum.c
+++ src/bignum.c
@@ -17,6 +17,9 @@ static void resolve_auto_bignum_program(void)
 {
   if(auto_bignum_program.type == T_INT)
   {
+    if(gmp_library_resolving)
+      fatal("Recursive GMP resolving!\n");
+
     gmp_library_resolving=1;
     push_text("Gmp.bignum");
     SAFE_APPLY_MASTER("resolv", 1);
