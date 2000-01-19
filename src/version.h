3ebf4780773791a0f80198c8bb38a8ccfaa68da1
Leif Stensson <leif@lysator.liu.se>
2000-01-19 17:44:44 +0100

    release number bumped to 91 by export.pike
    Rev: src/version.h:1.93

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.92 2000/01/10 01:26:01 hubbe Exp $ */
+/* $Id: version.h,v 1.93 2000/01/19 16:44:44 leif Exp $ */
 
 #define PIKE_MAJOR_VERSION 0
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 90
+#define PIKE_BUILD_VERSION 91
 
 /* Prototypes begin here */
 void f_version(INT32 args);
