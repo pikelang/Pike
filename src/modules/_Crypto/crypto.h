15b0af5f62ec9c819850f804bd8fd204000bc840
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-01-26 20:50:11 +0100

    Added pike_rsa_{in,ex}it().
    Rev: src/modules/_Crypto/crypto.h:1.3

--- src/modules/_Crypto/crypto.h
+++ src/modules/_Crypto/crypto.h
@@ -1,5 +1,5 @@
 /*
- * $Id: crypto.h,v 1.2 1997/11/16 22:25:42 nisse Exp $
+ * $Id: crypto.h,v 1.3 2000/01/26 19:50:11 grubba Exp $
  *
  * Prototypes for some functions.
  *
@@ -25,6 +25,8 @@ extern void pike_sha_init(void);
 extern void pike_sha_exit(void);
 extern void pike_cbc_init(void);
 extern void pike_cbc_exit(void);
+extern void pike_rsa_init(void);
+extern void pike_rsa_exit(void);
 extern void pike_pipe_init(void);
 extern void pike_pipe_exit(void);
 
