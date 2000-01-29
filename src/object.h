7250920b7c3dd624338168f9a5849740debced52
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-01-29 00:44:42 -0800

    bugfix for AIX cc
    Rev: src/object.h:1.36

--- src/object.h
+++ src/object.h
@@ -5,7 +5,7 @@
 \*/
 
 /*
- * $Id: object.h,v 1.35 1999/11/23 10:36:39 mast Exp $
+ * $Id: object.h,v 1.36 2000/01/29 08:44:42 hubbe Exp $
  */
 #ifndef OBJECT_H
 #define OBJECT_H
@@ -115,7 +115,7 @@ void check_all_objects(void);
 #endif
 
 #ifdef PIKE_DEBUG
-#define master() ( get_master() ? get_master() : ( fatal("Couldn't load master object at %s:%d.\n",__FILE__,__LINE__), NULL) )
+#define master() ( get_master() ? get_master() : ( fatal("Couldn't load master object at %s:%d.\n",__FILE__,__LINE__), (struct object *)0) )
 #else
 #define master() debug_master()
 #endif
