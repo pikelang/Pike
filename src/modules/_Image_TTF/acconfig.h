7bdb813dcb41c887ea3c27c4feab6c50efada893
Martin Stjernholm <mast@lysator.liu.se>
2000-02-02 10:38:39 +0100

    Fixed a misleading comment.
    Rev: src/modules/_Image_TTF/acconfig.h:1.3

--- src/modules/_Image_TTF/acconfig.h
+++ src/modules/_Image_TTF/acconfig.h
@@ -1,11 +1,11 @@
 /*
- * $Id: acconfig.h,v 1.2 1999/02/09 16:14:01 grubba Exp $
+ * $Id: acconfig.h,v 1.3 2000/02/02 09:38:39 mast Exp $
  */
 
 @TOP@
 @BOTTOM@
 
-/* Define this if you have -ljpeg */
+/* Define this if you have -lttf. */
 #undef HAVE_LIBTTF
 
 /* Define this if TT_Horizontal_Header has the member Reserved0 */
