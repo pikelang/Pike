0810f8fa3ea5df4456f58a9db99a052305e2fa66
Peter Bortas <zino@lysator.liu.se>
2000-02-27 18:17:55 +0100

    release number bumped to 120 by export.pike
    Rev: src/version.h:1.122

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.121 2000/02/27 17:16:24 peter Exp $ */
+/* $Id: version.h,v 1.122 2000/02/27 17:17:55 peter Exp $ */
 
 #define PIKE_MAJOR_VERSION 0
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 119
+#define PIKE_BUILD_VERSION 120
 
 /* Prototypes begin here */
 void f_version(INT32 args);
