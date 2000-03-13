e5510fe2da6d080d1fb073d1453c29e00315c4ac
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-03-13 20:12:00 +0100

    Now disables use of sendfile(2) on FreeBSD 4.0.16 and earlier.
    Rev: src/modules/files/sendfile.c:1.35

--- src/modules/files/sendfile.c
+++ src/modules/files/sendfile.c
@@ -1,5 +1,5 @@
 /*
- * $Id: sendfile.c,v 1.34 2000/03/13 16:01:38 grubba Exp $
+ * $Id: sendfile.c,v 1.35 2000/03/13 19:12:00 grubba Exp $
  *
  * Sends headers + from_fd[off..off+len-1] + trailers to to_fd asyncronously.
  *
@@ -30,6 +30,10 @@
 #include "file.h"
 
 #include <errno.h>
+
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif /* HAVE_SYS_PARAM_H */
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
@@ -99,6 +103,17 @@
 
 #undef THIS
 #define THIS	((struct pike_sendfile *)(fp->current_storage))
+
+/*
+ * Kludge for broken FreeBSD 4.0.16
+ */
+#ifdef HAVE_FREEBSD_SENDFILE
+#ifdef __FreeBSD_version
+#if __FreeBSD_version <= 400016
+#undef HAVE_FREEBSD_SENDFILE
+#endif /* __FreeBSD_version <= 400016 */
+#endif /* __FreeBSD_version */
+#endif /* HAVE_FREEBSD_SENDFILE */
 
 /*
  * Globals
