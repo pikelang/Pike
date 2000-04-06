8efc6a961ee96c4a1b99c94c5ddda0e0b26a918b
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-04-06 14:07:58 -0700

    oops
    Rev: src/object.c:1.98

--- src/object.c
+++ src/object.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: object.c,v 1.97 2000/04/06 21:00:20 hubbe Exp $");
+RCSID("$Id: object.c,v 1.98 2000/04/06 21:07:58 hubbe Exp $");
 #include "object.h"
 #include "dynamic_buffer.h"
 #include "interpret.h"
@@ -485,16 +485,16 @@ void low_destruct(struct object *o,int do_free)
     }
   }
 
+
   POP_FRAME();
 
+  debug_malloc_touch(o);
   if(o->parent)
   {
     /* fprintf(stderr, "destruct(): Zapping parent.\n"); */
-
     free_object(o->parent);
     o->parent=0;
   }
-
 
   free_program(p);
 }
@@ -532,7 +532,9 @@ void destruct_objects_to_destruct(void)
     first_object=o;
     o->prev=0;
 
+    add_ref(o);
     destruct(o);
+    free_object(o);
   }
   objects_to_destruct=0;
   if(destruct_object_evaluator_callback)
