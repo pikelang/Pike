ba7f65206fcf4b9ed837222b4593be71d584e229
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-04-11 11:35:28 -0700

    now 7.1.0
    Rev: src/version.h:1.191

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.190 2000/04/10 23:43:24 hubbe Exp $ */
+/* $Id: version.h,v 1.191 2000/04/11 18:35:28 hubbe Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
-#define PIKE_MINOR_VERSION 0
-#define PIKE_BUILD_VERSION 37
+#define PIKE_MINOR_VERSION 1
+#define PIKE_BUILD_VERSION 0
 
 /* Prototypes begin here */
 void f_version(INT32 args);
