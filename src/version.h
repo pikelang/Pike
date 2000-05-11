331491cd95293910d61d5cb88ceb388ffb993b84
Fredrik Noring <noring@nocrew.org>
2000-05-11 14:26:51 +0200

    release number bumped to 1 by me.
    Rev: src/version.h:1.193

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.192 2000/04/28 17:24:49 noring Exp $ */
+/* $Id: version.h,v 1.193 2000/05/11 12:26:51 noring Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 1
-#define PIKE_BUILD_VERSION 1
+#define PIKE_BUILD_VERSION 2
 
 /* Prototypes begin here */
 void f_version(INT32 args);
