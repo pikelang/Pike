f07e7462494a0cbd4e0929464b20ce3f45147b40
David Hedbor <david@hedbor.org>
2000-05-12 19:36:30 -0700

    Backpatch of sort from 7.0.
    Rev: src/fsort.c:1.12
    Rev: src/fsort_template.h:1.5

--- src/fsort.c
+++ src/fsort.c
@@ -7,9 +7,10 @@
 /* Optimized for minimum amount of compares */
 
 #include "global.h"
+#include "error.h"
 #include "fsort.h"
 
-RCSID("$Id: fsort.c,v 1.11 1998/04/28 15:36:41 grubba Exp $");
+RCSID("$Id: fsort.c,v 1.12 2000/05/13 02:36:30 neotron Exp $");
 
 #define CMP(X,Y) ( (*cmpfun)((void *)(X),(void *)(Y)) )
 #define EXTRA_ARGS ,fsortfun cmpfun
--- src/fsort_template.h
+++ src/fsort_template.h
@@ -1,5 +1,5 @@
 /*
- * $Id: fsort_template.h,v 1.4 1998/04/27 22:33:17 hubbe Exp $
+ * $Id: fsort_template.h,v 1.5 2000/05/13 02:36:30 neotron Exp $
  */
 
 #ifndef SWAP
@@ -16,10 +16,17 @@
 #define DEC(X) X=STEP(X,-1)
 #define SIZE ((long)(char *)STEP((TYPE *)0,1))
 
-static void ID(register TYPE *bas,
-	       register TYPE *last
+#define PARENT(X) (((X)-1)>>1)
+#define CHILD1(X) (((X)<<1)+1)
+
+#define MKNAME(X) MKNAME2(ID,X)
+#define MKNAME2(X,Y) PIKE_CONCAT(X,Y)
+
+static void MKNAME(_do_sort)(register TYPE *bas,
+			       register TYPE *last,
+			       int max_recursion
 #ifdef EXTRA_ARGS
-	       EXTRA_ARGS
+			       EXTRA_ARGS
 #else
 #define UNDEF_XARGS
 #define XARGS
@@ -27,66 +34,140 @@ static void ID(register TYPE *bas,
   )
 {
   register TYPE *a,*b, tmp;
-  if(bas >= last) return;
-  a = STEP(bas,1);
-  if(a == last)
+
+  while(bas < last)
   {
-    if( CMP(bas,last) > 0) SWAP(bas,last); 
-  }else{
-    b=STEP(bas,((((char *)last)-((char *)bas))/SIZE)>>1);
-    SWAP(bas,b);
-    b=last;
-    while(a < b)
+    a = STEP(bas,1);
+    if(a == last)
     {
-#if 1
-      while(a<b)
+      if( CMP(bas,last) > 0) SWAP(bas,last);
+      return;
+    }else{
+      if(--max_recursion <= 0)
       {
-	while(1)
+	long howmany,x,y,z;
+#ifdef PIKE_DEBUG
+	extern int d_flag;
+#endif
+	
+	howmany=((((char *)last)-((char *)bas))/SIZE)+1;
+	if(howmany<2) return;
+	
+	for(x=PARENT(howmany-1);x>=0;x--)
 	{
-	  if(a<=b && CMP(a,bas) <= 0)
-	    INC(a);
-	  else
+	  y=x;
+	  while(1)
 	  {
-	    while(a< b && CMP(bas,b) <= 0) DEC(b);
-	    break;
-	  }
-	  if(a< b && CMP(bas,b) <= 0)
-	    DEC(b);
-	  else
-	  {
-	    while(a<=b && CMP(a,bas) <= 0) INC(a);
-	    break;
+	    z=CHILD1(y);
+	    if(z>=howmany) break;
+	    if(z+1 < howmany && CMP(STEP(bas,z),STEP(bas,z+1))<=0) z++;
+	    if(CMP( STEP(bas, y), STEP(bas, z)) >0) break;
+	    SWAP( STEP(bas, y), STEP(bas, z));
+	    y=z;
 	  }
 	}
+	
+	for(x=howmany-1;x;x--)
+	{
+	  SWAP( STEP(bas,x), bas);
+	  y=0;
+	  while(1)
+	  {
+	    z=CHILD1(y);
+	    if(z>=x) break;
+	    if(z+1 < x && CMP(STEP(bas,z),STEP(bas,z+1))<=0) z++;
+	    if(CMP( STEP(bas, y), STEP(bas, z)) >0) break;
+	    SWAP( STEP(bas, y), STEP(bas, z));
+	    y=z;
+	  }
+	}
+	
+#ifdef PIKE_DEBUG
+	if(d_flag>1)
+	  for(x=howmany-1;x;x--)
+	    if( CMP( STEP(bas,x-1), STEP(bas,x)  ) > 0)
+	      fatal("Sorting failed!\n");
+#endif
+	
+	return;
+      }
 
+      b=STEP(bas,((((char *)last)-((char *)bas))/SIZE)>>1);
+      SWAP(bas,b);
+      b=last;
+      while(a < b)
+      {
+#if 1
+	while(a<b)
+	{
+	  while(1)
+	  {
+	    if(a<=b && CMP(a,bas) <= 0)
+	      INC(a);
+	    else
+	    {
+	      while(a< b && CMP(bas,b) <= 0) DEC(b);
+	      break;
+	    }
+	    if(a< b && CMP(bas,b) <= 0)
+	      DEC(b);
+	    else
+	    {
+	      while(a<=b && CMP(a,bas) <= 0) INC(a);
+	      break;
+	    }
+	  }
+	  
+	  if(a<b)
+	  {
+	    SWAP(a,b);
+	    INC(a);
+	    if(b > STEP(a,1)) DEC(b);
+	  }
+	}
+#else
+	while(a<=b && CMP(a,bas) < 0) INC(a);
+	while(a< b && CMP(bas,b) < 0) DEC(b);
 	if(a<b)
 	{
 	  SWAP(a,b);
 	  INC(a);
 	  if(b > STEP(a,1)) DEC(b);
 	}
-      }
-#else
-      while(a<=b && CMP(a,bas) < 0) INC(a);
-      while(a< b && CMP(bas,b) < 0) DEC(b);
-      if(a<b)
-      {
-        SWAP(a,b);
-	INC(a);
-	if(b > STEP(a,1)) DEC(b);
-      }
 #endif
+      }
+      DEC(a);
+      SWAP(a,bas);
+      DEC(a);
+      
+      if(  (char *)a - (char *)bas < (char *)last - (char *)b )
+      {
+	MKNAME(_do_sort)(bas,a,max_recursion XARGS);
+	bas=b;
+      } else {
+	MKNAME(_do_sort)(b,last,max_recursion XARGS);
+	last=a;
+      }
     }
-    DEC(a);
-    SWAP(a,bas);
-    DEC(a);
-    ID(bas,a XARGS);
-    ID(b,last XARGS);
   }
 }
 
+void ID(register TYPE *bas,
+	register TYPE *last
+#ifdef EXTRA_ARGS
+	EXTRA_ARGS
+#endif
+  )
+{
+  extern int my_log2(unsigned INT32 x);
+  MKNAME(_do_sort)(bas,last, my_log2( last-bas ) * 2 XARGS);;
+}
+
+
 #undef INC
 #undef DEC
+#undef PARENT
+#undef CHILD1
 
 #ifdef UNDEF_XARGS
 #undef XARGS
