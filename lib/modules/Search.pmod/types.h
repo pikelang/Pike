c63fed7e52c264812a4a8716df27f4c44c59239f
Martin Nilsson <mani@lysator.liu.se>
2000-05-15 17:39:25 +0200

    Context types
    Rev: lib/modules/Search.pmod/types.h:1.1

---
+++ lib/modules/Search.pmod/types.h
@@ -0,0 +1,19 @@
+// Copyright © 2000, Roxen IS.
+
+#define T_NONE     0
+#define T_TITLE    1
+#define T_KEYWORDS 2
+#define T_EXT_A    3
+#define T_H1       4
+#define T_H2       5
+#define T_H3       6
+#define T_H4       7
+#define T_H5       8
+#define T_H6       9
+#define T_ALT      9
+#define T_DESC     10
+#define T_TH       11
+#define T_B        12
+#define T_A        13
+#define T_I        14
+
