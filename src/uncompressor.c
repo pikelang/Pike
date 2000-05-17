88b98f02c8b21cd1ccf6d60a810e1e1daf947227
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-05-16 18:46:00 -0700

    bugfix
    Rev: src/uncompressor.c:1.4

--- src/uncompressor.c
+++ src/uncompressor.c
@@ -26,7 +26,6 @@
 
 /* #define DEBUG */
 
-
 gzFile gz;
 
 static void gr(char *ptr, int len)
@@ -149,7 +148,9 @@ void my_uncompress(char *file,int argc, char **argv)
 	  for(e=0;e<argc;e++)
 	  {
 	    char *c;
+#ifdef DEBUG
 	    fprintf(stderr,"argv[%d]=%s\n",e,argv[e]);
+#endif
 
 	    *(ptr++)=' ';
 	    *(ptr++)='"';
