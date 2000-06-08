73bd1b9c1988c03cb0a5e84c35b710ca78303b9b
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-06-08 15:06:48 -0700

    always use our own STRTOL and STRTOD
    Rev: src/port.c:1.23
    Rev: src/port.h:1.27

--- src/port.h
+++ src/port.h
@@ -5,7 +5,7 @@
 \*/
 
 /*
- * $Id: port.h,v 1.26 2000/04/01 11:50:58 grubba Exp $
+ * $Id: port.h,v 1.27 2000/06/08 22:06:48 hubbe Exp $
  */
 #ifndef PORT_H
 #define PORT_H
@@ -60,17 +60,8 @@ time_t TIME(time_t *);
 #define RINT rint
 #endif
 
-#if defined(HAVE_STRTOL) && defined(HAVE_WORKING_STRTOL)
-#  define STRTOL strtol
-#else
 long STRTOL(char *str,char **ptr,int base);
-#endif
-
-#ifndef HAVE_STRTOD
 double STRTOD(char * nptr, char **endptr);
-#else
-#  define STRTOD strtod
-#endif
 
 #ifndef HAVE_STRCSPN
 int STRCSPN(const char *s,const char * set);
