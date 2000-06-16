b6c28f62135121bc5967b109a3ff8130cb2b7579
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-06-16 19:49:51 +0200

    Even more improved support for UnixWare.
    Rev: src/configure.in:1.378
    Rev: src/smartlink.c:1.9

--- src/smartlink.c
+++ src/smartlink.c
@@ -1,5 +1,5 @@
 /*
- * $Id: smartlink.c,v 1.8 2000/06/16 17:45:14 grubba Exp $
+ * $Id: smartlink.c,v 1.9 2000/06/16 17:49:27 grubba Exp $
  *
  * smartlink - A smarter linker.
  * Based on the /bin/sh script smartlink 1.23.
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
 
   if (!strcmp(argv[1], "-v")) {
     fprintf(stdout,
-	    "$Id: smartlink.c,v 1.8 2000/06/16 17:45:14 grubba Exp $\n"
+	    "$Id: smartlink.c,v 1.9 2000/06/16 17:49:27 grubba Exp $\n"
 	    "Usage:\n"
 	    "\t%s binary [args]\n",
 	    argv[0]);
@@ -259,6 +259,11 @@ int main(int argc, char **argv)
 #ifdef USE_RPATH
     new_argv[new_argc++] = "-rpath";
     new_argv[new_argc++] = rpath;
+#elif defined(USE_YP_)
+    if (linking) {
+      new_argv[new_argc++] = "-YP,";
+      new_argv[new_argc++] = rpath;
+    }
 #elif defined(USE_XLINKER_YP_)
     if (linking) {
       new_argv[new_argc++] = "-Xlinker";
