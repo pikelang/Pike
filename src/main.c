07eda2eb7df8abefe2895d2a945540a96b00b80b
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-07-05 17:30:32 -0700

    fixilifix, allow binaries compiled on glibc2.0 run on glibc2.1
    Rev: src/main.c:1.92

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: main.c,v 1.91 2000/06/23 06:17:58 hubbe Exp $");
+RCSID("$Id: main.c,v 1.92 2000/07/06 00:30:32 hubbe Exp $");
 #include "fdlib.h"
 #include "backend.h"
 #include "module.h"
@@ -32,6 +32,14 @@ RCSID("$Id: main.c,v 1.91 2000/06/23 06:17:58 hubbe Exp $");
 #include "security.h"
 #include "constants.h"
 #include "version.h"
+
+#ifdef HAVE_PTHREAD_INITIAL_THREAD_BOS
+#if defined(HAVE_DLOPEN)
+#ifdef HAVE_DLFCN_H
+#include <dlfcn.h>
+#endif
+#endif
+#endif
 
 #include "las.h"
 
@@ -425,6 +433,26 @@ int dbm_main(int argc, char **argv)
       stack_top += STACK_DIRECTION * lim.rlim_cur;
 
 #ifdef HAVE_PTHREAD_INITIAL_THREAD_BOS
+#if defined(HAVE_DLOPEN) && defined(HAVE_DLFCN_H)
+      /* FIXME: this code should be used on all linux glibc systems
+       * even those without __pthread_initial_thread_bos
+       * -Hubbe
+       */
+      {
+	char ** bos_location;
+	void *handle;
+	if((handle=dlopen(0, RTLD_LAZY)))
+	{
+	  bos_location=dlsym(handle,"__pthread_initial_thread_bos");
+	  dlclose(handle);
+
+	  if(bos_location && *bos_location &&
+	     (*bos_location - stack_top) *STACK_DIRECTION < 0)
+	    stack_top=*bos_location;
+	}
+      }
+#else
+
       {
 	extern char * __pthread_initial_thread_bos;
 	/* Linux glibc threads are limited to a 4 Mb stack
@@ -435,6 +463,7 @@ int dbm_main(int argc, char **argv)
 	   (__pthread_initial_thread_bos - stack_top) *STACK_DIRECTION < 0)
 	  stack_top=__pthread_initial_thread_bos;
       }
+#endif
 #endif
       stack_top -= STACK_DIRECTION * 8192 * sizeof(char *);
 
