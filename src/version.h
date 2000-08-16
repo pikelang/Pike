b1bbbb604392c260fb088a5001d76ec5ca73f250
Fredrik Noring <noring@nocrew.org>
2000-08-16 20:29:47 +0200

    release number bumped to 100 by export.pike
    Rev: src/version.h:1.253

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.252 2000/08/16 16:15:55 peter Exp $ */
+/* $Id: version.h,v 1.253 2000/08/16 18:29:47 noring Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 0
-#define PIKE_BUILD_VERSION 99
+#define PIKE_BUILD_VERSION 100
 
 /* Prototypes begin here */
 void f_version(INT32 args);
