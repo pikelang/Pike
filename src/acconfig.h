2e8907d32f97041c6f2a4b2b2db1580f3c2e20d3
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-08-21 23:19:44 +0200

    Added HAVE_DECLSPEC and DECLSPEC().
    Rev: src/acconfig.h:1.62

--- src/acconfig.h
+++ src/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.61 2000/08/20 17:14:15 grubba Exp $
+ * $Id: acconfig.h,v 1.62 2000/08/21 21:19:44 grubba Exp $
  */
 #ifndef MACHINE_H
 #define MACHINE_H
@@ -187,8 +187,11 @@
 /* Define if your signals are one-shot */
 #undef SIGNAL_ONESHOT
 
-/* You have gcc stype function attributes? */
+/* You have gcc-type function attributes? */
 #undef HAVE_FUNCTION_ATTRIBUTES
+
+/* You have lc-type __declspec? */
+#undef HAVE_DECLSPEC
 
 /* Do your compiler grock 'volatile' */
 #define VOLATILE volatile
@@ -376,6 +379,12 @@
 #else
 #define ATTRIBUTE(X)
 #endif
+
+#ifdef HAVE_DECLSPEC
+#define DECLSPEC(X) __declspec(X)
+#else /* !HAVE_DECLSPEC */
+#define DECLSPEC(X)
+#endif /* HAVE_DECLSPEC */
 
 #ifndef HAVE_WORKING___FUNC__
 #ifdef HAVE_WORKING___FUNCTION__
