9cdb7f1482578111d4c528b72d75708ab1a9ba0d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-09-14 19:43:11 +0200

    Added HAVE_SQLSMALLINT and HAVE_SQLUSMALLINT.
    Rev: src/modules/Odbc/acconfig.h:1.5

--- src/modules/Odbc/acconfig.h
+++ src/modules/Odbc/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.4 2000/09/14 13:03:44 grubba Exp $
+ * $Id: acconfig.h,v 1.5 2000/09/14 17:43:11 grubba Exp $
  *
  * Config-file for the Pike ODBC-module.
  *
@@ -10,6 +10,12 @@
 #define PIKE_ODBC_CONFIG_H
 
 @TOP@
+
+/* Define if you have SQLSMALLINT */
+#undef HAVE_SQLSMALLINT
+
+/* Define if you have SQLUSMALLINT */
+#undef HAVE_SQLUSMALLINT
 
 /* Define if you have SQLINTEGER */
 #undef HAVE_SQLINTEGER
