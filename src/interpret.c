dffae566543ad646261760ccc6be6395ccd6280c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-09-19 17:21:44 +0200

    Backported infinite exit_on_error()/describe_svalue()/safe_apply_low() fix from Pike 7.1. Should fix [bug 185].
    Rev: src/interpret.c:1.142

--- src/interpret.c
+++ src/interpret.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: interpret.c,v 1.141 2000/08/28 22:09:54 hubbe Exp $");
+RCSID("$Id: interpret.c,v 1.142 2000/09/19 15:21:44 grubba Exp $");
 #include "interpret.h"
 #include "object.h"
 #include "program.h"
@@ -1422,12 +1422,19 @@ void safe_apply_low(struct object *o,int fun,int args)
   {
     if(throw_value.type == T_ARRAY)
     {
-      ONERROR tmp;
-      SET_ONERROR(tmp,exit_on_error,"Error in handle_error in master object!");
-      assign_svalue_no_free(sp++, & throw_value);
-      APPLY_MASTER("handle_error", 1);
-      pop_stack();
-      UNSET_ONERROR(tmp);
+      static int inside=0;
+      if(!inside)
+      {
+	ONERROR tmp;
+	/* We silently ignore errors if we are already describing one.. */
+        inside=1;
+	SET_ONERROR(tmp,exit_on_error,"Error in handle_error in master object!");
+	assign_svalue_no_free(sp++, & throw_value);
+	APPLY_MASTER("handle_error", 1);
+	pop_stack();
+	UNSET_ONERROR(tmp);
+	inside=0;
+      }
     }
       
     sp->u.integer = 0;
