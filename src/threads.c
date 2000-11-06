b21d9e15a3b6e7ce05141b054374f489b0e463a2
Martin Stjernholm <mast@lysator.liu.se>
2000-11-06 20:29:17 +0100

    Oops.
    Rev: src/threads.c:1.144

--- src/threads.c
+++ src/threads.c
@@ -1,5 +1,5 @@
 #include "global.h"
-RCSID("$Id: threads.c,v 1.143 2000/11/06 17:03:32 grubba Exp $");
+RCSID("$Id: threads.c,v 1.144 2000/11/06 19:29:17 mast Exp $");
 
 PMOD_EXPORT int num_threads = 1;
 PMOD_EXPORT int threads_disabled = 0;
@@ -1413,7 +1413,7 @@ void th_init(void)
   START_NEW_PROGRAM_ID(THREAD_LOCAL);
   ADD_STORAGE(struct thread_local);
   ADD_FUNCTION("get",f_thread_local_get,tFunc(tNone,tMix),0);
-  ADD_FUNCTION("set",f_thread_local_set,tFunc(tSetVar(1,tMix),tVar(1)),0);
+  ADD_FUNCTION("set",f_thread_local_set,tFunc(tSetvar(1,tMix),tVar(1)),0);
   thread_local_prog=Pike_compiler->new_program;
   add_ref(thread_local_prog);
   end_class("thread_local", 0);
