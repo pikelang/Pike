b54ca8f7586fd4b8da6485f7438aef9ece4f83e8
Henrik Grubbström (Grubba) <grubba@grubba.org>
2000-12-02 19:43:57 +0100

    Use INT64 instead of long long.
    Rev: src/modules/Mysql/dummy.c:1.5

--- src/modules/Mysql/dummy.c
+++ src/modules/Mysql/dummy.c
@@ -1,5 +1,5 @@
 /*
- * $Id: dummy.c,v 1.4 1998/03/28 14:44:34 grubba Exp $
+ * $Id: dummy.c,v 1.5 2000/12/02 18:43:57 grubba Exp $
  *
  * Glue needed on Solaris if libgcc.a isn't compiled with -fpic.
  *
@@ -10,10 +10,12 @@
 #include "config.h"
 #endif /* HAVE_CONFIG_H */
 
+#include "global.h"
+
 #ifdef HAVE_MYSQL
 
-typedef long long _ll_t;
-typedef unsigned long long _ull_t;
+typedef INT64 _ll_t;
+typedef unsigned INT64 _ull_t;
 
 static _ll_t mysql_dummy(_ull_t a, _ull_t b, _ll_t c, _ll_t d) {
   return(a%b+(c%d)+(c/d)+(a/b));
