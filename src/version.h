a40df98dad4fdb42d6bddc5482c6f29f1216a07e
Per Hedbor <ph@opera.com>
2000-12-04 01:02:34 +0100

    release number bumped to 12 by export.pike
    Rev: src/version.h:1.203

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.202 2000/10/03 19:29:59 nilsson Exp $ */
+/* $Id: version.h,v 1.203 2000/12/04 00:02:34 per Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 1
-#define PIKE_BUILD_VERSION 11
+#define PIKE_BUILD_VERSION 12
 
 /* Prototypes begin here */
 void f_version(INT32 args);
