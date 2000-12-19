19053a93b889fc44ec33dddbed3b3723166eb1c1
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-12-18 16:42:02 -0800

    nothing changed.. :)
    Rev: src/callback.h:1.12

--- src/callback.h
+++ src/callback.h
@@ -5,7 +5,7 @@
 \*/
 
 /*
- * $Id: callback.h,v 1.11 2000/12/16 04:58:18 marcus Exp $
+ * $Id: callback.h,v 1.12 2000/12/19 00:42:02 hubbe Exp $
  */
 #ifndef CALLBACK_H
 #define CALLBACK_H
@@ -30,9 +30,9 @@ struct callback;
 BLOCK_ALLOC(callback, CALLBACK_CHUNK)
 PMOD_EXPORT void call_callback(struct callback_list *lst, void *arg);
 PMOD_EXPORT struct callback *debug_add_to_callback(struct callback_list *lst,
-				       callback_func call,
-				       void *arg,
-				       callback_func free_func);
+						   callback_func call,
+						   void *arg,
+						   callback_func free_func);
 PMOD_EXPORT void *remove_callback(struct callback *l);
 void free_callback_list(struct callback_list *lst);
 void cleanup_callbacks(void);
