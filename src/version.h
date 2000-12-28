35989f487509f498fa567161abc0e9f8511953f8
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2000-12-27 18:16:19 -0800

    release number bumped to 20 by export.pike
    Rev: src/version.h:1.211

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.210 2000/12/28 02:14:43 hubbe Exp $ */
+/* $Id: version.h,v 1.211 2000/12/28 02:16:19 hubbe Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 1
-#define PIKE_BUILD_VERSION 19
+#define PIKE_BUILD_VERSION 20
 
 /* Prototypes begin here */
 void f_version(INT32 args);
