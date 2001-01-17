d9c458d18081dcd77a7c6fe596693a97c1847cc2
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-01-18 00:46:14 +0100

    Workaround for systems without PIKE_THREADS support.
    Rev: src/modules/Mysql/acconfig.h:1.3

--- src/modules/Mysql/acconfig.h
+++ src/modules/Mysql/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.2 2000/03/22 23:20:08 grubba Exp $
+ * $Id: acconfig.h,v 1.3 2001/01/17 23:46:14 grubba Exp $
  *
  * Config-file for the Pike mySQL-module.
  *
@@ -9,6 +9,9 @@
 #ifndef PIKE_MYSQL_CONFIG_H
 #define PIKE_MYSQL_CONFIG_H
 
+/* We don't support Mysql without threads. */
+#ifdef PIKE_THREADS
+
 @TOP@
 @BOTTOM@
 
@@ -17,5 +20,7 @@
 
 /* Return type of mysql_fetch_lengths(). Usually unsigned long. */
 #undef FETCH_LENGTHS_TYPE
+
+#endif /* PIKE_THREADS */
 
 #endif /* PIKE_MYSQL_CONFIG_H */
