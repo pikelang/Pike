851ea4b8fa896b5bda9b86744fd4c28a03d3125f
Tomas Nilsson <tomas@roxen.com>
2001-02-07 16:20:58 +0100

    Backported bugfix for binary data in mysql on NT from 7.3
    Rev: src/modules/Mysql/acconfig.h:1.3
    Rev: src/modules/Mysql/configure.in:1.20

--- src/modules/Mysql/acconfig.h
+++ src/modules/Mysql/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.2 2000/03/22 23:20:08 grubba Exp $
+ * $Id: acconfig.h,v 1.3 2001/02/07 15:20:57 tomas Exp $
  *
  * Config-file for the Pike mySQL-module.
  *
@@ -17,5 +17,17 @@
 
 /* Return type of mysql_fetch_lengths(). Usually unsigned long. */
 #undef FETCH_LENGTHS_TYPE
+
+/* Define if you have mysql_fetch_lengths(). */
+#undef HAVE_MYSQL_FETCH_LENGTHS
+
+/* Define if you have mysql_real_query(). */
+#undef HAVE_MYSQL_REAL_QUERY
+
+/* Define if you have the mysql_port variable. */
+#undef HAVE_MYSQL_PORT
+
+/* Define if you have the mysql_unix_port variable. */
+#undef HAVE_MYSQL_UNIX_PORT
 
 #endif /* PIKE_MYSQL_CONFIG_H */
