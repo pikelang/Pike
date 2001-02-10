0be037b4ab576578896d48079ec299af1a03545e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-02-10 17:33:18 +0100

    Converted from MirarDoc to AutoDoc mk II.
    Rev: src/modules/Mysql/mysql.c:1.42

--- src/modules/Mysql/mysql.c
+++ src/modules/Mysql/mysql.c
@@ -1,5 +1,5 @@
 /*
- * $Id: mysql.c,v 1.41 2001/01/15 22:14:34 mast Exp $
+ * $Id: mysql.c,v 1.42 2001/02/10 16:33:18 grubba Exp $
  *
  * SQL database functionality for Pike
  *
@@ -91,30 +91,29 @@ typedef struct dynamic_buffer_s dynamic_buffer;
  * Globals
  */
 
-RCSID("$Id: mysql.c,v 1.41 2001/01/15 22:14:34 mast Exp $");
+RCSID("$Id: mysql.c,v 1.42 2001/02/10 16:33:18 grubba Exp $");
 
-/*
-**! module Mysql
-**!
-**!	This module enables access to the Mysql database from within Pike.
-**!
-**!	Mysql is available from www.tcx.se.
-**!
-**! see also: Mysql.mysql, Mysql.result, Sql.sql
-**!
-**! note
-**!	$Id: mysql.c,v 1.41 2001/01/15 22:14:34 mast Exp $
-**!
-**! class mysql
-**!
-**!	Mysql.mysql is a pre-compiled Pike program. It enables
-**!	access to the Mysql database from within Pike. Mysql.mysql
-**!	is a part of the Mysql module.
-**!
-**!	Mysql is available from	www.tcx.se.
-**!
-**! see also: Mysql.result, Sql.sql
-*/
+/*! @module Mysql
+ *!
+ *! This module enables access to the Mysql database from within Pike.
+ *!
+ *! Mysql is available from www.myswl.com.
+ *!
+ *! @seealso
+ *!  @[Mysql.mysql], @[Mysql.result], @[Sql.sql]
+ */
+
+/*! @class mysql
+ *!
+ *! Interface to the Mysql database.
+ *!
+ *! This class enables access to the Mysql database from within Pike.
+ *!
+ *! Mysql is available from www.mysql.com.
+ *!
+ *! @seealso
+ *!   @[Mysql.result], @[Sql.sql]
+ */
 
 struct program *mysql_program = NULL;
 
@@ -344,23 +343,21 @@ static void pike_mysql_reconnect(void)
  * Methods
  */
 
-/*
-**! method void create()
-**! method void create(string host)
-**! method void create(string host, string database)
-**! method void create(string host, string database, string user)
-**! method void create(string host, string database, string user, string password)
-**!
-**!	Connects to a Mysql database.
-**!
-**!	To access the Mysql database, you must first connect to it. This is
-**!	done with the function mysql().
-**!
-**!	If you give no argument, or give "" as hostname it will connect with
-**!	a UNIX-domain socket, which is a big performance gain.
-*/
-
-/* void create(string|void host, string|void database, string|void user, string|void password) */
+/*! @decl void create()
+ *! @decl void create(string host)
+ *! @decl void create(string host, string database)
+ *! @decl void create(string host, string database, string user)
+ *! @decl void create(string host, string database, string user, @
+ *!                   string password)
+ *!
+ *! Connect to a Mysql database.
+ *!
+ *! To access the Mysql database, you must first connect to it. This is
+ *! done with this function.
+ *!
+ *! If you give no argument, or give "" as hostname it will connect with
+ *! a UNIX-domain socket, which can be a big performance gain.
+ */
 static void f_create(INT32 args)
 {
   if (args >= 1) {
@@ -404,13 +401,10 @@ static void f_create(INT32 args)
   pike_mysql_reconnect();
 }
 
-/*
-**! method int affected_rows()
-**!
-**!	Returns the number of rows affected by the last query.
-**!
-*/
-/* int affected_rows() */
+/*! @decl int affected_rows()
+ *!
+ *! Returns the number of rows affected by the last query.
+ */
 static void f_affected_rows(INT32 args)
 {
   MYSQL *socket;
@@ -429,14 +423,11 @@ static void f_affected_rows(INT32 args)
   push_int(count);
 }
 
-/*
-**! method int insert_id()
-**!
-**!	Returns the id of the last INSERT query into a table with
-**!	an AUTO INCREMENT field.
-**!
-*/
-/* int insert_id() */
+/*! @decl int insert_id()
+ *!
+ *! Returns the id of the last INSERT query into a table with
+ *! an AUTO INCREMENT field.
+ */
 static void f_insert_id(INT32 args)
 {
   MYSQL *socket;
@@ -456,13 +447,12 @@ static void f_insert_id(INT32 args)
   push_int(id);
 }
 
-/*
-**! method string Pike_error()
-**!
-**!	Returns a string describing the last error from the Mysql-server.
-**!
-*/
-/* int|string Pike_error() */
+/*! @decl string Pike_error()
+ *!
+ *! Returns a string describing the last error from the Mysql-server.
+ *!
+ *! Returns @tt{0@} (zero) if there was no error.
+ */
 static void f_error(INT32 args)
 {
   MYSQL *socket;
@@ -489,16 +479,16 @@ static void f_error(INT32 args)
   }
 }
 
-/*
-**! method void select_db(string database)
-**!
-**!	The Mysql-server can hold several databases. You select which one
-**!	you want to access with this function.
-**!
-**! see also: create, create_db, drop_db
-**!
-*/
-/* void select_db(string database) */
+/*! @decl void select_db(string database)
+ *!
+ *! Select database.
+ *!
+ *! The Mysql-server can hold several databases. You select which one
+ *! you want to access with this function.
+ *!
+ *! @seealso
+ *!   @[create()], @[create_db()], @[drop_db()]
+ */
 static void f_select_db(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -552,20 +542,19 @@ static void f_select_db(INT32 args)
   pop_n_elems(args);
 }
 
-/*
-**! method object(Mysql.mysql_result) big_query(string q)
-**!	Make an SQL query
-**! arg string q
-**!	The SQL query
-**!
-**!	This function sends an SQL query to the Mysql-server. The result
-**!	of the query is returned as a Mysql.mysql_result object.
-**!	Returns 0 if the query didn't return any result (e.g. INSERT or
-**!	similar).
-**!
-**! see also: mysql_result
-*/
-/* object(mysql_result) big_query(string q) */
+/*! @decl object(Mysql.mysql_result) big_query(string query)
+ *!
+ *! Make an SQL query.
+ *!
+ *! This function sends the SQL query @[query] to the Mysql-server. The result
+ *! of the query is returned as a @[Mysql.mysql_result] object.
+ *!
+ *! Returns @tt{0@} (zero) if the query didn't return any result
+ *! (e.g. @tt{INSERT@} or similar).
+ *!
+ *! @seealso
+ *!   @[Mysql.mysql_result]
+ */
 static void f_big_query(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -681,18 +670,16 @@ static void f_big_query(INT32 args)
   }
 }
 
-/*
-**! method void create_db(string database)
-**!	Create a new database
-**! arg string database
-**!	Name of the database to be created
-**!
-**!	This function creates a new database in the Mysql-server.
-**!
-**! see also: select_db, drop_db
-**!
-*/
-/* void create_db(string database) */
+/*! @decl void create_db(string database)
+ *!
+ *! Create a new database
+ *!
+ *! This function creates a new database named @[database]
+ *! in the Mysql-server.
+ *!
+ *! @seealso
+ *!   @[select_db()], @[drop_db()]
+ */
 static void f_create_db(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -744,18 +731,15 @@ static void f_create_db(INT32 args)
   pop_n_elems(args);
 }
 
-/*
-**! method void drop_db(string database)
-**!	Drop a database
-**! arg string database
-**!	Name of database to be dropped
-**!
-**!	This function drops a database from a Mysql-server.
-**!
-**! see also: create_db, select_db
-**!
-*/
-/* void drop_db(string database) */
+/*! @decl void drop_db(string database)
+ *!
+ *! Drop a database
+ *!
+ *! This function drops the database named @[database] from the Mysql-server.
+ *!
+ *! @seealso
+ *!  @[create_db()], @[select_db()]
+ */
 static void f_drop_db(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -807,17 +791,15 @@ static void f_drop_db(INT32 args)
   pop_n_elems(args);
 }
 
-/*
-**! method void shutdown()
-**!	Shutdown the Mysql-server
-**!
-**!	This function shuts down a running Mysql-server.
-**!
-**! see also:
-**!	reload
-**!
-*/
-/* void shutdown() */
+/*! @decl void shutdown()
+ *!
+ *! Shutdown the Mysql-server
+ *!
+ *! This function shuts down a running Mysql-server.
+ *!
+ *! @seealso
+ *!   @[reload()]
+ */
 static void f_shutdown(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -850,16 +832,15 @@ static void f_shutdown(INT32 args)
   pop_n_elems(args);
 }
 
-/*
-**! method void reload()
-**!	Reload security tables
-**!
-**!	This function causes the Mysql-server to reload its access tables.
-**!
-**! see also: shutdown
-**!
-*/
-/* void reload() */
+/*! @decl void reload()
+ *!
+ *! Reload security tables
+ *!
+ *! This function causes the Mysql-server to reload its access tables.
+ *!
+ *! @seealso
+ *!   @[shutdown()]
+ */
 static void f_reload(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -892,24 +873,15 @@ static void f_reload(INT32 args)
   pop_n_elems(args);
 }
 
-/*
-**! method string statistics()
-**!	Some Mysql-server statistics
-**!
-**!	This function returns some server statistics.
-**!
-**! 	Example:
-**!	<pre>
-**!	int main()
-**!	{
-**!	  write(mysql()->statistics());
-**!	  return(0);
-**!	}
-**!	</pre>
-**! see also: server_info, host_info, protocol_info
-**!
-*/
-/* string statistics() */
+/*! @decl string statistics()
+ *!
+ *! Some Mysql-server statistics
+ *!
+ *! This function returns some server statistics.
+ *!
+ *! @seealso
+ *!   @[server_info()], @[host_info()], @[protocol_info()]
+ */
 static void f_statistics(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -931,16 +903,13 @@ static void f_statistics(INT32 args)
   push_text(stats);
 }
 
-/*
-**! method string server_info()
-**!	Give the version number of the Mysql-server
-**!
-**!	This function returns the version number of the Mysql-server.
-**!
-**! see also: statistics, host_info, protocol_info
-**!
-*/
-/* string server_info() */
+/*! @decl string server_info()
+ *!
+ *! Get the version number of the Mysql-server.
+ *!
+ *! @seealso
+ *!   @[statistics()], @[host_info()], @[protocol_info()]
+ */
 static void f_server_info(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -965,17 +934,13 @@ static void f_server_info(INT32 args)
   f_add(2);
 }
 
-/*
-**! method string host_info()
-**!	Give information about the Mysql-server connection
-**!
-**!	This function returns a string describing the connection to
-**!	the Mysql-server.
-**!
-**! see also: statistics, server_info, protocol_info
-**!
-*/
-/* string host_info() */
+/*! @decl string host_info()
+ *!
+ *! Get information about the Mysql-server connection
+ *!
+ *! @seealso
+ *!   @[statistics()], @[server_info()], @[protocol_info()]
+ */
 static void f_host_info(INT32 args)
 {
   MYSQL *socket;
@@ -998,17 +963,16 @@ static void f_host_info(INT32 args)
   push_text(info);
 }
 
-/*
-**! method int protocol_info()
-**!	Give the Mysql protocol version
-**!
-**!	This function returns the version number of the protocol the
-**!	Mysql-server uses.
-**!
-**! see also: statistics, server_info, host_info
-**!
-*/
-/* int protocol_info() */
+/*! @decl int protocol_info()
+ *!
+ *! Give the Mysql protocol version
+ *!
+ *! This function returns the version number of the protocol the
+ *! Mysql-server uses.
+ *!
+ *! @seealso
+ *!   @[statistics()], @[server_info()], @[host_info()]
+ */
 static void f_protocol_info(INT32 args)
 {
   MYSQL *socket;
@@ -1029,19 +993,19 @@ static void f_protocol_info(INT32 args)
   push_int(prot);
 }
 
-/*
-**! method object(Mysql.mysql_result) list_dbs()
-**! method object(Mysql.mysql_result) list_dbs(string wild)
-**!	List databases
-**!
-**!	Returns a table containing the names of all databases in the
-**!	Mysql-server. If an argument is specified, only those matching
-**!	wild are returned.
-**!
-**! see also: list_tables, list_fields, list_processes, Mysql.mysql_result
-**!
-*/
-/* object(mysql_res) list_dbs(void|string wild) */
+/*! @decl object(Mysql.mysql_result) list_dbs()
+ *! @decl object(Mysql.mysql_result) list_dbs(string wild)
+ *!
+ *! List databases
+ *!
+ *! Returns a table containing the names of all databases in the
+ *! Mysql-server. If the argument @[wild] is specified, only those matching
+ *! it will be returned.
+ *!
+ *! @seealso
+ *!   @[list_tables()], @[list_fields()], @[list_processes()],
+ *!   @[Mysql.mysql_result]
+ */
 static void f_list_dbs(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -1103,21 +1067,19 @@ static void f_list_dbs(INT32 args)
   push_object(clone_object(mysql_result_program, 1));
 }
 
-/*
-**! method object(Mysql.mysql_result) list_tables()
-**! method object(Mysql.mysql_result) list_tables(string wild)
-**!	List tables in the current database
-**! arg string wild
-**!	Wildcard to filter with.
-**!
-**!	Returns a table containing the names of all tables in the current
-**!	database. If an argument is given, only those matching wild are
-**!	returned.
-**!
-**! see also: list_dbs, list_fields, list_processes, Mysql.mysql_result
-**!
-*/
-/* object(mysql_res) list_tables(void|string wild) */
+/*! @decl object(Mysql.mysql_result) list_tables()
+ *! @decl object(Mysql.mysql_result) list_tables(string wild)
+ *!
+ *! List tables in the current database
+ *!
+ *! Returns a table containing the names of all tables in the current
+ *! database. If the argument @[wild] is given, only those matching it
+ *! will be returned.
+ *!
+ *! @seealso
+ *!   @[list_dbs()], @[list_fields()], @[list_processes()],
+ *!   @[Mysql.mysql_result]
+ */
 static void f_list_tables(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -1179,51 +1141,59 @@ static void f_list_tables(INT32 args)
   push_object(clone_object(mysql_result_program, 1));
 }
 
-/*
-**! method array(int|mapping(string:mixed)) list_fields(string table)
-**! method array(int|mapping(string:mixed)) list_fields(string table, string wild)
-**!	List all fields.
-**!
-**!	Returns an array of mappings with information about the fields in the
-**!	specified table.
-**!
-**!	The mappings contain the following entries:
-**!	<pre>
-**!	 "name":	string	The name of the field.
-**!	 "table":	string	The name of the table.
-**!	 "default":	string	The default value for the field.
-**!	 "type":	string	The type of the field.
-**!	 "length":	int	The length of the field.
-**!	 "max_length":	int	The length of the longest element in this field.
-**!	 "flags":	multiset(string)	Some flags.
-**!	 "decimals":	int	The number of decimalplaces.
-**!	</pre>
-**!
-**!	The type of the field can be any of:
-**!	"decimal", "char", "short", "long", "float", "double", "null",
-**!	"time", "longlong", "int24", "tiny blob", "medium blob",
-**!	"long blob", "var string", "string" or "unknown".
-**!
-**!	The flags multiset can contain any of
-**!	 "primary_key":	This field is part of the primary key for this table.
-**!	 "not_null":	This field may not be NULL.
-**!	 "blob":	This field is a blob field.
-**!
-**! arg string table
-**!	Name of table to list the fields of
-**! arg string wild
-**!	Wildcard to filter the result with
-**!
-**! note
-**!	Michael Widenius recomends usage of the following query instead:
-**!	<pre>
-**!	 show fields in 'table' like "wild"
-**!	</pre>
-**!
-**! see also: list_dbs, list_tables, list_processes, fetch_fields
-**!
-*/
-/* array(int|mapping(string:mixed)) list_fields(string table, void|string wild) */
+/*! @decl array(int|mapping(string:mixed)) list_fields(string table)
+ *! @decl array(int|mapping(string:mixed)) list_fields(string table, @
+ *!                                                    string wild)
+ *!
+ *! List all fields.
+ *!
+ *! Returns an array of mappings with information about the fields in the
+ *! table named @[teble]. If the argument @[wild] is given, only those
+ *! fields matching it will be returned
+ *!
+ *! The mappings contain the following entries:
+ *! @mapping
+ *!   @member string "name"
+ *!     The name of the field.
+ *!   @member string "table"
+ *!	The name of the table.
+ *!   @member string "default"
+ *!	The default value for the field.
+ *!   @member string "type"
+ *!	The SQL type of the field.
+ *!   @member int "length"
+ *!	The length of the field.
+ *!   @member int "max_length"
+ *!	The length of the longest element in this field.
+ *!   @member multiset(string) "flags"
+ *!	Some flags.
+ *!   @member int decimals
+ *!	The number of decimalplaces.
+ *! @endmapping
+ *!
+ *! The type of the field can be any of:
+ *!   @tt{"decimal"@}, @tt{"char"@}, @tt{"short"@}, @tt{"long"@},
+ *!   @tt{"float"@}, @tt{"double"@}, @tt{"null"@}, @tt{"time"@},
+ *!   @tt{"longlong"@}, @tt{"int24"@}, @tt{"tiny blob"@}, @tt{"medium blob"@},
+ *!   @tt{"long blob"@}, @tt{"var string"@}, @tt{"string"@} or @tt{"unknown"@}.
+ *!
+ *! The flags multiset can contain any of:
+ *! @multiset
+ *!   @index "primary_key"
+ *!     This field is part of the primary key for this table.
+ *!   @index "not_null"
+ *!     This field may not be NULL.
+ *!   @index "blob"
+ *!     This field is a blob field.
+ *! @endmultiset
+ *!
+ *! @note
+ *!   Michael Widenius recomends usage of the following query instead:
+ *!   @tt{show fields in 'table' like "wild"@}.
+ *!
+ *! @seealso
+ *!   @[list_dbs()], @[list_tables()], @[list_processes()], @[fetch_fields()]
+ */
 static void f_list_fields(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -1312,17 +1282,16 @@ static void f_list_fields(INT32 args)
   f_aggregate(i);
 }
 
-/*
-**! method object(Mysql.mysql_result) list_processes()
-**!	List all processes in the Mysql-server
-**!
-**!	Returns a table containing the names of all processes in the
-**!	Mysql-server.
-**!
-**! see also: list_dbs, list_tables, list_fields, Mysql.mysql_result
-**!
-*/
-/* object(mysql_res) list_processes() */
+/*! @decl object(Mysql.mysql_result) list_processes()
+ *!
+ *! List all processes in the Mysql-server
+ *!
+ *! Returns a table containing the names of all processes in the
+ *! Mysql-server.
+ *!
+ *! @seealso
+ *!   @[list_dbs()], @[list_tables()], @[list_fields()], @[Mysql.mysql_result]
+ */
 static void f_list_processes(INT32 args)
 {
   MYSQL *socket = PIKE_MYSQL->socket;
@@ -1367,20 +1336,16 @@ static void f_list_processes(INT32 args)
   push_object(clone_object(mysql_result_program, 1));
 }
 
-/*
-**! method int binary_data()
-**!	Inform if this version of mysql supports binary data
-**!
-**!	This function returns non-zero if binary data can be reliably stored
-**!	and retreived with this version of the mysql-module.
-**!
-**!	Usually, there is no problem storing binary data in mysql-tables,
-**!	but data containing '\0' (NUL) couldn't be fetched with old
-**!	versions (prior to 3.20.5) of the mysql-library.
-**!
-*/
-/*
- * Support for binary data in tables
+/*! @decl int binary_data()
+ *!
+ *! Inform if this version of @[Mysql.mysql] supports binary data
+ *!
+ *! This function returns non-zero if binary data can be reliably stored
+ *! and retreived with this version of the mysql-module.
+ *!
+ *! Usually, there is no problem storing binary data in mysql-tables,
+ *! but data containing @tt{'\0'@} (NUL) couldn't be fetched with old
+ *! versions (prior to 3.20.5) of the mysql-library.
  */
 static void f_binary_data(INT32 args)
 {
@@ -1391,6 +1356,12 @@ static void f_binary_data(INT32 args)
   push_int(0);
 #endif /* HAVE_MYSQL_FETCH_LENGTHS */
 }
+
+/*! @endclass
+ */
+
+/*! @endmodule
+ */
 
 #endif /* HAVE_MYSQL */
 
