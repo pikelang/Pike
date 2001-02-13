a4c8f384a5fcfad06a9340e7236649d292a28c7d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-02-13 17:20:08 +0100

    Fixed some types.
    Rev: src/modules/_Crypto/sha.c:1.20

--- src/modules/_Crypto/sha.c
+++ src/modules/_Crypto/sha.c
@@ -1,4 +1,4 @@
-/* $Id: sha.c,v 1.19 2001/02/13 16:14:13 grubba Exp $
+/* $Id: sha.c,v 1.20 2001/02/13 16:20:08 grubba Exp $
  *
  * Written by Niels Möller
  */
@@ -16,7 +16,7 @@
 #include "module_support.h"
 #include "las.h"
 
-RCSID("$Id: sha.c,v 1.19 2001/02/13 16:14:13 grubba Exp $");
+RCSID("$Id: sha.c,v 1.20 2001/02/13 16:20:08 grubba Exp $");
 
 #include <sha.h>
 
@@ -141,12 +141,12 @@ void pike_sha_init(void)
   start_new_program();
   ADD_STORAGE(struct sha_ctx);
   /* function(void:string) */
-  ADD_FUNCTION("name", f_name,tFunc(tVoid,tStr), OPT_TRY_OPTIMIZE);
+  ADD_FUNCTION("name", f_name, tFunc(tNone, tStr), OPT_TRY_OPTIMIZE);
   /* function(void|object:void) */
-  ADD_FUNCTION("create", f_create,tFunc(tOr(tVoid,tObj),tVoid), 0);
+  ADD_FUNCTION("create", f_create, tFunc(tOr(tVoid,tObj), tVoid), 0);
   /* function(string:object) */
-  ADD_FUNCTION("update", f_update,tFunc(tStr,tObj), 0);
+  ADD_FUNCTION("update", f_update, tFunc(tStr, tObj), 0);
   /* function(void:string) */
-  ADD_FUNCTION("digest", f_digest,tFunc(tVoid,tStr), 0);
+  ADD_FUNCTION("digest", f_digest, tFunc(tNone, tStr), 0);
   end_class("sha", 0);
 }
