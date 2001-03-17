9896efeabac0489a7b664c90ba840ebb8f0dbaca
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-17 17:56:58 +0100

    Bumped build to 7.3.4 since the dump-format has changed sligthly again.
    Rev: src/version.h:1.272

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.271 2001/02/01 23:45:30 noring Exp $ */
+/* $Id: version.h,v 1.272 2001/03/17 16:56:58 grubba Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 2
+#define PIKE_BUILD_VERSION 4
 
 /* Prototypes begin here */
 void f_version(INT32 args);
