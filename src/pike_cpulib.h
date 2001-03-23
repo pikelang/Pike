4c060d314ad581039561f977eb1e2173f4aebfc0
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-24 00:09:14 +0100

    Fixed some bugs. Added support for ECL/IA64.
    Rev: src/pike_cpulib.h:1.5

--- src/pike_cpulib.h
+++ src/pike_cpulib.h
@@ -31,18 +31,28 @@ pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
 {
   INT32 ret;
 
-  register ptrdiff_t cmpval __asm__("ar.ccv") = oldval;
-
   __asm__ __volatile__ ("mov ar.ccv = %1;\n"
 			"\t;;\n"
 			"\tcmpxchg4.rel.nta %0 = [%2], %3, ar.ccv"
 			: "=r" (ret)
-			: "r" (cmpval), "r" (p), "r" (newval)
+			: "r" (oldval), "r" (p), "r" (newval)
 			: "memory", "ar.ccv");
-  return ret;
+  return ret == oldval;
 }
 
 #endif /* __i386__ && __GNUC__ */
+
+#if defined(_M_IA64)
+
+#define PIKE_HAS_COMPARE_AND_SWAP
+
+static inline int
+pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
+{
+  return _InterlockedCompareExchange(p, newval, oldval) == oldval;
+}
+
+#endif /* _M_IA64 */
 
 #if defined(__sparc_v9__) && defined(__GNUC__)
 
@@ -56,7 +66,7 @@ pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
 			: "=r" (newval)
 			: "0" (newval), "r" (p), "r" (oldval)
 			: "memory");
-  return newval;
+  return newval == oldval;
 }
 
 #endif /* __sparc_v9__ && __GNUC__ */
@@ -69,17 +79,19 @@ pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
 static inline int
 pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
 {
+  INT32 cmpval = oldval;
+
   /* if (oldval == *p) {
    *   *p = newval;
    * } else {
    *   oldval = *p;
    * }
    */
-  __asm__ __volatile__ ("casl %0, %3, %1"
+  __asm__ __volatile__ ("casl %0, %3, %1;"
 			: "=d" (oldval), "=m", (*p)
 			: "0" (oldval), "d" (newval), "0" (p)
 			: "memory");
-  return oldval;
+  return oldval == cmpval;
 }
 
 #endif /* __m68k__ && __GNUC__ */
