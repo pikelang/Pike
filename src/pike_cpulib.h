963075d330255d919dbfecca5717206e34e95606
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-23 21:47:18 +0100

    Support for IA64, Sparc V9 and M68k.
    Rev: src/pike_cpulib.h:1.2

--- src/pike_cpulib.h
+++ src/pike_cpulib.h
@@ -20,10 +20,66 @@ pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
   return ret;
 }
 
-#endif
+#endif /* __i386__ && __GNUC__ */
+
+#if defined(__ia64__) && defined(__GNUC__)
+
+#define PIKE_HAS_COMPARE_AND_SWAP
+
+static inline int
+pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
+{
+  INT32 ret;
+
+  __asm__ __volatile__ ("mov ar.ccv = %1\n"
+			"cmpxchg4.rel.nta %0 = [%2], %3, ar.ccv"
+			: "=r" (ret)
+			: "r" (oldval), "r" (p), "r" (newval)
+			: "ar.ccv", "memory");
+  return ret;
+}
+
+#endif /* __i386__ && __GNUC__ */
+
+#if defined(__sparc_v9__) && defined(__GNUC__)
+
+#define PIKE_HAS_COMPARE_AND_SWAP
+
+static inline int
+pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
+{
+  __asm__ __volatile__ ("membar #LoadStore\n"
+			"cas [%2], %3, %0"
+			: "=r" (newval)
+			: "0" (newval), "r" (p), "r" (oldval)
+			: "memory");
+  return newval;
+}
+
+#endif /* __sparc_v9__ && __GNUC__ */
+
+#if defined(__m68k__) && defined(__GNUC__)
 
 
+#define PIKE_HAS_COMPARE_AND_SWAP
 
+static inline int
+pike_atomic_compare_and_swap (INT32 *p, INT32 oldval, INT32 newval)
+{
+  /* if (oldval == *p) {
+   *   *p = newval;
+   * } else {
+   *   oldval = *p;
+   * }
+   */
+  __asm__ __volatile__ ("casl %0, %3, %1"
+			: "=d" (oldval), "=m", (p)
+			: "0" (oldval), "d" (newval), "0" (p)
+			: "memory");
+  return oldval;
+}
+
+#endif /* __m68k__ && __GNUC__ */
 
 #ifdef PIKE_HAS_COMPARE_AND_SWAP
 static inline INT32 pike_atomic_add_ref(INT32 *ref)
