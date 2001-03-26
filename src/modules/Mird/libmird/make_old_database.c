78ea82dcac9788fd9fc9e177120dfde4a7116f69
Mirar (Pontus Hagland) <pike@sort.mirar.org>
2001-03-26 14:33:00 +0200

    including libmird sources
    Rev: src/modules/Mird/Makefile.in:1.3
    Rev: src/modules/Mird/configure.in:1.4
    Rev: src/modules/Mird/libmird/Makefile.in:1.1
    Rev: src/modules/Mird/libmird/acconfig.h:1.1
    Rev: src/modules/Mird/libmird/blocks.c:1.1
    Rev: src/modules/Mird/libmird/cells.c:1.1
    Rev: src/modules/Mird/libmird/configure.in:1.1
    Rev: src/modules/Mird/libmird/database.c:1.1
    Rev: src/modules/Mird/libmird/debug.c:1.1
    Rev: src/modules/Mird/libmird/dmalloc.c:1.1
    Rev: src/modules/Mird/libmird/dmalloc.h:1.1
    Rev: src/modules/Mird/libmird/errors.c:1.1
    Rev: src/modules/Mird/libmird/frags.c:1.1
    Rev: src/modules/Mird/libmird/freelist.c:1.1
    Rev: src/modules/Mird/libmird/hashtrie.c:1.1
    Rev: src/modules/Mird/libmird/internal.h:1.1
    Rev: src/modules/Mird/libmird/journal.c:1.1
    Rev: src/modules/Mird/libmird/make_old_database.c:1.1
    Rev: src/modules/Mird/libmird/mird.h.in:1.1
    Rev: src/modules/Mird/libmird/physical.h:1.1
    Rev: src/modules/Mird/libmird/skeys.c:1.1
    Rev: src/modules/Mird/libmird/tables.c:1.1
    Rev: src/modules/Mird/libmird/test.c:1.1
    Rev: src/modules/Mird/libmird/testsuite.c:1.1
    Rev: src/modules/Mird/libmird/transaction.c:1.1
    Rev: src/modules/Mird/libmird/usage.c:1.1
    Rev: src/modules/Mird/libmird/win32-config.h:1.1

---
+++ src/modules/Mird/libmird/make_old_database.c
@@ -0,0 +1,59 @@
+/*
+** libMird by Mirar <mirar@mirar.org>
+** please submit bug reports and patches to the author
+**
+** also see http://www.mirar.org/mird/
+**
+** for licence, read the LICENCE file
+**
+** $Id: make_old_database.c,v 1.1 2001/03/26 12:32:51 mirar Exp $
+**
+*/ 
+#include <stdio.h>
+#include "mird.h"
+
+#define TRY(WHAT) 							\
+   do {									\
+      MIRD_RES res;							\
+      res=(WHAT);							\
+      if (res)								\
+      {									\
+	 char *s;							\
+	 mird_describe_error(res,&s);					\
+	 fprintf(stderr,"fail\n   %s:%d:\n   %s:\n   %s\n\n",		\
+                 __FILE__,__LINE__,#WHAT,s);				\
+	 mird_free(s);							\
+         mird_free_error(res);						\
+	 exit(1);							\
+      }									\
+   } while(0)
+
+int main()
+{
+   struct mird *db;
+   struct mird_transaction *mtr;
+
+   TRY(mird_initialize("old-database.mird",&db));
+   db->block_size=128;
+   db->frag_bits=2;
+   db->hashtrie_bits=2;
+   db->flags|=MIRD_EXCL;
+   TRY(mird_open(db));
+   
+   TRY(mird_transaction_new(db,&mtr));
+
+   TRY(mird_key_new_table(mtr,17));
+   TRY(mird_s_key_new_table(mtr,18));
+
+   TRY(mird_key_store(mtr,17,1001,"\0\0\3\351",4));
+   TRY(mird_key_store(mtr,17,1,"\0\0\0\1",4));
+
+   TRY(mird_s_key_store(mtr,18,"eva",3,"\0\0\0\1",4));
+   TRY(mird_s_key_store(mtr,18,"lena",4,"\0\0\3\351",4));
+   TRY(mird_s_key_store(mtr,18,"sofia",5,"\0\17F)",4));
+
+   TRY(mird_transaction_close(mtr));
+   TRY(mird_close(db));
+
+   return 0;
+}
