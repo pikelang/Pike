e95a16496f525334fb9e51d9f37e0d4f4c205fd7
Mirar (Pontus Hagland) <pike@sort.mirar.org>
2001-03-26 14:57:15 +0200

    lost?
    Rev: src/modules/Mird/acconfig.h:1.3

--- src/modules/Mird/acconfig.h
+++ src/modules/Mird/acconfig.h
@@ -1,1 +1,8 @@
-/* $Id: acconfig.h,v 1.2 2000/05/11 06:29:33 mirar Exp $ */
+/* $Id: acconfig.h,v 1.3 2001/03/26 12:57:15 mirar Exp $ */
+
+/* Define if you have the `mird' library (-lmird). */
+#undef HAVE_LIBMIRD
+
+/* Define if you have the <mird.h> header file. */
+#undef HAVE_MIRD_H
+
