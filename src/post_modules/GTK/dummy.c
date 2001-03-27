d74227f01fb991876f958fcaf7e8d0951c1fdd6e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-27 17:50:08 +0200

    Renamed config.h to pgtk_config.h. This works around the stupidity of Postgres, that installs a config.h file...
    Rev: src/post_modules/GTK/.cvsignore:1.8
    Rev: src/post_modules/GTK/Makefile.in:1.31
    Rev: src/post_modules/GTK/configure.in:1.30
    Rev: src/post_modules/GTK/dependencies:1.10
    Rev: src/post_modules/GTK/dummy.c:1.3
    Rev: src/post_modules/GTK/pgtk.h:1.25

--- src/post_modules/GTK/dummy.c
+++ src/post_modules/GTK/dummy.c
@@ -1,8 +1,8 @@
 #include "global.h"
-#include "config.h"
+#include "pgtk_config.h"
 #include "module_magic.h"
 
-RCSID("$Id: dummy.c,v 1.2 2000/07/28 07:16:22 hubbe Exp $");
+RCSID("$Id: dummy.c,v 1.3 2001/03/27 15:50:08 grubba Exp $");
 
 /* Well... Sort of basic, right? :-) */
 
