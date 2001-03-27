94b2fbd3edc61a85270410a99b8e25c4145a6399
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-03-28 00:07:55 +0200

    Improved support for DEBUG_MALLOC.
    Rev: src/pike_types.c:1.169

--- src/pike_types.c
+++ src/pike_types.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: pike_types.c,v 1.168 2001/03/27 20:18:37 grubba Exp $");
+RCSID("$Id: pike_types.c,v 1.169 2001/03/27 22:07:55 grubba Exp $");
 #include <ctype.h>
 #include "svalue.h"
 #include "pike_types.h"
@@ -72,6 +72,10 @@ PMOD_EXPORT struct pike_type *void_type_string;
 PMOD_EXPORT struct pike_type *zero_type_string;
 PMOD_EXPORT struct pike_type *any_type_string;
 PMOD_EXPORT struct pike_type *weak_type_string;	/* array|mapping|multiset|function */
+
+#if defined(USE_PIKE_TYPE) && defined(DEBUG_MALLOC)
+struct pike_type_location *all_pike_type_locations = NULL;
+#endif /* USE_PIKE_TYPE && DEBUG_MALLOC */
 
 static struct pike_type *a_markers[10], *b_markers[10];
 
@@ -7750,6 +7754,15 @@ void init_types(void)
 
 void cleanup_pike_types(void)
 {
+#if defined(USE_PIKE_TYPE) && defined(DEBUG_MALLOC)
+  struct pike_type *t = all_pike_type_locations;
+
+  while(t) {
+    free_type(t->t);
+    t = t->next;
+  }
+#endif /* USE_PIKE_TYPE && DEBUG_MALLOC */
+
   free_type(string_type_string);
   free_type(int_type_string);
   free_type(float_type_string);
