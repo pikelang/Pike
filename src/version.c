2187c6d1f7b0a9084039319b7d9f05a60455be13
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-04-06 17:40:57 +0200

    Bumped version.
    Rev: src/version.c:1.156

--- src/version.c
+++ src/version.c
@@ -3,16 +3,17 @@
 ||| Pike is distributed as GPL (General Public License)
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
+/**/
 
 #include "global.h"
 #include "svalue.h"
 #include "interpret.h"
 #include "stralloc.h"
 
-RCSID("$Id: version.c,v 1.155 2001/04/06 15:35:52 grubba Exp $");
+RCSID("$Id: version.c,v 1.156 2001/04/06 15:40:57 grubba Exp $");
 
 void f_version(INT32 args)
 {
   pop_n_elems(args);
-  push_text("Pike v0.6 release 138");
+  push_text("Pike v0.6 release 139");
 }
