4b2b5fb19c65c57bb1050d05c9429edd2b7539d1
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-04-07 18:00:09 +0200

    Bumped version by hand.
    Rev: src/version.c:1.82

--- src/version.c
+++ src/version.c
@@ -3,7 +3,7 @@
 ||| Pike is distributed as GPL (General Public License)
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
-
+/**/
 #include "global.h"
 #include "svalue.h"
 #include "interpret.h"
@@ -12,5 +12,5 @@
 void f_version(INT32 args)
 {
   pop_n_elems(args);
-  push_text("Pike v0.5 release 102");
+  push_text("Pike v0.5 release 103");
 }
