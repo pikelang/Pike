07005ccd85583215c567ed271eb968c1a503de9d
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2001-05-17 16:57:12 -0700

    bugfix for #string
    Rev: src/preprocessor.h:1.37

--- src/preprocessor.h
+++ src/preprocessor.h
@@ -1,5 +1,5 @@
 /*
- * $Id: preprocessor.h,v 1.36 2000/12/05 21:08:21 per Exp $
+ * $Id: preprocessor.h,v 1.37 2001/05/17 23:57:12 hubbe Exp $
  *
  * Preprocessor template.
  * Based on cpp.c 1.45
@@ -264,7 +264,7 @@ static void PUSH_STRING(WCHAR *str,
 	  string_builder_putchar(buf, ((c>>6)&7)+'0');
 	  string_builder_putchar(buf, ((c>>3)&7)+'0');
 	  string_builder_putchar(buf, (c&7)+'0');
-	  if((str[p2+1] >= '0') && (str[p2+1] <= '7'))
+	  if((str[p2+1] >= '0') && (str[p2+1] <= '9'))
 	  {
 	    string_builder_putchar(buf, '"');
 	    string_builder_putchar(buf, '"');
