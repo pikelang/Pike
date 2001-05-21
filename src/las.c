f3ff92d83cb33c43ac5a8ad9a3c01ae21b583a69
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-05-22 00:24:28 +0200

    Backported fix from Pike 7.3.
    Rev: src/las.c:1.234

--- src/las.c
+++ src/las.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: las.c,v 1.233 2001/02/09 17:30:19 hubbe Exp $");
+RCSID("$Id: las.c,v 1.234 2001/05/21 22:24:28 grubba Exp $");
 
 #include "language.h"
 #include "interpret.h"
@@ -4850,7 +4850,7 @@ int dooptcode(struct pike_string *name,
 		      type,
 		      (unsigned INT8)modifiers,
 		      (unsigned INT8)(IDENTIFIER_PIKE_FUNCTION | vargs),
-		      &tmp,
+		      Pike_compiler->num_parse_error?NULL:&tmp,
 		      (unsigned INT16)
 		      (Pike_compiler->compiler_frame->opt_flags));
 
