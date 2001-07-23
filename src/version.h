0abf086f5cd7b7f8592ccd3cad3633c910def123
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-07-23 14:39:07 +0200

    Bumped version to Pike 7.3.11.
    Rev: src/version.h:1.277

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.276 2001/07/08 19:55:05 grubba Exp $ */
+/* $Id: version.h,v 1.277 2001/07/23 12:39:07 grubba Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 9
+#define PIKE_BUILD_VERSION 11
 
 /* Prototypes begin here */
 void f_version(INT32 args);
