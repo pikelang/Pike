e7ca3fc4a5161992590602b565a5472e314b9d1c
Martin Stjernholm <mast@lysator.liu.se>
2001-08-29 19:13:21 +0200

    Fix to be able to compile with --with-profiling on systems without long long.
    Rev: src/threads.h:1.112

--- src/threads.h
+++ src/threads.h
@@ -1,5 +1,5 @@
 /*
- * $Id: threads.h,v 1.111 2001/01/24 08:17:28 hubbe Exp $
+ * $Id: threads.h,v 1.112 2001/08/29 17:13:21 mast Exp $
  */
 #ifndef THREADS_H
 #define THREADS_H
@@ -386,7 +386,11 @@ struct thread_state {
   struct mapping *thread_local;
   struct thread_state *hashlink, **backlink;
 #ifdef PROFILING
+#if SIZEOF_LONG_LONG - 0 != 0
   long long time_base;
+#else
+  long time_base;
+#endif
 #endif /* PROFILING */
 };
 
