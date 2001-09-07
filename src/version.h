220cba54b904849559ae269bb36c0d5535ae4e02
Martin Stjernholm <mast@lysator.liu.se>
2001-09-07 13:24:23 +0200

    release number bumped to 189 by export.pike
    Rev: src/version.h:1.458

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.457 2001/09/06 22:46:48 distmaker Exp $ */
+/* $Id: version.h,v 1.458 2001/09/07 11:24:23 mast Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 188
+#define PIKE_BUILD_VERSION 189
 
 /* Prototypes begin here */
 void f_version(INT32 args);
