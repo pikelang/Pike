6f9b545477e18bb4a945fcb59330c04e0f772d6e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-09-12 13:36:14 +0200

    release number bumped to 197 by export.pike
    Rev: src/version.h:1.466

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.465 2001/09/11 11:50:25 distmaker Exp $ */
+/* $Id: version.h,v 1.466 2001/09/12 11:36:14 grubba Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 196
+#define PIKE_BUILD_VERSION 197
 
 /* Prototypes begin here */
 void f_version(INT32 args);
