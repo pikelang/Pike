471c2ce58329c3ea35e99e437278cc07f0e8c2f6
Fredrik Hübinette (Hubbe) <hubbe@hubbe.net>
2001-10-02 12:18:32 -0700

    bugfix for mv() on Win9x, Pike/Roxen installer may work on Win9x now.
    Rev: src/modules/files/efuns.c:1.97

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -25,7 +25,7 @@
 #include "file_machine.h"
 #include "file.h"
 
-RCSID("$Id: efuns.c,v 1.96 2001/06/29 15:00:17 grubba Exp $");
+RCSID("$Id: efuns.c,v 1.97 2001/10/02 19:18:32 hubbe Exp $");
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
@@ -1261,9 +1261,25 @@ void init_files_efuns(void)
   set_close_on_exec(2,1);
 
 #ifdef __NT__
-  /* MoveFileEx doesn't exist in W98 and earlier. */
-  if ((kernel32lib = LoadLibrary ("kernel32")))
-    movefileex = (movefileextype) GetProcAddress (kernel32lib, "MoveFileExA");
+  {
+    /* MoveFileEx doesn't exist in W98 and earlier. */
+    /* Correction, it exists but does not work -Hubbe */
+
+    OSVERSIONINFO osversion;
+    osversion.dwOSVersionInfoSize=sizeof(osversion);
+    if(GetVersionEx(&osversion))
+    {
+      switch(osversion.dwPlatformId)
+      {
+	case VER_PLATFORM_WIN32s: /* win32s */
+	case VER_PLATFORM_WIN32_WINDOWS: /* win9x */
+	  break;
+	default:
+	  if ((kernel32lib = LoadLibrary ("kernel32")))
+	    movefileex = (movefileextype) GetProcAddress (kernel32lib, "MoveFileExA");
+      }
+    }
+  }
 #endif
 
 /* function(string,int|void:int *) */
