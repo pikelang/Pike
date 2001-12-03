5901b4c3834466e6f1d42f919ec4a8f32826a960
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-12-03 15:19:56 +0100

    Improved fix for [bug 2584].
    Rev: src/builtin_functions.c:1.414

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: builtin_functions.c,v 1.413 2001/12/03 13:29:13 grubba Exp $");
+RCSID("$Id: builtin_functions.c,v 1.414 2001/12/03 14:19:56 grubba Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "pike_macros.h"
@@ -4097,7 +4097,7 @@ PMOD_EXPORT void f_mktime (INT32 args)
     date.tm_gmtoff=Pike_sp[7-args].u.integer;
   }else{
     time_t tmp = 0;
-    data.tm_gmtoff=localtime(&tmp)->tm_gmtoff;
+    date.tm_gmtoff=localtime(&tmp)->tm_gmtoff;
   }
   retval=mktime(&date);
 #else
