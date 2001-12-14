54ecf6fbed27ef2bea0c02f4332eaa90d1876367
Henrik Grubbström (Grubba) <grubba@grubba.org>
2001-12-14 11:03:53 +0100

    Fix for [bug 2504].
    Rev: src/program.c:1.391

--- src/program.c
+++ src/program.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: program.c,v 1.390 2001/12/14 04:09:07 mast Exp $");
+RCSID("$Id: program.c,v 1.391 2001/12/14 10:03:53 grubba Exp $");
 #include "program.h"
 #include "object.h"
 #include "dynamic_buffer.h"
@@ -6264,21 +6264,23 @@ PMOD_EXPORT void *parent_storage(int depth)
 
 PMOD_EXPORT void change_compiler_compatibility(int major, int minor)
 {
-  if(major == Pike_compiler->compat_major &&
-     minor == Pike_compiler->compat_minor) {
-    if (compat_handler) {
-      add_ref(compat_handler);
-    }
-    return;
-  }
-
   if(major == PIKE_MAJOR_VERSION && minor == PIKE_MINOR_VERSION)
   {
     push_int(0); /* optimization */
-  }else{
-    push_int(major);
-    push_int(minor);
-    SAFE_APPLY_MASTER("get_compilation_handler",2);
+  } else {
+    if(major == Pike_compiler->compat_major &&
+       minor == Pike_compiler->compat_minor) {
+      /* Optimization -- reuse the current compat handler. */
+      if (compat_handler) {
+	ref_push_object(compat_handler);
+      } else {
+	push_int(0);
+      }
+    } else {
+      push_int(major);
+      push_int(minor);
+      SAFE_APPLY_MASTER("get_compilation_handler",2);
+    }
   }
 
   if(compat_handler)
