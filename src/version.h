b3a91604e3d8aed40ded39d1bbca198696c2bf38
Martin Stjernholm <mast@lysator.liu.se>
2001-12-18 20:12:32 +0100

    release number bumped to 13 by export.pike
    Rev: src/version.h:1.279

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.278 2001/11/24 20:51:41 nilsson Exp $ */
+/* $Id: version.h,v 1.279 2001/12/18 19:12:32 mast Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 12
+#define PIKE_BUILD_VERSION 13
 
 /* Prototypes begin here */
 void f_version(INT32 args);
