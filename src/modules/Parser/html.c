276bb3bafa69db537fdb21b917e87c1020c46d46
Martin Stjernholm <mast@lysator.liu.se>
2002-01-14 12:34:26 +0100

    Fix for the last change.
    Rev: src/modules/Parser/html.c:1.132

--- src/modules/Parser/html.c
+++ src/modules/Parser/html.c
@@ -1,4 +1,4 @@
-/* $Id: html.c,v 1.131 2002/01/11 17:57:50 mast Exp $ */
+/* $Id: html.c,v 1.132 2002/01/14 11:34:26 mast Exp $ */
 
 #include "global.h"
 #include "config.h"
@@ -4666,8 +4666,11 @@ static void html_clone(INT32 args)
    DEBUG((stderr,"parse_html_clone object %p\n",THISOBJ));
 
    /* clone the current object, same class (!) */
-   push_object(o=parent_clone_object(THISOBJ->prog,THISOBJ->parent,
-				     THISOBJ->parent_identifier,args));
+   if (THISOBJ->parent)
+     push_object(o=parent_clone_object(THISOBJ->prog,THISOBJ->parent,
+				       THISOBJ->parent_identifier,args));
+   else
+     push_object(o=clone_object(THISOBJ->prog,args));
 
    p=(struct parser_html_storage*)get_storage(o,parser_html_program);
 
