6a118fa20c640383297e89b3b9d32b43d6d055c0
Per Hedbor <ph@opera.com>
2002-01-29 14:08:33 +0100

    Check for the presence of X11 instead of assuming it's there if __NT__ is not defined. Might have to extend the tests, though
    Rev: src/post_modules/GTK/acconfig.h:1.6
    Rev: src/post_modules/GTK/configure.in:1.36
    Rev: src/post_modules/GTK/options.in:1.9

--- src/post_modules/GTK/acconfig.h
+++ src/post_modules/GTK/acconfig.h
@@ -1,5 +1,6 @@
-/* $Id: acconfig.h,v 1.5 2001/01/07 14:33:54 per Exp $ */
+/* $Id: acconfig.h,v 1.6 2002/01/29 13:08:33 per-bash Exp $ */
 #undef FUNCPROTO
 #undef HAVE_GNOME
+#undef HAVE_DPMS
 #undef HAVE_GTK_20
 #undef PGTK_AUTO_UTF8
