13a80a2a5734411ffc68f43598959991fe987d37
Martin Nilsson <mani@lysator.liu.se>
2002-03-02 14:35:33 +0100

    Use @[] for internal references.
    Rev: src/modules/system/passwords.c:1.34

--- src/modules/system/passwords.c
+++ src/modules/system/passwords.c
@@ -1,5 +1,5 @@
 /*
- * $Id: passwords.c,v 1.33 2002/03/02 12:07:53 agehall%shadowbyte.com Exp $
+ * $Id: passwords.c,v 1.34 2002/03/02 13:35:33 nilsson Exp $
  *
  * Password handling for Pike.
  *
@@ -22,7 +22,7 @@
 #include "system_machine.h"
 #include "system.h"
 
-RCSID("$Id: passwords.c,v 1.33 2002/03/02 12:07:53 agehall%shadowbyte.com Exp $");
+RCSID("$Id: passwords.c,v 1.34 2002/03/02 13:35:33 nilsson Exp $");
 
 #include "module_support.h"
 #include "interpret.h"
@@ -220,7 +220,7 @@ void push_grent(struct group *ent)
 #ifdef HAVE_GETGRGID
 /*! @decl array(int|string|array(string)) getgrgid(int gid)
  *!
- *!   Get the group entry for the group with the id @tt{gid@} using the systemfunction
+ *!   Get the group entry for the group with the id @[gid] using the systemfunction
  *!   @tt{getgrid(3)@}.
  *!
  *! @param gid
@@ -263,7 +263,7 @@ void f_getgrgid(INT32 args)
 
 #ifdef HAVE_GETGRNAM
 /*! @decl array(int|string|array(string)) getgrnam(string str)
- *!   Get the group entry for the group with the name @tt{str@} using the
+ *!   Get the group entry for the group with the name @[str] using the
  *!   systemfunction @tt{getgrnam(3)@}.
  *!
  *! @param str
@@ -307,7 +307,7 @@ void f_getgrnam(INT32 args)
 #ifdef HAVE_GETPWNAM
 /*! @decl array(int|string) getpwnam(string str)
  *!
- *!   Get the user entry for login @tt{str@} using the systemfunction @tt{getpwnam(3)@}.
+ *!   Get the user entry for login @[str] using the systemfunction @tt{getpwnam(3)@}.
  *!
  *! @param str
  *!   The login name of the user whos userrecord is requested.
@@ -358,7 +358,7 @@ void f_getpwnam(INT32 args)
 #ifdef HAVE_GETPWUID
 /*! @decl array(int|string) getpwuid(int uid)
  *!
- *!   Get the user entry for UID @tt{uid@} using the systemfunction @tt{getpwuid(3)@}.
+ *!   Get the user entry for UID @[uid] using the systemfunction @tt{getpwuid(3)@}.
  *!
  *! @param uid
  *!   The uid of the user whos userrecord is requested.
@@ -409,7 +409,7 @@ void f_getpwuid(INT32 args)
 #ifdef HAVE_SETPWENT
 /*! @decl int setpwent()
  *!
- *!   Resets the @tt{getpwent()@} function to the first entry in the passwd source
+ *!   Resets the @[getpwent] function to the first entry in the passwd source
  *!   using the systemfunction @tt{setpwent(3)@}.
  *!
  *! @returns
@@ -436,7 +436,7 @@ void f_setpwent(INT32 args)
 #ifdef HAVE_ENDPWENT
 /*! @decl int endpwent()
  *!
- *!   Closes the passwd source opened by @tt{getpwent()@} function using the
+ *!   Closes the passwd source opened by @[getpwent] function using the
  *!   systemfunction @tt{endpwent(3)@}.
  *!
  *! @returns
@@ -463,11 +463,11 @@ void f_endpwent(INT32 args)
 #ifdef HAVE_GETPWENT
 /*! @decl array(int|string) getpwent()
  *!
- *!   When first called, the @tt{getpwent()@} function opens the passwd source
+ *!   When first called, the @[getpwent] function opens the passwd source
  *!   and returns the first record using the systemfunction @tt{getpwent(3)@}.
  *!   For each following call, it returns the next record until EOF.
  *!
- *!   Call @tt{endpwent()@} when done using @tt{getpwent()@}.
+ *!   Call @[endpwent] when done using @[getpwent].
  *!
  *! @returns
  *!   An array with the information about the user
@@ -523,7 +523,7 @@ void f_getpwent(INT32 args)
  *! Returns an array with all users in the system.
  *!
  *! @returns
- *!   An array with arrays of userinfo as in @tt{getpwent()@}
+ *!   An array with arrays of userinfo as in @[getpwent].
  *!
  *! @seealso
  *!   @[getpwent()]
@@ -567,7 +567,7 @@ void f_get_all_users(INT32 args)
 #ifdef HAVE_SETGRENT
 /*! @decl int setgrent()
  *!
- *!   Rewinds the @tt{getgrent()@} pointer to the first entry
+ *!   Rewinds the @[getgrent] pointer to the first entry
  *!
  *! @seealso
  *!   @[getgrent()]
@@ -591,7 +591,7 @@ void f_setgrent(INT32 args)
 #ifdef HAVE_ENDGRENT
 /*! @decl int endgrent()
  *!
- *!   Closes the /etc/groups file after using the @tt{getgrent()@} function.
+ *!   Closes the /etc/groups file after using the @[getgrent] function.
  *!
  *! @seealso
  *!   @[getgrent()]
@@ -616,10 +616,10 @@ void f_endgrent(INT32 args)
 /*! @decl array(int|string|array(string)) getgrent()
  *!
  *!   Get a group entry from /etc/groups file.
- *!   @tt{getgrent()@} interates thru the groups source and returns
+ *!   @[getgrent] interates thru the groups source and returns
  *!   one entry per call using the systemfunction @tt{getgrent(3)@}.
  *!
- *!   Always call @tt{endgrent()@} when done using @tt{getgrent()@}!
+ *!   Always call @[endgrent] when done using @[getgrent]!
  *!
  *! @returns
  *!   An array with the information about the group
@@ -664,7 +664,7 @@ void f_getgrent(INT32 args)
  *!
  *!   Returns an array of arrays with all groups in the system groups source.
  *!   Each element in the returned array has the same structure as in
- *!   @tt{getgrent()@} function.
+ *!   @[getgrent] function.
  *!
  *! @note
  *!   The groups source is system dependant. Refer to your system manuals for information
