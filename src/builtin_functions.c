47ba81d2e717589bdf032b4ef8b2dccb31b04903
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-03-21 15:04:28 +0100

    Added one more check for tm.__tm_gmtoff.
    Rev: src/builtin_functions.c:1.425

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -5,7 +5,7 @@
 \*/
 /**/
 #include "global.h"
-RCSID("$Id: builtin_functions.c,v 1.424 2002/03/21 13:41:06 grubba Exp $");
+RCSID("$Id: builtin_functions.c,v 1.425 2002/03/21 14:04:28 grubba Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "pike_macros.h"
@@ -4008,17 +4008,23 @@ PMOD_EXPORT void f_localtime(INT32 args)
   pop_n_elems(args);
   encode_struct_tm(tm);
 
-#ifdef HAVE_EXTERNAL_TIMEZONE
-  push_string(make_shared_string("timezone"));
-  push_int(timezone);
-  f_aggregate_mapping(20);
-#else
 #ifdef STRUCT_TM_HAS_GMTOFF
   push_string(make_shared_string("timezone"));
   push_int(-tm->tm_gmtoff);
   f_aggregate_mapping(20);
 #else
+#ifdef STRUCT_TM_HAS___TM_GMTOFF
+  push_string(make_shared_string("timezone"));
+  push_int(-tm->__tm_gmtoff);
+  f_aggregate_mapping(20);
+#else
+#ifdef HAVE_EXTERNAL_TIMEZONE
+  push_string(make_shared_string("timezone"));
+  push_int(timezone);
+  f_aggregate_mapping(20);
+#else
   f_aggregate_mapping(18);
+#endif
 #endif
 #endif
 }
