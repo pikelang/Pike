bfda9e9a0db9029145074d488d1d4d4151f01aa7
Mikael Brandström <mikael@brandstrom.org>
2002-04-08 23:37:29 +0200

    release number bumped to 27 by export.pike
    Rev: src/version.h:1.294

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.293 2002/04/08 21:14:48 mikael%unix.pp.se Exp $ */
+/* $Id: version.h,v 1.294 2002/04/08 21:37:29 mikael%unix.pp.se Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 26
+#define PIKE_BUILD_VERSION 27
 
 /* Prototypes begin here */
 void f_version(INT32 args);
