bedc7898592a3635607e56b0ae67030725724a58
Mikael Brandström <mikael@brandstrom.org>
2002-04-29 22:37:41 +0200

    release number bumped to 36 by export.pike
    Rev: src/version.h:1.303

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.302 2002/04/29 18:44:09 mast Exp $ */
+/* $Id: version.h,v 1.303 2002/04/29 20:37:41 mikael%brandstrom.org Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 35
+#define PIKE_BUILD_VERSION 36
 
 /* Prototypes begin here */
 void f_version(INT32 args);
