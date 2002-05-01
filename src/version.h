a517ef6a5a0d297956feb54a3f4dff5dbbef1508
Martin Stjernholm <mast@lysator.liu.se>
2002-05-01 23:26:35 +0200

    Another bump now when everything is checked in..
    Rev: src/version.h:1.308

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.307 2002/05/01 21:15:40 mast Exp $ */
+/* $Id: version.h,v 1.308 2002/05/01 21:26:35 mast Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 40
+#define PIKE_BUILD_VERSION 41
 
 /* Prototypes begin here */
 void f_version(INT32 args);
