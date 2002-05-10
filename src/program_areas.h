21d6622d4cf88068019c9bd427976896bb3b1656
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-05-10 17:30:31 +0200

    The relocations field is now always present.
    Rev: src/program_areas.h:1.10

--- src/program_areas.h
+++ src/program_areas.h
@@ -1,8 +1,9 @@
-/* $Id: program_areas.h,v 1.9 2001/07/19 16:20:27 grubba Exp $ */
+/* $Id: program_areas.h,v 1.10 2002/05/10 15:30:31 grubba Exp $ */
 /* Who needs templates anyway? / Hubbe */
 
 /* Program *must* be first! */
 FOO(size_t,PIKE_OPCODE_T,program)
+FOO(size_t,size_t,relocations)
 FOO(size_t,char,linenumbers)
 FOO(unsigned INT16,unsigned INT16,identifier_index)
 FOO(unsigned INT16,unsigned INT16,variable_index)
@@ -11,8 +12,5 @@ FOO(unsigned INT16,struct pike_string *,strings)
 FOO(unsigned INT16,struct inherit,inherits)
 FOO(unsigned INT16,struct identifier,identifiers)
 FOO(unsigned INT16,struct program_constant, constants)
-#ifdef PIKE_USE_MACHINE_CODE
-FOO(size_t,size_t,relocations)
-#endif /* PIKE_USE_MACHINE_CODE */
 #undef FOO
 
