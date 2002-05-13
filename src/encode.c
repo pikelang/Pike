45ab0cd8d7f7de988594944eeb934c8a19ff6cf3
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-05-13 22:43:17 +0200

    Bugfix for the bytecode case.
    Rev: src/encode.c:1.145

--- src/encode.c
+++ src/encode.c
@@ -26,7 +26,7 @@
 #include "bignum.h"
 #include "pikecode.h"
 
-RCSID("$Id: encode.c,v 1.144 2002/05/11 00:28:59 nilsson Exp $");
+RCSID("$Id: encode.c,v 1.145 2002/05/13 20:43:17 grubba Exp $");
 
 /* #define ENCODE_DEBUG */
 
@@ -2729,7 +2729,10 @@ static void decode_value2(struct decode_data *data)
 	  getdata2(p->relocations, p->num_relocations);
 
 #ifdef DECODE_PROGRAM
-	  DECODE_PROGRAM(p);
+	  {
+	    int byteorder = PIKE_BYTEORDER;	/* FIXME: Used by bytecode.h */
+	    DECODE_PROGRAM(p);
+	  }
 #endif /* DECODE_PROGRAM */
 	  make_program_executable(p);
 
