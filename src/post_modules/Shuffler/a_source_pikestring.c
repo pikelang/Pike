0d9bf2f53ee1658b8435b8e83784be39ffdf8ef9
Per Hedbor <ph@opera.com>
2002-05-29 07:47:26 +0200

    Added CVS-id. Do not try to send non-8bit strings
    Rev: src/post_modules/Shuffler/a_source_pikestring.c:1.2

--- src/post_modules/Shuffler/a_source_pikestring.c
+++ src/post_modules/Shuffler/a_source_pikestring.c
@@ -4,6 +4,7 @@
 #include "interpret.h"
 
 #include "shuffler.h"
+/* $Id: a_source_pikestring.c,v 1.2 2002/05/29 05:47:26 per Exp $ */
 
 struct ps_source
 {
@@ -46,9 +47,9 @@ struct source *source_pikestring_make( struct svalue *s,
 {
   struct ps_source *res;
 
-  if( s->type != PIKE_T_STRING )
-    return 0;
-
+  if( s->type != PIKE_T_STRING )   return 0;
+  if( s->u.string->size_shift )    return 0;
+  
   res = malloc( sizeof( struct ps_source ) );
   MEMSET( res, 0, sizeof( struct ps_source ) );
 
