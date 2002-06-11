71b8263a89a962f767806ade2889c6986255fed2
Martin Nilsson <mani@lysator.liu.se>
2002-06-12 01:37:29 +0200

    release number bumped to 47 by export.pike
    Rev: src/version.h:1.315

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| for more information.
 \*/
 
-/* $Id: version.h,v 1.314 2002/06/11 23:34:31 nilsson Exp $ */
+/* $Id: version.h,v 1.315 2002/06/11 23:37:29 nilsson Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 46
+#define PIKE_BUILD_VERSION 47
 
 /* Prototypes begin here */
 void f_version(INT32 args);
