26832c18a0173aa316e9b52781fe9baf6c78c98c
Martin Nilsson <mani@lysator.liu.se>
2002-07-22 15:12:52 +0200

    Didn't help, as far as I can see. Revert.
    Rev: bin/fixdepends.sh:1.11

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -e "s@ $1/\([-a-zA-Z0-9.,_]*\)@ \$(SRCDIR)/\1@g" | sed -e "s@$1/\([-a-zA-Z0-9.,_]*\)@\1@g" | sed 's/^\([-a-zA-Z0-9.,_]*\)\.o: /\1.o \1.protos: /g' | sed -e 's@/[^ 	]*@@g'  >$1/dependencies
+sed -e "s@ $1/\([-a-zA-Z0-9.,_]*\)@ \$(SRCDIR)/\1@g" | sed -e "s@$1/\([-a-zA-Z0-9.,_]*\)@\1@g" | sed 's/^\([-a-zA-Z0-9.,_]*\)\.o: /\1.o \1.protos: /g' | sed 's@ /[^ 	]*@@g'  >$1/dependencies
 
 #sed -e "s@/./@/@g
 #s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
