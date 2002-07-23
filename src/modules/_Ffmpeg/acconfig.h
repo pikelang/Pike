697c06fea61439ae43b41944f4522a193aefeeef
Honza Petrous <hop@unibase.cz>
2002-07-23 14:14:25 +0200

    Forgot to check in.
    Rev: src/modules/_Ffmpeg/acconfig.h:1.1

---
+++ src/modules/_Ffmpeg/acconfig.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: acconfig.h,v 1.1 2002/07/23 12:14:25 hop Exp $
+ *
+ * config file for Pike Ffmpeg glue module
+ *
+ * Honza Petrous, hop@unibase.cz
+ */
+
+#ifndef FFMPEG_CONFIG_H
+#define FFMPEG_CONFIG_H
+
+@TOP@
+@BOTTOM@
+
+#endif
