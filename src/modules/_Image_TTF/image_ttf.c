6e5ce5a63d764f6b84c578bee80625d84529ccbc
Xavier Beaudouin <kiwi@oav.net>
2002-08-05 20:32:29 +0200

    Some yet another place to look around to find freetype1 libs.. (eg for BSD's)
    Rev: src/modules/_Image_TTF/configure.in:1.10
    Rev: src/modules/_Image_TTF/image_ttf.c:1.43

--- src/modules/_Image_TTF/image_ttf.c
+++ src/modules/_Image_TTF/image_ttf.c
@@ -1,14 +1,19 @@
 /*
- * $Id: image_ttf.c,v 1.42 2002/05/11 00:27:04 nilsson Exp $
+ * $Id: image_ttf.c,v 1.43 2002/08/05 18:32:29 kiwi Exp $
  */
 
 #include "config.h"
 
 
 #include "global.h"
-RCSID("$Id: image_ttf.c,v 1.42 2002/05/11 00:27:04 nilsson Exp $");
+RCSID("$Id: image_ttf.c,v 1.43 2002/08/05 18:32:29 kiwi Exp $");
 
 #ifdef HAVE_LIBTTF
+#if defined(HAVE_FREETYPE1_FREETYPE_FREETYPE_H) && defined(HAVE_FREETYPE1_FREETYPE_FTXKERN_H)
+#include <freetype1/freetype/freetype.h>
+#include <freetype1/freetype/ftxkern.h>
+
+#else /* !HAVE_FREETYPE1_FREETYPE_FREETYPE_H || !HAVE_FREETYPE1_FREETYPE_FTXKERN_H */
 #if defined(HAVE_FREETYPE_FREETYPE_H) && defined(HAVE_FREETYPE_FTXKERN_H)
 
 #include <freetype/freetype.h>
@@ -20,6 +25,7 @@ RCSID("$Id: image_ttf.c,v 1.42 2002/05/11 00:27:04 nilsson Exp $");
 #include <ftxkern.h>
 
 #endif /* HAVE_FREETYPE_FREETYPE_H && HAVE_FREETYPE_FTXKERN_H */
+#endif /* HAVE_FREETYPE1_FREETYPE_FREETYPE_H || HAVE_FREETYPE1_FREETYPE_FTXKERN_H */
 #endif /* HAVE_LIBTTF */
 
 #include "pike_macros.h"
