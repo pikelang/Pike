0bb045d04dd26a7535c52e8e7cdbeab687712775
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-08-14 17:12:57 +0200

    Always used by Pike.
    Rev: src/modules/_Crypto/include/crypto_types.h:1.7

--- src/modules/_Crypto/include/crypto_types.h
+++ src/modules/_Crypto/include/crypto_types.h
@@ -1,17 +1,11 @@
-/* $Id: crypto_types.h,v 1.6 2002/08/13 17:27:14 grubba Exp $
+/* $Id: crypto_types.h,v 1.7 2002/08/14 15:12:57 grubba Exp $
  *
  * Defines the types INT32 and INT8 */
 
 #ifndef CRYPTO_TYPES_H_INCLUDED
 #define CRYPTO_TYPES_H_INCLUDED
 
-#ifdef PIKE
 #include "global.h"
 #include "pike_types.h"
-#else /* !PIKE */
-#define INT32 long
-#define INT16 short
-#define INT8 char
-#endif
 
 #endif /* CRYPTO_TYPES_H_INCLUDED */
