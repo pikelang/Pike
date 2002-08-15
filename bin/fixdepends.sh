4a288fefb1c1ca97b127e07998783db875878bba
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-08-15 15:26:18 +0200

    Now only adds dependencies for .protos if needed.
    Rev: bin/fixdepends.sh:1.13

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -11,11 +11,24 @@
 # Paths starting with $2 ==> $(PIKE_SRC_DIR)
 # Paths starting with $3 ==> $(BUILD_BASE)
 # Paths starting with `pwd` ==> empty
-# Rules for object files are duplicated for protos files.
+# Rules for object files are duplicated for protos files (if needed).
 # Remaining absolute paths are removed.
 # Lines with only white-space are removed.
 
-sed -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" -e "s@\([ 	]\)$3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" -e "s@\([ 	]\)`pwd`\([^ 	]\)*@\1\2@" -e 's/^\([-a-zA-Z0-9.,_]*\)\.o: /\1.o \1.protos: /g' -e 's@\([ 	]\)/[^ 	]*@\1@g' -e '/^[ 	]*\\$/d' >$1/dependencies
+if grep .protos $1/Makefile.in </dev/null 2>&1; then
+  sed -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
+      -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
+      -e "s@\([ 	]\)$3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
+      -e "s@\([ 	]\)`pwd`\([^ 	]\)*@\1\2@" \
+      -e 's/^\([-a-zA-Z0-9.,_]*\)\.o: /\1.o \1.protos: /g' \
+      -e 's@\([ 	]\)/[^ 	]*@\1@g' -e '/^[ 	]*\\$/d'
+else
+  sed -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
+      -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
+      -e "s@\([ 	]\)$3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
+      -e "s@\([ 	]\)`pwd`\([^ 	]\)*@\1\2@" \
+      -e 's@\([ 	]\)/[^ 	]*@\1@g' -e '/^[ 	]*\\$/d'
+fi >$1/dependencies
 
 #sed -e "s@/./@/@g
 #s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
