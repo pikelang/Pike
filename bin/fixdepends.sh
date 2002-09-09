92e58e70401db82556699f6ae8d627661eb18452
Marcus Comstedt <marcus@mc.pp.se>
2002-09-09 14:41:35 +0200

    Escape regexp special characters (such as .) before using them in sed exprs.
    Rev: bin/fixdepends.sh:1.17

--- bin/fixdepends.sh
+++ bin/fixdepends.sh
@@ -16,25 +16,29 @@
 # Lines with only white-space and a terminating backslash are removed.
 # Backslashes that precede empty lines with only white-space are removed.
 
+d1="`echo \"$1\" | sed -e 's:[.*[\\^$@]:\\\\&:g'`"
+d2="`echo \"$2\" | sed -e 's:[.*[\\^$@]:\\\\&:g'`"
+d3="`echo \"$3\" | sed -e 's:[.*[\\^$@]:\\\\&:g'`"
+
 if grep .protos $1/Makefile.in >/dev/null 2>&1; then
-  sed -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
-      -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
-      -e "s@\([ 	]\)$3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
+  sed -e "s@\([ 	]\)$d1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
+      -e "s@\([ 	]\)$d1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
+      -e "s@\([ 	]\)$d3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
       -e "s@\([ 	]\)`pwd`\([^ 	]\)*@\1\2@" \
       -e 's/^\([-a-zA-Z0-9.,_]*\)\.o: /\1.o \1.protos: /g' \
       -e 's@\([ 	]\)/[^ 	]*@\1@g' \
       -e '/^[ 	]*\\$/d'
 else
-  sed -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
-      -e "s@\([ 	]\)$1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
-      -e "s@\([ 	]\)$3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
+  sed -e "s@\([ 	]\)$d1/\([-a-zA-Z0-9.,_]*\)@\1\$(SRCDIR)/\2@g" \
+      -e "s@\([ 	]\)$d1/\([-a-zA-Z0-9.,_]*\)@\1\$(PIKE_SRC_DIR)/\2@g" \
+      -e "s@\([ 	]\)$d3/\([-a-zA-Z0-9.,_]*\)@\1\$(BUILD_BASE)/\2@g" \
       -e "s@\([ 	]\)`pwd`\([^ 	]\)*@\1\2@" \
       -e 's@\([ 	]\)/[^ 	]*@\1@g' \
       -e '/^[ 	]*\\$/d'
 fi | sed -e '/\\$/{;N;/\n[ 	]*$/s/\\\(\n\)/\1/;P;D;}' >$1/dependencies
 
 #sed -e "s@/./@/@g
-#s@$1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
+#s@$d1/\([-a-zA-Z0-9.,_]*\)@\$(SRCDIR)/\1@g" >$1/dependencies
 
 # sed "s@[-/a-zA-Z0-9.,_][-/a-zA-Z0-9.,_]*/\([-a-zA-Z0-9.,_]*\)@\1@g" > $1/dependencies
 
