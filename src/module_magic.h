d0d5a046315890ec3c40fe1b9c9d5fbc1a9f6d4d
Martin Nilsson <mani@lysator.liu.se>
2002-09-10 02:44:26 +0200

    Removed code marked as obsolete.
    Rev: src/module_magic.h:1.5

--- src/module_magic.h
+++ src/module_magic.h
@@ -2,38 +2,14 @@
 #define MODULE_MAGIC_H
 
 #ifdef DYNAMIC_MODULE
-
 #ifdef __NT__
-
-/* This section is obsolete */
-#if 0
-
-/* UGLY - hubbe */
-#undef HIDE_GLOBAL_VARIABLES
-#undef REVEAL_GLOBAL_VARIABLES
-
-#define HIDE_GLOBAL_VARIABLES()
-#define REVEAL_GLOBAL_VARIABLES()
-
-#undef PMOD_EXPORT
-#define PMOD_EXPORT __declspec(dllexport)
-
-/* #define FAKE_DYNAMIC_LOAD */
-
-#include "import_functions.h"
-
-#define pike_module_init mYDummyFunctioN1(void); __declspec(dllexport) void **PikeSymbol; __declspec(dllexport) void pike_module_init
-#define pike_module_exit mYDummyFunctioN2(void); __declspec(dllexport) void pike_module_exit
-
-#endif /* 0 */
 
 #define pike_module_init mYDummyFunctioN1(void); __declspec(dllexport) void pike_module_init
 #define pike_module_exit mYDummyFunctioN2(void); __declspec(dllexport) void pike_module_exit
 
-
-
 #endif /* __NT__ */
 #endif /* DYNAMIC_MODULE */
+
 
 #ifndef PMOD_EXPORT
 #define PMOD_EXPORT
