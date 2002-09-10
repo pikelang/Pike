925fab8d318313f0ec58598c3afbbd0573f8124f
Marcus Comstedt <marcus@mc.pp.se>
2002-09-10 17:10:15 +0200

    release number bumped to 55 by export.pike
    Rev: src/version.h:1.323

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| for more information.
 \*/
 
-/* $Id: version.h,v 1.322 2002/09/10 15:06:36 marcus Exp $ */
+/* $Id: version.h,v 1.323 2002/09/10 15:10:15 marcus Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 54
+#define PIKE_BUILD_VERSION 55
 
 /* Prototypes begin here */
 void f_version(INT32 args);
