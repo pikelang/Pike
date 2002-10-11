eccfff14be1255feb3cdf30480ae850eac01acce
Martin Nilsson <mani@lysator.liu.se>
2002-10-11 03:41:28 +0200

    Removed broken CVS id.
    Rev: src/block_alloc_h.h:1.14

--- src/block_alloc_h.h
+++ src/block_alloc_h.h
@@ -2,10 +2,8 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: block_alloc_h.h,v 1.13 2002/10/11 01:39:29 nilsson Exp $
+|| $Id: block_alloc_h.h,v 1.14 2002/10/11 01:41:28 nilsson Exp $
 */
-
-/* $Id */
 
 #undef BLOCK_ALLOC
 #undef PTR_HASH_ALLOC
