ebba5cf84139a161724b8dc04208360b5328652e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2002-11-07 18:19:51 +0100

    Somewhat improved code generation.
    Rev: src/code/sparc.c:1.22

--- src/code/sparc.c
+++ src/code/sparc.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: sparc.c,v 1.21 2002/11/07 16:58:49 grubba Exp $
+|| $Id: sparc.c,v 1.22 2002/11/07 17:19:51 grubba Exp $
 */
 
 /*
@@ -166,7 +166,7 @@ unsigned INT32 sparc_codegen_state = 0;
 int sparc_last_pc = 0;
 
 #define LOAD_PIKE_FP() do {					\
-    if (1 || !(sparc_codegen_state & SPARC_CODEGEN_FP_IS_SET)) {	\
+    if (!(sparc_codegen_state & SPARC_CODEGEN_FP_IS_SET)) {	\
       SET_REG(SPARC_REG_PIKE_FP,				\
 	      ((INT32)(&Pike_interpreter.frame_pointer)));	\
       /* lduw [ %i0 ], %i0 */					\
@@ -175,6 +175,8 @@ int sparc_last_pc = 0;
       sparc_codegen_state |= SPARC_CODEGEN_FP_IS_SET;		\
     }								\
   } while(0)
+
+#define UNLOAD_PIKE_FP() (sparc_codegen_state &= ~SPARC_CODEGEN_FP_IS_SET)
 
 /*
  * Allocate a stack frame.
@@ -265,6 +267,9 @@ static void low_ins_f_byte(unsigned int b, int delay_ok)
   }
 
   ADD_CALL(addr, delay_ok);
+
+  /* This is probably only needed for some instructions, but... */
+  UNLOAD_PIKE_FP();
 }
 
 void ins_f_byte(unsigned int opcode)
