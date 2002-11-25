b7be0833780a5623857d27f4090b3cc3669ad99e
Marcus Comstedt <marcus@mc.pp.se>
2002-11-25 16:35:32 +0100

    Fixed a spelling misteak and added some xrefs.
    Rev: src/modules/MIME/mime.c:1.36

--- src/modules/MIME/mime.c
+++ src/modules/MIME/mime.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: mime.c,v 1.35 2002/10/21 17:06:16 marcus Exp $
+|| $Id: mime.c,v 1.36 2002/11/25 15:35:32 marcus Exp $
 */
 
 /*
@@ -15,7 +15,7 @@
 
 #include "config.h"
 
-RCSID("$Id: mime.c,v 1.35 2002/10/21 17:06:16 marcus Exp $");
+RCSID("$Id: mime.c,v 1.36 2002/11/25 15:35:32 marcus Exp $");
 #include "module.h"
 #include "stralloc.h"
 #include "pike_macros.h"
@@ -928,7 +928,8 @@ static void low_tokenize( INT32 args, int mode )
  *! and not the set specified in RFC822.
  *!
  *! @seealso
- *!   @[MIME.quote()], @[tokenize_labled()]
+ *!   @[MIME.quote()], @[tokenize_labled()],
+ *!   @[decode_words_tokenized_remapped()].
  */
 static void f_tokenize( INT32 args )
 {
@@ -946,12 +947,12 @@ static void f_tokenize( INT32 args )
 
 /*! @decl array(array(string|int)) tokenize_labled(string header)
  *!
- *! Similar to @[tokenize()], but lables the contents, by making
+ *! Similar to @[tokenize()], but labels the contents, by making
  *! arrays with two elements; the first a label, and the second
  *! the value that @[tokenize()] would have put there, except
  *! for that comments are kept.
  *!
- *! The following lables exist:
+ *! The following labels exist:
  *! @string
  *!   @value "encoded-word"
  *!     Word encoded according to =?...
@@ -966,7 +967,8 @@ static void f_tokenize( INT32 args )
  *! @endstring
  *!
  *! @seealso
- *!   @[MIME.quote()], @[tokenize()]
+ *!   @[MIME.quote()], @[tokenize()],
+ *!   @[decode_words_tokenized_labled_remapped()]
  */
 static void f_tokenize_labled( INT32 args )
 {
