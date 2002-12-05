409906f356a894b71aea5566dafa4244d71f2c6c
Martin Nilsson <mani@lysator.liu.se>
2002-12-05 18:46:07 +0100

    Version 7.4.1
    Rev: src/version.h:1.333

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.332 2002/11/25 02:57:50 nilsson Exp $
+|| $Id: version.h,v 1.333 2002/12/05 17:46:07 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
-#define PIKE_MINOR_VERSION 3
-#define PIKE_BUILD_VERSION 62
+#define PIKE_MINOR_VERSION 4
+#define PIKE_BUILD_VERSION 1
 
 /* Prototypes begin here */
 void f_version(INT32 args);
