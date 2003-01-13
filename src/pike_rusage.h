616c2d4e32ca81dc2095c2c74c55cf92bda9a510
Martin Stjernholm <mast@lysator.liu.se>
2003-01-13 14:15:14 +0100

    MSVC can't cast unsigned __int64 to double, but signed __int64 works. We probably don't need that last bit anyway.
    Rev: src/pike_rusage.h:1.12

--- src/pike_rusage.h
+++ src/pike_rusage.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pike_rusage.h,v 1.11 2003/01/13 02:07:04 mast Exp $
+|| $Id: pike_rusage.h,v 1.12 2003/01/13 13:15:14 mast Exp $
 */
 
 #ifndef PIKE_RUSAGE_H
@@ -18,7 +18,7 @@ long *low_rusage(void);
 #ifdef INT64
 /* The time is returned in nanoseconds. (There's no guarantee that the
  * returned value has nanosecond resolution.) */
-typedef unsigned INT64 cpu_time_t;
+typedef INT64 cpu_time_t;
 #define LONG_CPU_TIME
 #define CPU_TIME_TICKS /* per second */ ((cpu_time_t) 1000000000)
 #define CPU_TIME_UNIT "ns"
