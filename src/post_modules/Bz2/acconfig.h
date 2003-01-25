5e583fbf56a997c6277e4c4c6e964c36ea723bb6
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-01-25 18:08:58 +0100

    A few fixes.
    Rev: src/post_modules/Bz2/acconfig.h:1.3
    Rev: src/post_modules/Bz2/configure.in:1.6
    Rev: src/post_modules/Bz2/libbzip2mod.cmod:1.10

--- src/post_modules/Bz2/acconfig.h
+++ src/post_modules/Bz2/acconfig.h
@@ -1,2 +1,7 @@
+/* $Id: acconfig.h,v 1.3 2003/01/25 17:08:58 grubba Exp $ */
+
 /* Define if you have a working libbz2  */
 #undef HAVE_BZ2LIB
+
+/* Define if your bz_stream has the field total_out */
+#undef HAVE_BZ_STREAM_TOTAL_OUT
