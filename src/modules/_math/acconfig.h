596371f48c831f25dcf1b85c643ddf71446e6877
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-02-05 18:47:02 +0100

    Backported fpsetmask() paranoia from Pike 7.5. Adds support for OpenBSD 3.2/alpha.
    Rev: src/modules/_math/acconfig.h:1.5
    Rev: src/modules/_math/configure.in:1.10
    Rev: src/modules/_math/math.c:1.55

--- src/modules/_math/acconfig.h
+++ src/modules/_math/acconfig.h
@@ -2,12 +2,15 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.4 2002/10/11 01:39:54 nilsson Exp $
+|| $Id: acconfig.h,v 1.5 2003/02/05 17:47:02 grubba Exp $
 */
 
 @TOP@
 
 /* Define if <math.h> defines FP_RZ */
 #undef HAVE_FP_RZ
+
+/* Define if fpsetmask actually works. */
+#undef HAVE_WORKING_FPSETMASK
 
 @BOTTOM@
--- src/modules/_math/math.c
+++ src/modules/_math/math.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: math.c,v 1.54 2002/10/21 17:28:24 grubba Exp $
+|| $Id: math.c,v 1.55 2003/02/05 17:47:02 grubba Exp $
 */
 
 #include "global.h"
@@ -29,7 +29,7 @@
 
 #define sp Pike_sp
 
-RCSID("$Id: math.c,v 1.54 2002/10/21 17:28:24 grubba Exp $");
+RCSID("$Id: math.c,v 1.55 2003/02/05 17:47:02 grubba Exp $");
 
 #ifndef M_PI
 #define M_PI 3.1415926535897932384626433832795080
@@ -506,7 +506,9 @@ void f_sgn(INT32 args)
 PIKE_MODULE_INIT
 {
 #ifdef HAVE_FPSETMASK
+#ifdef HAVE_WORKING_FPSETMASK
   fpsetmask(0);
+#endif
 #endif
 #ifdef HAVE_FPSETROUND
 #ifndef HAVE_FP_RZ
