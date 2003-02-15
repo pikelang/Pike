ad28d8462d81106b174416f3bb343157d3d3c690
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-02-15 15:27:18 +0100

    Fixed bug in object_set_index(); Pike-code should never see LVALUE svalues.
    Rev: src/object.c:1.221

--- src/object.c
+++ src/object.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: object.c,v 1.220 2003/02/12 23:59:17 mast Exp $
+|| $Id: object.c,v 1.221 2003/02/15 14:27:18 grubba Exp $
 */
 
 #include "global.h"
-RCSID("$Id: object.c,v 1.220 2003/02/12 23:59:17 mast Exp $");
+RCSID("$Id: object.c,v 1.221 2003/02/15 14:27:18 grubba Exp $");
 #include "object.h"
 #include "dynamic_buffer.h"
 #include "interpret.h"
@@ -1140,8 +1140,8 @@ PMOD_EXPORT void object_index_no_free(struct svalue *to,
 
 
 PMOD_EXPORT void object_low_set_index(struct object *o,
-			  int f,
-			  struct svalue *from)
+				      int f,
+				      struct svalue *from)
 {
   struct identifier *i;
   struct program *p = NULL;
@@ -1219,8 +1219,8 @@ PMOD_EXPORT void object_low_set_index(struct object *o,
 }
 
 PMOD_EXPORT void object_set_index2(struct object *o,
-		      struct svalue *index,
-		      struct svalue *from)
+				   struct svalue *index,
+				   struct svalue *from)
 {
   struct program *p;
   int f = -1;
@@ -1265,11 +1265,10 @@ PMOD_EXPORT void object_set_index2(struct object *o,
 }
 
 PMOD_EXPORT void object_set_index(struct object *o,
-		       struct svalue *index,
-		       struct svalue *from)
+				  struct svalue *index,
+				  struct svalue *from)
 {
   struct program *p = NULL;
-  int lfun;
 
   if(!o || !(p=o->prog))
   {
@@ -1277,16 +1276,20 @@ PMOD_EXPORT void object_set_index(struct object *o,
     return; /* make gcc happy */
   }
 
-  lfun=ARROW_INDEX_P(index) ? LFUN_ASSIGN_ARROW : LFUN_ASSIGN_INDEX;
+  if (index->type != T_LVALUE) {
+    int lfun=ARROW_INDEX_P(index) ? LFUN_ASSIGN_ARROW : LFUN_ASSIGN_INDEX;
 
-  if(FIND_LFUN(p,lfun) != -1)
-  {
-    push_svalue(index);
-    push_svalue(from);
-    apply_lfun(o,lfun,2);
-    pop_stack();
+    if(FIND_LFUN(p, lfun) != -1)
+    {
+      push_svalue(index);
+      push_svalue(from);
+      apply_lfun(o, lfun, 2);
+      pop_stack();
+    } else {
+      object_set_index2(o, index, from);
+    }
   } else {
-    object_set_index2(o,index,from);
+    object_low_set_index(o, index->u.integer, from);
   }
 }
 
