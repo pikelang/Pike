2373c11165f63999ae2536fef8ab12d3edff9b67
Mirar (Pontus Hagland) <pike@sort.mirar.org>
2003-02-17 08:56:03 +0100

    documentation (ie, comment) update around va_get_args
    Rev: src/module_support.c:1.52

--- src/module_support.c
+++ src/module_support.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: module_support.c,v 1.51 2002/10/11 01:39:33 nilsson Exp $
+|| $Id: module_support.c,v 1.52 2003/02/17 07:56:03 mirar Exp $
 */
 
 #include "global.h"
@@ -18,7 +18,7 @@
 
 #define sp Pike_sp
 
-RCSID("$Id: module_support.c,v 1.51 2002/10/11 01:39:33 nilsson Exp $");
+RCSID("$Id: module_support.c,v 1.52 2003/02/17 07:56:03 mirar Exp $");
 
 /* Checks that args_to_check arguments are OK.
  * Returns 1 if everything worked ok, zero otherwise.
@@ -133,6 +133,10 @@ PMOD_EXPORT void check_all_args(const char *fnname, int args, ... )
  * usage: get_args(sp-args, args, "%i",&an_int)
  * format specifiers:
  *   %i: INT_TYPE
+ *   %I: int of float -> INT_TYPE 
+ *   %d: int (the c type "int" which may vary from INT_TYPE)
+ *   %D: int of float -> int
+ *   %+: positive int -> INT_TYPE
  *   %s: char *				Only 8bit strings
  *   %S: struct pike_string *		Only 8bit strings
  *   %W: struct pike_string *		Allow wide strings
