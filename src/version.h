a3a8d424673e2a2f0a65d9cc970678ad76de2c22
Build system <distmaker@roxen.com>
2003-03-03 13:45:19 +0100

    release number bumped to 463 by export.pike
    Rev: src/version.h:1.732

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.731 2003/02/19 15:42:34 distmaker Exp $ */
+/* $Id: version.h,v 1.732 2003/03/03 12:45:19 distmaker Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 462
+#define PIKE_BUILD_VERSION 463
 
 /* Prototypes begin here */
 void f_version(INT32 args);
