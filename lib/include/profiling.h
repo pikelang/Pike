0adcccc2116b58eb310829594eb1c48382cd3289
Martin Nilsson <mani@lysator.liu.se>
2003-03-10 02:33:07 +0100

    Some profiling macros I keep writing over and over again.
    Rev: lib/include/profiling.h:1.1

---
+++ lib/include/profiling.h
@@ -0,0 +1,28 @@
+//
+// $Id: profiling.h,v 1.1 2003/03/10 01:33:07 nilsson Exp $
+
+#ifdef PROF_REALTIME
+
+private mapping __prof_data = ([]);
+
+#define PROF_BEGIN(X) __prof_data[X] = gethrtime()
+#define PROF_END(X)   werror("%s : %3.3f\n", X, \
+  (gethrtime()-__prof_data[X])/1000000.0)
+#define PROF_RESULT()
+
+#elif defined(PROF_OFF)
+
+#define PROF_BEGIN(X)
+#define PROF_END(X)
+#define PROF_RESULT()
+
+#else
+
+private mapping __prof_data = ([]);
+
+#define PROF_BEGIN(X) __prof_data[X]-=gethrtime()
+#define PROF_END(X)   __prof_data[X]+=gethrtime()
+#define PROF_RESULT() foreach(__prof_data; string idx; int val) \
+  werror("%15s : %3.3f\n", idx, val/1000000.0);
+
+#endif
