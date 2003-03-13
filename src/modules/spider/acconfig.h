d82d4c50c5d0b96eb82ba9d4a6bf4bbff6468f04
Martin Nilsson <mani@lysator.liu.se>
2003-03-13 01:44:20 +0100

    This looks like useless stuff, but then I know little about these things...
    Rev: src/modules/spider/acconfig.h:1.6
    Rev: src/modules/spider/configure.in:1.26

--- src/modules/spider/acconfig.h
+++ src/modules/spider/acconfig.h
@@ -2,14 +2,8 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.5 2002/10/11 01:39:55 nilsson Exp $
+|| $Id: acconfig.h,v 1.6 2003/03/13 00:44:20 nilsson Exp $
 */
-
-/* Define this if you have an integer timezone. */
-#undef HAVE_INT_TIMEZONE
 
 /* If you do not have an 'time_t', define this to 'int' */
 #undef time_t
-
-/* Define this to enable the streamed parser module */
-#undef ENABLE_STREAMED_PARSER
