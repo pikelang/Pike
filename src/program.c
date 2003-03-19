91f1c64facc9ab7bf15fed2729500ed001acfa88
Martin Nilsson <mani@lysator.liu.se>
2003-03-19 20:55:24 +0100

    PROG_GMP_MPZ_ID is Gmp.mpz
    Rev: src/program.c:1.472

--- src/program.c
+++ src/program.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: program.c,v 1.471 2003/03/19 18:32:00 mast Exp $
+|| $Id: program.c,v 1.472 2003/03/19 19:55:24 nilsson Exp $
 */
 
 #include "global.h"
-RCSID("$Id: program.c,v 1.471 2003/03/19 18:32:00 mast Exp $");
+RCSID("$Id: program.c,v 1.472 2003/03/19 19:55:24 nilsson Exp $");
 #include "program.h"
 #include "object.h"
 #include "dynamic_buffer.h"
@@ -1233,7 +1233,7 @@ struct program *id_to_program(INT32 id)
       module = "Parser._parser";
       break;
     case PROG_GMP_MPZ_ID:
-      module = "Gmp";
+      module = "Gmp.mpz";
       break;
     case PROG_MODULE_MIME_ID:
       module = "___MIME";
