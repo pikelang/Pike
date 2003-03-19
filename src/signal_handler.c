b7e82d1868273cd579ee024769a958f4d0623047
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-03-19 15:23:29 +0100

    Fixed bug in use of wait4().
    Rev: src/signal_handler.c:1.171

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -25,7 +25,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.170 2000/08/29 01:09:54 hubbe Exp $");
+RCSID("$Id: signal_handler.c,v 1.171 2003/03/19 14:23:29 grubba Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -852,7 +852,7 @@ static void f_signame(int args)
 #define MY_WAIT_ANY(STATUS,OPT) wait3((STATUS),(OPT),0 )
 #else
 #ifdef HAVE_WAIT4
-#define MY_WAIT_ANY(STATUS,OPT) wait4(-1,(STATUS),(OPT),0 )
+#define MY_WAIT_ANY(STATUS,OPT) wait4(0,(STATUS),(OPT),0 )
 #else
 #define MY_WAIT_ANY(STATUS,OPT) ((errno=ENOTSUP),-1)
 #endif
