b73c45f958f46c490ddee8531162b4cd3df2097e
Martin Nilsson <mani@lysator.liu.se>
2003-03-25 21:26:53 +0100

    Disabled PROC_DEBUG
    Rev: src/signal_handler.c:1.258

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: signal_handler.c,v 1.257 2003/03/25 20:13:33 grubba Exp $
+|| $Id: signal_handler.c,v 1.258 2003/03/25 20:26:53 nilsson Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.257 2003/03/25 20:13:33 grubba Exp $");
+RCSID("$Id: signal_handler.c,v 1.258 2003/03/25 20:26:53 nilsson Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -234,7 +234,7 @@ RCSID("$Id: signal_handler.c,v 1.257 2003/03/25 20:13:33 grubba Exp $");
 #define PIKE_BADF_LIMIT	1024
 #endif /* !PIKE_BADF_LIMIT */
 
-#define PROC_DEBUG
+/* #define PROC_DEBUG */
 
 #ifndef __NT__
 #define USE_PID_MAPPING
