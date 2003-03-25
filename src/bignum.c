5cec3b21d694009ed4baac7145fbafb766e70c21
Martin Stjernholm <mast@lysator.liu.se>
2003-03-25 20:07:34 +0100

    Fixed stack error in int64_from_bignum for negative integers. Use negation instead of complement since gmp implements negation faster.
    Rev: src/bignum.c:1.23

--- src/bignum.c
+++ src/bignum.c
@@ -169,7 +169,7 @@ PMOD_EXPORT void push_int64(INT64 i)
 
 
     if(neg) {
-      apply_low(sp[-1].u.object,FIND_LFUN(sp[-1].u.object->prog,LFUN_COMPL),0);
+      apply_low(sp[-1].u.object,FIND_LFUN(sp[-1].u.object->prog,LFUN_SUBTRACT),0);
       stack_pop_n_elems_keep_top(1);
     }
   }
@@ -189,13 +189,13 @@ PMOD_EXPORT int int64_from_bignum(INT64 *i, struct object *bignum)
   neg = (--sp)->u.integer;
 
   if(neg)
-    apply_low(bignum, FIND_LFUN(bignum->prog, LFUN_COMPL), 0);
+    apply_low(bignum, FIND_LFUN(bignum->prog, LFUN_SUBTRACT), 0);
+  else
+    ref_push_object(bignum);
 
   rshfun = FIND_LFUN(bignum->prog, LFUN_RSH);
   andfun = FIND_LFUN(bignum->prog, LFUN_AND);
-  
-  ref_push_object(bignum);
-    
+
   for(pos = 0; sp[-1].type != T_INT; )
   {
     push_int(BIGNUM_INT64_MASK);
@@ -207,14 +207,13 @@ PMOD_EXPORT int int64_from_bignum(INT64 *i, struct object *bignum)
     
     push_int(BIGNUM_INT64_SHIFT);
     apply_low(sp[-2].u.object, rshfun, 1);
-    stack_swap();
-    pop_stack();
+    stack_pop_n_elems_keep_top(1);
   }
   
   *i |= (INT64)(--sp)->u.integer << (INT64)pos;
 
   if(neg)
-    *i = ~*i;
+    *i = -*i;
   
   return 1;   /* We may someday return 0 if the conversion fails. */
 }
