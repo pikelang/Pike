e7e14625ba7eee0c6f07e1f639760f064827843c
Marcus Comstedt <marcus@mc.pp.se>
2003-03-28 16:31:31 +0100

    Bah.
    Rev: src/modules/Mysql/acconfig.h:1.10

--- src/modules/Mysql/acconfig.h
+++ src/modules/Mysql/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.9 2002/10/11 01:39:47 nilsson Exp $
+|| $Id: acconfig.h,v 1.10 2003/03/28 15:31:31 marcus Exp $
 */
 
 /*
@@ -37,5 +37,8 @@
 
 /* Define if you have the mysql_unix_port variable. */
 #undef HAVE_MYSQL_UNIX_PORT
+
+/* Define if you have the `CreateSemaphoreA' function. */
+#undef HAVE_CREATESEMAPHOREA
 
 #endif /* PIKE_MYSQL_CONFIG_H */
