37a4630d555c7d44dd77190752ae29b206bfa93c
Marcus Comstedt <marcus@mc.pp.se>
2003-03-29 11:52:20 +0100

    Dummy calls to stdio functions to make NT work.
    Rev: src/modules/Mysql/configure.in:1.37
    Rev: src/modules/Mysql/dummy.c:1.10

--- src/modules/Mysql/dummy.c
+++ src/modules/Mysql/dummy.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: dummy.c,v 1.9 2003/03/28 21:33:31 marcus Exp $
+|| $Id: dummy.c,v 1.10 2003/03/29 10:52:20 marcus Exp $
 */
 
 /*
@@ -23,6 +23,31 @@ typedef INT64 _ll_t;
 typedef unsigned INT64 _ull_t;
 
 static _ll_t mysql_dummy(_ull_t a, _ull_t b, _ll_t c, _ll_t d) {
+#ifdef HAVE_LDIV
+  ldiv(10, 3);
+#endif
+#ifdef HAVE_OPEN
+  open(0, 0);
+#endif
+#ifdef HAVE_SOPEN
+  sopen(0, 0, 0);
+#endif
+#ifdef HAVE_CLOSE
+  close(0);
+#endif
+#ifdef HAVE_READ
+  read(0, 0, 0);
+#endif
+#ifdef HAVE_FILENO
+  fileno(0);
+#endif
+#ifdef HAVE_PUTS
+  puts(0);
+#endif
+#ifdef HAVE_FGETS
+  fgets(0, 0, 0);
+#endif
+
   return(a%b+(c%d)+(c/d)+(a/b));
 }
 
