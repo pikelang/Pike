0477c19a2c8ee3572f178c32aff3e47d6ebcd271
Marcus Comstedt <marcus@mc.pp.se>
2003-03-29 15:22:27 +0100

    Don't make dummy function static or compiler might remove it completely.
    Rev: src/modules/Mysql/dummy.c:1.11

--- src/modules/Mysql/dummy.c
+++ src/modules/Mysql/dummy.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: dummy.c,v 1.10 2003/03/29 10:52:27 marcus Exp $
+|| $Id: dummy.c,v 1.11 2003/03/29 14:22:27 marcus Exp $
 */
 
 /*
@@ -22,7 +22,7 @@
 typedef INT64 _ll_t;
 typedef unsigned INT64 _ull_t;
 
-static _ll_t mysql_dummy(_ull_t a, _ull_t b, _ll_t c, _ll_t d) {
+_ll_t mysql_dummy_dum_dum(_ull_t a, _ull_t b, _ll_t c, _ll_t d) {
 #ifdef HAVE_LDIV
   ldiv(10, 3);
 #endif
