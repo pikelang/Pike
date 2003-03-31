0531a6f44c37fe74ad55a76c03e2ed1c0eed6896
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-03-31 16:44:18 +0200

    Backported fix for nesting bug from Pike 7.5.
    Rev: src/modules/Image/blit.c:1.51

--- src/modules/Image/blit.c
+++ src/modules/Image/blit.c
@@ -1,10 +1,10 @@
-/* $Id: blit.c,v 1.50 2002/04/10 21:27:23 stewa Exp $ */
+/* $Id: blit.c,v 1.51 2003/03/31 14:44:18 grubba Exp $ */
 #include "global.h"
 
 /*
 **! module Image
 **! note
-**!	$Id: blit.c,v 1.50 2002/04/10 21:27:23 stewa Exp $
+**!	$Id: blit.c,v 1.51 2003/03/31 14:44:18 grubba Exp $
 **! class Image
 */
 
@@ -156,28 +156,32 @@ void img_box_nocheck(INT32 x1,INT32 y1,INT32 x2,INT32 y2)
    foo=this->img+x1+y1*this->xsize;
    end=this->img+x2+y2*this->xsize+1;
 
-   THREADS_ALLOW();
-   do {
-     if(!this->alpha)
-     {
-       if(!mod)
-	 img_clear(foo,rgb,end-foo);
-       else {
+   if(!this->alpha)
+   {
+     if(!mod)
+       img_clear(foo,rgb,end-foo);
+     else {
+       THREADS_ALLOW();
+       do {
 	 int length = x2-x1+1, xs=this->xsize, y=y2-y1+1;
 	 rgb_group *from = foo;
 	 if(!length)
 	   break;	/* Break to the while(0). */
 	 for(x=0; x<length; x++)  *(foo+x) = rgb;
 	 while(--y)  MEMCPY((foo+=xs), from, length*sizeof(rgb_group)); 
-       }
-     } 
-     else 
-     {
+       } while(0);
+       THREADS_DISALLOW();
+     }
+   } 
+   else 
+   {
+     THREADS_ALLOW();
+     do {
        for (; foo<=end; foo+=mod) for (x=x1; x<=x2; x++,foo++) 
 	 set_rgb_group_alpha(*foo,rgb,this->alpha);
-     }
-   } while(0);
-   THREADS_DISALLOW();
+     } while(0);
+     THREADS_DISALLOW();
+   }
 }
 
 
