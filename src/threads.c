2dd641a1187552722ddccd9e41120839938e7fbb
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-04-01 16:19:11 +0200

    Bugfix for dmalloc.
    Rev: src/threads.c:1.196

--- src/threads.c
+++ src/threads.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: threads.c,v 1.195 2003/03/31 18:35:47 grubba Exp $
+|| $Id: threads.c,v 1.196 2003/04/01 14:19:11 grubba Exp $
 */
 
 #include "global.h"
-RCSID("$Id: threads.c,v 1.195 2003/03/31 18:35:47 grubba Exp $");
+RCSID("$Id: threads.c,v 1.196 2003/04/01 14:19:11 grubba Exp $");
 
 PMOD_EXPORT int num_threads = 1;
 PMOD_EXPORT int threads_disabled = 0;
@@ -801,10 +801,12 @@ TH_RETURN_TYPE new_thread_func(void * data)
 		      (unsigned int)Pike_interpreter.thread_id));
 
   cleanup_interpret();
-  DO_IF_DMALLOC(
+  DO_IF_DMALLOC({
+    struct object *o = Pike_interpreter.thread_id;
     SWAP_OUT_THREAD(OBJ2THREAD(Pike_interpreter.thread_id)); /* de-Init struct */
+    Pike_interpreter.thread_id = o;
     OBJ2THREAD(Pike_interpreter.thread_id)->swapped=0;
-    )
+  })
   thread_table_delete(Pike_interpreter.thread_id);
   free_object(Pike_interpreter.thread_id);
   Pike_interpreter.thread_id=0;
