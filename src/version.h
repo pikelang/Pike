32a020d0f0c8f954cc08ed36402e1a4abb69781b
Marcus Comstedt <marcus@mc.pp.se>
2003-04-04 16:51:56 +0200

    release number bumped to 21 by export.pike
    Rev: src/version.h:1.353

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.352 2003/04/04 14:50:10 marcus Exp $
+|| $Id: version.h,v 1.353 2003/04/04 14:51:56 marcus Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 20
+#define PIKE_BUILD_VERSION 21
 
 /* Prototypes begin here */
 void f_version(INT32 args);
