375664bcaa13fe8bcf290fa15e822d8d0b578589
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-04-22 13:32:43 +0200

    Bumped version to 7.5.7 to make sure dumped programs are updated.
    Rev: src/version.h:1.339

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.338 2003/03/26 00:48:23 nilsson Exp $
+|| $Id: version.h,v 1.339 2003/04/22 11:32:43 grubba Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 5
-#define PIKE_BUILD_VERSION 5
+#define PIKE_BUILD_VERSION 7
 
 /* Prototypes begin here */
 void f_version(INT32 args);
