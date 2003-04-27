3c759ee476ca3b23ea99e30784c5313d64e0edc8
Martin Stjernholm <mast@lysator.liu.se>
2003-04-27 19:32:57 +0200

    Added array_fix_bad_type_field macro.
    Rev: src/array.h:1.49

--- src/array.h
+++ src/array.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: array.h,v 1.48 2003/04/27 16:17:33 mast Exp $
+|| $Id: array.h,v 1.49 2003/04/27 17:32:57 mast Exp $
 */
 
 #ifndef ARRAY_H
@@ -264,5 +264,13 @@ PMOD_EXPORT struct array *implode_array(struct array *a, struct array *b);
   free_svalue(&tmp_);							 \
 }while(0)
 
+#define array_fix_bad_type_field(A) do {				\
+    struct array *a_ = (A);						\
+    if (a_->type_field == BIT_MIXED ||					\
+	a_->type_field == (BIT_MIXED|BIT_UNFINISHED)) {			\
+      DO_IF_DEBUG (array_check_type_field (a_));			\
+      array_fix_type_field (a_);					\
+    }									\
+  } while (0)
 
 #endif /* ARRAY_H */
