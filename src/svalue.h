3689a5f2f5eff4108dc80cbc4f63ad640671f2cd
Martin Stjernholm <mast@lysator.liu.se>
2003-04-28 02:44:28 +0200

    Fixed a prototype.
    Rev: src/svalue.h:1.120

--- src/svalue.h
+++ src/svalue.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: svalue.h,v 1.119 2003/04/27 23:34:14 mast Exp $
+|| $Id: svalue.h,v 1.120 2003/04/28 00:44:28 mast Exp $
 */
 
 #ifndef SVALUE_H
@@ -566,6 +566,7 @@ PMOD_EXPORT void copy_svalues_recursively_no_free(struct svalue *to,
 				      struct processing *p);
 void check_short_svalue(const union anything *u, TYPE_T type);
 void debug_check_svalue(const struct svalue *s);
+void debug_check_type_hint (const struct svalue *svals, size_t num, TYPE_FIELD type_hint);
 PMOD_EXPORT void real_gc_xmark_svalues(const struct svalue *s, ptrdiff_t num);
 PMOD_EXPORT void real_gc_check_svalues(const struct svalue *s, size_t num);
 void gc_check_weak_svalues(const struct svalue *s, size_t num);
