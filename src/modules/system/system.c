7dbc9dc4e86de436fc7a05e2bfaa20726d65678a
Martin Stjernholm <mast@lysator.liu.se>
2003-05-04 01:21:12 +0200

    Some more doc.
    Rev: src/modules/system/system.c:1.150

--- src/modules/system/system.c
+++ src/modules/system/system.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: system.c,v 1.149 2003/04/30 18:19:24 grubba Exp $
+|| $Id: system.c,v 1.150 2003/05/03 23:21:12 mast Exp $
 */
 
 /*
@@ -20,7 +20,7 @@
 #include "system_machine.h"
 #include "system.h"
 
-RCSID("$Id: system.c,v 1.149 2003/04/30 18:19:24 grubba Exp $");
+RCSID("$Id: system.c,v 1.150 2003/05/03 23:21:12 mast Exp $");
 
 #ifdef HAVE_WINDOWS_H
 #include <windows.h>
@@ -1165,6 +1165,11 @@ f_get(f_getppid, getppid)
  *! @decl int chroot(Stdio.File newroot)
  *!
  *! Changes the root directory for this process to the indicated directory.
+ *!
+ *! @returns
+ *!   A nonzero value is returned if the call is successful. If
+ *!   there's an error then zero is returned and @[errno] is set
+ *!   appropriately.
  *!
  *! @note
  *!   Since this function modifies the directory structure as seen from
