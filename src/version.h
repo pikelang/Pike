8762a8871405f75d38be3995f9399498be21fd5d
Build system <distmaker@roxen.com>
2003-05-20 18:25:12 +0200

    release number bumped to 501 by export.pike
    Rev: src/version.h:1.769

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 ||| See the files COPYING and DISCLAIMER for more information.
 \*/
 
-/* $Id: version.h,v 1.768 2003/05/16 06:43:12 distmaker Exp $ */
+/* $Id: version.h,v 1.769 2003/05/20 16:25:12 distmaker Exp $ */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 2
-#define PIKE_BUILD_VERSION 500
+#define PIKE_BUILD_VERSION 501
 
 /* Prototypes begin here */
 void f_version(INT32 args);
