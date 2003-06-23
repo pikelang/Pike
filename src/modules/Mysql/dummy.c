687a54bb86b9b9c5d7cf99f8c1e8453ac81ffbab
Xavier Beaudouin <kiwi@oav.net>
2003-06-23 22:03:24 +0200

    Was a bad idea...
    Rev: src/modules/Mysql/dummy.c:1.13

--- src/modules/Mysql/dummy.c
+++ src/modules/Mysql/dummy.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: dummy.c,v 1.12 2003/06/23 18:10:49 kiwi Exp $
+|| $Id: dummy.c,v 1.13 2003/06/23 20:03:24 kiwi Exp $
 */
 
 /*
@@ -10,8 +10,6 @@
  *
  * Henrik Grubbström 1997-03-06
  */
-
-#include <stdio.h>
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
