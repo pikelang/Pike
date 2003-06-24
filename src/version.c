bd799291372950ec95052315132b40759557f9d9
Martin Nilsson <mani@lysator.liu.se>
2003-06-24 22:32:41 +0200

    More documentation
    Rev: src/version.c:1.140

--- src/version.c
+++ src/version.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.c,v 1.139 2002/10/11 01:39:39 nilsson Exp $
+|| $Id: version.c,v 1.140 2003/06/24 20:32:41 nilsson Exp $
 */
 
 #include "global.h"
@@ -11,11 +11,15 @@
 #include "stralloc.h"
 #include "version.h"
 
-RCSID("$Id: version.c,v 1.139 2002/10/11 01:39:39 nilsson Exp $");
+RCSID("$Id: version.c,v 1.140 2003/06/24 20:32:41 nilsson Exp $");
 
 /*! @decl string version()
  *!
- *! Report the version of Pike.
+ *! Report the version of Pike. Does the same as
+ *! @code
+ *! sprintf("Pike v%d.%d release %d", __REAL_VERSION__,
+ *!         __REAL_MINOR__, __REAL_BUILD__);
+ *! @endcode
  *!
  *! @seealso
  *!   @[__VERSION__], @[__MINOR__], @[__BUILD__],
