10dfe5d6332c4540900659c63f093b96104bd0a5
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-07-18 11:58:42 +0200

    We're not targetting C9x (yet).
    Rev: src/interpret.c:1.309

--- src/interpret.c
+++ src/interpret.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: interpret.c,v 1.308 2003/07/16 14:11:17 mast Exp $
+|| $Id: interpret.c,v 1.309 2003/07/18 09:58:42 grubba Exp $
 */
 
 #include "global.h"
-RCSID("$Id: interpret.c,v 1.308 2003/07/16 14:11:17 mast Exp $");
+RCSID("$Id: interpret.c,v 1.309 2003/07/18 09:58:42 grubba Exp $");
 #include "interpret.h"
 #include "object.h"
 #include "program.h"
@@ -1806,8 +1806,8 @@ PMOD_EXPORT void call_handle_error(void)
       UNSET_ONERROR(tmp);
     }
     else {
-      fprintf (stderr, "There's no master to handle the error. Dumping it raw:\n");
       char *s;
+      fprintf (stderr, "There's no master to handle the error. Dumping it raw:\n");
       init_buf();
       describe_svalue (Pike_sp - 1, 0, 0);
       s=simple_free_buf();
