c68a50afaba75001d9f287e2791e5667d3819f66
Martin Stjernholm <mast@lysator.liu.se>
2003-07-22 01:23:19 +0200

    Looks like werror() fails miserably on wide strings, but I'm not up to fixing that right now.
    Rev: src/modules/files/efuns.c:1.131

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: efuns.c,v 1.130 2003/07/03 14:09:03 grubba Exp $
+|| $Id: efuns.c,v 1.131 2003/07/21 23:23:19 mast Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "file_machine.h"
 #include "file.h"
 
-RCSID("$Id: efuns.c,v 1.130 2003/07/03 14:09:03 grubba Exp $");
+RCSID("$Id: efuns.c,v 1.131 2003/07/21 23:23:19 mast Exp $");
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
@@ -593,6 +593,7 @@ void f_werror(INT32 args)
 
   VALID_FILE_IO("werror","werror");
 
+  /* FIXME: Wide string handling. */
   write_to_stderr(sp[-args].u.string->str, sp[-args].u.string->len);
   pop_n_elems(args);
 }
