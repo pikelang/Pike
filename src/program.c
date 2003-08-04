70a7dba7fb64e57dda4cda3b1a19dbaa6169a054
Martin Stjernholm <mast@lysator.liu.se>
2003-08-04 18:36:17 +0200

    Missed one place.
    Rev: src/program.c:1.514

--- src/program.c
+++ src/program.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: program.c,v 1.513 2003/08/04 16:14:21 mast Exp $
+|| $Id: program.c,v 1.514 2003/08/04 16:36:17 mast Exp $
 */
 
 #include "global.h"
-RCSID("$Id: program.c,v 1.513 2003/08/04 16:14:21 mast Exp $");
+RCSID("$Id: program.c,v 1.514 2003/08/04 16:36:17 mast Exp $");
 #include "program.h"
 #include "object.h"
 #include "dynamic_buffer.h"
@@ -5536,14 +5536,8 @@ void my_yyerror(char *fmt,...)  ATTRIBUTE((format(printf,1,2)))
 {
   va_list args;
   char buf[8192];
-
   va_start(args,fmt);
-
   VSNPRINTF (buf, sizeof (buf), fmt, args);
-
-  if((size_t)strlen(buf) >= (size_t)sizeof(buf))
-    Pike_fatal("Buffer overflow in my_yyerror.\n");
-
   yyerror(buf);
   va_end(args);
 }
