60c25938112f716fa7c0efd9c391e6ba7814e3b8
Martin Nilsson <mani@lysator.liu.se>
2003-08-08 14:27:30 +0200

    Removed USE_SYSTEM_REGEXP.
    Rev: src/modules/Regexp/acconfig.h:1.4
    Rev: src/modules/Regexp/configure.in:1.7
    Rev: src/modules/Regexp/glue.c:1.34

--- src/modules/Regexp/acconfig.h
+++ src/modules/Regexp/acconfig.h
@@ -2,16 +2,13 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.3 2002/10/11 01:39:48 nilsson Exp $
+|| $Id: acconfig.h,v 1.4 2003/08/08 12:27:30 nilsson Exp $
 */
 
 #ifndef PIKE_MODULES_REGEXP_H
 #define PIKE_MODULES_REGEXP_H
 
 @TOP@
-
-/* Define if you want to use the systems regexp's */
-#undef USE_SYSTEM_REGEXP
 
 @BOTTOM@
 
--- src/modules/Regexp/glue.c
+++ src/modules/Regexp/glue.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: glue.c,v 1.33 2003/08/08 00:53:59 nilsson Exp $
+|| $Id: glue.c,v 1.34 2003/08/08 12:24:30 nilsson Exp $
 */
 
 #include "global.h"
@@ -22,28 +22,14 @@
 #include "module_support.h"
 #include "builtin_functions.h"
 
-RCSID("$Id: glue.c,v 1.33 2003/08/08 00:53:59 nilsson Exp $");
+RCSID("$Id: glue.c,v 1.34 2003/08/08 12:24:30 nilsson Exp $");
 
-#ifdef USE_SYSTEM_REGEXP
-#include <regexp.h>
-
-struct regexp_glue
-{
-  regex_t regexp;
-  int num_paren;
-};
-#else
 #include "pike_regexp.h"
 
 struct regexp_glue
 {
   struct regexp *regexp;
 };
-
-#endif /* USE_SYSTEM_REGEXP */
-
-
-#define sp Pike_sp
 
 #ifdef THIS
 #undef THIS
@@ -85,12 +71,8 @@ static void do_free(void)
 {
   if(THIS->regexp)
   {
-#ifdef USE_SYSTEM_REGEXP
-    regfree(&(THIS->regexp));
-#else
     free((char *)THIS->regexp);
     THIS->regexp=0;
-#endif /* USE_SYSTEM_REGEXP */
   }
 }
 
@@ -110,43 +92,10 @@ static void regexp_create(INT32 args)
   do_free();
   if(args)
   {
-    get_all_args("Regexp.regexp->create", args, "%s", &str);
-
-#ifdef USE_SYSTEM_REGEXP
-    {
-      int err = regcomp(&(THIS->regexp), str, 0);
-      int i;
-      char *paren_ptr;
-
-      if (err) {
-	char buf[1024];
-
-	regerror(err, &(THIS->regexp), buf, 1024);
-	Pike_error("Regexp.regexp->create(): Compilation failed:%s\n", buf);
-      }
-      for (i=0,paren_ptr=str; paren_ptr = strchr(paren_ptr, '('); i++) {
-	paren_ptr++;
-      }
-      THIS->num_paren = i;
-    }
-#else
-    THIS->regexp=pike_regcomp(sp[-args].u.string->str, 0);
-#endif
+    get_all_args("Regexp.SimpleRegexp->create", args, "%s", &str);
+    THIS->regexp=pike_regcomp(Pike_sp[-args].u.string->str, 0);
   }
 }
-
-#ifdef USE_SYSTEM_REGEXP
-int regexp_match_low(regex_t *regexp, char *str)
-{
-  int i;
-  THREADS_ALLOW();
-  i = !regexec(regexp, str, 0, NULL, 0);
-  THREADS_DISALLOW();
-  return i;
-}
-#else
-#define regexp_match_low pike_regexec
-#endif /* USE_SYSTEM_REGEXP */
 
 /*! @decl int match(string str)
  *!
@@ -169,21 +118,18 @@ int regexp_match_low(regex_t *regexp, char *str)
 static void regexp_match(INT32 args)
 {
   int i;
-#ifdef USE_SYSTEM_REGEXP
-  regex_t *regexp = &(THIS->regexp);
-#else
   struct regexp *regexp = THIS->regexp;
-#endif /* USE_SYSTEM_REGEXP */
 
   if(args < 1)
-    SIMPLE_TOO_FEW_ARGS_ERROR("Regexp.regexp->match", 1);
+    SIMPLE_TOO_FEW_ARGS_ERROR("Regexp.SimpleRegexp->match", 1);
   
   if(Pike_sp[-args].type == T_STRING)
   {
     if(Pike_sp[-args].u.string->size_shift)
-      SIMPLE_BAD_ARG_ERROR("Regexp.regexp->match", 1, "Expected string (8bit)");
+      SIMPLE_BAD_ARG_ERROR("Regexp.SimpleRegexp->match", 1,
+			   "Expected string (8bit)");
     
-    i = regexp_match_low(regexp, STR0(Pike_sp[-args].u.string));
+    i = pike_regexec(regexp, STR0(Pike_sp[-args].u.string));
     pop_n_elems(args);
     push_int(i);
     return;
@@ -200,9 +146,10 @@ static void regexp_match(INT32 args)
       struct svalue *sv = ITEM(arr) + i;
       
       if(sv->type != T_STRING || sv->u.string->size_shift)
-	SIMPLE_BAD_ARG_ERROR("Regexp.regexp->match",1,"Expected string (8bit)");
+	SIMPLE_BAD_ARG_ERROR("Regexp.SimpleRegexp->match", 1,
+			     "Expected string (8bit)");
 
-      if(regexp_match_low(regexp, STR0(sv->u.string)))
+      if(pike_regexec(regexp, STR0(sv->u.string)))
       {
 	ref_push_string(sv->u.string);
 	n++;
@@ -214,7 +161,8 @@ static void regexp_match(INT32 args)
     return;
   }
   else
-    SIMPLE_BAD_ARG_ERROR("Regexp.regexp->match", 1, "string|array(string)");
+    SIMPLE_BAD_ARG_ERROR("Regexp.SimpleRegexp->match", 1,
+			 "string|array(string)");
 }
 
 /*! @decl array(string) split(string s)
@@ -237,47 +185,10 @@ static void regexp_match(INT32 args)
 static void regexp_split(INT32 args)
 {
   struct pike_string *s;
-#ifdef USE_SYSTEM_REGEXP
-  regex_t *r;
-  regmatch_t *pmatch;
-  size_t nmatch;
-  int match;
-#else  
   struct regexp *r;
-#endif /* USE_SYSTEM_REGEXP */
 
-  get_all_args("Regexp.regexp->split", args, "%S", &s);
+  get_all_args("Regexp.SimpleRegexp->split", args, "%S", &s);
 
-#ifdef USE_SYSTEM_REGEXP
-  r = &(THIS->regexp);
-  nmatch = THIS->num_paren+1;
-  pmatch = xalloc(sizeof(regmatch_t)*nmatch);
-
-  THREADS_ALLOW();
-  match = !regexec(r, s->str, nmatch, pmatch, 0);
-  THREADS_DISALLOW();
-
-  if (match) {
-    int i,j;
-    
-    add_ref(s);
-    pop_n_elems(args);
-
-    for (i=1; i < nmatch; i++) {
-      if (pmatch[i].rm_sp && pmatch[i].rm_so != -1) {
-	push_string(make_shared_binary_string(pmatch[i].rm_sp,
-					      pmatch[i].rm_eo-pmatch[i].rm_so));
-      } else {
-	push_int(0);
-      }
-    }
-    f_aggregate_array(nmatch-1);
-    free_string(s);
-  } else {
-    pop_n_elems(args);
-    push_int(0);
-  }
-#else
   if(pike_regexec(r=THIS->regexp, s->str))
   {
     int i,j;
@@ -301,16 +212,11 @@ static void regexp_split(INT32 args)
     pop_n_elems(args);
     push_int(0);
   }
-#endif /* USE_SYSTEM_REGEXP */
 }
 
 static void init_regexp_glue(struct object *o)
 {
-#ifdef USE_SYSTEM_REGEXP
-  MEMCLR(THIS, sizeof(struct regexp_glue));
-#else
   THIS->regexp=0;
-#endif /* USE_SYSTEM_REGEXP */
 }
 
 static void exit_regexp_glue(struct object *o)
