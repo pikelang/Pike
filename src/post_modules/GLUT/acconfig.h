c1e8b94c21ece7610da0fbd2ba6b048ba551d3e0
Marcus Comstedt <marcus@mc.pp.se>
2003-08-17 15:51:03 +0200

    Backported AIX GLUT fixes from 7.5.
    Rev: src/post_modules/GLUT/acconfig.h:1.4
    Rev: src/post_modules/GLUT/configure.in:1.9
    Rev: src/post_modules/GLUT/gen.pike:1.3

--- src/post_modules/GLUT/acconfig.h
+++ src/post_modules/GLUT/acconfig.h
@@ -2,8 +2,54 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.3 2002/10/11 01:39:59 nilsson Exp $
+|| $Id: acconfig.h,v 1.4 2003/08/17 13:51:03 marcus Exp $
 */
 
 /* Define if you have libGL */
 #undef HAVE_LIBGLUT
+
+@TOP@
+
+@BOTTOM@
+
+/* Define if your libglut is missing glutEnterGameMode(). */
+#undef MISSING_GLUTENTERGAMEMODE
+
+/* Define if your libglut is missing glutForceJoystickFunc(). */
+#undef MISSING_GLUTFORCEJOYSTICKFUNC
+
+/* Define if your libglut is missing glutGameModeGet(). */
+#undef MISSING_GLUTGAMEMODEGET
+
+/* Define if your libglut is missing glutGameModeString(). */
+#undef MISSING_GLUTGAMEMODESTRING
+
+/* Define if your libglut is missing glutIgnoreKeyRepeat(). */
+#undef MISSING_GLUTIGNOREKEYREPEAT
+
+/* Define if your libglut is missing glutInitDisplayString(). */
+#undef MISSING_GLUTINITDISPLAYSTRING
+
+/* Define if your libglut is missing glutJoystickFunc(). */
+#undef MISSING_GLUTJOYSTICKFUNC
+
+/* Define if your libglut is missing glutKeyboardUpFunc(). */
+#undef MISSING_GLUTKEYBOARDUPFUNC
+
+/* Define if your libglut is missing glutLeaveGameMode(). */
+#undef MISSING_GLUTLEAVEGAMEMODE
+
+/* Define if your libglut is missing glutPostWindowOverlayRedisplay(). */
+#undef MISSING_GLUTPOSTWINDOWOVERLAYREDISPLAY
+
+/* Define if your libglut is missing glutPostWindowRedisplay(). */
+#undef MISSING_GLUTPOSTWINDOWREDISPLAY
+
+/* Define if your libglut is missing glutReportErrors(). */
+#undef MISSING_GLUTREPORTERRORS
+
+/* Define if your libglut is missing glutSetKeyRepeat(). */
+#undef MISSING_GLUTSETKEYREPEAT
+
+/* Define if your libglut is missing glutSpecialUpFunc(). */
+#undef MISSING_GLUTSPECIALUPFUNC
