b41f3abfef70bfadb4cabda7ac4937c4ba81d5d3
Martin Stjernholm <mast@lysator.liu.se>
2003-09-05 17:19:49 +0200

    Bumped build version due to opcode change.
    Rev: src/version.h:1.344

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.343 2003/09/04 21:55:49 nilsson Exp $
+|| $Id: version.h,v 1.344 2003/09/05 15:19:49 mast Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 5
-#define PIKE_BUILD_VERSION 11
+#define PIKE_BUILD_VERSION 12
 
 /* Prototypes begin here */
 void f_version(INT32 args);
