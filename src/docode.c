57c287352950895cc1e6bd829c5d31c5ae517ee5
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-09-19 15:37:22 +0200

    Fixed some more + is not a sequence point errors.
    Rev: src/docode.c:1.169

--- src/docode.c
+++ src/docode.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: docode.c,v 1.168 2003/09/19 13:08:07 grubba Exp $
+|| $Id: docode.c,v 1.169 2003/09/19 13:37:22 grubba Exp $
 */
 
 #include "global.h"
-RCSID("$Id: docode.c,v 1.168 2003/09/19 13:08:07 grubba Exp $");
+RCSID("$Id: docode.c,v 1.169 2003/09/19 13:37:22 grubba Exp $");
 #include "las.h"
 #include "program.h"
 #include "pike_types.h"
@@ -599,6 +599,7 @@ static void emit_builtin_svalue(char *func)
 static int do_docode2(node *n, int flags)
 {
   ptrdiff_t tmp1,tmp2,tmp3;
+  int ret;
 
   if(!n) return 0;
 
@@ -1215,7 +1216,8 @@ static int do_docode2(node *n, int flags)
   }
 
   case ' ':
-    return do_docode(CAR(n),0)+do_docode(CDR(n),DO_LVALUE);
+    ret = do_docode(CAR(n),0);
+    return ret + do_docode(CDR(n),DO_LVALUE);
 
   case F_FOREACH:
   {
@@ -1974,7 +1976,8 @@ static int do_docode2(node *n, int flags)
   }
 
   case F_LVALUE_LIST:
-    return do_docode(CAR(n),DO_LVALUE)+do_docode(CDR(n),DO_LVALUE);
+    ret = do_docode(CAR(n),DO_LVALUE);
+    return ret + do_docode(CDR(n),DO_LVALUE);
 
     case F_ARRAY_LVALUE:
       tmp1=do_docode(CAR(n),DO_LVALUE);
