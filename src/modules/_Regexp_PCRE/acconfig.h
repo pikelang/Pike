bfd0cb60bfc81871bc5782c5006599dd938de652
Mirar (Pontus Hagland) <pike@sort.mirar.org>
2003-09-20 18:17:53 +0200

    first checkin of what will become Regexp.PCRE; not functional yet, but it should compile
    Rev: src/modules/_Regexp_PCRE/.cvsignore:1.1
    Rev: src/modules/_Regexp_PCRE/Makefile.in:1.1
    Rev: src/modules/_Regexp_PCRE/acconfig.h:1.1
    Rev: src/modules/_Regexp_PCRE/configure.in:1.1
    Rev: src/modules/_Regexp_PCRE/pcre_glue.cmod:1.1
    Rev: src/modules/_Regexp_PCRE/testsuite.in:1.1

---
+++ src/modules/_Regexp_PCRE/acconfig.h
@@ -0,0 +1,13 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.1 2003/09/20 16:16:05 mirar Exp $
+*/
+
+@TOP@
+
+/* Define this if you have -lpcre */
+#undef HAVE_LIBPCRE
+
+@BOTTOM@
