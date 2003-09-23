f09383895f635de3906939af3694875eeeb8b1c0
Martin Stjernholm <mast@lysator.liu.se>
2003-09-23 19:59:40 +0200

    Fixed cut'n'paste bug in UTF7_5enc.
    Rev: src/modules/_Charset/charsetmod.c:1.41

--- src/modules/_Charset/charsetmod.c
+++ src/modules/_Charset/charsetmod.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: charsetmod.c,v 1.40 2003/09/23 17:53:52 mast Exp $
+|| $Id: charsetmod.c,v 1.41 2003/09/23 17:59:40 mast Exp $
 */
 
 #ifdef HAVE_CONFIG_H
@@ -10,7 +10,7 @@
 #endif /* HAVE_CONFIG_H */
 
 #include "global.h"
-RCSID("$Id: charsetmod.c,v 1.40 2003/09/23 17:53:52 mast Exp $");
+RCSID("$Id: charsetmod.c,v 1.41 2003/09/23 17:59:40 mast Exp $");
 #include "program.h"
 #include "interpret.h"
 #include "stralloc.h"
@@ -1078,7 +1078,7 @@ static void feed_utf7_5e(struct std_cs_stor *cs, struct string_builder *sb,
 	  string_builder_putchar(sb, 0xc0|((c>>6)&0x3f));
 	  string_builder_putchar(sb, 0xc0|(c&0x3f));	
 	} else
-	  REPLACE_CHAR(c, feed_utf8e, cs);
+	  REPLACE_CHAR(c, feed_utf7_5e, cs);
       /* FIXME: Encode using surrogates? */
     }
     break;
