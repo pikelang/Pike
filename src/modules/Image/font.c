905bbcdbaba61072dc758a8185e5e15f44cbf343
Martin Stjernholm <mast@lysator.liu.se>
2003-10-07 18:21:01 +0200

    Fixed some races if Image.Font.load() is called in several threads. (Probably not a serious bug since Image.TTF or Image.FreeType mostly is used instead of these bitmapped fonts.) The bug did not affect use of the builtin font.
    Rev: src/modules/Image/font.c:1.80

--- src/modules/Image/font.c
+++ src/modules/Image/font.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: font.c,v 1.79 2003/03/26 14:15:41 mast Exp $
+|| $Id: font.c,v 1.80 2003/10/07 16:21:01 mast Exp $
 */
 
 #include "global.h"
@@ -341,7 +341,7 @@ void font_load(INT32 args)
       size = (size_t) file_size(fd);
       if (size > 0)
       {
-	 new_font=THIS=(struct font *)xalloc(sizeof(struct font));
+	 new_font=(struct font *)xalloc(sizeof(struct font));
 
 	 THREADS_ALLOW();
 #ifdef HAVE_MMAP
@@ -370,6 +370,11 @@ void font_load(INT32 args)
 	 }
 #endif
 	 THREADS_DISALLOW();
+
+	 if (THIS)
+	   /* In case font_load got called again in the THREADS_ALLOW block. */
+	   free_font_struct(THIS);
+	 THIS = new_font;
 
 loading_default:
 
@@ -622,7 +627,9 @@ void font_write(INT32 args)
      {
       case 0:
 	to_write0 = STR0(sp[j-args].u.string);
-	THREADS_ALLOW();
+	/* THREADS_ALLOW(); */
+	/* Can't release the thread here; this would point to freed
+	 * memory if font_load got called. */
 	for (i = 0; i < to_write_len; i++)
         {
 	  c=*(to_write0++);
@@ -638,11 +645,11 @@ void font_write(INT32 args)
 	    xsize += char_space(this, c);
 	  }
 	}
-	THREADS_DISALLOW();
+	/* THREADS_DISALLOW(); */
 	break;
       case 1:
 	to_write1 = STR1(sp[j-args].u.string);
-	THREADS_ALLOW();
+	/* THREADS_ALLOW(); */
 	for (i = 0; i < to_write_len; i++)
         {
 	  c=*(to_write1++);
@@ -658,11 +665,11 @@ void font_write(INT32 args)
 	    xsize += char_space(this, c);
 	  }
 	}
-	THREADS_DISALLOW();
+	/* THREADS_DISALLOW(); */
 	break;
       case 2:
 	to_write2 = STR2(sp[j-args].u.string);
-	THREADS_ALLOW();
+	/* THREADS_ALLOW(); */
 	for (i = 0; i < to_write_len; i++)
         {
 	  c=*(to_write2++);
@@ -678,7 +685,7 @@ void font_write(INT32 args)
 	    xsize += char_space(this, c);
 	  }
 	}
-	THREADS_DISALLOW();
+	/* THREADS_DISALLOW(); */
 	break;
       default:
 	Pike_fatal("Illegal shift size!\n");
