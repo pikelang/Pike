fda2e882255966fa0494cdb2f9af59b40381b212
Martin Stjernholm <mast@lysator.liu.se>
2003-10-14 01:20:24 +0200

    Fixed a warning.
    Rev: src/modules/Mysql/mysql.c:1.65

--- src/modules/Mysql/mysql.c
+++ src/modules/Mysql/mysql.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: mysql.c,v 1.64 2003/10/13 15:41:59 mast Exp $
+|| $Id: mysql.c,v 1.65 2003/10/13 23:20:24 mast Exp $
 */
 
 /*
@@ -94,7 +94,7 @@
  * Globals
  */
 
-RCSID("$Id: mysql.c,v 1.64 2003/10/13 15:41:59 mast Exp $");
+RCSID("$Id: mysql.c,v 1.65 2003/10/13 23:20:24 mast Exp $");
 
 /*! @module Mysql
  *!
@@ -158,7 +158,8 @@ static MUTEX_T stupid_port_lock;
 #define CHECK_8BIT_NONBINARY_STRING(FUNC, ARG) do {			\
     if (sp[ARG-1-args].type != T_STRING ||				\
 	sp[ARG-1-args].u.string->size_shift ||				\
-	strlen (sp[ARG-1-args].u.string->str) != sp[ARG-1-args].u.string->len) \
+	((ptrdiff_t) strlen (sp[ARG-1-args].u.string->str) !=		\
+	 sp[ARG-1-args].u.string->len))					\
       SIMPLE_BAD_ARG_ERROR (FUNC, ARG, "string (nonbinary 8bit)");	\
   } while (0)
 
