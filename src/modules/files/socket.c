d849aba44c195481f7e362e473613497e5284ad7
Martin Stjernholm <mast@lysator.liu.se>
2003-10-30 15:42:58 +0100

    Fixed a function name in the autodoc.
    Rev: src/modules/files/socket.c:1.82

--- src/modules/files/socket.c
+++ src/modules/files/socket.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: socket.c,v 1.81 2003/10/27 23:25:35 mast Exp $
+|| $Id: socket.c,v 1.82 2003/10/30 14:42:58 mast Exp $
 */
 
 #define NO_PIKE_SHORTHAND
@@ -24,7 +24,7 @@
 #include "file_machine.h"
 #include "file.h"
 
-RCSID("$Id: socket.c,v 1.81 2003/10/27 23:25:35 mast Exp $");
+RCSID("$Id: socket.c,v 1.82 2003/10/30 14:42:58 mast Exp $");
 
 #ifdef HAVE_SYS_TYPE_H
 #include <sys/types.h>
@@ -373,7 +373,7 @@ static void port_create(INT32 args)
 
 extern struct program *file_program;
 
-/*! @decl Stdio.File port_accept()
+/*! @decl Stdio.File accept()
  */
 
 static void port_accept(INT32 args)
