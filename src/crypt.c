d356c716d5b062f1db630de7ece66634455f1e0d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2003-11-11 14:47:26 +0100

    Less inline.
    Rev: src/crypt.c:1.8

--- src/crypt.c
+++ src/crypt.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: crypt.c,v 1.7 2003/11/10 14:15:56 nilsson Exp $
+|| $Id: crypt.c,v 1.8 2003/11/11 13:47:26 grubba Exp $
 */
 
 /*
@@ -183,7 +183,7 @@ static void f(int i, struct block *key, struct block *a, struct block *x)
         transpose(x, &ptr, 32);
 }
 
-static inline void definekey(char *k)
+static void definekey(char *k)
 {
 
         key = *((struct block *) k);
