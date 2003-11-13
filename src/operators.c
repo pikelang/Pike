01f9ccd75a8b9f9101550aa73f07a61f44edcb55
Martin Stjernholm <mast@lysator.liu.se>
2003-11-13 22:55:34 +0100

    Fixed range overflow when INT_TYPE is larger than ptrdiff_t. Use PRINTPIKEINT and PRINTPIKEFLOAT when formatting integers and floats for concatenation with strings.
    Rev: src/operators.c:1.183

--- src/operators.c
+++ src/operators.c
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: operators.c,v 1.182 2003/11/12 18:59:17 mast Exp $
+|| $Id: operators.c,v 1.183 2003/11/13 21:55:34 mast Exp $
 */
 
 #include "global.h"
 #include <math.h>
-RCSID("$Id: operators.c,v 1.182 2003/11/12 18:59:17 mast Exp $");
+RCSID("$Id: operators.c,v 1.183 2003/11/13 21:55:34 mast Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "multiset.h"
@@ -507,11 +507,11 @@ PMOD_EXPORT void f_add(INT32 args)
 	break;
 
       case T_INT:
-	sprintf(buffer,"%ld",(long)sp[e].u.integer);
+	sprintf(buffer,"%"PRINTPIKEINT"d",sp[e].u.integer);
 	goto append_buffer;
 
       case T_FLOAT:
-	sprintf(buffer,"%f",(double)sp[e].u.float_number);
+	sprintf(buffer,"%"PRINTPIKEFLOAT"f",sp[e].u.float_number);
       append_buffer:
 	switch(max_shift)
 	{
@@ -3262,7 +3262,7 @@ PMOD_EXPORT void o_negate(void)
 
 PMOD_EXPORT void o_range(void)
 {
-  ptrdiff_t from,to;
+  INT_TYPE from, to;
 
   if(sp[-3].type==T_OBJECT)
   {
