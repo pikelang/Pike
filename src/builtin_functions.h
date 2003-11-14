30a1f4d08618f261b89992aa57c960e038fab2e7
Martin Stjernholm <mast@lysator.liu.se>
2003-11-14 04:50:55 +0100

    Added prototypes for all the efuns in builtin.cmod, to avoid the extern-decl-where-used sickness.
    Rev: src/builtin_functions.h:1.28

--- src/builtin_functions.h
+++ src/builtin_functions.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: builtin_functions.h,v 1.27 2003/09/05 15:19:20 mast Exp $
+|| $Id: builtin_functions.h,v 1.28 2003/11/14 03:50:55 mast Exp $
 */
 
 #ifndef BUILTIN_EFUNS_H
@@ -114,10 +114,27 @@ PMOD_EXPORT void f_get_iterator(INT32 args);
 int foreach_iterate(struct object *o, int do_step);
 
 /* From builtin.cmod. */
+PMOD_EXPORT void f_cq__describe_program(INT32 args);
+PMOD_EXPORT void f_basetype(INT32 args);
+PMOD_EXPORT void f_int2char(INT32 args);
+PMOD_EXPORT void f_int2hex(INT32 args);
+PMOD_EXPORT void f_string2hex(INT32 args);
+PMOD_EXPORT void f_hex2string(INT32 args);
 PMOD_EXPORT void f_column(INT32 args);
+PMOD_EXPORT void f_mkmultiset(INT32 args);
 PMOD_EXPORT void f_trace(INT32 args);
+PMOD_EXPORT void f_gc_parameters(INT32 args);
 PMOD_EXPORT void f_ctime(INT32 args);
 PMOD_EXPORT void f_mkmapping(INT32 args);
+PMOD_EXPORT void f_string_count(INT32 args);
+PMOD_EXPORT void f_string_trim_whites(INT32 args);
+PMOD_EXPORT void f_string_trim_all_whites(INT32 args);
+PMOD_EXPORT void f_program_implements(INT32 args);
+PMOD_EXPORT void f_program_inherits(INT32 args);
+PMOD_EXPORT void f_program_defined(INT32 args);
+PMOD_EXPORT void f_string_width(INT32 args);
+PMOD_EXPORT void f_m_delete(INT32 args);
+PMOD_EXPORT void f_get_weak_flag(INT32 args);
 PMOD_EXPORT void f_function_name(INT32 args);
 PMOD_EXPORT void f_function_object(INT32 args);
 PMOD_EXPORT void f_function_program(INT32 args);
