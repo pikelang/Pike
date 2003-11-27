6bcd1b58b1be1ef1397cbbfd26e3430a37f00c1f
Martin Nilsson <mani@lysator.liu.se>
2003-11-27 03:31:53 +0100

    idea_crypt isn't called directly, so we can hide it.
    Rev: src/post_modules/Nettle/idea.h:1.2

--- src/post_modules/Nettle/idea.h
+++ src/post_modules/Nettle/idea.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: idea.h,v 1.1 2003/11/27 02:20:39 nilsson Exp $
+|| $Id: idea.h,v 1.2 2003/11/27 02:31:53 nilsson Exp $
 */
 
 #define IDEA_KEY_SIZE 16
@@ -24,7 +24,3 @@ void idea_expand(unsigned INT16 *ctx,
 
 void idea_invert(unsigned INT16 *d,
 		 const unsigned INT16 *e);
-
-void idea_crypt(const unsigned INT16 *ctx,
-		unsigned INT8 *dest,
-		const unsigned INT8 *src);
