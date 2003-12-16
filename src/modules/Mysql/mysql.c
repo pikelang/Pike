f3ddd84882496c11cb674a22354e0152b8956cb3
Johan Sundström <oyasumi@gmail.com>
2003-12-16 14:40:17 +0100

    Mysql.mysql->big_query(): Query "REPLACE INTO [...]" failed (Commands out of sync;  You can't run this command now) is what happens. Reverted nonworking patch.
    Rev: src/modules/Mysql/mysql.c:1.78

--- src/modules/Mysql/mysql.c
+++ src/modules/Mysql/mysql.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: mysql.c,v 1.77 2003/12/15 22:02:43 nilsson Exp $
+|| $Id: mysql.c,v 1.78 2003/12/16 13:40:17 jhs Exp $
 */
 
 /*
@@ -96,7 +96,7 @@
  * Globals
  */
 
-RCSID("$Id: mysql.c,v 1.77 2003/12/15 22:02:43 nilsson Exp $");
+RCSID("$Id: mysql.c,v 1.78 2003/12/16 13:40:17 jhs Exp $");
 
 /*! @module Mysql
  *!
@@ -822,7 +822,7 @@ static void f_select_db(INT32 args)
   pop_n_elems(args);
 }
 
-/*! @decl Mysql.mysql_result big_query(string query)
+/*! @decl object(Mysql.mysql_result) big_query(string query)
  *!
  *! Make an SQL query.
  *!
@@ -865,7 +865,7 @@ static void f_big_query(INT32 args)
 #endif /* HAVE_MYSQL_REAL_QUERY */
 
     if (tmp >= 0) {
-      result = mysql_use_result(socket);
+      result = mysql_store_result(socket);
     }
 
     MYSQL_DISALLOW();
@@ -897,7 +897,7 @@ static void f_big_query(INT32 args)
 #endif /* HAVE_MYSQL_REAL_QUERY */
 
     if (tmp >= 0) {
-      result = mysql_use_result(socket);
+      result = mysql_store_result(socket);
     }
 
     MYSQL_DISALLOW();
@@ -961,7 +961,6 @@ static void f_big_query(INT32 args)
     res->result = result;
   }
 }
-
 /*! @decl void create_db(string database)
  *!
  *! Create a new database
@@ -1282,8 +1281,8 @@ static void f_protocol_info(INT32 args)
   push_int(prot);
 }
 
-/*! @decl Mysql.mysql_result list_dbs()
- *! @decl Mysql.mysql_result list_dbs(string wild)
+/*! @decl object(Mysql.mysql_result) list_dbs()
+ *! @decl object(Mysql.mysql_result) list_dbs(string wild)
  *!
  *! List databases
  *!
@@ -1368,8 +1367,8 @@ static void f_list_dbs(INT32 args)
   }
 }
 
-/*! @decl Mysql.mysql_result list_tables()
- *! @decl Mysql.mysql_result list_tables(string wild)
+/*! @decl object(Mysql.mysql_result) list_tables()
+ *! @decl object(Mysql.mysql_result) list_tables(string wild)
  *!
  *! List tables in the current database
  *!
@@ -1595,7 +1594,7 @@ static void f_list_fields(INT32 args)
   f_aggregate(i);
 }
 
-/*! @decl Mysql.mysql_result list_processes()
+/*! @decl object(Mysql.mysql_result) list_processes()
  *!
  *! List all processes in the Mysql-server
  *!
