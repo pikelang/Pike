546965024e211910c71212ed73643c35de183bff
Martin Nilsson <mani@lysator.liu.se>
2004-02-05 20:38:07 +0100

    Poof
    Rev: src/modules/_Crypto/.cvsignore:1.8(DEAD)
    Rev: src/modules/_Crypto/CREDITS:1.5(DEAD)
    Rev: src/modules/_Crypto/Makefile.in:1.32(DEAD)
    Rev: src/modules/_Crypto/acconfig.h:1.4(DEAD)
    Rev: src/modules/_Crypto/arcfour.c:1.22(DEAD)
    Rev: src/modules/_Crypto/cast.c:1.17(DEAD)
    Rev: src/modules/_Crypto/cbc.c:1.25(DEAD)
    Rev: src/modules/_Crypto/configure.in:1.17(DEAD)
    Rev: src/modules/_Crypto/crypto.c:1.57(DEAD)
    Rev: src/modules/_Crypto/crypto.h:1.13(DEAD)
    Rev: src/modules/_Crypto/des.c:1.29(DEAD)
    Rev: src/modules/_Crypto/idea.c:1.23(DEAD)
    Rev: src/modules/_Crypto/include/RCSID.h:1.9(DEAD)
    Rev: src/modules/_Crypto/include/arcfour.h:1.7(DEAD)
    Rev: src/modules/_Crypto/include/cast.h:1.5(DEAD)
    Rev: src/modules/_Crypto/include/crypto_types.h:1.10(DEAD)
    Rev: src/modules/_Crypto/include/des.h:1.7(DEAD)
    Rev: src/modules/_Crypto/include/idea.h:1.7(DEAD)
    Rev: src/modules/_Crypto/include/md2.h:1.4(DEAD)
    Rev: src/modules/_Crypto/include/md4.h:1.4(DEAD)
    Rev: src/modules/_Crypto/include/md5.h:1.5(DEAD)
    Rev: src/modules/_Crypto/include/rijndael.h:1.5(DEAD)
    Rev: src/modules/_Crypto/include/sha.h:1.7(DEAD)
    Rev: src/modules/_Crypto/lib/.cvsignore:1.8(DEAD)
    Rev: src/modules/_Crypto/lib/Makefile.in:1.40(DEAD)
    Rev: src/modules/_Crypto/lib/arcfour.c:1.10(DEAD)
    Rev: src/modules/_Crypto/lib/cast.c:1.5(DEAD)
    Rev: src/modules/_Crypto/lib/cast_sboxes.h:1.5(DEAD)
    Rev: src/modules/_Crypto/lib/configure.in:1.15(DEAD)
    Rev: src/modules/_Crypto/lib/crypt_md5.c:1.5(DEAD)
    Rev: src/modules/_Crypto/lib/desCode.h:1.5(DEAD)
    Rev: src/modules/_Crypto/lib/desKerb.c:1.6(DEAD)
    Rev: src/modules/_Crypto/lib/desQuick.c:1.7(DEAD)
    Rev: src/modules/_Crypto/lib/desTest.c:1.6(DEAD)
    Rev: src/modules/_Crypto/lib/desUtil.c:1.5(DEAD)
    Rev: src/modules/_Crypto/lib/descore.README:1.2(DEAD)
    Rev: src/modules/_Crypto/lib/descore.shar.gz:1.2(DEAD)
    Rev: src/modules/_Crypto/lib/desdata.c:1.8(DEAD)
    Rev: src/modules/_Crypto/lib/desdata.pike:1.4(DEAD)
    Rev: src/modules/_Crypto/lib/desinfo.h:1.6(DEAD)
    Rev: src/modules/_Crypto/lib/idea.c:1.9(DEAD)
    Rev: src/modules/_Crypto/lib/install-sh:1.3(DEAD)
    Rev: src/modules/_Crypto/lib/md2.c:1.6(DEAD)
    Rev: src/modules/_Crypto/lib/md4.c:1.4(DEAD)
    Rev: src/modules/_Crypto/lib/md5.c:1.6(DEAD)
    Rev: src/modules/_Crypto/lib/rijndael-boxes.dat:1.2(DEAD)
    Rev: src/modules/_Crypto/lib/rijndael.README:1.2(DEAD)
    Rev: src/modules/_Crypto/lib/rijndael.README.Pike:1.2(DEAD)
    Rev: src/modules/_Crypto/lib/rijndael.c:1.4(DEAD)
    Rev: src/modules/_Crypto/lib/sha.c:1.10(DEAD)
    Rev: src/modules/_Crypto/md2.c:1.18(DEAD)
    Rev: src/modules/_Crypto/md4.c:1.7(DEAD)
    Rev: src/modules/_Crypto/md5.c:1.22(DEAD)
    Rev: src/modules/_Crypto/nt.c:1.16(DEAD)
    Rev: src/modules/_Crypto/pipe.c:1.30(DEAD)
    Rev: src/modules/_Crypto/rijndael.c:1.12(DEAD)
    Rev: src/modules/_Crypto/rijndael_cbc_d_m.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_cbc_e_m.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_d_m.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_e_m.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_iv.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_tbl.txt:1.4(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_vk.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndael_ecb_vt.txt:1.3(DEAD)
    Rev: src/modules/_Crypto/rijndaeltest.pike:1.11(DEAD)
    Rev: src/modules/_Crypto/sha.c:1.26(DEAD)
    Rev: src/modules/_Crypto/test_crypto.pike:1.6(DEAD)
    Rev: src/modules/_Crypto/testsuite.in:1.43(DEAD)

--- src/modules/_Crypto/acconfig.h
+++
@@ -1,8 +0,0 @@
-/*
-|| This file is part of Pike. For copyright information see COPYRIGHT.
-|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
-|| for more information.
-|| $Id: acconfig.h,v 1.3 2002/10/11 01:39:50 nilsson Exp $
-*/
-
-
