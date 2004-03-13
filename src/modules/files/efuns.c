1e097808f25ac73f885e7296adb0589dc9b53cc6
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-03-13 20:25:00 +0100

    get_dir() now doesn't waste as much stack. Thanks to Hedda for noticing.
    Rev: src/modules/files/efuns.c:1.138

--- src/modules/files/efuns.c
+++ src/modules/files/efuns.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: efuns.c,v 1.137 2004/02/14 05:07:04 nilsson Exp $
+|| $Id: efuns.c,v 1.138 2004/03/13 19:25:00 grubba Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "file_machine.h"
 #include "file.h"
 
-RCSID("$Id: efuns.c,v 1.137 2004/02/14 05:07:04 nilsson Exp $");
+RCSID("$Id: efuns.c,v 1.138 2004/03/13 19:25:00 grubba Exp $");
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
@@ -826,7 +826,6 @@ void f_get_dir(INT32 args)
   struct svalue *save_sp=sp;
   DIR *dir;
   struct dirent *d;
-  struct array *a=0;
   struct pike_string *str;
 
   VALID_FILE_IO("get_dir","read");
@@ -868,6 +867,8 @@ void f_get_dir(INT32 args)
       closedir(dir);
       Pike_error("get_dir(): Out of memory\n");
     }
+
+    BEGIN_AGGREGATE_ARRAY(10);
 
     while(1)
     {
@@ -975,17 +976,23 @@ void f_get_dir(INT32 args)
 	push_string(make_shared_binary_string(d->d_name,NAMLEN(d)));
       else
 	break;
+      DO_AGGREGATE_ARRAY(120);
     }
     THREADS_ALLOW();
     free(tmp);
     closedir(dir);
     THREADS_DISALLOW();
-    a = aggregate_array(DO_NOT_WARN((INT32)(sp - save_sp)));
+
+    END_AGGREGATE_ARRAY;
+
+    stack_pop_n_elems_keep_top(args);
   }
 #else
   dir = opendir(str->str);
   if(dir)
   {
+    BEGIN_AGGREGATE_ARRAY(10);
+
     for(d=readdir(dir); d; d=readdir(dir))
     {
       /* Filter "." and ".." from the list. */
@@ -995,17 +1002,19 @@ void f_get_dir(INT32 args)
 	if(d->d_name[1]=='.' && NAMLEN(d)==2) continue;
       }
       push_string(make_shared_binary_string(d->d_name, NAMLEN(d)));
+
+      DO_AGGREGATE_ARRAY(120);
     }
     closedir(dir);
-    a=aggregate_array(sp-save_sp);
+    END_AGGREGATE_ARRAY;
+
+    stack_pop_n_elems_keep_top(args);
   }
 #endif
-
-  pop_n_elems(args);
-  if(a)
-    push_array(a);
-  else
+  else {
+    pop_n_elems(args);
     push_int(0);
+  }
 }
 
 /*! @decl int cd(string s)
