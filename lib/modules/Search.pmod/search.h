752b22fd2a48256eca989c8972821acec2d85d50
Stefan Wallström <stewa@roxen.com>
2004-03-15 18:58:55 +0100

    Set default field ranking for path1 and path2 to 0.
    Rev: lib/modules/Search.pmod/RankingProfile.pike:1.19
    Rev: lib/modules/Search.pmod/search.h:1.1

---
+++ lib/modules/Search.pmod/search.h
@@ -0,0 +1,6 @@
+// This file is part of Roxen Search
+// Copyright © 2001-2004 Roxen IS. All rights reserved.
+//
+// $Id: search.h,v 1.1 2004/03/15 17:58:55 stewa Exp $
+
+#define FIELD_RANKING_DEFAULT 100
