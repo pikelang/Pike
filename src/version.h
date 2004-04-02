8f0d8af418ba884f50ca7525d982b05772ddf311
Martin Nilsson <mani@lysator.liu.se>
2004-04-02 18:01:07 +0200

    release number bumped to 24 by export.pike
    Rev: src/version.h:1.355

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.354 2004/03/22 02:35:49 nilsson Exp $
+|| $Id: version.h,v 1.355 2004/04/02 16:01:07 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 5
-#define PIKE_BUILD_VERSION 23
+#define PIKE_BUILD_VERSION 24
 
 /* Prototypes begin here */
 void f_version(INT32 args);
