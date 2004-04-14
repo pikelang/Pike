75876b73bee6bd8d7d6f17cd143257c3117fa834
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-04-14 21:08:35 +0200

    Oops again.
    Rev: src/module.c:1.26

--- src/module.c
+++ src/module.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: module.c,v 1.25 2004/04/14 19:07:31 grubba Exp $
+|| $Id: module.c,v 1.26 2004/04/14 19:08:35 grubba Exp $
 */
 
 #include "global.h"
@@ -22,7 +22,7 @@
 #include "modules/modlist_headers.h"
 #include "post_modules/modlist_headers.h"
 
-RCSID("$Id: module.c,v 1.25 2004/04/14 19:07:31 grubba Exp $");
+RCSID("$Id: module.c,v 1.26 2004/04/14 19:08:35 grubba Exp $");
 
 /* Define this to trace the initialization and cleanup of static modules. */
 /* #define TRACE_MODULE */
@@ -66,7 +66,7 @@ void init_modules(void)
     JMP_BUF recovery;
     if (!p) {
       start_new_program();
-      p = Pike_compiler.new_program;
+      p = Pike_compiler->new_program;
     }
     if(SETJMP(recovery)) {
       /* FIXME: We could loop here until we find p. */
@@ -77,7 +77,7 @@ void init_modules(void)
       TRACE((stderr, "Initializing static module #%d: \"%s\"...\n",
 	     e, module_list[e].name));
       module_list[e].init();
-      if (Pike_compiler.new_program->num_identifier_references) {
+      if (Pike_compiler->new_program->num_identifier_references) {
 	debug_end_class(module_list[e].name,strlen(module_list[e].name),0);
 	p = NULL;
       } else {
