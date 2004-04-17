839e387705a357c8160e6e6afc208128684fbfc8
Per Hedbor <ph@opera.com>
2004-04-17 17:53:50 +0200

    release number bumped to 27 by export.pike
    Rev: packaging/debian/changelog:1.37
    Rev: src/version.h:1.358

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.357 2004/04/17 15:52:34 per Exp $
+|| $Id: version.h,v 1.358 2004/04/17 15:53:48 per Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 5
-#define PIKE_BUILD_VERSION 26
+#define PIKE_BUILD_VERSION 27
 
 /* Prototypes begin here */
 void f_version(INT32 args);
