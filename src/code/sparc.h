3b334a9d6d841a017c8e6d6db0ff6fde880f8795
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-04-20 14:21:08 +0200

    Backported fix for [bug 3646] from Pike 7.5.
    Rev: src/code/sparc.h:1.21

--- src/code/sparc.h
+++ src/code/sparc.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: sparc.h,v 1.20 2002/11/08 17:37:00 grubba Exp $
+|| $Id: sparc.h,v 1.21 2004/04/20 12:21:08 grubba Exp $
 */
 
 #define PIKE_OPCODE_ALIGN	4
@@ -72,7 +72,7 @@ void sparc_update_pc(void);
 extern const unsigned INT32 sparc_flush_instruction_cache[];
 #define FLUSH_INSTRUCTION_CACHE(ADDR, LEN)			\
   (((void (*)(void *,size_t))sparc_flush_instruction_cache)	\
-    (ADDR, (LEN)*sizeof(PIKE_OPCODE_T)))
+    (ADDR, (LEN)+sizeof(PIKE_OPCODE_T)))
 
 struct dynamic_buffer_s;
 
