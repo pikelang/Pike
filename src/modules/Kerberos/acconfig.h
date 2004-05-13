6c80643cf52cc095d46219d7c4aab7711d470397
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-05-13 11:03:39 +0200

    First version.
    Rev: src/modules/Kerberos/.cvsignore:1.1
    Rev: src/modules/Kerberos/Makefile.in:1.1
    Rev: src/modules/Kerberos/acconfig.h:1.1
    Rev: src/modules/Kerberos/configure.in:1.1
    Rev: src/modules/Kerberos/kerberos.cmod:1.1
    Rev: src/modules/Kerberos/testsuite.in:1.1

---
+++ src/modules/Kerberos/acconfig.h
@@ -0,0 +1,8 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.1 2004/05/13 08:57:07 grubba Exp $
+*/
+
+
