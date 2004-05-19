fa6e31a2b97fb6a8f0d0c7adcc9d4027e2ff78d1
Martin Nilsson <mani@lysator.liu.se>
2004-05-19 02:13:13 +0200

    Used ATTRIBUTE const on no depend no side effect functions with no pointer arguments.
    Rev: src/stuff.h:1.17

--- src/stuff.h
+++ src/stuff.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: stuff.h,v 1.16 2002/10/11 01:39:38 nilsson Exp $
+|| $Id: stuff.h,v 1.17 2004/05/19 00:13:13 nilsson Exp $
 */
 
 #ifndef STUFF_H
@@ -11,12 +11,12 @@
 #include "global.h"
 
 /* Prototypes begin here */
-PMOD_EXPORT int my_log2(size_t x);
-PMOD_EXPORT int count_bits(unsigned INT32 x);
-PMOD_EXPORT int is_more_than_one_bit(unsigned INT32 x);
+PMOD_EXPORT int my_log2(size_t x) ATTRIBUTE((const));
+PMOD_EXPORT int count_bits(unsigned INT32 x) ATTRIBUTE((const));
+PMOD_EXPORT int is_more_than_one_bit(unsigned INT32 x) ATTRIBUTE((const));
 PMOD_EXPORT double my_strtod(char *nptr, char **endptr);
-PMOD_EXPORT unsigned INT32 my_sqrt(unsigned INT32 n);
-unsigned long find_good_hash_size(unsigned long x);
+PMOD_EXPORT unsigned INT32 my_sqrt(unsigned INT32 n) ATTRIBUTE((const));
+unsigned long find_good_hash_size(unsigned long x) ATTRIBUTE((const));
 /* Prototypes end here */
 
 PMOD_EXPORT extern INT32 hashprimes[32];
