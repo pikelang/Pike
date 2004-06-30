20c006b08d35897a186d8760a27734058aafa575
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-06-30 11:23:29 +0200

    Fixed some bugs in recode_string().
    Rev: src/cpp.c:1.143

--- src/cpp.c
+++ src/cpp.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: cpp.c,v 1.142 2004/06/30 08:43:35 grubba Exp $
+|| $Id: cpp.c,v 1.143 2004/06/30 09:23:29 grubba Exp $
 */
 
 #include "global.h"
@@ -1267,7 +1267,7 @@ static struct pike_string *recode_string(struct cpp *this, struct pike_string *d
     MEMCPY(new_str->str, p, len);
 
     pop_stack();
-    ref_push_string(end_shared_string(new_str));
+    ref_push_string(new_str = end_shared_string(new_str));
 		
     /* Decode the string */
 
@@ -1275,6 +1275,7 @@ static struct pike_string *recode_string(struct cpp *this, struct pike_string *d
 			     2, BIT_STRING)) {
       cpp_handle_exception (this, "Error decoding with charset %S", new_str);
       free_string (new_str);
+      pop_stack();
       return data;
     }
     free_string (new_str);
