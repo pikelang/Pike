3c68a4e0b215d158357eb2fa9c0657ad56215f78
Stephen R. van den Berg <srb@cuci.nl>
2004-07-04 22:11:04 +0200

    Weed out f_werror
    Rev: src/builtin_functions.c:1.561

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: builtin_functions.c,v 1.560 2004/07/04 17:23:02 srb Exp $
+|| $Id: builtin_functions.c,v 1.561 2004/07/04 20:11:04 srb Exp $
 */
 
 #include "global.h"
-RCSID("$Id: builtin_functions.c,v 1.560 2004/07/04 17:23:02 srb Exp $");
+RCSID("$Id: builtin_functions.c,v 1.561 2004/07/04 20:11:04 srb Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "pike_macros.h"
@@ -2177,7 +2177,7 @@ PMOD_EXPORT void f_exit(INT32 args)
   in_exit=1;
 
   if(args>1 && Pike_sp[1-args].type==T_STRING) {
-    f_werror(args-1);
+    safe_apply(get_master(), "werror", args-1);
     args=1;
   }
 
