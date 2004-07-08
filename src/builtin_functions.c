a108f47e039b46871dbaca6b1c4ba0745fcf844c
Marcus Comstedt <marcus@mc.pp.se>
2004-07-08 15:22:42 +0200

    Replaced werror stuff in f_exit with code that is shorter, faster, and actually works...
    Rev: src/builtin_functions.c:1.563

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: builtin_functions.c,v 1.562 2004/07/04 20:47:06 nilsson Exp $
+|| $Id: builtin_functions.c,v 1.563 2004/07/08 13:22:42 marcus Exp $
 */
 
 #include "global.h"
-RCSID("$Id: builtin_functions.c,v 1.562 2004/07/04 20:47:06 nilsson Exp $");
+RCSID("$Id: builtin_functions.c,v 1.563 2004/07/08 13:22:42 marcus Exp $");
 #include "interpret.h"
 #include "svalue.h"
 #include "pike_macros.h"
@@ -2177,19 +2177,8 @@ PMOD_EXPORT void f_exit(INT32 args)
   in_exit=1;
 
   if(args>1 && Pike_sp[1-args].type==T_STRING) {
-    struct svalue *_sp_ = Pike_sp;
-    int i=1;
-    push_svalue(simple_mapping_string_lookup(get_builtin_constants(),
-					     "werror"));
-
-    /* Push top of stack below all werror args. */
-    for(; i<args; i++) {
-      struct svalue _=_sp_[-(i)];
-      _sp_[-(i)]=_sp_[-(i+1)];
-      _sp_[-(i+1)]=_;
-    }
-
-    f_call_function(args);
+    apply_svalue(simple_mapping_string_lookup(get_builtin_constants(),
+					      "werror"), args-1);
     pop_stack();
     args=1;
   }
