70e0744997baa5a034bf16c0cefc709878c4d883
Build system <distmaker@roxen.com>
2004-09-01 10:35:43 +0200

    release number bumped to 146 by export.pike
    Rev: packaging/debian/changelog:1.161
    Rev: src/version.h:1.477

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.476 2004/08/29 11:14:00 distmaker Exp $
+|| $Id: version.h,v 1.477 2004/09/01 08:35:41 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 145
+#define PIKE_BUILD_VERSION 146
 
 /* Prototypes begin here */
 void f_version(INT32 args);
