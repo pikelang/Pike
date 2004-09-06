53f68fde3aaaf55e540ac393298ab0fc76a5a70f
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-09-06 15:02:53 +0200

    IMUTEX'es MUST be unlocked in a THREADS_ALLOW context.
    Rev: src/signal_handler.c:1.298

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: signal_handler.c,v 1.297 2004/07/02 12:24:13 grubba Exp $
+|| $Id: signal_handler.c,v 1.298 2004/09/06 13:02:53 grubba Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.297 2004/07/02 12:24:13 grubba Exp $");
+RCSID("$Id: signal_handler.c,v 1.298 2004/09/06 13:02:53 grubba Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -2785,9 +2785,10 @@ void f_create_process(INT32 args)
 		      &info,
 		      &proc);
     err=GetLastError();
-    THREADS_DISALLOW_UID();
     
     UNLOCK_IMUTEX(&handle_protection_mutex);
+
+    THREADS_DISALLOW_UID();
 
     if(env) pop_stack();
     if(command_line) free(command_line);
