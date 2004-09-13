76b0aaf3c53a2f876cf3a3551cf0621c7ac066ff
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-09-13 13:48:45 +0200

    Reverted broken IMUTEX change.
    Rev: src/signal_handler.c:1.303

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: signal_handler.c,v 1.302 2004/09/06 13:03:17 grubba Exp $
+|| $Id: signal_handler.c,v 1.303 2004/09/13 11:48:45 grubba Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "main.h"
 #include <signal.h>
 
-RCSID("$Id: signal_handler.c,v 1.302 2004/09/06 13:03:17 grubba Exp $");
+RCSID("$Id: signal_handler.c,v 1.303 2004/09/13 11:48:45 grubba Exp $");
 
 #ifdef HAVE_PASSWD_H
 # include <passwd.h>
@@ -2782,9 +2782,9 @@ void f_create_process(INT32 args)
 		      &proc);
     err=GetLastError();
     
-    UNLOCK_IMUTEX(&handle_protection_mutex);
-
     THREADS_DISALLOW_UID();
+
+    UNLOCK_IMUTEX(&handle_protection_mutex);
 
     if(env) pop_stack();
     if(command_line) free(command_line);
