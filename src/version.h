e08b5525050ebc6f001e22b4169c6a4164b019db
Martin Nilsson <mani@lysator.liu.se>
2004-09-22 17:49:53 +0200

    release number bumped to 22 by export.pike
    Rev: packaging/debian/changelog:1.74
    Rev: packaging/windows/pike.iss:1.11
    Rev: src/version.h:1.379

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.378 2004/09/20 22:24:03 nilsson Exp $
+|| $Id: version.h,v 1.379 2004/09/22 15:49:51 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 21
+#define PIKE_BUILD_VERSION 22
 
 /* Prototypes begin here */
 void f_version(INT32 args);
