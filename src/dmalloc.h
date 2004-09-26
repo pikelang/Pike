78a4789ed09dbb886f21e9914b7e5a3910c9939c
Martin Stjernholm <mast@lysator.liu.se>
2004-09-27 00:21:17 +0200

    DO_PIKE_CLEANUP requires PIKE_DEBUG.
    Rev: src/dmalloc.h:1.52

--- src/dmalloc.h
+++ src/dmalloc.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: dmalloc.h,v 1.51 2004/09/02 11:17:16 grubba Exp $
+|| $Id: dmalloc.h,v 1.52 2004/09/26 22:21:17 mast Exp $
 */
 
 #ifndef DMALLOC_H
@@ -37,12 +37,9 @@ extern size_t dmalloc_tracelogptr;
 extern int verbose_debug_exit;
 extern int gc_external_refs_zapped;
 void gc_check_zapped (void *a, TYPE_T type, const char *file, int line);
-#endif
-
-#ifdef DO_PIKE_CLEANUP
-#  define DO_IF_PIKE_CLEANUP(X) X
+#define DO_IF_PIKE_CLEANUP(X) X
 #else
-#  define DO_IF_PIKE_CLEANUP(X)
+#define DO_IF_PIKE_CLEANUP(X)
 #endif
 
 typedef void describe_block_fn (void *);
