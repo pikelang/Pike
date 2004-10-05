55355aaaa851fc02c79d6486961d2cfb58f7e461
Martin Nilsson <mani@lysator.liu.se>
2004-10-05 23:14:47 +0200

    Pike parser, take 1.
    Rev: src/modules/Parser/pike.c:1.1
    Rev: src/modules/Parser/pike_tokenizer.h:1.1

---
+++ src/modules/Parser/pike.c
@@ -0,0 +1,120 @@
+/*! array(array(string),string) split( string x )
+ *! Returns an array with Pike-level tokens and the remainder (a
+ *! partial token), if any.
+ */
+#include "global.h"
+#include "config.h"
+
+#include "pike_macros.h"
+#include "object.h"
+#include "constants.h"
+#include "interpret.h"
+#include "svalue.h"
+#include "threads.h"
+#include "array.h"
+#include "pike_error.h"
+#include "operators.h"
+#include "builtin_functions.h"
+#include "module_support.h"
+#include "mapping.h"
+#include "stralloc.h"
+#include "program_id.h"
+#include "block_alloc.h"
+#include <ctype.h>
+
+#include "parser.h"
+
+INLINE static int m_isidchar( unsigned int x )
+{
+  if( (x >= 'a' && x <= 'z') || (x>='A' && x<='Z') || x>128 || x == '_')
+    return 1;
+  return 0;
+}
+
+INLINE static int m_isidchar2( unsigned int x )
+{
+  if( (x >= 'a' && x <= 'z') || (x>='A' && x<='Z') || x>128 || x=='_' ||
+      (x>='0'&&x<='9')||x=='$')
+    return 1;
+  return 0;
+}
+
+
+#define PUSH_TOKEN push_token0
+#define TOKENIZE     tokenize0
+#define CHAR       p_wchar0
+#include "pike_tokenizer.h"
+#undef TOKENIZE
+#undef CHAR
+#undef PUSH_TOKEN
+
+#define PUSH_TOKEN push_token1
+#define TOKENIZE   tokenize1
+#define CHAR       p_wchar1
+#include "pike_tokenizer.h"
+#undef TOKENIZE
+#undef CHAR
+#undef PUSH_TOKEN
+
+#define PUSH_TOKEN push_token2
+#define TOKENIZE   tokenize2
+#define CHAR       p_wchar2
+#include "pike_tokenizer.h"
+#undef TOKENIZE
+#undef CHAR
+#undef PUSH_TOKEN
+
+static void do_free_arrayptr( struct array **x )
+{
+  free_array( *x );
+}			   
+
+static void f_tokenize( INT32 args )
+{
+  struct array *res = allocate_array_no_init( 0, 128 );
+  struct pike_string *left_s = 0; /* Make gcc happy. */
+  int left;
+  ONERROR tmp;
+
+  if( Pike_sp[-1].type != PIKE_T_STRING )
+    Pike_error("Expected string argument\n");
+
+  SET_ONERROR(tmp, do_free_arrayptr, &res);
+  
+  switch( Pike_sp[-1].u.string->size_shift )
+  {
+    case 0:
+      left=tokenize0(&res,(p_wchar0*)Pike_sp[-1].u.string->str,Pike_sp[-1].u.string->len);
+      left_s = make_shared_binary_string0( (p_wchar0*)Pike_sp[-1].u.string->str+left,
+					   Pike_sp[-1].u.string->len-left);
+      break;
+    case 1:
+      left=tokenize1(&res,(p_wchar1*)Pike_sp[-1].u.string->str,Pike_sp[-1].u.string->len);
+      left_s = make_shared_binary_string1( (p_wchar1*)Pike_sp[-1].u.string->str+left,
+					   Pike_sp[-1].u.string->len-left);
+      break;
+    case 2:
+      left=tokenize2(&res,(p_wchar2*)Pike_sp[-1].u.string->str,Pike_sp[-1].u.string->len);
+      left_s = make_shared_binary_string1( (p_wchar1*)Pike_sp[-1].u.string->str+left,
+					   Pike_sp[-1].u.string->len-left);
+      break;
+#ifdef PIKE_DEBUG
+    default:
+      Pike_error("Unknown shift size %d.\n", Pike_sp[-1].u.string->size_shift);
+#endif
+  }
+  pop_n_elems(args);
+  push_array(res);
+  push_string( left_s );
+  f_aggregate( 2 );
+}
+
+
+void init_parser_pike()
+{
+  ADD_FUNCTION("tokenize",f_tokenize,tFunc(tStr,tArr(tStr)),0);
+}
+
+void exit_parser_pike()
+{
+}
---
+++ src/modules/Parser/pike_tokenizer.h
@@ -0,0 +1,224 @@
+#define NEWLINE() do { while( pos<len && data[pos]!='\n' )pos++; } while(0)
+
+static unsigned int TOKENIZE(struct array **res, CHAR *data, unsigned int len)
+{
+  unsigned int start=0;
+  unsigned int pos;
+  for( pos=0;pos<len; pos++ )
+  {
+    switch(data[pos])
+    {
+      case '.':
+	if( data[pos+1]=='.' )
+	{
+	  pos++;
+	  if( data[pos+1] == '.')
+	    pos++;
+	  break;
+	}
+	/* fallthrough. */
+      case '0':
+	if( data[pos+1]=='x' || data[pos+1]=='X' )
+	{
+	  pos+=2;
+	  while( (pos < len) &&
+		 ((data[pos]>='0' && data[pos] <='9') ||
+		  (data[pos]>='a' && data[pos] <='f') ||
+		  (data[pos]>='A' && data[pos] <='F')))
+	    pos++;
+	  if( pos != len )
+	    pos--;
+	  break;
+	}
+	else if( data[pos+1]=='b' || data[pos+1]=='B' )
+	{
+	  pos+=2;
+	  while( pos<len && (data[pos]==0 || data[pos]==1) )
+	    pos++;
+	  if( pos != len )
+	    pos--;
+	  break;
+	}
+
+	/* fallthrough. */
+      case '1':  case '2':    case '3':  case '4':
+      case '5':  case '6':    case '7':  case '8':
+      case '9':
+	while(pos<len && data[pos]>='0' && data[pos]<='9') pos++;
+	if( pos == len ) break;
+	if(data[pos]=='.')
+	{
+	  pos++;
+	  while(pos<len && data[pos]>='0' && data[pos]<='9') pos++;
+	  if(data[pos]=='e' || data[pos]=='E')
+	  {
+	    pos++;
+	    if(data[pos]=='-' || data[pos]=='+') pos++;
+	    while(data[pos]>='0' && data[pos]<='9') pos++;
+	  }
+	}
+	if(data[pos]=='e' || data[pos]=='E')
+	{
+	  pos++;
+	  while(data[pos]>='0' && data[pos]<='9') pos++;
+	}
+	if( pos != len )
+	  pos--;
+	break;
+
+      case  '`':
+	if(data[pos+1]=='`') pos++;
+	switch(data[pos+1]) {
+	case '<':
+	case '>':
+	  if(data[pos+2]==data[pos+1]) pos++;
+	  break;
+	case '-':
+	  if(data[pos+2]=='>') pos++;
+	  break;
+	case '(':
+	  if(data[pos+2]==')') pos++;
+	  break;
+	case '[':
+	  if(data[pos+2]==']') pos++;
+	  break;
+	}
+	if(data[pos+2]=='=') pos++;
+	break;
+
+
+      case '\\': continue; /* IGNORED */
+
+      case '/':
+	if( pos == len-1 )
+	  break;
+	switch( data[pos+1] )
+	{
+	  case '/':
+	    NEWLINE();
+	    /* line comment */
+	    break;
+	  case '*':
+	    pos += 2;
+	    while( pos < len && !(data[pos] == '/' && data[pos-1] == '*') )
+	      pos++;
+	    if( pos == len )
+	      goto failed_to_find_end;
+	    break;
+	  default: break;
+	}
+      case '{': case '}':
+      case '[': case ']':
+      case '(': case ')':
+      case ';': case ':':
+      case ',': case '?':
+      case '@': /* Hm. Pike specific if I ever saw one. */
+	break;  /* all done, one character token */
+
+      case '<':
+	if( data[pos+1] == '<' ) pos++;
+	if( data[pos+1] == '=' ) pos++;
+	break;
+
+      case '-':
+	if( data[pos+1] == '>' ) pos++;
+	else
+	{
+	  if( data[pos+1] == '-' ) pos++;
+	  if( data[pos+1] == '=' ) pos++;
+	}
+	break;
+
+      case '>':
+	if( data[pos+1] == '>' ) pos++;
+	if( data[pos+1] == '=' ) pos++;
+	break;
+
+      case '+': case '&':  case '|':
+	if( data[pos+1] == data[pos] ) pos++;
+	else if( data[pos+1] == '=' ) pos++;
+
+
+      case '*': case '%':  
+      case '^': case '!':  case '~':  case '=':
+	if( data[pos+1] == '=' )
+	  pos++;
+	break;
+	
+      case ' ':
+      case '\n':
+      case '\r':
+      case '\t':
+      case '\14':
+	pos++;
+	while( pos < len )
+	{
+	  switch(data[pos])
+	  {
+	    case ' ':
+	    case '\n':
+	    case '\r':
+	    case '\t':
+	    case '\14':
+	      pos++;
+	      continue;
+	  }
+	  break;
+	}
+	if( pos != len ) pos--;
+	break;
+
+      case '\'':
+	pos++;
+	while( pos < len )
+	{
+	  if( data[pos] == '\\' )
+	    pos++;
+	  else if( data[pos] == '\'' )
+	    break;
+	  pos++;
+	}
+	if( pos >= len ) goto failed_to_find_end;
+	break;
+
+      case '"':
+	pos++;
+	while( pos < len )
+	{
+	  if( data[pos] == '\\' )
+	    pos++;
+	  else if( data[pos] == '"' )
+	    break;
+	  pos++;
+	}
+	if( pos >= len )
+	  goto failed_to_find_end;
+	break;
+	
+      case '#':
+	NEWLINE();
+	while( data[pos-1]=='\\' || (pos>1 && data[pos-1]=='\r' && data[pos-2]=='\\') )
+	{
+	  pos++;
+	  NEWLINE();
+	}
+	break;
+
+      default:
+	if( m_isidchar( data[pos] ) )
+	{
+	  pos++;
+	  while( m_isidchar2( data[pos] ) && pos < len) pos++;
+	  if( pos != len )
+	    pos--;
+	}
+	else
+	  Pike_error("Unexpected character %x (%c) at position %d.\n",
+		     data[pos], (isprint(data[pos])?data[pos]:'?'), pos );
+    }
+    PUSH_TOKEN( res, data+start, ( pos == len )?pos-start:pos-start+1 );
+    start = pos+1;
+  }
+failed_to_find_end:
+  return MINIMUM(start,len);
+}
