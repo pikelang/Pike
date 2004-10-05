b94c8ea7655503866a410a0b381d8c9a8549eaa9
Martin Nilsson <mani@lysator.liu.se>
2004-10-05 23:17:15 +0200

    More prototypes
    Rev: src/modules/Parser/parser.h:1.5

--- src/modules/Parser/parser.h
+++ src/modules/Parser/parser.h
@@ -2,9 +2,20 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: parser.h,v 1.4 2004/02/23 17:40:40 per Exp $
+|| $Id: parser.h,v 1.5 2004/10/05 21:17:15 nilsson Exp $
 */
 
 void init_parser_html(void);
+void exit_parser_html(void);
 void init_parser_rcs(void);
 void exit_parser_rcs(void);
+void init_parser_c(void);
+void exit_parser_c(void);
+void init_parser_pike(void);
+void exit_parser_pike(void);
+
+/* c.c */
+
+void push_token0( struct array **a, p_wchar0 *x, int l);
+void push_token1( struct array **a, p_wchar1 *x, int l);
+void push_token2( struct array **a, p_wchar2 *x, int l);
