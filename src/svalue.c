c5d1e83c314ac5ba527b792fd7544e6f635f6c6b
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-11-28 19:51:49 +0100

    Potential fix for type comparison being broken.
    Rev: src/svalue.c:1.209

--- src/svalue.c
+++ src/svalue.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: svalue.c,v 1.208 2004/11/28 14:21:02 grubba Exp $
+|| $Id: svalue.c,v 1.209 2004/11/28 18:51:49 grubba Exp $
 */
 
 #include "global.h"
@@ -1091,6 +1091,13 @@ PMOD_EXPORT int is_lt(const struct svalue *a, const struct svalue *b)
 PMOD_EXPORT int is_le(const struct svalue *a, const struct svalue *b)
 {
   /* Can't optimize this to !is_gt (a, b) since that'd assume total order. */
+  if (a->type == T_TYPE &&
+      b->type == T_TYPE) {
+    /* NOTE: Special case, since is_eq() below only does a pointer comparison.
+     */
+    return pike_types_le(a->u.type, b->u.type);
+  }
+  /* FIXME: Consider using is_equal()? */
   return is_lt (a, b) || is_eq (a, b);
 }
 
