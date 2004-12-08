6f829fd7976f4712ced2db31dc340e00f59fb8d2
Martin Nilsson <mani@lysator.liu.se>
2004-12-08 01:11:00 +0100

    release number bumped to 13 by export.pike
    Rev: packaging/debian/changelog:1.54
    Rev: src/version.h:1.370

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.369 2004/12/08 00:06:40 nilsson Exp $
+|| $Id: version.h,v 1.370 2004/12/08 00:10:58 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 12
+#define PIKE_BUILD_VERSION 13
 
 /* Prototypes begin here */
 void f_version(INT32 args);
