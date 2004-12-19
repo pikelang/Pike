497c0091d1ba8e3af6bbf7092503c92c9315981d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-12-19 20:24:04 +0100

    Added HAVE_X86_64_CPU.
    Rev: src/modules/Java/acconfig.h:1.7

--- src/modules/Java/acconfig.h
+++ src/modules/Java/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.6 2003/05/17 20:29:27 grubba Exp $
+|| $Id: acconfig.h,v 1.7 2004/12/19 19:24:04 grubba Exp $
 */
 
 /*
@@ -31,6 +31,9 @@
 
 /* Define if you have an x86 CPU */
 #undef HAVE_X86_CPU
+
+/* Define if you have an x86_64 CPU */
+#undef HAVE_X86_64_CPU
 
 /* Define if you have a PowerPC CPU */
 #undef HAVE_PPC_CPU
