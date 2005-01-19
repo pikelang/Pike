7855bf03bc1ab6a2d0eab8f0b2cfe45fe3bd1bfa
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-01-19 14:05:55 +0100

    Oops.
    Rev: src/stralloc.c:1.158

--- src/stralloc.c
+++ src/stralloc.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: stralloc.c,v 1.157 2005/01/18 17:54:15 grubba Exp $
+|| $Id: stralloc.c,v 1.158 2005/01/19 13:05:55 grubba Exp $
 */
 
 #include "global.h"
@@ -24,7 +24,7 @@
 #include <ctype.h>
 #include <math.h>
 
-RCSID("$Id: stralloc.c,v 1.157 2005/01/18 17:54:15 grubba Exp $");
+RCSID("$Id: stralloc.c,v 1.158 2005/01/19 13:05:55 grubba Exp $");
 
 /* #define STRALLOC_USE_PRIMES */
 
@@ -1561,6 +1561,7 @@ PMOD_EXPORT struct pike_string *modify_shared_string(struct pike_string *a,
 
     if((((unsigned int)index) >= HASH_PREFIX) && (index < a->len-8))
     {
+      struct pike_string *old;
       /* Doesn't change hash value - sneak it in there */
       low_set_index(a,index,c);
       unlink_pike_string(a);
