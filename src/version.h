01b9463d2b856dee1a96a6067ac9789854270073
Martin Stjernholm <mast@lysator.liu.se>
2005-02-10 18:51:35 +0100

    release number bumped to 266 by export.pike
    Rev: packaging/debian/changelog:1.281
    Rev: src/version.h:1.597

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.596 2005/02/08 10:22:44 distmaker Exp $
+|| $Id: version.h,v 1.597 2005/02/10 17:51:26 mast Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 265
+#define PIKE_BUILD_VERSION 266
 
 /* Prototypes begin here */
 void f_version(INT32 args);
