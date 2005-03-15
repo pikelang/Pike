b13d5d935321dbe960b66817ff67cd93cd9e35c6
Per Hedbor <ph@opera.com>
2005-03-15 21:56:28 +0100

    Added _encode/_decode
    Rev: src/modules/Image/image.c:1.220

--- src/modules/Image/image.c
+++ src/modules/Image/image.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: image.c,v 1.219 2004/10/07 22:49:56 nilsson Exp $
+|| $Id: image.c,v 1.220 2005/03/15 20:56:28 per Exp $
 */
 
 /*
@@ -4703,6 +4703,43 @@ void image_tobitmap(INT32 args)
    push_string(end_shared_string(res));
 }
 
+
+/***************** Serialization methods ***********************/
+void image__encode( INT32 args )
+{
+    push_int( THIS->xsize );
+    push_int( THIS->ysize );
+    push_string( make_shared_binary_string((char *)THIS->img,
+					   THIS->xsize*THIS->ysize
+					   *sizeof(rgb_group)) );
+    f_aggregate( 3 );
+}
+
+void image__decode( INT32 args )
+{
+    struct array *a;
+    if( Pike_sp[-1].type != PIKE_T_ARRAY ||
+	Pike_sp[-1].u.array->size != 3 ||
+	(a=Pike_sp[-1].u.array)->item[2].type != PIKE_T_STRING )
+	Pike_error( "Illegal arguments to decode\n");
+
+    int w = a->item[0].u.integer;
+    int h = a->item[1].u.integer;
+
+    if( w*h*sizeof(rgb_group) != a->item[2].u.string->len )
+	Pike_error("Illegal image data\n");
+
+    if( THIS->img )
+	free( THIS->img );
+
+    THIS->xsize = w;
+    THIS->ysize = h;
+    THIS->img = xalloc( sizeof(rgb_group)*w*h );
+
+    memcpy( THIS->img, a->item[2].u.string->str, a->item[2].u.string->len );
+    pop_stack();
+}
+
 /***************** global init etc *****************************/
 
 #define tRGB tOr3(tColor,tVoid,tInt) tOr(tInt,tVoid) tOr(tInt,tVoid)
@@ -4718,6 +4755,10 @@ void init_image_image(void)
    ADD_STORAGE(struct image);
    
    ADD_FUNCTION("_sprintf", image__sprintf, tFunc(tInt , tString), 0 );
+
+
+   ADD_FUNCTION("_encode", image__encode, tFunc(tVoid,tArray), 0 );
+   ADD_FUNCTION("_decode", image__decode, tFunc(tArray,tVoid), 0 );
 
    ADD_FUNCTION("create",image_create,
 		tOr(tFunc(tOr(tInt,tVoid) tOr(tInt,tVoid) tRGB,tVoid),
