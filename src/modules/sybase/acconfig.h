d037da677b967bac3a0b3805d17ff74435d3ace3
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-03-21 16:16:09 +0100

    Added detection and support for the SybaseOpenClient framework on MacOS X.
    Rev: src/modules/sybase/acconfig.h:1.6
    Rev: src/modules/sybase/configure.in:1.7
    Rev: src/modules/sybase/sybase.h:1.7

--- src/modules/sybase/acconfig.h
+++ src/modules/sybase/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.5 2002/10/11 01:39:55 nilsson Exp $
+|| $Id: acconfig.h,v 1.6 2005/03/21 15:16:09 grubba Exp $
 */
 
 /*
@@ -15,17 +15,33 @@
 #define __PIKE_SYBASE_CONFIG_H
 
 @TOP@
+
+/* Define if you have libintl */
+#undef PIKE_HAVE_LIBINTL
+
+/* Define if you have libcomn */
+#undef PIKE_HAVE_LIBCOMN
+
+/* Define if you have libcs */
+#undef PIKE_HAVE_LIBCS
+
+/* Define if you have libsystcl */
+#undef PIKE_HAVE_LIBSYBTCL
+
+/* Define if you have libct */
+#undef PIKE_HAVE_LIBCT
+
+/* Define if you have -framework SybaseOpenClient */
+#undef HAVE_FRAMEWORK_SYBASEOPENCLIENT
+
 @BOTTOM@
 
 /* End of automatic session. Doing stuff now */
 
-#undef PIKE_HAVE_LIBCOMN
-#undef PIKE_HAVE_LIBCS
-#undef PIKE_HAVE_LIBCT
-#undef PIKE_HAVE_LIBINTL
-#undef PIKE_HAVE_LIBSYBTCL
-
-#if defined(PIKE_HAVE_LIBCOMN) && defined(PIKE_HAVE_LIBCS) \
+#if defined(HAVE_SYBASEOPENCLIENT_SYBASEOPENCLIENT_H) && \
+  defined(HAVE_FRAMEWORK_SYBASEOPENCLIENT)
+#define HAVE_SYBASE
+#elif defined(PIKE_HAVE_LIBCOMN) && defined(PIKE_HAVE_LIBCS) \
   && defined(PIKE_HAVE_LIBCT) && defined(PIKE_HAVE_LIBINTL) \
   && defined(PIKE_HAVE_LIBSYBTCL) && defined(HAVE_CTPUBLIC_H)
 #define HAVE_SYBASE 
