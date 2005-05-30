fadddcd411b2d5cd5cc88fc35a0493efc3d4440c
Build system <distmaker@roxen.com>
2005-05-30 15:31:46 +0200

    release number bumped to 301 by export.pike
    Rev: packaging/debian/changelog:1.316
    Rev: src/version.h:1.632

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.631 2005/05/30 13:29:12 distmaker Exp $
+|| $Id: version.h,v 1.632 2005/05/30 13:31:44 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 300
+#define PIKE_BUILD_VERSION 301
 
 /* Prototypes begin here */
 void f_version(INT32 args);
