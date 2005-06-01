3ae56304956ee00e153945ca55e12c9871d0f4b5
Per Hedbor <ph@opera.com>
2005-06-01 16:08:53 +0200

    SDL.framework support
    Rev: src/post_modules/SDL/SDL.cmod:1.28
    Rev: src/post_modules/SDL/acconfig.h:1.5
    Rev: src/post_modules/SDL/configure.in:1.8

--- src/post_modules/SDL/acconfig.h
+++ src/post_modules/SDL/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.4 2003/09/10 12:40:50 mirar Exp $
+|| $Id: acconfig.h,v 1.5 2005/06/01 14:08:53 per Exp $
 */
 
 #ifndef SDL_CONFIG_H
@@ -15,6 +15,8 @@
 #undef HAVE_LIBSDL_JOYSTICK
 
 #undef HAVE_SDL
+
+#undef HAVE_SDL_SDL_H
 
 # if defined(HAVE_LIBSDL_MIXER) && (defined(HAVE_SDL_MIXER_H) || defined(HAVE_SDL_SDL_MIXER_H))
 #  define HAVE_SDL_MIXER
