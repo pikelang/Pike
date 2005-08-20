316b114982b9b68fb4d55a42c3035a9def91e022
Andreas Sigfridsson <sigge@lysator.liu.se>
2005-08-20 16:24:46 +0200

    release number bumped to 34 by export.pike
    Rev: packaging/debian/changelog:1.95
    Rev: packaging/windows/pike.iss:1.23
    Rev: src/version.h:1.391

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.390 2005/07/28 11:04:40 nilsson Exp $
+|| $Id: version.h,v 1.391 2005/08/20 14:24:40 sigge Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 33
+#define PIKE_BUILD_VERSION 34
 
 /* Prototypes begin here */
 void f_version(INT32 args);
