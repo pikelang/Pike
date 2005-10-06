f2767681553094f9d7e5f37250f54fd317adb4a6
Martin Nilsson <mani@lysator.liu.se>
2005-10-06 02:21:14 +0200

    Dummy module
    Rev: src/post_modules/GTK2/dummy.c:1.1

---
+++ src/post_modules/GTK2/dummy.c
@@ -0,0 +1,22 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: dummy.c,v 1.1 2005/10/06 00:21:14 nilsson Exp $
+*/
+
+#include "global.h"
+#include "pgtk_config.h"
+#include "module.h"
+
+RCSID("$Id: dummy.c,v 1.1 2005/10/06 00:21:14 nilsson Exp $");
+
+/* Well... Sort of basic, right? :-) */
+
+PIKE_MODULE_INIT
+{
+}
+
+PIKE_MODULE_EXIT
+{
+}
