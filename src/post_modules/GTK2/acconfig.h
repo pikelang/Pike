8cee2f5ad1f3211367c8183cbb2f73004e1ef24f
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-10-07 18:24:03 +0200

    Added BETA defines.
    Rev: src/post_modules/GTK2/acconfig.h:1.2

--- src/post_modules/GTK2/acconfig.h
+++ src/post_modules/GTK2/acconfig.h
@@ -2,18 +2,20 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.1 2005/08/20 03:02:16 nilsson Exp $
+|| $Id: acconfig.h,v 1.2 2005/10/07 16:24:03 grubba Exp $
 */
 
 #undef FUNCPROTO
 #undef HAVE_GNOME
 #undef HAVE_DPMS
 #undef PGTK_AUTO_UTF8
+#undef HAVE_GTK_BETA
 #undef HAVE_GTK22
 #undef HAVE_GTK24
 #undef HAVE_GTK26
 #undef HAVE_ATK
 #undef HAVE_ATK18
+#undef HAVE_PANGO_BETA
 #undef HAVE_PANGO
 #undef HAVE_PANGO16
 #undef HAVE_PANGO14
