d4afa9b1a4cb24141fa32a2b4cdeae6d3dc9b065
Martin Nilsson <mani@lysator.liu.se>
2005-10-31 12:44:07 +0100

    release number bumped to 48 by export.pike
    Rev: packaging/debian/changelog:1.109
    Rev: packaging/windows/pike.iss:1.37
    Rev: src/version.h:1.405

--- packaging/windows/pike.iss
+++ packaging/windows/pike.iss
@@ -2,10 +2,10 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 [Setup]
-AppName=Pike 7.6 *BETA*
-AppVerName=Pike 7.6.47, SDL, OpenGL, MySQL, Freetype, Gz and GTK+
-AppVersion=7.6.47
-VersionInfoVersion=7.6.47.0
+AppName=Pike 7.6
+AppVerName=Pike 7.6.48, SDL, OpenGL, MySQL, Freetype, Gz and GTK+
+AppVersion=7.6.48
+VersionInfoVersion=7.6.48.0
 AppPublisher=The Pike Team
 AppPublisherURL=http://pike.ida.liu.se/
 AppSupportURL=http://pike.ida.liu.se/
--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.404 2005/10/26 07:51:35 nilsson Exp $
+|| $Id: version.h,v 1.405 2005/10/31 11:44:03 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 47
+#define PIKE_BUILD_VERSION 48
 
 /* Prototypes begin here */
 void f_version(INT32 args);
