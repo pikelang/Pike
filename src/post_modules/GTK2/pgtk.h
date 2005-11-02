9cda46a9cb1889c12b6172e8d646cc2b7bdc986d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-11-02 17:41:14 +0100

    Fixed typo.
    Rev: src/post_modules/GTK2/pgtk.h:1.3

--- src/post_modules/GTK2/pgtk.h
+++ src/post_modules/GTK2/pgtk.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pgtk.h,v 1.2 2005/11/02 14:16:19 grubba Exp $
+|| $Id: pgtk.h,v 1.3 2005/11/02 16:41:14 grubba Exp $
 */
 
 /* Sort of unnessesary, and decreases code-size with 140Kb */
@@ -30,7 +30,7 @@
 */
 #include <gtk/gtk.h>
 #if defined(HAVE_GNOME)
-# include <libgnome.h>
+# include <libgnome/libgnome.h>
 /*# include <libgnorba/gnorba.h> */
 #endif
 
