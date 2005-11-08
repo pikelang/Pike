fdb8378303b17092b0bb7656b7a6fd22b214a741
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-11-08 12:34:23 +0100

    Improved diagnostics when the master_object is dead.
    Rev: src/svalue.c:1.224

--- src/svalue.c
+++ src/svalue.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: svalue.c,v 1.223 2005/09/09 15:09:09 mast Exp $
+|| $Id: svalue.c,v 1.224 2005/11/08 11:34:23 grubba Exp $
 */
 
 #include "global.h"
@@ -1508,7 +1508,7 @@ PMOD_EXPORT void describe_svalue(const struct svalue *s,int indent,struct proces
 	prog = prog->inherits[s->subtype].prog;
 
 	if ((prog->flags & PROGRAM_FINISHED) &&
-	    Pike_interpreter.evaluator_stack && !Pike_in_gc && master_object) {
+	    Pike_interpreter.evaluator_stack && !Pike_in_gc) {
 	  DECLARE_CYCLIC();
 	  int fun=FIND_LFUN(prog, LFUN__SPRINTF);
 	  debug_malloc_touch(prog);
@@ -1530,7 +1530,7 @@ PMOD_EXPORT void describe_svalue(const struct svalue *s,int indent,struct proces
 	      push_constant_text("indent");
 	      push_int(indent);
 	      f_aggregate_mapping(2);					      
-	      safe_apply_low2(obj, fun ,2,1);
+	      safe_apply_low2(obj, fun ,2, !!master_object);
 
 	      debug_malloc_touch(obj);
 
@@ -1560,7 +1560,7 @@ PMOD_EXPORT void describe_svalue(const struct svalue *s,int indent,struct proces
 	    END_CYCLIC();
 	  }
 	  
-	  if (!BEGIN_CYCLIC(0, obj)) {
+	  if (!BEGIN_CYCLIC(0, obj) && master_object) {
 	    /* We require some tricky coding to make this work
 	     * with tracing...
 	     */
