a8cc3e9d3eff5277427fd6fa30e887858de1b972
Henrik Grubbström (Grubba) <grubba@grubba.org>
2005-11-23 16:07:57 +0100

    The prototype for pike_do_exit() shouldn't be here.
    Rev: src/main.h:1.29

--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: main.h,v 1.28 2005/11/23 10:40:40 grubba Exp $
+|| $Id: main.h,v 1.29 2005/11/23 15:07:57 grubba Exp $
 */
 
 #ifndef MAIN_H
@@ -10,7 +10,6 @@
 
 /* Prototypes begin here */
 int main(int argc, char **argv);
-DECLSPEC(noreturn) void pike_do_exit(int num) ATTRIBUTE((noreturn));
 /* Prototypes end here */
 
 #endif /* !MAIN_H */
