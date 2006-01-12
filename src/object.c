d842af36bcaa4a21d5764a1f13cf3c730051492f
Henrik Grubbström (Grubba) <grubba@grubba.org>
2006-01-12 13:31:50 +0100

    Fix for LysLysKOM 14020704 where objects without storage were indexed.
    Rev: src/object.c:1.268

--- src/object.c
+++ src/object.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: object.c,v 1.267 2005/05/19 22:35:30 mast Exp $
+|| $Id: object.c,v 1.268 2006/01/12 12:31:50 grubba Exp $
 */
 
 #include "global.h"
@@ -1129,17 +1129,27 @@ PMOD_EXPORT void low_object_index_no_free(struct svalue *to,
     }
 
   case IDENTIFIER_VARIABLE:
-  {
-    void *ptr=LOW_GET_GLOBAL(o,f,i);
-    switch(i->run_time_type)
-    {
-      case T_MIXED:
-      {
-	struct svalue *s=(struct svalue *)ptr;
-	check_destructed(s);
-	assign_svalue_no_free(to, s);
-	break;
+    if (!PIKE_OBJ_STORAGE(o)) {
+      /* Variable storage not allocated. */
+#ifdef PIKE_DEBUG
+      if (p->flags & PROGRAM_FINISHED) {
+	Pike_fatal("Object without variable storage!\n");
       }
+#endif /* PIKE_DEBUG */
+      to->type = T_INT;
+      to->subtype = NUMBER_NUMBER;
+      to->u.integer = 0;      
+    } else {
+      void *ptr=LOW_GET_GLOBAL(o,f,i);
+      switch(i->run_time_type)
+      {
+      case T_MIXED:
+	{
+	  struct svalue *s=(struct svalue *)ptr;
+	  check_destructed(s);
+	  assign_svalue_no_free(to, s);
+	  break;
+	}
 
       case T_FLOAT:
 	to->type=T_FLOAT;
@@ -1154,23 +1164,23 @@ PMOD_EXPORT void low_object_index_no_free(struct svalue *to,
 	break;
 
       default:
-      {
-	struct ref_dummy *dummy;
-
-	to->subtype=0;
-	if((dummy=*(struct ref_dummy  **)ptr))
 	{
-	  add_ref(to->u.dummy=dummy);
-	  to->type=i->run_time_type;
-	}else{
-	  to->type=T_INT;
-	  to->u.integer=0;
+	  struct ref_dummy *dummy;
+
+	  to->subtype=0;
+	  if((dummy=*(struct ref_dummy  **)ptr))
+	  {
+	    add_ref(to->u.dummy=dummy);
+	    to->type=i->run_time_type;
+	  }else{
+	    to->type=T_INT;
+	    to->u.integer=0;
+	  }
+	  break;
 	}
-	break;
       }
     }
     break;
-  }
 
   default:;
 #ifdef PIKE_DEBUG
