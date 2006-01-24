ac9b159b0a3b0a3e351f40fa49f9bcfec5ff83fd
Martin Stjernholm <mast@lysator.liu.se>
2006-01-25 00:19:23 +1000

    Delay the warning a bit to avoid false alarms.
    Rev: src/module.c:1.38

--- src/module.c
+++ src/module.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: module.c,v 1.37 2006/01/24 13:10:27 mast Exp $
+|| $Id: module.c,v 1.38 2006/01/24 14:19:23 mast Exp $
 */
 
 #include "global.h"
@@ -354,7 +354,6 @@ void exit_modules(void)
      * always report destructed objects. We use the gc in a special mode
      * for this to get a reasonably sane destruct order. */
     gc_destruct_everything = 1;
-    exit_cleanup_in_progress = 1; /* Warn about object creation from now on. */
     count = do_gc (NULL, 1);
     while (count) {
       size_t new_count = do_gc (NULL, 1);
@@ -366,6 +365,7 @@ void exit_modules(void)
       count = new_count;
     }
     gc_destruct_everything = 0;
+    exit_cleanup_in_progress = 1; /* Warn about object creation from now on. */
   }
 
   /* Unload dynamic modules before static ones. */
