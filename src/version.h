070f91db13d28743247e38d2c9f19fdd85fc136d
Peter Bortas <zino@lysator.liu.se>
2006-03-02 23:47:26 +0100

    release number bumped to 68 by export.pike
    Rev: packaging/debian/changelog:1.138
    Rev: src/version.h:1.425

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.424 2006/01/22 20:47:59 peter Exp $
+|| $Id: version.h,v 1.425 2006/03/02 22:47:24 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 67
+#define PIKE_BUILD_VERSION 68
 
 /* Prototypes begin here */
 void f_version(INT32 args);
