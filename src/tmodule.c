9479719047e5d716514ff040585711843fa8ad96
Martin Stjernholm <mast@lysator.liu.se>
2006-08-02 22:16:38 +0200

    Implemented a thorough separation of the different build orders necessary to build static modules (where a tpike is required) and dynamic modules (where a pike.lib that fits the pike core exe is required with --enable-dll).
    tpike is now built and used only when dynamic modules are disabled. Post
    modules can no longer affect the link options when the pike binary is built
    (something that wouldn't work reliably anyway).
    
    Rev: src/Makefile.in:1.438
    Rev: src/configure.in:1.976
    Rev: src/make_variables.in:1.26
    Rev: src/modules/common_module_makefile.in:1.44
    Rev: src/modules/dynamic_module_makefile.in:1.120
    Rev: src/modules/static_module_makefile.in:1.104
    Rev: src/post_modules/configure.in:1.7
    Rev: src/tmodule.c:1.2

--- src/tmodule.c
+++ src/tmodule.c
@@ -2,10 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: tmodule.c,v 1.1 2006/02/27 12:33:55 mast Exp $
+|| $Id: tmodule.c,v 1.2 2006/08/02 20:16:38 mast Exp $
 */
 
-/* This variant of module.c is used with pre-pike and doesn't link
- * in any post-modules. */
+/* This variant of module.c is used when modules are dynamic and when
+ * building tpike (which only is done when modules are static). It
+ * doesn't link in any post-modules. */
 #define PRE_PIKE
 #include "module.c"
