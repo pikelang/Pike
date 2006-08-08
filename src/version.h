114ab6a8ab32f1e60361de6ea1a07ed8f5bd0aa1
Marek Habersack <mhabersack@novell.com>
2006-08-09 01:51:32 +0200

    release number bumped to 27 by export.pike
    Rev: packaging/debian/changelog:1.73
    Rev: src/version.h:1.385

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.384 2006/08/08 23:49:51 grendel Exp $
+|| $Id: version.h,v 1.385 2006/08/08 23:51:28 grendel Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 26
+#define PIKE_BUILD_VERSION 27
 
 /* Prototypes begin here */
 void f_version(INT32 args);
