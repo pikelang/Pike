57366468ff6cd1f3f08f703c5cd9e192acdd7d8b
Martin Nilsson <mani@lysator.liu.se>
2006-08-09 01:47:31 +0200

    Removing Ssleay from standard Pike
    Rev: src/configure.in:1.992
    Rev: src/modules/Ssleay/.cvsignore:1.7(DEAD)
    Rev: src/modules/Ssleay/Makefile.in:1.8(DEAD)
    Rev: src/modules/Ssleay/acconfig.h:1.5(DEAD)
    Rev: src/modules/Ssleay/configure.in:1.9(DEAD)
    Rev: src/modules/Ssleay/ssleay.c:1.23(DEAD)
    Rev: src/modules/Ssleay/testsuite.in:1.2(DEAD)

--- src/modules/Ssleay/acconfig.h
+++
@@ -1,9 +0,0 @@
-/*
-|| This file is part of Pike. For copyright information see COPYRIGHT.
-|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
-|| for more information.
-|| $Id: acconfig.h,v 1.4 2002/10/11 01:39:49 nilsson Exp $
-*/
-
-/* Define if you have SSLeay */
-#undef HAVE_SSLEAY
