9f9f3cabffd521f2a1781e75ec50ec3c0982d9db
Marek Habersack <mhabersack@novell.com>
2006-08-09 02:21:32 +0200

    release number bumped to 28 by export.pike
    Rev: src/version.h:1.386

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.385 2006/08/08 23:51:28 grendel Exp $
+|| $Id: version.h,v 1.386 2006/08/09 00:21:32 grendel Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 27
+#define PIKE_BUILD_VERSION 28
 
 /* Prototypes begin here */
 void f_version(INT32 args);
