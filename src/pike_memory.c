95e3e999e701dba6d736960872c5e49555c8f493
Martin Stjernholm <mast@lysator.liu.se>
2006-08-09 16:02:10 +0200

    mmap(2) on SunOS 5 doesn't ignore the fd argument when the MAP_ANON flag is given.
    Rev: src/pike_memory.c:1.184

--- src/pike_memory.c
+++ src/pike_memory.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pike_memory.c,v 1.183 2006/08/09 01:55:45 mast Exp $
+|| $Id: pike_memory.c,v 1.184 2006/08/09 14:02:10 mast Exp $
 */
 
 #include "global.h"
@@ -378,7 +378,7 @@ char *debug_qalloc(size_t size)
 #endif /* !MAP_ANONYMOUS && MAP_ANON */
 
 #ifdef MAP_ANONYMOUS
-#define dev_zero 0
+#define dev_zero -1
 #else
 static int dev_zero = -1;
 #define INIT_DEV_ZERO
@@ -390,8 +390,8 @@ static INLINE void *mexec_do_alloc (void *start, size_t length)
   void *blk = mmap(start, length, PROT_EXEC|PROT_READ|PROT_WRITE,
 		   MAP_PRIVATE|MAP_ANONYMOUS, dev_zero, 0);
   if (blk == MAP_FAILED) {
-    fprintf(stderr, "mmap of %"PRINTSIZET"u bytes failed, errno=%d.\n",
-	    length, errno);
+    fprintf(stderr, "mmap of %"PRINTSIZET"u bytes failed, errno=%d. "
+	    "(dev_zero=%d)\n", length, errno, dev_zero);
     return NULL;
   }
   return blk;
