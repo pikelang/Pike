1979ffa4be6d9cf4e96bc46805c49f14e09a799e
Martin Stjernholm <mast@lysator.liu.se>
2006-08-21 14:22:14 +0200

    Bumped build version to test windows xenofarm clients.
    Rev: src/version.h:1.387

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.386 2006/08/09 00:21:32 grendel Exp $
+|| $Id: version.h,v 1.387 2006/08/21 12:22:14 mast Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 28
+#define PIKE_BUILD_VERSION 29
 
 /* Prototypes begin here */
 void f_version(INT32 args);
