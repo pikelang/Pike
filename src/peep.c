83a9e7e02390301f85a418ecc5d4b7bfddf36665
Henrik Grubbström (Grubba) <grubba@grubba.org>
2006-09-05 14:10:40 +0200

    Backported fix for [bug 4173] and [bug 4231] from Pike 7.7.
    Rev: src/peep.c:1.89

--- src/peep.c
+++ src/peep.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: peep.c,v 1.88 2004/05/11 15:47:39 grubba Exp $
+|| $Id: peep.c,v 1.89 2006/09/05 12:10:40 grubba Exp $
 */
 
 #include "global.h"
@@ -26,7 +26,7 @@
 #include "interpret.h"
 #include "pikecode.h"
 
-RCSID("$Id: peep.c,v 1.88 2004/05/11 15:47:39 grubba Exp $");
+RCSID("$Id: peep.c,v 1.89 2006/09/05 12:10:40 grubba Exp $");
 
 static void asm_opt(void);
 
@@ -72,7 +72,7 @@ void exit_bytecode(void)
   c=(p_instr *)instrbuf.s.str;
   length=instrbuf.s.len / sizeof(p_instr);
 
-  for(e=0;e<length;e++) free_string(c->file);
+  for(e=0;e<length;e++) free_string(c[e].file);
   
   toss_buffer(&instrbuf);
 }
