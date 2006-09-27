cbb407f10f8443ea4944047e8451e2ddb6deb58d
Build system <distmaker@roxen.com>
2006-09-27 11:25:27 +0200

    release number bumped to 438 by export.pike
    Rev: packaging/debian/changelog:1.463
    Rev: src/version.h:1.769

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.768 2006/09/14 23:56:11 distmaker Exp $
+|| $Id: version.h,v 1.769 2006/09/27 09:25:26 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 437
+#define PIKE_BUILD_VERSION 438
 
 /* Prototypes begin here */
 void f_version(INT32 args);
