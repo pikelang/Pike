d90e758385d961c17bdb59ef35999e12fea39842
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-04-28 20:19:09 +0200

    Renamed FDATASYNC() to MIRD_FDATASYNC() to avoid clash with NetBSD header-files.
    Rev: src/modules/Mird/libmird/database.c:1.8
    Rev: src/modules/Mird/libmird/internal.h:1.7
    Rev: src/modules/Mird/libmird/transaction.c:1.6

--- src/modules/Mird/libmird/transaction.c
+++ src/modules/Mird/libmird/transaction.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: transaction.c,v 1.5 2003/01/03 20:52:49 grubba Exp $
+|| $Id: transaction.c,v 1.6 2007/04/28 18:19:09 grubba Exp $
 */
 
 /*
@@ -33,7 +33,7 @@
 
 
 static const char RCSID[]=
-   "$Id: transaction.c,v 1.5 2003/01/03 20:52:49 grubba Exp $";
+   "$Id: transaction.c,v 1.6 2007/04/28 18:19:09 grubba Exp $";
 
 /*
  * start a new transaction
@@ -381,10 +381,10 @@ clean:
    if ( (mtr->db->flags & MIRD_SYNC_END) )
    {
       MIRD_SYSCALL_COUNT(mtr->db,0);
-      if ( FDATASYNC(mtr->db->db_fd)==-1 )
+      if ( MIRD_FDATASYNC(mtr->db->db_fd)==-1 )
 	 return mird_generate_error(MIRDE_DB_SYNC,0,errno,0);
       MIRD_SYSCALL_COUNT(mtr->db,0);
-      if ( FDATASYNC(mtr->db->jo_fd)==-1 )
+      if ( MIRD_FDATASYNC(mtr->db->jo_fd)==-1 )
 	 return mird_generate_error(MIRDE_JO_SYNC,0,errno,0);
       if ( ( mtr->db->flags & MIRD_CALL_SYNC ) )
       {
