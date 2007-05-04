a19e183d59d52b31a68166c07ce11cb2d1375a4f
Martin Stjernholm <mast@lysator.liu.se>
2007-05-04 20:47:42 +0200

    Backported Regexp.PCRE shamelessly to get widestring regexp support. The impact on the already existing code is minimal, at least.
    Rev: src/modules/Regexp/module.pmod.in:1.6
    Rev: src/modules/_Regexp_PCRE/.cvsignore:1.1
    Rev: src/modules/_Regexp_PCRE/Makefile.in:1.1
    Rev: src/modules/_Regexp_PCRE/acconfig.h:1.1
    Rev: src/modules/_Regexp_PCRE/configure.in:1.1
    Rev: src/modules/_Regexp_PCRE/module.pmod.in:1.1
    Rev: src/modules/_Regexp_PCRE/pcre_glue.cmod:1.1
    Rev: src/modules/_Regexp_PCRE/testsuite.in:1.1

---
+++ src/modules/_Regexp_PCRE/acconfig.h
@@ -0,0 +1,13 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.1 2007/05/04 18:47:42 mast Exp $
+*/
+
+@TOP@
+
+/* Define this if you have -lpcre */
+#undef HAVE_LIBPCRE
+
+@BOTTOM@
