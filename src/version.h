f32dcffa163986a823d9df3e212833148a794cc3
Per Hedbor <ph@opera.com>
2007-05-10 16:41:26 +0200

    release number bumped to 32 by export.pike
    Rev: packaging/debian/changelog:1.79
    Rev: src/version.h:1.390

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.389 2007/05/10 14:40:02 per Exp $
+|| $Id: version.h,v 1.390 2007/05/10 14:41:25 per Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 31
+#define PIKE_BUILD_VERSION 32
 
 /* Prototypes begin here */
 void f_version(INT32 args);
