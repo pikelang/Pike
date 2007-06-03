48f3331eabea4dcb48b033630bc02b28e30000f4
Peter Bortas <zino@lysator.liu.se>
2007-06-04 00:41:14 +0200

    release number bumped to 117 by export.pike
    Rev: packaging/debian/changelog:1.194
    Rev: packaging/windows/pike.iss:1.56
    Rev: src/version.h:1.474

--- packaging/windows/pike.iss
+++ packaging/windows/pike.iss
@@ -3,7 +3,7 @@
 
 #define MAJOR "7"
 #define MINOR "6"
-#define BUILD "116"
+#define BUILD "117"
 #define INST "1"
 
 [Setup]
--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.473 2007/06/03 22:35:36 peter Exp $
+|| $Id: version.h,v 1.474 2007/06/03 22:41:08 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 116
+#define PIKE_BUILD_VERSION 117
 
 /* Prototypes begin here */
 void f_version(INT32 args);
