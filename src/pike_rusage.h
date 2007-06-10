5478802953e63969c5951c95085177fefdc8e999
Martin Stjernholm <mast@lysator.liu.se>
2007-06-10 21:02:10 +0200

    Fixed the CPU_TIME_TICKS constants to work both with the compiler and the preprocessor.
    Rev: src/pike_rusage.h:1.19

--- src/pike_rusage.h
+++ src/pike_rusage.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pike_rusage.h,v 1.18 2007/06/10 18:11:13 mast Exp $
+|| $Id: pike_rusage.h,v 1.19 2007/06/10 19:02:10 mast Exp $
 */
 
 #ifndef PIKE_RUSAGE_H
@@ -145,14 +145,14 @@ long *low_rusage(void);
 /* The time is returned in nanoseconds. */
 typedef INT64 cpu_time_t;
 #define LONG_CPU_TIME
-#define CPU_TIME_TICKS /* per second */ ((cpu_time_t) 1000000000)
+#define CPU_TIME_TICKS /* per second */ 1000000000LL
 #define CPU_TIME_UNIT "ns"
 #define PRINT_CPU_TIME PRINTINT64 "d"
 #else
 /* The time is returned in milliseconds. (Note that the value will
  * wrap after about 49 days.) */
 typedef unsigned long cpu_time_t;
-#define CPU_TIME_TICKS /* per second */ ((cpu_time_t) 1000)
+#define CPU_TIME_TICKS /* per second */ 1000
 #define CPU_TIME_UNIT "ms"
 #define PRINT_CPU_TIME "lu"
 #endif
