6510769968eecd0eabe0d06a0b17e972db914602
Martin Stjernholm <mast@lysator.liu.se>
2007-06-10 20:53:28 +0200

    Fix to make the fix in builtin_functions.c work.
    Rev: src/pike_rusage.h:1.17

--- src/pike_rusage.h
+++ src/pike_rusage.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pike_rusage.h,v 1.16 2003/11/27 19:57:35 mast Exp $
+|| $Id: pike_rusage.h,v 1.17 2007/06/10 18:53:28 mast Exp $
 */
 
 #ifndef PIKE_RUSAGE_H
@@ -29,14 +29,14 @@ long *low_rusage(void);
 /* The time is returned in nanoseconds. */
 typedef INT64 cpu_time_t;
 #define LONG_CPU_TIME
-#define CPU_TIME_TICKS /* per second */ ((cpu_time_t) 1000000000)
+#define CPU_TIME_TICKS 1000000000LL /* per second */
 #define CPU_TIME_UNIT "ns"
 #define PRINT_CPU_TIME PRINTINT64 "d"
 #else
 /* The time is returned in milliseconds. (Note that the value will
  * wrap after about 49 days.) */
 typedef unsigned long cpu_time_t;
-#define CPU_TIME_TICKS /* per second */ ((cpu_time_t) 1000)
+#define CPU_TIME_TICKS 1000	/* per second */
 #define CPU_TIME_UNIT "ms"
 #define PRINT_CPU_TIME "lu"
 #endif
