902cc91e9a1ccb86897f848374fe5bb23216043d
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-06-11 18:13:27 +0200

    Fixed case where the += opcode can zap lvalues.
    Rev: src/interpret_functions.h:1.194

--- src/interpret_functions.h
+++ src/interpret_functions.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: interpret_functions.h,v 1.193 2006/08/15 10:43:25 grubba Exp $
+|| $Id: interpret_functions.h,v 1.194 2007/06/11 16:13:27 grubba Exp $
 */
 
 /*
@@ -720,6 +720,7 @@ OPCODE0(F_LTOSVAL1, "ltosval1", I_UPDATE_SP, {
 });
 
 OPCODE0(F_ADD_TO, "+=", I_UPDATE_SP, {
+  ONERROR uwp;
   move_svalue (Pike_sp, Pike_sp - 1);
   Pike_sp[-1].type=PIKE_T_INT;
   Pike_sp++;
@@ -775,8 +776,13 @@ OPCODE0(F_ADD_TO, "+=", I_UPDATE_SP, {
       goto add_to_done;
     }
   }
+  /* NOTE: Pike_sp-4 is the lvalue, Pike_sp-2 is the original value.
+   *       If an error gets thrown, the original value will thus be restored.
+   *       If f_add() succeeds, Pike_sp-2 will hold the result.
+   */
+  SET_ONERROR(uwp, o_assign_lvalue, Pike_sp-4);
   f_add(2);
-  assign_lvalue(Pike_sp-3,Pike_sp-1);
+  CALL_AND_UNSET_ONERROR(uwp);	/* assign_lvalue(Pike_sp-3,Pike_sp-1); */
   stack_pop_2_elems_keep_top();
  add_to_done:
    ; /* make gcc happy */
