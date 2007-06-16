7ea251811f8b42629382eb3285974d8ae5cfcb02
Martin Nilsson <mani@lysator.liu.se>
2007-06-16 19:55:06 +0200

    Readded dup2 change, but commented out box.revents usage.
    Rev: src/modules/files/file.c:1.326

--- src/modules/files/file.c
+++ src/modules/files/file.c
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: file.c,v 1.325 2007/06/16 17:20:56 nilsson Exp $
+|| $Id: file.c,v 1.326 2007/06/16 17:55:06 nilsson Exp $
 */
 
 #define NO_PIKE_SHORTHAND
 #include "global.h"
-RCSID("$Id: file.c,v 1.325 2007/06/16 17:20:56 nilsson Exp $");
+RCSID("$Id: file.c,v 1.326 2007/06/16 17:55:06 nilsson Exp $");
 #include "fdlib.h"
 #include "pike_netlib.h"
 #include "interpret.h"
@@ -2838,19 +2838,28 @@ static void file_dup2(INT32 args)
     SIMPLE_BAD_ARG_ERROR("Stdio.File->dup2", 1, "Stdio.File");
 
 
-  if(fd->box.fd < 0)
-    Pike_error("File given to dup2 not open.\n");
+  if(fd->box.fd < 0) {
+    /* FIXME: Use change_fd_for_box here! */
+    /* fd->box.revents = 0; */
+    if((fd->box.fd = fd_dup(FD)) < 0)
+    {
+      ERRNO = errno;
+      pop_n_elems(args);
+      push_int(0);
+      return;
+    }
+  } else {
+    if (fd->flags & FILE_LOCK_FD) {
+      Pike_error("File has been temporarily locked from closing.\n");
+    }
 
-  if (fd->flags & FILE_LOCK_FD) {
-    Pike_error("File has been temporarily locked from closing.\n");
-  }
-
-  if(fd_dup2(FD,fd->box.fd) < 0)
-  {
-    ERRNO=errno;
-    pop_n_elems(args);
-    push_int(0);
-    return;
+    if(fd_dup2(FD,fd->box.fd) < 0)
+    {
+      ERRNO=errno;
+      pop_n_elems(args);
+      push_int(0);
+      return;
+    }
   }
   ERRNO=0;
   low_dup(o, fd, THIS);
