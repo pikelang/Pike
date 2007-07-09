3ec9892384595f226c164855a9134519a8a97fe7
Lance Dillon <riffraff169@yahoo.com>
2007-07-08 21:45:46 -0500

    GTK2 2.12 support
    Rev: src/post_modules/GTK2/acconfig.h:1.10
    Rev: src/post_modules/GTK2/configure.in:1.17
    Rev: src/post_modules/GTK2/source/gtkwidget.pre:1.14

--- src/post_modules/GTK2/acconfig.h
+++ src/post_modules/GTK2/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.9 2006/08/03 16:50:25 ldillon Exp $
+|| $Id: acconfig.h,v 1.10 2007/07/09 02:45:46 ldillon Exp $
 */
 
 #undef FUNCPROTO
@@ -14,6 +14,7 @@
 #undef HAVE_GTK26
 #undef HAVE_GTK28
 #undef HAVE_GTK210
+#undef HAVE_GTK212
 #undef HAVE_ATK
 #undef HAVE_ATK18
 #undef HAVE_PANGO
