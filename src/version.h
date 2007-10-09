8e5482fc5ed89c8d4d53e9497710fc6c0dcd4281
Martin Nilsson <mani@lysator.liu.se>
2007-10-09 14:33:46 +0200

    Let even numbers be the build and odd be the working revisions.
    Rev: src/version.h:1.395

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.394 2007/09/29 15:09:03 grubba Exp $
+|| $Id: version.h,v 1.395 2007/10/09 12:33:46 nilsson Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 38
+#define PIKE_BUILD_VERSION 39
 
 /* Prototypes begin here */
 void f_version(INT32 args);
