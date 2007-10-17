7b4e042a56ee7a42c5532e1ee021b8486d3d27d2
Martin Stjernholm <mast@lysator.liu.se>
2007-10-17 13:46:17 +0200

    Backported change from 7.6:
    Backported new INT_TYPE_{ADD,NEG}_OVERFLOW implementation from 7.7.
    The old one does not work with GCC 4.1.
    
    Rev: src/bignum.h:1.20

--- src/bignum.h
+++ src/bignum.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: bignum.h,v 1.19 2006/05/29 18:11:38 mast Exp $
+|| $Id: bignum.h,v 1.20 2007/10/17 11:46:17 mast Exp $
 */
 
 #include "global.h"
@@ -30,13 +30,11 @@ extern int gmp_library_loaded;
 
 #define INT_TYPE_NEG_OVERFLOW(x)     ((x) && (x) == -(x))
 
-#define INT_TYPE_ADD_OVERFLOW(a, b)                                        \
-        (INT_TYPE_SIGN(a) == INT_TYPE_SIGN(b) &&                           \
-	 INT_TYPE_SIGN(a) != INT_TYPE_SIGN((a)+(b)))
+#define INT_TYPE_ADD_OVERFLOW(a, b)		\
+  ((((a)^(b)) >= 0) && (((a)^((a)+(b))) < 0))
 
 #define INT_TYPE_SUB_OVERFLOW(a, b)                                        \
-        (INT_TYPE_SIGN(a) != INT_TYPE_SIGN(b) &&                           \
-	 INT_TYPE_SIGN(a) != INT_TYPE_SIGN((a)-(b)))
+  ((((a)^(b)) < 0) && (((a)^((a)-(b))) < 0))
 
 #define INT_TYPE_LSH_OVERFLOW(a, b)                                        \
         ((((INT_TYPE)sizeof(INT_TYPE))*CHAR_BIT <= (b) && (a)) ||          \
