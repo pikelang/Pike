367720aebe5e8069e905041b3115c6012fe63556
Henrik Grubbström (Grubba) <grubba@grubba.org>
2007-12-31 13:39:16 +0100

    Got rid of several C++-style comments.
    Rev: src/port.h:1.64

--- src/port.h
+++ src/port.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: port.h,v 1.63 2006/07/04 21:48:33 mast Exp $
+|| $Id: port.h,v 1.64 2007/12/31 12:39:16 grubba Exp $
 */
 
 #ifndef PORT_H
@@ -13,7 +13,7 @@
 
 #ifdef __MINGW32__
 /******************************************************/
-/* First we must ensure that all defines are in mingw */#
+/* First we must ensure that all defines are in mingw */
 /******************************************************/
 #ifndef PROCESSOR_PPC_601
 #define PROCESSOR_PPC_601       601
@@ -32,7 +32,7 @@
 #endif
 
 #ifndef PROCESSOR_OPTIL
-#define PROCESSOR_OPTIL         0x494f  // MSIL
+#define PROCESSOR_OPTIL         0x494f  /* MSIL */
 #endif
 
 #ifndef PROCESSOR_ARCHITECTURE_MSIL
@@ -48,43 +48,43 @@
 #endif
 
 #ifndef PROCESSOR_HITACHI_SH3
-#define PROCESSOR_HITACHI_SH3   10003   // Windows CE
+#define PROCESSOR_HITACHI_SH3   10003   /* Windows CE */
 #endif
 
 #ifndef PROCESSOR_HITACHI_SH3E
-#define PROCESSOR_HITACHI_SH3E  10004   // Windows CE
+#define PROCESSOR_HITACHI_SH3E  10004   /* Windows CE */
 #endif
 
 #ifndef PROCESSOR_HITACHI_SH4
-#define PROCESSOR_HITACHI_SH4   10005   // Windows CE
+#define PROCESSOR_HITACHI_SH4   10005   /* Windows CE */
 #endif
 
 #ifndef PROCESSOR_SHx_SH3
-#define PROCESSOR_SHx_SH3       103     // Windows CE
+#define PROCESSOR_SHx_SH3       103     /* Windows CE */
 #endif
 
 #ifndef PROCESSOR_SHx_SH4
-#define PROCESSOR_SHx_SH4       104     // Windows CE
+#define PROCESSOR_SHx_SH4       104     /* Windows CE */
 #endif
 
 #ifndef PROCESSOR_STRONGARM
-#define PROCESSOR_STRONGARM     2577    // Windows CE - 0xA11
+#define PROCESSOR_STRONGARM     2577    /* Windows CE - 0xA11 */
 #endif
 
 #ifndef PROCESSOR_ARM720
-#define PROCESSOR_ARM720        1824    // Windows CE - 0x720
+#define PROCESSOR_ARM720        1824    /* Windows CE - 0x720 */
 #endif
 
 #ifndef PROCESSOR_ARM820
-#define PROCESSOR_ARM820        2080    // Windows CE - 0x820
+#define PROCESSOR_ARM820        2080    /* Windows CE - 0x820 */
 #endif
 
 #ifndef PROCESSOR_ARM920
-#define PROCESSOR_ARM920        2336    // Windows CE - 0x920
+#define PROCESSOR_ARM920        2336    /* Windows CE - 0x920 */
 #endif
 
 #ifndef PROCESSOR_ARM_7TDMI
-#define PROCESSOR_ARM_7TDMI     70001   // Windows CE
+#define PROCESSOR_ARM_7TDMI     70001   /* Windows CE */
 #endif
 
 #ifndef LOGON32_LOGON_NETWORK
