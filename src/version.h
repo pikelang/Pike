9649f457ed2d0e356a398f7f5ea7fe5b78a07128
Per Hedbor <ph@opera.com>
2008-01-23 05:39:39 +0100

    release number bumped to 41 by export.pike
    Rev: packaging/debian/changelog:1.83
    Rev: src/version.h:1.397

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.396 2008/01/23 04:37:03 per Exp $
+|| $Id: version.h,v 1.397 2008/01/23 04:39:37 per Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 40
+#define PIKE_BUILD_VERSION 41
 
 /* Prototypes begin here */
 void f_version(INT32 args);
