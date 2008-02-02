e36a0521d6be61db70efa5191a3f6e8b7b2397e3
Henrik Grubbström (Grubba) <grubba@grubba.org>
2008-02-02 22:40:32 +0100

    Bumped version to 7.7.43 since dump format has changed somewhat.
    Rev: src/version.h:1.398

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.397 2008/01/23 04:39:37 per Exp $
+|| $Id: version.h,v 1.398 2008/02/02 21:40:32 grubba Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 41
+#define PIKE_BUILD_VERSION 43
 
 /* Prototypes begin here */
 void f_version(INT32 args);
