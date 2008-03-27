ef56c1e4c5a258ca1f8ce9e7ec886c4d839b9dbb
Henrik Grubbström (Grubba) <grubba@grubba.org>
2008-03-27 14:10:33 +0100

    Added builtin convenience #define: #define __deprecated__(TYPE)	__attribute__("deprecated", TYPE)
    Rev: src/cpp.c:1.165

--- src/cpp.c
+++ src/cpp.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: cpp.c,v 1.164 2008/01/26 22:34:19 mast Exp $
+|| $Id: cpp.c,v 1.165 2008/03/27 13:10:33 grubba Exp $
 */
 
 #include "global.h"
@@ -1874,6 +1874,14 @@ void f_cpp(INT32 args)
   do_magic_define(&this,"__MINOR__",insert_current_minor);
 
   {
+    struct define *def =
+      alloc_empty_define(make_shared_string("__deprecated__"), 1);
+    def->args = 1;
+    REF_MAKE_CONST_STRING(def->first, "__attribute__(\"deprecated\", ");
+    def->parts[0].argument = 0;
+    REF_MAKE_CONST_STRING(def->parts[0].postfix, ")");
+    this.defines = hash_insert(this.defines, &def->link);
+
     simple_add_define(&this, "__PIKE__", " 1 ");
 
     simple_add_define(&this, "__REAL_VERSION__",
