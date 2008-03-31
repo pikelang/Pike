57d5bbe286b42304affc2f3c42f102fd94934361
Martin Stjernholm <mast@lysator.liu.se>
2008-03-31 16:39:21 +0200

    Kludge for bindtextdomain(3) which (contrary to the documentation) in some early versions of libintl doesn't accept NULL as dirname. This is known to affect libintl-0.14.4 from the GnuWin32 project.
    Rev: src/modules/Gettext/acconfig.h:1.5
    Rev: src/modules/Gettext/configure.in:1.3
    Rev: src/modules/Gettext/gettext.c:1.15

--- src/modules/Gettext/acconfig.h
+++ src/modules/Gettext/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.4 2002/10/11 01:39:40 nilsson Exp $
+|| $Id: acconfig.h,v 1.5 2008/03/31 14:39:21 mast Exp $
 */
 
 #ifndef GETTEXT_CONFIG_H
@@ -12,6 +12,9 @@
 
 /* define if you have gettext */
 #undef HAVE_GETTEXT
+
+/* Defined if bindtextdomain(3) copes with NULL as dirname */
+#undef BINDTEXTDOMAIN_HANDLES_NULL
 
 @BOTTOM@
 
