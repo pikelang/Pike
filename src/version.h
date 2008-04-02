ef59eccaf2784fa9df43ea5d9ca6071ae1e72cbb
Martin Stjernholm <mast@lysator.liu.se>
2008-04-02 16:05:23 +0200

    Bump after windows builds going to the opera folks.
    Rev: src/version.h:1.399

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.398 2008/02/02 21:40:32 grubba Exp $
+|| $Id: version.h,v 1.399 2008/04/02 14:05:23 mast Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 43
+#define PIKE_BUILD_VERSION 44
 
 /* Prototypes begin here */
 void f_version(INT32 args);
