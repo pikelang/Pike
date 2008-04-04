a51ade2754b3b7137b45e5c46ec0c72d9544e01c
Build system <distmaker@roxen.com>
2008-04-04 14:31:54 +0200

    release number bumped to 534 by export.pike
    Rev: packaging/debian/changelog:1.559
    Rev: src/version.h:1.865

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.864 2008/04/04 07:53:44 distmaker Exp $
+|| $Id: version.h,v 1.865 2008/04/04 12:31:53 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 533
+#define PIKE_BUILD_VERSION 534
 
 /* Prototypes begin here */
 void f_version(INT32 args);
