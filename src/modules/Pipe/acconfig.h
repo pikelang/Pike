0335d8d00656cb9cbcbdfb2129385148e8c293ec
Stephen R. van den Berg <srb@cuci.nl>
2008-04-18 12:36:33 +0200

    Bringing back files from the Attic is more complicated than I thought
    Rev: src/modules/Pipe/.cvsignore:1.8
    Rev: src/modules/Pipe/Makefile.in:1.8
    Rev: src/modules/Pipe/acconfig.h:1.5
    Rev: src/modules/Pipe/configure.in:1.9
    Rev: src/modules/Pipe/pipe.c:1.62
    Rev: src/modules/Pipe/testsuite.in:1.3

---
+++ src/modules/Pipe/acconfig.h
@@ -0,0 +1,14 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.5 2008/04/18 10:36:33 srb Exp $
+*/
+
+#ifndef PIPE_MACHINE_H
+#define PIPE_MACHINE_H
+
+@TOP@
+@BOTTOM@
+
+#endif
