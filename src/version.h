b01245ac33a6bdb9b27aa6414f2aeecd912bcbd0
Peter Bortas <zino@lysator.liu.se>
2008-05-04 03:20:50 +0200

    CVS versions should be odd numbers. Always bumb to builds if you are doing a manual marker.
    Rev: src/version.h:1.400

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.399 2008/04/02 14:05:23 mast Exp $
+|| $Id: version.h,v 1.400 2008/05/04 01:20:50 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 44
+#define PIKE_BUILD_VERSION 45
 
 /* Prototypes begin here */
 void f_version(INT32 args);
