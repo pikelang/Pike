b3f673605358d2036667b566946e54927549fd6b
Martin Stjernholm <mast@lysator.liu.se>
2008-05-11 04:38:27 +0200

    Updated to the f_count_memory interface.
    Rev: src/modules/Gmp/mpf.cmod:1.36
    Rev: src/modules/Gmp/mpq.cmod:1.28
    Rev: src/modules/Gmp/mpz_glue.c:1.179

--- src/modules/Gmp/mpz_glue.c
+++ src/modules/Gmp/mpz_glue.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: mpz_glue.c,v 1.178 2008/05/02 04:15:18 mast Exp $
+|| $Id: mpz_glue.c,v 1.179 2008/05/11 02:38:27 mast Exp $
 */
 
 #include "global.h"
@@ -2151,8 +2151,8 @@ static void exit_mpz_glue(struct object *o)
 
 static void gc_recurse_mpz (struct object *o)
 {
-  if (Pike_in_gc == GC_PASS_COUNT_MEMORY)
-    gc_counted_bytes +=
+  if (mc_count_bytes (o))
+    mc_counted_bytes +=
 #ifdef MPZ_T_HAS__MP_ALLOC
       THIS[0]._mp_alloc * sizeof (mp_limb_t) +
 #else
