e8d990ca33c0721115526b3e62c5c56d752c948d
Martin Stjernholm <mast@lysator.liu.se>
2008-05-12 00:42:14 +0200

    Fixed a warning in non-rtldebug mode.
    Rev: src/iterators.cmod:1.67
    Rev: src/sscanf.c:1.173

--- src/sscanf.c
+++ src/sscanf.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: sscanf.c,v 1.172 2008/05/03 15:29:25 nilsson Exp $
+|| $Id: sscanf.c,v 1.173 2008/05/11 22:41:03 mast Exp $
 */
 
 #include "global.h"
@@ -1335,6 +1335,12 @@ INT32 low_sscanf(struct pike_string *data, struct pike_string *format)
   check_c_stack(sizeof(struct sscanf_set)*2 + 512);
 
   switch(data->size_shift*3 + format->size_shift) {
+  default:
+#ifdef PIKE_DEBUG
+    Pike_fatal("Unsupported shift-combination to low_sscanf(): %d:%d\n",
+	       data->size_shift, format->size_shift);
+    break;
+#endif
     /* input_shift : match_shift */
   case 0:
     /*      0      :      0 */
@@ -1390,12 +1396,6 @@ INT32 low_sscanf(struct pike_string *data, struct pike_string *format)
 			    STR2(format), format->len,
 			    &matched_chars, &x);
     break;
-#ifdef PIKE_DEBUG
-  default:
-    Pike_fatal("Unsupported shift-combination to low_sscanf(): %d:%d\n",
-	       data->size_shift, format->size_shift);
-    break;
-#endif
   }
   return i;
 }
