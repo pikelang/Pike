626c31535dffad1f1356ba9c8f2f8327bcbcf23a
Martin Stjernholm <mast@lysator.liu.se>
2008-06-03 16:55:04 +0200

    Added GSS-API v2 glue.
    Rev: src/post_modules/GSSAPI/Makefile.in:1.1
    Rev: src/post_modules/GSSAPI/acconfig.h:1.1
    Rev: src/post_modules/GSSAPI/configure.in:1.1
    Rev: src/post_modules/GSSAPI/gssapi.cmod:1.1

---
+++ src/post_modules/GSSAPI/acconfig.h
@@ -0,0 +1,8 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.1 2008/06/03 14:55:04 mast Exp $
+*/
+
+#undef HAVE_GSSAPI
