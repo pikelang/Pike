39d6a2d3413c256203855c91c8c661c74ade1973
Peter Bortas <zino@lysator.liu.se>
2008-07-16 15:52:48 +0200

    Fixed broken CVS version.
    Rev: src/version.h:1.453

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.452 2008/07/16 12:43:08 distmaker Exp $
+|| $Id: version.h,v 1.453 2008/07/16 13:52:48 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 98
+#define PIKE_BUILD_VERSION 99
 
 /* Prototypes begin here */
 void f_version(INT32 args);
