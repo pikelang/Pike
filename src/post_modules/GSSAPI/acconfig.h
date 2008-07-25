2ac949ffdca90fd8a20c25050afa939991cb022c
Martin Stjernholm <mast@lysator.liu.se>
2008-07-25 16:37:48 +0200

    Bad idea to remove acconfig.h - autoheader uses the one in the src dir then.
    Rev: src/post_modules/GSSAPI/acconfig.h:1.4

---
+++ src/post_modules/GSSAPI/acconfig.h
@@ -0,0 +1,6 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id: acconfig.h,v 1.4 2008/07/25 14:37:48 mast Exp $
+*/
