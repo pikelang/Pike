aca9c94c91bf510da7dd11aa55be8e6a5e582c2b
Martin Stjernholm <mast@lysator.liu.se>
2008-07-25 16:25:07 +0200

    Removed the need for acconfig.h altogether.
    Rev: src/post_modules/GSSAPI/acconfig.h:1.3(DEAD)
    Rev: src/post_modules/GSSAPI/configure.in:1.4

--- src/post_modules/GSSAPI/acconfig.h
+++
@@ -1,9 +0,0 @@
-/*
-|| This file is part of Pike. For copyright information see COPYRIGHT.
-|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
-|| for more information.
-|| $Id: acconfig.h,v 1.2 2008/07/23 21:45:31 grubba Exp $
-*/
-
-/* Define if you have a working GSSAPI. */
-#undef HAVE_GSSAPI
