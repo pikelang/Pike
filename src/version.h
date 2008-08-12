56673b9e1f44d4a6c811de0732d26ad384674059
Peter Bortas <zino@lysator.liu.se>
2008-08-12 03:02:22 +0200

    7.8 please.
    Rev: src/version.h:1.512

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.511 2008/08/08 16:35:29 distmaker Exp $
+|| $Id: version.h,v 1.512 2008/08/12 01:02:22 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
-#define PIKE_MINOR_VERSION 7
-#define PIKE_BUILD_VERSION 157
+#define PIKE_MINOR_VERSION 8
+#define PIKE_BUILD_VERSION 1
 
 /* Prototypes begin here */
 void f_version(INT32 args);
