148ec62f1987f17c1b4cead5a70d280f311c7dc8
Martin Stjernholm <mast@lysator.liu.se>
2008-09-08 15:44:14 +0200

    Fixed a mutex init bug when pthread isn't used (e.g. on Windows).
    Rev: src/modules/Odbc/odbc.c:1.47

--- src/modules/Odbc/odbc.c
+++ src/modules/Odbc/odbc.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: odbc.c,v 1.46 2008/01/29 16:13:29 mast Exp $
+|| $Id: odbc.c,v 1.47 2008/09/08 13:44:14 mast Exp $
 */
 
 /*
@@ -21,7 +21,7 @@
 #include "config.h"
 #endif /* HAVE_CONFIG_H */
 
-RCSID("$Id: odbc.c,v 1.46 2008/01/29 16:13:29 mast Exp $");
+RCSID("$Id: odbc.c,v 1.47 2008/09/08 13:44:14 mast Exp $");
 
 #include "interpret.h"
 #include "object.h"
@@ -587,6 +587,10 @@ PIKE_MODULE_INIT
     /*    Pike_error("odbc_module_init(): SQLAllocEnv() failed with code %08x\n", err); */
   }
 
+#if defined (PIKE_THREADS) && !defined (HAS_STATIC_MUTEX_INIT)
+  mt_init (&connect_mutex);
+#endif
+
   start_new_program();
   ADD_STORAGE(struct precompiled_odbc);
 
@@ -652,6 +656,10 @@ PIKE_MODULE_EXIT
 {
 #ifdef HAVE_ODBC
   exit_odbc_res();
+
+#if defined (PIKE_THREADS) && !defined (HAS_STATIC_MUTEX_INIT)
+  mt_destroy (&connect_mutex);
+#endif
  
   if (odbc_program) {
     free_program(odbc_program);
