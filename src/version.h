b76cf9bbd04e4ac0d55f65452e411d3278dfe6ef
Peter Bortas <zino@lysator.liu.se>
2008-11-23 23:33:57 +0100

    Completing version bump manually after network failure.
    Rev: src/version.h:1.582

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.581 2008/11/23 21:29:44 peter Exp $
+|| $Id: version.h,v 1.582 2008/11/23 22:33:57 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 70
+#define PIKE_BUILD_VERSION 71
 
 /* Prototypes begin here */
 void f_version(INT32 args);
