c5e2f3e7cc4059772f29d3c0300584a3f523951d
Build system <distmaker@roxen.com>
2008-12-01 16:13:51 +0100

    release number bumped to 82 by export.pike
    Rev: src/version.h:1.593

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.592 2008/12/01 01:43:00 peter Exp $
+|| $Id: version.h,v 1.593 2008/12/01 15:13:51 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 81
+#define PIKE_BUILD_VERSION 82
 
 /* Prototypes begin here */
 void f_version(INT32 args);
