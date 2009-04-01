cdc814018101b93c18a791e36860b66814e68421
Build system <distmaker@roxen.com>
2009-04-01 08:38:16 +0200

    release number bumped to 228 by export.pike
    Rev: src/version.h:1.739

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.738 2009/03/31 09:43:18 distmaker Exp $
+|| $Id: version.h,v 1.739 2009/04/01 06:38:16 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 227
+#define PIKE_BUILD_VERSION 228
 
 /* Prototypes begin here */
 void f_version(INT32 args);
