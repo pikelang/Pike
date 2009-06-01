a8fc363738232120779e6d451e867a4dd4c05d4d
Lance Dillon <riffraff169@yahoo.com>
2009-06-01 14:48:22 -0500

    * adding support for GTK 2.14 and GTK 2.16
    Rev: src/post_modules/GTK2/acconfig.h:1.11
    Rev: src/post_modules/GTK2/configure.in:1.24

--- src/post_modules/GTK2/acconfig.h
+++ src/post_modules/GTK2/acconfig.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: acconfig.h,v 1.10 2007/07/09 02:45:46 ldillon Exp $
+|| $Id: acconfig.h,v 1.11 2009/06/01 19:48:22 ldillon Exp $
 */
 
 #undef FUNCPROTO
@@ -15,6 +15,8 @@
 #undef HAVE_GTK28
 #undef HAVE_GTK210
 #undef HAVE_GTK212
+#undef HAVE_GTK214
+#undef HAVE_GTK216
 #undef HAVE_ATK
 #undef HAVE_ATK18
 #undef HAVE_PANGO
