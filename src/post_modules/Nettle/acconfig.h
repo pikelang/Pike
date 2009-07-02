4df9fb491c8b022e996b0a3eddbda75a87b229db
Henrik Grubbström (Grubba) <grubba@grubba.org>
2009-07-02 18:35:38 +0200

    More Nettle 2.0 related fixes. Added check for whether the nettle_crypt_func typedef is a pointer type or not.
    Rev: src/post_modules/Nettle/acconfig.h:1.3
    Rev: src/post_modules/Nettle/cipher.cmod:1.41
    Rev: src/post_modules/Nettle/configure.in:1.23

--- src/post_modules/Nettle/acconfig.h
+++ src/post_modules/Nettle/acconfig.h
@@ -1,2 +1,5 @@
 /* Define this if your struct yarrow256_ctx has the field seed_file. */
 #undef HAVE_STRUCT_YARROW256_CTX_SEED_FILE
+
+/* Define this if the nettle_crypt_func typedef is a pointer type. */
+#undef HAVE_NETTLE_CRYPT_FUNC_IS_POINTER
