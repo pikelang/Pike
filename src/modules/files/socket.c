caa2e4cb9a212986fbd22a9c72d925254c2db440
Henrik Grubbström (Grubba) <grubba@grubba.org>
2009-07-23 17:20:06 +0200

    Fixed typo.
    Rev: src/modules/files/socket.c:1.106

--- src/modules/files/socket.c
+++ src/modules/files/socket.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: socket.c,v 1.105 2009/07/23 15:19:44 grubba Exp $
+|| $Id: socket.c,v 1.106 2009/07/23 15:20:06 grubba Exp $
 */
 
 #define NO_PIKE_SHORTHAND
@@ -364,7 +364,7 @@ static void unix_bind(INT32 args)
   addr->sun_family = AF_UNIX;
 #ifdef HAVE_STRUCT_SOCKADDR_UN_SUN_LEN
   /* Length including NUL. */
-  name->sun_len = path->len + 1;
+  addr->sun_len = path->len + 1;
 #endif
 
   fd=fd_socket(AF_UNIX, SOCK_STREAM, 0);
