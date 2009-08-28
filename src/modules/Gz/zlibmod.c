644dc835a134c351c9d03da0b3d73d511ce50037
Martin Stjernholm <mast@lysator.liu.se>
2009-08-28 15:30:53 +0200

    Fixed stray lock if deflate/inflate throws an error (typically oom).
    Rev: src/modules/Gz/zlibmod.c:1.67

--- src/modules/Gz/zlibmod.c
+++ src/modules/Gz/zlibmod.c
@@ -2,11 +2,11 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: zlibmod.c,v 1.66 2004/03/19 14:36:24 nilsson Exp $
+|| $Id: zlibmod.c,v 1.67 2009/08/28 13:30:53 mast Exp $
 */
 
 #include "global.h"
-RCSID("$Id: zlibmod.c,v 1.66 2004/03/19 14:36:24 nilsson Exp $");
+RCSID("$Id: zlibmod.c,v 1.67 2009/08/28 13:30:53 mast Exp $");
 
 #include "zlib_machine.h"
 #include "module.h"
@@ -168,15 +168,23 @@ static void gz_deflate_create(INT32 args)
   }
 }
 
+static void do_mt_unlock (PIKE_MUTEX_T *lock)
+{
+  mt_unlock (lock);
+}
+
 static int do_deflate(dynamic_buffer *buf,
 		      struct zipper *this,
 		      int flush)
 {
    int ret=0;
+   ONERROR uwp;
 
    THREADS_ALLOW();
    mt_lock(& this->lock);
    THREADS_DISALLOW();
+   SET_ONERROR (uwp, do_mt_unlock, &this->lock);
+
    if(!this->gz.state)
       ret=Z_STREAM_ERROR;
    else
@@ -201,7 +209,7 @@ static int do_deflate(dynamic_buffer *buf,
       }
       while (ret==Z_OK && (this->gz.avail_in || !this->gz.avail_out));
 
-   mt_unlock(& this->lock);
+   CALL_AND_UNSET_ONERROR (uwp);
    return ret;
 }
 
@@ -411,9 +419,13 @@ static int do_inflate(dynamic_buffer *buf,
 		      int flush)
 {
   int fail=0;
+  ONERROR uwp;
+
   THREADS_ALLOW();
   mt_lock(& this->lock);
   THREADS_DISALLOW();
+  SET_ONERROR (uwp, do_mt_unlock, &this->lock);
+
   if(!this->gz.state)
   {
     fail=Z_STREAM_ERROR;
@@ -462,7 +474,8 @@ static int do_inflate(dynamic_buffer *buf,
       }
     } while(!this->gz.avail_out || flush==Z_FINISH || this->gz.avail_in);
   }
-  mt_unlock(& this->lock);
+
+  CALL_AND_UNSET_ONERROR (uwp);
   return fail;
 }
 
