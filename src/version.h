ce028d2431c4dc08d1ce08c86c72413efeadcbcf
Peter Bortas <zino@lysator.liu.se>
2009-08-29 15:12:21 +0200

    release number bumped to 344 by export.pike
    Rev: src/version.h:1.854

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.853 2009/08/26 17:10:07 peter Exp $
+|| $Id: version.h,v 1.854 2009/08/29 13:12:21 peter Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 343
+#define PIKE_BUILD_VERSION 344
 
 /* Prototypes begin here */
 void f_version(INT32 args);
