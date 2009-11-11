7cd6faa3f4640a0cab67aa6247b04c96fd606c1b
Henrik Grubbström (Grubba) <grubba@grubba.org>
2009-11-11 13:53:49 +0100

    Added SqlNULL_object.
    Rev: src/builtin.cmod:1.238
    Rev: src/builtin_functions.h:1.39

--- src/builtin_functions.h
+++ src/builtin_functions.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: builtin_functions.h,v 1.38 2009/11/09 14:33:02 grubba Exp $
+|| $Id: builtin_functions.h,v 1.39 2009/11/11 12:53:49 grubba Exp $
 */
 
 #ifndef BUILTIN_EFUNS_H
@@ -46,6 +46,7 @@ struct replace_many_context
   int flags;
 };
 
+extern struct object *SqlNULL_object;
 extern struct program *SqlNull_program;
 
 PMOD_EXPORT void f_equal(INT32 args);
