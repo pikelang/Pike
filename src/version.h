212294fa531f064340d5b52b9d772b04002b5bb1
Build system <distmaker@roxen.com>
2010-01-25 11:11:57 +0100

    release number bumped to 396 by export.pike
    Rev: src/version.h:1.906

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.905 2010/01/21 10:05:34 distmaker Exp $
+|| $Id: version.h,v 1.906 2010/01/25 10:11:57 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 395
+#define PIKE_BUILD_VERSION 396
 
 /* Prototypes begin here */
 void f_version(INT32 args);
