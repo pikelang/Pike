76c8b0dd0eb6e4eed9062e56b929c5ed5781114c
Build system <distmaker@roxen.com>
2010-03-03 15:00:55 +0100

    release number bumped to 639 by export.pike
    Rev: packaging/debian/changelog:1.664
    Rev: src/version.h:1.970

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.969 2010/03/03 13:57:51 distmaker Exp $
+|| $Id: version.h,v 1.970 2010/03/03 14:00:52 distmaker Exp $
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 4
-#define PIKE_BUILD_VERSION 638
+#define PIKE_BUILD_VERSION 639
 
 /* Prototypes begin here */
 void f_version(INT32 args);
