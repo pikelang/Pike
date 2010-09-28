38cf095365db7cd1a090f87821d9eef8cfa5f36a
Henrik Grubbström (Grubba) <grubba@grubba.org>
2010-09-28 18:13:51 +0200

    debug_locking_thread needs to be PMOD_EXPORT to keep WIN32 happy.
    Rev: src/pike_threadlib.h:1.58

--- src/pike_threadlib.h
+++ src/pike_threadlib.h
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: pike_threadlib.h,v 1.57 2010/09/27 14:23:54 grubba Exp $
+|| $Id: pike_threadlib.h,v 1.58 2010/09/28 16:13:51 grubba Exp $
 */
 
 #ifndef PIKE_THREADLIB_H
@@ -504,7 +504,7 @@ PMOD_EXPORT extern clock_t thread_start_clock;
 /* This is a debug wrapper to enable checks that the interpreter lock
  * is hold by the current thread. */
 
-extern THREAD_T debug_locking_thread;
+PMOD_EXPORT extern THREAD_T debug_locking_thread;
 #define SET_LOCKING_THREAD (debug_locking_thread = th_self(), 0)
 
 #define low_mt_lock_interpreter()					\
