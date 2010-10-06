52081eac076838d12a4806727ab6dd8e8a2949c0
Henrik Grubbström (Grubba) <grubba@grubba.org>
2010-10-06 15:10:48 +0200

    release number bumped to 482 by export.pike

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: version.h,v 1.991 2010/09/28 10:04:19 distmaker Exp $
+|| $Id$
 */
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 481
+#define PIKE_BUILD_VERSION 482
 
 /* Prototypes begin here */
 void f_version(INT32 args);
