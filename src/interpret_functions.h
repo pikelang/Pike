c5bd7d6d14b5d6636add7dbce9b45d66b052fb5c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2010-11-19 16:11:24 +0100

    Fixed over-caching issue in F_COND_RECUR.

--- src/interpret_functions.h
+++ src/interpret_functions.h
@@ -2574,7 +2574,7 @@ OPCODE1_PTRJUMP(F_COND_RECUR, "recur if not overloaded", I_UPDATE_ALL, {
    *
    *	/grubba 2003-03-25
    */
-  if(((p != Pike_fp->context->prog) &&
+  if(((p != Pike_fp->context->prog) ||
       (Pike_fp->context !=
        &p->inherits[p->identifier_references[Pike_fp->context->identifier_level +
 					     arg1].inherit_offset])) ||
