pikelang/Pike

commit a833e6022e7837070616e01d2d913a18db2511b8

Improved typed mode for DECIMAL fields.
They are now returned as integers if they are declared to have no decimals,
otherwise they are returned as Gmp.mpq objects.

Compatibility note: This (along with the new handling of BIT fields) is an
incompatible change in typed mode, but there's probably not much code that
uses it yet, and there has been no official release with the old behavior.
It has been around in git for 1½ years, though.

src/modules/Mysql/result.c
@@ -111,6 +111,8 @@
 
 struct program *mysql_result_program = NULL;
 
+static struct svalue mpq_program = SVALUE_INIT_FREE;
+
 /*
  * Functions
  */
@@ -637,10 +639,56 @@ static void f_fetch_row(INT32 args)
 	  case FIELD_TYPE_DOUBLE:
 	    push_float(atof(row[i]));
 	    break;
+
 	  case FIELD_TYPE_DECIMAL:
 	  case FIELD_TYPE_NEWDECIMAL:
-	    /* Fixed-point number. */
-	    /* FIXME: Convert to Gmp.mpr? */
+	    if (!field->decimals) {
+#ifdef AUTO_BIGNUM
+	      if (
+#ifdef HAVE_MYSQL_FETCH_LENGTHS
+		row_lengths[i]
+#else
+		strlen(row[i])
+#endif
+		>= 10
+	      ) {
+#ifdef HAVE_MYSQL_FETCH_LENGTHS
+		push_string(make_shared_binary_string(row[i], row_lengths[i]));
+#else
+		push_text(row[i]);
+#endif
+		convert_stack_top_string_to_inumber(10);
+		break;
+	      }
+#endif
+	      push_int(STRTOL(row[i], 0, 10));
+	      break;
+	    }
+
+	    /* Fixed-point number with fraction part. Make an mpq. */
+
+	    if (mpq_program.type == PIKE_T_FREE) {
+	      push_text ("Gmp.mpq");
+	      SAFE_APPLY_MASTER ("resolv", 1);
+	      if (Pike_sp[-1].type == T_PROGRAM)
+		move_svalue (&mpq_program, --Pike_sp);
+	      else {
+		pop_stack();
+		mpq_program.type = T_INT;
+	      }
+	    }
+
+	    if (mpq_program.type = T_PROGRAM) {
+#ifdef HAVE_MYSQL_FETCH_LENGTHS
+	      push_string(make_shared_binary_string(row[i], row_lengths[i]));
+#else
+	      push_text(row[i]);
+#endif
+	      apply_svalue (&mpq_program, 1);
+	      break;
+	    }
+	    /* FALL_THROUGH */
+
 	  default:
 #ifdef HAVE_MYSQL_FETCH_LENGTHS
 	    push_string(make_shared_binary_string(row[i], row_lengths[i]));

commit 6cf6d03cabfec3012918b1f95fe95106bc43131d

Fixed refcount bug introduced in a833e60.

src/modules/Mysql/result.c
@@ -905,6 +905,7 @@ void exit_mysql_res(void)
     free_program(mysql_result_program);
     mysql_result_program = NULL;
   }
+  free_svalue (&mpq_program);
 }
 
 #else