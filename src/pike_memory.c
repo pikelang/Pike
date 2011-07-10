c06198277aaad0425aebb6a481d68ba3443d8b62
Marcus Comstedt <marcus@mc.pp.se>
2011-07-11 00:47:01 +0200

    DO_HASHMEM is used on all arches, so don't define it for x86 only...

--- src/pike_memory.c
+++ src/pike_memory.c
@@ -185,6 +185,78 @@ void reorder(char *memory, INT32 nitems, INT32 size,INT32 *order)
   free(tmp);
 }
 
+/* MLEN is the length of the longest prefix of A to use for hashing.
+ * (If A is longer then additionally some bytes at the end are
+ * included.) */
+/* NB: RET should be an lvalue of type size_t. */
+#define DO_HASHMEM(RET, A, LEN, MLEN)			\
+  do {							\
+    const unsigned char *a = A;				\
+    size_t len = LEN;					\
+    size_t mlen = MLEN;					\
+    size_t ret;						\
+  							\
+    ret = 9248339*len;					\
+    if(len<=mlen)					\
+      mlen=len;						\
+    else						\
+    {							\
+      switch(len-mlen)					\
+      {							\
+  	default: ret^=(ret<<6) + a[len-7];		\
+  	case 7:						\
+  	case 6: ret^=(ret<<7) + a[len-5];		\
+  	case 5:						\
+  	case 4: ret^=(ret<<4) + a[len-4];		\
+  	case 3: ret^=(ret<<3) + a[len-3];		\
+  	case 2: ret^=(ret<<3) + a[len-2];		\
+  	case 1: ret^=(ret<<3) + a[len-1];		\
+      }							\
+    }							\
+    a += mlen & 7;					\
+    switch(mlen&7)					\
+    {							\
+      case 7: ret^=a[-7];				\
+      case 6: ret^=(ret<<4)+a[-6];			\
+      case 5: ret^=(ret<<7)+a[-5];			\
+      case 4: ret^=(ret<<6)+a[-4];			\
+      case 3: ret^=(ret<<3)+a[-3];			\
+      case 2: ret^=(ret<<7)+a[-2];			\
+      case 1: ret^=(ret<<5)+a[-1];			\
+    }							\
+  							\
+    DO_IF_ELSE_UNALIGNED_MEMORY_ACCESS(			\
+      {							\
+  	size_t *b;					\
+  	b=(size_t *)a;					\
+    							\
+  	for(DIVIDE_BY_2_CHAR_P(mlen);mlen--;)		\
+  	{						\
+  	  ret^=(ret<<7)+*(b++);				\
+  	  ret^=(ret>>6)+*(b++);				\
+  	}						\
+      }							\
+    ,							\
+      for(mlen >>= 3; mlen--;)				\
+      {							\
+  	register size_t t1;				\
+  	register size_t t2;				\
+  	t1= a[0];					\
+  	t2= a[1];					\
+  	t1=(t1<<5) + a[2];				\
+  	t2=(t2<<4) + a[3];				\
+  	t1=(t1<<7) + a[4];				\
+  	t2=(t2<<5) + a[5];				\
+  	t1=(t1<<3) + a[6];				\
+  	t2=(t2<<4) + a[7];				\
+  	a += 8;						\
+  	ret^=(ret<<7) + (ret>>6) + t1 + (t2<<6);	\
+      }							\
+    )							\
+  							\
+    RET = ret;						\
+  } while(0)
+
 #if (defined(__i386__) || defined(__amd64__)) && defined(__GNUC__)
 /*
  * This would look much better using the compiler intrinsics, or even the
@@ -322,79 +394,6 @@ static inline size_t hashmem_ia32_crc32( const void *s, size_t len, size_t nbyte
 #define DIVIDE_BY_2_CHAR_P(X)	(X /= 2*sizeof(size_t))
 #endif /* sizeof(char *) == 8 */
 #endif /* sizeof(char *) == 4 */
-
-/* MLEN is the length of the longest prefix of A to use for hashing.
- * (If A is longer then additionally some bytes at the end are
- * included.) */
-/* NB: RET should be an lvalue of type size_t. */
-#define DO_HASHMEM(RET, A, LEN, MLEN)			\
-  do {							\
-    const unsigned char *a = A;				\
-    size_t len = LEN;					\
-    size_t mlen = MLEN;					\
-    size_t ret;						\
-  							\
-    ret = 9248339*len;					\
-    if(len<=mlen)					\
-      mlen=len;						\
-    else						\
-    {							\
-      switch(len-mlen)					\
-      {							\
-  	default: ret^=(ret<<6) + a[len-7];		\
-  	case 7:						\
-  	case 6: ret^=(ret<<7) + a[len-5];		\
-  	case 5:						\
-  	case 4: ret^=(ret<<4) + a[len-4];		\
-  	case 3: ret^=(ret<<3) + a[len-3];		\
-  	case 2: ret^=(ret<<3) + a[len-2];		\
-  	case 1: ret^=(ret<<3) + a[len-1];		\
-      }							\
-    }							\
-    a += mlen & 7;					\
-    switch(mlen&7)					\
-    {							\
-      case 7: ret^=a[-7];				\
-      case 6: ret^=(ret<<4)+a[-6];			\
-      case 5: ret^=(ret<<7)+a[-5];			\
-      case 4: ret^=(ret<<6)+a[-4];			\
-      case 3: ret^=(ret<<3)+a[-3];			\
-      case 2: ret^=(ret<<7)+a[-2];			\
-      case 1: ret^=(ret<<5)+a[-1];			\
-    }							\
-  							\
-    DO_IF_ELSE_UNALIGNED_MEMORY_ACCESS(			\
-      {							\
-  	size_t *b;					\
-  	b=(size_t *)a;					\
-    							\
-  	for(DIVIDE_BY_2_CHAR_P(mlen);mlen--;)		\
-  	{						\
-  	  ret^=(ret<<7)+*(b++);				\
-  	  ret^=(ret>>6)+*(b++);				\
-  	}						\
-      }							\
-    ,							\
-      for(mlen >>= 3; mlen--;)				\
-      {							\
-  	register size_t t1;				\
-  	register size_t t2;				\
-  	t1= a[0];					\
-  	t2= a[1];					\
-  	t1=(t1<<5) + a[2];				\
-  	t2=(t2<<4) + a[3];				\
-  	t1=(t1<<7) + a[4];				\
-  	t2=(t2<<5) + a[5];				\
-  	t1=(t1<<3) + a[6];				\
-  	t2=(t2<<4) + a[7];				\
-  	a += 8;						\
-  	ret^=(ret<<7) + (ret>>6) + t1 + (t2<<6);	\
-      }							\
-    )							\
-  							\
-    RET = ret;						\
-  } while(0)
-
 
 #ifdef __i386__
 __attribute__((fastcall))
