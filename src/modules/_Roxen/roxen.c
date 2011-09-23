ec70a8e79fd16887491012abdcc7f86487313e74
Martin Stjernholm <mast@lysator.liu.se>
2011-09-23 11:33:55 +0200

    Revert encoding change in _Roxen.html_encode_string.
    The risk for compat trouble is too big.
    
    This reverts commit bf2661f6.

--- src/modules/_Roxen/roxen.c
+++ src/modules/_Roxen/roxen.c
@@ -541,7 +541,6 @@ static void f_html_encode_string( INT32 args )
 	case '>': newlen+=3; break;/* &gt; 	*/			\
 	case '&': 		   /* &amp;	*/			\
 	case '"': 		   /* &#34;	*/			\
-        case '=':		   /* &#61;	*/			\
 	case '\'': newlen+=4;break;/* &#39;	*/		  	\
       }									\
     }
@@ -567,7 +566,6 @@ static void f_html_encode_string( INT32 args )
 	case '>': ADD("&gt;");  break;					\
 	case '"': ADD("&#34;"); break;					\
 	case '\'':ADD("&#39;"); break;					\
-        case '=': ADD("&#61;"); break;					\
 	default: *d = *s;   break;					\
       }									\
   }									\
