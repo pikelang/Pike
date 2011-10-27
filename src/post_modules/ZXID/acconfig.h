bf8ec9500deebc67f1cfd6823a8da0dcecaf9698
Henrik Grubbström (Grubba) <grubba@grubba.org>
2011-10-27 14:43:14 +0200

    ZXID: Initial implementation.

---
+++ src/post_modules/ZXID/acconfig.h
@@ -0,0 +1,17 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+|| $Id$
+*/
+
+#ifndef ZXID_CONFIG_H
+#define ZXID_CONFIG_H
+
+@TOP@
+@BOTTOM@
+
+/* Define this if you want the ZXID module. */
+#undef HAVE_ZXID
+
+#endif
