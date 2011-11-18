edf308897de31b529c5b9509c9af37d2e2714945
Build system <distmaker@roxen.com>
2011-11-18 15:23:28 +0100

    release number bumped to 595 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 594
+#define PIKE_BUILD_VERSION 595
 
 /* Prototypes begin here */
 void f_version(INT32 args);
