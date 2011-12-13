3107f5b7f463e67d4fec96d70fea458b5a00cb30
James Harton <jamesotron@gmail.com>
2011-12-14 10:56:09 +1300

    Rewriting the sync script to be a little smarter.

---
+++ sync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# setup:
+#   git init
+#   git remote add --mirror -f origin git://pike-git.lysator.liu.se/pike
+#   git remote add -f github git@github.com:pikelang/Pike.git
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+if [ ! -e ".git" ]; then
+  echo git init
+fi
+
+if [ "x1" == "x`git remote -v  | egrep -c "^origin\s+git://pike-git.lysator.liu.se/pike\s+\(push\)$"`" ]; then
+  echo git remote add --mirror -f origin git://pike-git.lysator.liu.se/pike
+fi
+
+if [ "x1" == "x`git remote -v  | egrep -c "^github\s+git@github.com:pikelang/Pike.git\s+\(push\)$"`" ]; then
+  echo git remote add -f github git@github.com:pikelang/Pike.git
+fi
+
+git fetch -q origin
+git fetch -q github
+git checkout github_sync
+git push -q --mirror github
+
+cd -
+
