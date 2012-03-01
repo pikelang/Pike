8ff2017cf08afbdf4ddb545eb4e1d7424969e96b
Build system <distmaker@roxen.com>
2012-03-01 15:39:00 +0100

    release number bumped to 642 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 641
+#define PIKE_BUILD_VERSION 642
 
 /* Prototypes begin here */
 void f_version(INT32 args);
