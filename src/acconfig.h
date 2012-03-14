3e4a7e22c282447f88e4415aa1c78c8579a5387e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2012-03-14 15:37:34 +0100

    Added prototype for mallinfo(). Fixes [bug 6275] for WIN32.

--- src/acconfig.h
+++ src/acconfig.h
@@ -675,6 +675,8 @@ struct mallinfo {
   MALLINFO_FIELD_TYPE keepcost; /* releasable (via malloc_trim) space */
 };
 
+struct mallinfo dlmallinfo(void);
+
 #endif /* HAVE_USR_INCLUDE_MALLOC_H */
 
 #endif
