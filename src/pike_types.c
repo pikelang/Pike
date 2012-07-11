e4b9a45798c45872ebf540b776fbc2b2bc5fda12
Henrik Grubbström (Grubba) <grubba@grubba.org>
2012-07-11 18:54:01 +0200

    Compiler: Improved robustness in new_check_call().

--- src/pike_types.c
+++ src/pike_types.c
@@ -7217,7 +7217,7 @@ struct pike_type *new_check_call(struct pike_string *fun_name,
 	prev->parent = orig_arg_parent;
 	break;
       }
-      if ((CAR(args) == prev) && CDR(args)) {
+      if ((CDR(args) != prev) && CDR(args)) {
 	/* NOTE: The above test is NOT SHARED_NODES safe! */
 	if (!--num_cdr) {
 	  flags = orig_flags | (num_lvalue?CALL_ARG_LVALUE:0);
