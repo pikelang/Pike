2376c221c05378afcfbc02b19ea85cc42ad7667c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2012-08-31 17:05:26 +0200

    Java: Fixed broken config-test on NT.

--- src/modules/Java/ntdl.c
+++ src/modules/Java/ntdl.c
@@ -13,7 +13,9 @@
  */
 
 #include <tchar.h>
+#ifndef CONFIGURE
 #include "program.h"
+#endif
 
 #define JNI_CreateJavaVM createjavavm
 typedef jint (JNICALL *createjavavmtype)(JavaVM **, void **, void *);
@@ -71,7 +73,9 @@ static int open_nt_dll(void)
     RegCloseKey(key);
   }
   if((jvmdll=LoadLibrary(libname))==NULL) {
+#ifndef CONFIGURE
     yywarning("Failed to load JVM: '%s'\n", libname);
+#endif
     return -1;
   }
   else {
@@ -81,7 +85,9 @@ static int open_nt_dll(void)
     else {
       if(FreeLibrary(jvmdll))
 	jvmdll = NULL;
+#ifndef CONFIGURE
       yywarning("Failed to create JVM.\n");
+#endif
       return -2;
     }
   }
