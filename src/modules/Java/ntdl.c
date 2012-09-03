1a3d135113fca9441368b0c800f57130b08e1f26
Henrik Grubbström (Grubba) <grubba@grubba.org>
2012-09-03 17:20:36 +0200

    Java: Attempt to support loading of Java 6 and 7 on NT.
    cf http://www.duckware.com/tech/java6msvcr71.html

--- src/modules/Java/ntdl.c
+++ src/modules/Java/ntdl.c
@@ -29,10 +29,14 @@ static int open_nt_dll(void)
   TCHAR buffer[2*MAX_PATH+32];
   DWORD type, len = sizeof(buffer)-16;
   DWORD l;
+  HINSTANCE kernel;
+  DWORD (*getdlldir)(DWORD nBufferLength, LPTSTR lpBuffer);
+  BOOL (*setdlldir)(LPCTSTR lpPathname);
   
   l = GetEnvironmentVariable("PIKE_JRE_JVMDLL", buffer, len);
   if (l > 0) {
     libname = buffer;
+    len = l;
   }
   else if(RegOpenKeyEx(HKEY_CURRENT_USER, keyname, 0,
                        KEY_READ, &key) == ERROR_SUCCESS ||
@@ -60,10 +64,12 @@ static int open_nt_dll(void)
 	 case REG_EXPAND_SZ:
 	   {
 	     LPTSTR subbuffer = buffer+((len+1)/sizeof(TCHAR));
-	     type = ExpandEnvironmentStrings(buffer, subbuffer,
+	     l = ExpandEnvironmentStrings(buffer, subbuffer,
 					     sizeof(buffer)-len-2);
-	     if(type && type<=sizeof(buffer)-len-2)
+	     if(l && l <= sizeof(buffer)-len-2) {
 	       libname = subbuffer;
+	       len = l;
+	     }
 	   }
 	   break;
 	}
@@ -71,7 +77,68 @@ static int open_nt_dll(void)
     }
     RegCloseKey(key);
   }
-  if((jvmdll=LoadLibrary(libname))==NULL) {
+
+  /* Java 6 and 7 jvm.dll have dependencies on msvcr71.dll and msvcr100.dll
+   * respectively. They are located in the parent directory of the one
+   * containing jvm.dll. cf http://www.duckware.com/tech/java6msvcr71.html
+   * for details.
+   *
+   * We temporarily truncate the "C:\Program Files\Java\jre\bin\client\jvm.dll"
+   * from the registry to "C:\Program Files\Java\jre\bin", and call
+   * SetDllDirectory() with it.
+   */
+  kernel = GetModuleHandle("kernel32");
+  getdlldir = (void *)GetModuleHandle(kernel, "GetDllDirectoryA");
+  setdlldir = (void *)GetModuleHandle(kernel, "SetDllDirectoryA");
+
+  if (setdlldir) {
+    int cnt = 0;
+
+    for(l = len; l--;) {
+      if (jvmdll[l] == '\\') {
+	/* Go up two directory levels. */
+	if (++cnt == 2) break;
+      }
+    }
+    if (cnt == 2) {
+      TCHAR dlldirbuffer[2*MAX_PATH];
+      LPTSTR origdlldir = NULL;
+
+      libname[l] = 0;
+
+      if (getdlldir) {
+	/* Retain the dlldir load path.
+	 *
+	 * FIXME: This does not take into account the newer
+	 *        AddDllDirectory() API, and will thus lose
+	 *        directories from the dlldir load path if
+	 *        it has been used.
+	 */
+	if ((len = getdlldir(2*MAX_PATH, dlldirbuffer))) {
+	  dlldirbuffer[len] = 0;
+	  origdlldir = dlldirbuffer;
+	}
+      }
+
+      setdlldir(libname);
+
+      /* Restore the zapped diretory separator. */
+      libname[l] = '\\';
+
+      jvmdll = LoadLibrary(libname);
+
+      /* Restore the original dlldir load path. */
+      setdlldir(origdlldir);
+    } else {
+      /* Not enough directory levels. */
+      jvmdll = LoadLibrary(libname);
+    }
+  } else {
+    /* No SetDllDirectory(). */
+    jvmdll = LoadLibrary(libname);
+  }
+
+  if (!jvmdll) {
 #ifndef CONFIGURE
     yywarning("Failed to load JVM: '%s'\n", libname);
 #endif
