e37acaa075485213e0679eaa733ece0f755a68ad
Bill Welliver <bill@welliver.org>
2012-10-19 00:34:23 -0400

    release number bumped to 7.6.133 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 6
-#define PIKE_BUILD_VERSION 132
+#define PIKE_BUILD_VERSION 133
 
 /* Prototypes begin here */
 void f_version(INT32 args);
