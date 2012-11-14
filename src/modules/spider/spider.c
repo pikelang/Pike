f3c83e3703d64ba30d001367dd6f4d7096b3a851
Arne Goedeke <el@laramies.com>
2012-11-14 12:36:54 +0100

    spider: line numbers are now INT_TYPE

--- src/modules/spider/spider.c
+++ src/modules/spider/spider.c
@@ -1042,7 +1042,7 @@ void f_fd_info(INT32 args)
 static void program_name(struct program *p)
 {
   char *f;
-  INT32 n=0;
+  INT_TYPE n=0;
   ref_push_program(p);
   APPLY_MASTER("program_name", 1);
   if(TYPEOF(sp[-1]) == T_STRING)
