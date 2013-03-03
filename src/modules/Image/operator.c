ade7033b465f1d93c9cd1b33ce60e0b00d4685a5
Henrik Grubbström (Grubba) <grubba@grubba.org>
2013-03-03 17:17:06 +0100

    Image: Fixed broken lfun::`<() and lfun::`>().

--- src/modules/Image/operator.c
+++ src/modules/Image/operator.c
@@ -690,7 +690,7 @@ void image_operator_lesser(INT32 args)
    if (s2)
       while (i--)
       {
-	 if (s1->r>=s1->r || s1->g>=s1->g || s1->b>=s1->b) { res=0; break; }
+	 if (s1->r>=s2->r || s1->g>=s2->g || s1->b>=s2->b) { res=0; break; }
 	 s1++; s2++;
       }
    else
@@ -762,7 +762,7 @@ void image_operator_greater(INT32 args)
    if (s2)
       while (i--)
       {
-	 if (s1->r<=s1->r || s1->g<=s1->g || s1->b<=s1->b) { res=0; break; }
+	 if (s1->r<=s2->r || s1->g<=s2->g || s1->b<=s2->b) { res=0; break; }
 	 s1++; s2++;
       }
    else
