b7e55a7be853d1d955b563c8f55a3774ea250a96
Per Hedbor <ph@opera.com>
2013-05-17 14:30:21 +0200

    Explicitly define low_hashmem to be extern.
    Otherwise there is one instance per .so-file, which was not intended.

--- src/pike_memory.h
+++ src/pike_memory.h
@@ -159,11 +159,11 @@ PMOD_EXPORT void reverse(char *memory, size_t nitems, size_t size);
 PMOD_EXPORT void reorder(char *memory, INT32 nitems, INT32 size,INT32 *order);
 
 #if (defined(__i386__) || defined(__amd64__)) && defined(__GNUC__)
-PMOD_EXPORT
+extern PMOD_EXPORT
 #ifdef __i386__
 __attribute__((fastcall)) 
 #endif
- size_t (*low_hashmem)(const void *, size_t, size_t, size_t);
+size_t (*low_hashmem)(const void *, size_t, size_t, size_t);
 #else
 PMOD_EXPORT size_t low_hashmem(const void *, size_t len, size_t mlen, size_t key) ATTRIBUTE((pure));
 #endif
