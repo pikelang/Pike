e0f8a54c00ae58062b355f121f39fcd4bbeb5d6b
Arne Goedeke <el@laramies.com>
2014-01-11 19:30:47 -0500

    program: check lfun number before accesing array
    this led to out of bounds reads when accessing lfuns like _random

--- src/program.c
+++ src/program.c
@@ -6736,16 +6736,22 @@ static void f_dispatch_variant(INT32 args)
 
 PMOD_EXPORT int low_find_lfun(struct program *p, ptrdiff_t lfun)
 {
-  struct pike_string *lfun_name = lfun_strings[lfun];
+  struct pike_string *lfun_name;
   unsigned int flags = 0;
+  int i;
 #if 0
   struct identifier *id;
 #endif
-  int i =
-    really_low_find_shared_string_identifier(lfun_name,
-					     dmalloc_touch(struct program *,
-							   p),
-					     SEE_PROTECTED);
+  if ((size_t)lfun >= NELEM(lfun_strings)) {
+      return -1;
+  }
+
+  lfun_name = lfun_strings[lfun];
+
+  i = really_low_find_shared_string_identifier(lfun_name,
+					       dmalloc_touch(struct program *,
+							     p),
+					       SEE_PROTECTED);
 #if 0
   if (i < 0 || !(p->flags & PROGRAM_FIXED)) return i;
   id = ID_FROM_INT(p, i);
