7df93c15daa2e352ac000dbd22b1013aa70d9954
Per Hedbor <ph@opera.com>
2014-01-13 16:59:50 +0100

    Older versions of nettle (2.0) does not have the qbits argument to generate_dsa_keypair.

--- src/post_modules/Nettle/acconfig.h
+++ src/post_modules/Nettle/acconfig.h
@@ -3,3 +3,6 @@
 
 /* Define this if the nettle_crypt_func typedef is a pointer type. */
 #undef HAVE_NETTLE_CRYPT_FUNC_IS_POINTER
+
+/* Define this if generage_dsa_keypair takes the qbits argument */
+#undef HAVE_DSA_QBITS_KEYPAIR_ARG
