8b3568c32deb4993b55484dc4ce19d8278d34523
Per Hedbor <ph@opera.com>
2014-02-25 12:19:04 +0100

    Fixed warnings by not using deprecated functions.
    This makes g_type_char consistent on x86 and ppc as an example.

--- src/post_modules/GTK2/source/support.c
+++ src/post_modules/GTK2/source/support.c
@@ -543,7 +543,7 @@ static int pgtk2_push_int_param(const GValue *a) {
       retval=(LONGEST)g_value_get_long(a); 
       break;
     case G_TYPE_CHAR:
-      retval=(LONGEST)g_value_get_char(a); 
+      retval=(LONGEST)g_value_get_schar(a); 
       break;
     default:
       retval=(LONGEST)g_value_get_uint(a); 
@@ -1201,7 +1201,7 @@ void pgtk2_set_gvalue(GValue *gv, GType gt, struct svalue *sv) {
       g_value_set_uint(gv,(guint)PGTK_GETINT(sv));
       break;
     case G_TYPE_CHAR:
-      g_value_set_char(gv,(gchar)PGTK_GETINT(sv));
+      g_value_set_schar(gv,(gchar)PGTK_GETINT(sv));
       break;
     case G_TYPE_UCHAR:
       g_value_set_uchar(gv,(guchar)PGTK_GETINT(sv));
