15d407b5078e7f1b45566a1aea0909d419c5eead
Per Hedbor <ph@opera.com>
2014-05-22 01:59:24 +0200

    Added missing acconfig.h

---
+++ src/post_modules/Shuffler/acconfig.h
@@ -0,0 +1,13 @@
+/*
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+*/
+
+#ifndef SHUFFLER_CONFIG_H
+#define SHUFFLER_CONFIG_H
+
+@TOP@
+@BOTTOM@
+
+#endif
