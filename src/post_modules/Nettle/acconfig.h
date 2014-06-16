63d6374cf154ebf04d154d572c51381ea38e80c7
Martin Nilsson <nilsson@opera.com>
2014-06-16 15:48:43 +0200

    Removed optimization of Yarrow seed files for Nettle < 2.0

--- src/post_modules/Nettle/acconfig.h
+++ src/post_modules/Nettle/acconfig.h
@@ -1,6 +1,3 @@
-/* Define this if your struct yarrow256_ctx has the field seed_file. */
-#undef HAVE_STRUCT_YARROW256_CTX_SEED_FILE
-
 /* Define this if the nettle_crypt_func typedef is a pointer type. */
 #undef HAVE_NETTLE_CRYPT_FUNC_IS_POINTER
 
