c4b8521848d0d60203e114eef2c437792797629e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2014-06-24 16:20:58 +0200

    Build: Added lots of missing TYPEOF()s.

--- src/post_modules/CritBit/critbit/value_svalue.h
+++ src/post_modules/CritBit/critbit/value_svalue.h
@@ -12,8 +12,8 @@ typedef struct svalue CB_NAME(value);
 #define CB_SET_VALUE(node, v)	assign_svalue(&((node)->value), (v))
 #define CB_GET_VALUE(node, v)	assign_svalue_no_free((v), &((node)->value))
 #define CB_FREE_VALUE(v)	free_svalue(v)
-#define CB_HAS_VALUE(node)	((node)->value.type != T_VOID)
-#define CB_INIT_VALUE(node)	do { (node)->value.type = T_VOID; } while(0)
+#define CB_HAS_VALUE(node)	(TYPEOF((node)->value) != T_VOID)
+#define CB_INIT_VALUE(node)	do { SET_SVAL_TYPE((node)->value, T_VOID); } while(0)
 #define CB_RM_VALUE(node)	do { if (CB_HAS_VALUE(node)) \
     free_svalue(&((node)->value)); CB_INIT_VALUE(node); } while(0)
 #define CB_PUSH_VALUE(v)	push_svalue(&(v))
