fb3e166fffe72828ef8c81f38ca0adf795d0cd55
Tobias S. Josefowitz <tobij@tobij.de>
2014-07-21 15:34:00 +0200

    [Fuse] remove redundant code around call_with_interpreter()
    Fixes compilation, and removes unneccessary code.

--- src/modules/Fuse/fuse.c
+++ src/modules/Fuse/fuse.c
@@ -459,35 +459,6 @@ static void dispatch_fuse_command( struct fuse *f, struct fuse_cmd *cmd, void *U
 	.f = f,
 	.cmd = cmd
     };
-    struct thread_state *state;
-
-    if((state = thread_state_for_id(th_self()))==NULL)
-    {
-	struct object *thread_obj;
-/*	fprintf( stderr, "Creating a new pike-thread\n");*/
-
-	mt_lock_interpreter();
-        /* Note: LEAKS. Better than a crash.
-
-           Since we never stop the threads anyway, currently, this is
-           less important than it might seem.
-        */
-        Pike_interpreter_pointer = malloc( sizeof( struct Pike_interpreter_struct ) );
-	init_interpreter();
-#ifdef PROFILING
-        Pike_interpreter.stack_bottom=((char *)&x);
-#endif
-	Pike_interpreter.stack_top=((char *)&state) + (thread_stack_size-16384) * STACK_DIRECTION;
-	Pike_interpreter.recoveries = NULL;
-	thread_obj = fast_clone_object(thread_id_prog);
-	INIT_THREAD_STATE((struct thread_state *)(thread_obj->storage +
-						  thread_storage_offset));
-	num_threads++;
-	thread_table_insert(Pike_interpreter.thread_state);
-	state = Pike_interpreter.thread_state;
-	SWAP_OUT_THREAD(state);
-	mt_unlock_interpreter();
-    }
 
     call_with_interpreter(low_dispatch_fuse_command, &x );
 }
