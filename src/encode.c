2cca1eafe55a92fa413ab172752d246cba242542
Arne Goedeke <el@laramies.com>
2014-10-16 20:14:55 +0200

    decode_value: fix memory leak in PIKE_DEBUG
    free_decode_data() might throw errors in PIKE_DEBUG with
    !free_after_error, so keep the error handler around to prevent decode
    data from leaking.
    
    This is currently triggered when decoding SSL.State and SSL.Session.

--- src/encode.c
+++ src/encode.c
@@ -4302,8 +4302,6 @@ static INT32 my_decode(struct pike_string *tmp,
 
   low_do_decode (data);
 
-  UNSET_ONERROR(err);
-
   {
     int delay;
 #if TWO_PASS_DECODE_WORKS
@@ -4314,6 +4312,8 @@ static INT32 my_decode(struct pike_string *tmp,
 #endif
     free_decode_data (data, delay, 0);
   }
+
+  UNSET_ONERROR(err);
 
   return 1;
 }
