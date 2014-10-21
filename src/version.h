c4f2f5c3aca25eec364458b1233c399fdd931f0c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2014-10-21 16:06:39 +0200

    release number bumped to 894 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 893
+#define PIKE_BUILD_VERSION 894
 
 /* Prototypes begin here */
 void f_version(INT32 args);
