47b7eecc48097ee634f787a8450473c085184cf8
Jonas Wallden <jonasw@roxen.com>
2014-10-22 15:23:00 +0200

    release number bumped to 896 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 895
+#define PIKE_BUILD_VERSION 896
 
 /* Prototypes begin here */
 void f_version(INT32 args);
