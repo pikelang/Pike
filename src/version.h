5a14b8c48aed3f74da861d06d316c59a1447e42b
Marcus Wellhardh <wellhardh@roxen.com>
2014-11-26 11:47:31 +0100

    release number bumped to 909 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 908
+#define PIKE_BUILD_VERSION 909
 
 /* Prototypes begin here */
 void f_version(INT32 args);
