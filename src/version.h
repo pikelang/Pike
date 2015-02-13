96d067274cb90ba1e56c1305fd00d6dfcba0c403
Martin Karlgren <marty@roxen.com>
2015-02-13 14:30:31 +0100

    release number bumped to 916 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 915
+#define PIKE_BUILD_VERSION 916
 
 /* Prototypes begin here */
 void f_version(INT32 args);
