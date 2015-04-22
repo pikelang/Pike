12bb2acc022affddaa541fa424a752b91e5f401e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2015-04-22 18:48:45 +0200

    Lexer: Added fall-through marker. Fixes [CID 742662] and others.

--- src/lexer.h
+++ src/lexer.h
@@ -971,6 +971,7 @@ unknown_directive:
 	return TOK_NUMBER;
       }
     }
+    /* FALL_THROUGH */
 
     case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
