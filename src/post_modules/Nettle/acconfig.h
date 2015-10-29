e7c1df3b77addb024e992312631dc7e292d82c8c
Henrik Grubbström (Grubba) <grubba@grubba.org>
2015-10-29 16:33:22 +0100

    Build [Nettle]: Survive shadowing with older nettle.
    Correct erroneous configuration results.
    A typical cause of such is shadowing a more recent system nettle
    with an older local version, where configure will find headerfiles
    belonging to the system version if the header files aren't present
    in the (intended) older version.

--- src/post_modules/Nettle/acconfig.h
+++ src/post_modules/Nettle/acconfig.h
@@ -1,5 +1,18 @@
+/* Configuration settings for Nettle. */
+
+#ifndef PIKE_NETTLE_CONFIG_H
+#define PIKE_NETTLE_CONFIG_H
+
+@TOP@
+
 /* Define this if the nettle_crypt_func typedef is a pointer type. */
 #undef HAVE_NETTLE_CRYPT_FUNC_IS_POINTER
 
 /* Define this if generage_dsa_keypair takes the qbits argument */
 #undef HAVE_DSA_QBITS_KEYPAIR_ARG
+
+@BOTTOM@
+
+#include "nettle_config_fixup.h"
+
+#endif /* !PIKE_NETTLE_CONFIG_H */
