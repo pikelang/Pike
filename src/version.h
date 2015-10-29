69bfe55950a2dd3b15b07f4771b9a2f12a382721
Build system <distmaker@roxen.com>
2015-10-29 17:29:52 +0100

    release number bumped to 934 by export.pike

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define PIKE_MAJOR_VERSION 7
 #define PIKE_MINOR_VERSION 8
-#define PIKE_BUILD_VERSION 933
+#define PIKE_BUILD_VERSION 934
 
 /* Prototypes begin here */
 void f_version(INT32 args);
