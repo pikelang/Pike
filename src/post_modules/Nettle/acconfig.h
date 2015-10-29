04ef1e7784b6b200044276ea96115c6ddb601e8e
Henrik Grubbström (Grubba) <grubba@grubba.org>
2015-10-29 16:33:22 +0100

    Build [Nettle]: Survive shadowing with older nettle.
    Correct erroneous configuration results.
    A typical cause of such is shadowing a more recent system nettle
    with an older local version, where configure will find headerfiles
    belonging to the system version if the header files aren't present
    in the (intended) older version.

--- src/post_modules/Nettle/acconfig.h
+++ src/post_modules/Nettle/acconfig.h
@@ -1,5 +1,18 @@
+/* Configuration settings for Nettle. */
+
+#ifndef PIKE_NETTLE_CONFIG_H
+#define PIKE_NETTLE_CONFIG_H
+
+@TOP@
+
 /* Define this if your struct yarrow256_ctx has the field seed_file. */
 #undef HAVE_STRUCT_YARROW256_CTX_SEED_FILE
 
 /* Define this if the nettle_crypt_func typedef is a pointer type. */
 #undef HAVE_NETTLE_CRYPT_FUNC_IS_POINTER
+
+@BOTTOM@
+
+#include "nettle_config_fixup.h"
+
+#endif /* !PIKE_NETTLE_CONFIG_H */
---
+++ src/post_modules/Nettle/nettle_config_fixup.h
@@ -0,0 +1,47 @@
+/*
+ * Correct false positives from configure.
+ *
+ * Included from nettle_config.h to correct erroneous settings.
+ * A typical cause of such is shadowing a more recent system nettle
+ * with an older local version, where configure will find headerfiles
+ * belonging to the system version if the header files aren't present
+ * in the (intended) older version.
+ *
+ * Note: This must be in a separate file from nettle_config.h to
+ *       avoid configure rewriting the #undef directives.
+ */
+
+#ifndef PIKE_NETTLE_CONFIG_FIXUP_H
+#define PIKE_NETTLE_CONFIG_FIXUP_H
+
+/* MD4 */
+#ifndef HAVE_NETTLE_MD4_INIT
+#undef HAVE_NETTLE_MD4_H
+#endif
+
+/* Camellia */
+#ifndef HAVE_NETTLE_CAMELLIA128_CRYPT
+#undef HAVE_NETTLE_CAMELLIA_H
+#endif
+
+/* GCM */
+#ifndef HAVE_NETTLE_GCM_CRYPT
+#undef HAVE_NETTLE_GCM_H
+#endif
+
+/* RIPEMD160 */
+#ifndef HAVE_NETTLE_RIPEMD160_INIT
+#undef HAVE_NETTLE_RIPEMD160_H
+#endif
+
+/* SHA3 */
+#ifndef HAVE_NETTLE_SHA3_256_INIT
+#undef HAVE_NETTLE_SHA3_H
+#endif
+
+/* GOST94 */
+#ifndef HAVE_NETTLE_GOSTHASH94_INIT
+#undef HAVE_NETTLE_GOSTHASH94_H
+#endif
+
+#endif /* !PIKE_NETTLE_CONFIG_FIXUP_H */
