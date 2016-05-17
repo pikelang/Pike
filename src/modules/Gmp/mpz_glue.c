6088888e80cff0ec2381619d2270bcb15b6e15f7
Per Hedbor <ph@opera.com>
2016-05-17 16:47:56 +0200

    Use the same exponentiation result size limit in both locations, and set it to 1G

--- src/modules/Gmp/mpz_glue.c
+++ src/modules/Gmp/mpz_glue.c
@@ -1891,6 +1891,8 @@ static void mpzmod_pow(INT32 args)
   MP_INT *mi;
   INT_TYPE size = (INT_TYPE)mpz_size(THIS);
 
+#define POW_RAM_LIMIT (1024*1024*1024)
+
   if (args != 1)
     SIMPLE_WRONG_NUM_ARGS_ERROR ("pow", 1);
   if (TYPEOF(sp[-1]) == T_INT) {
@@ -1898,7 +1900,7 @@ static void mpzmod_pow(INT32 args)
     if (e < 0)
       SIMPLE_ARG_ERROR ("pow", 1, "Negative exponent.");
     /* Cut off at 1 MB. */
-    if (INT_TYPE_MUL_OVERFLOW(e, size) || size * e > (INT_TYPE)(0x100000/sizeof(mp_limb_t))) {
+    if (INT_TYPE_MUL_OVERFLOW(e, size) || size * e > (INT_TYPE)(POW_RAM_LIMIT/sizeof(mp_limb_t))) {
       if(mpz_cmp_si(THIS, -1)<0 || mpz_cmp_si(THIS, 1)>0)
 	 goto too_large;
     }
@@ -1911,7 +1913,7 @@ too_large:
       SIMPLE_ARG_ERROR ("pow", 1, "Negative exponent.");
     i=mpz_get_si(mi);
     /* Cut off at 100 MB. */
-    if(mpz_cmp_si(mi, i) || INT_TYPE_MUL_OVERFLOW(size, i) || (size*i>(INT_TYPE)(0x10000000/sizeof(mp_limb_t))))
+    if(mpz_cmp_si(mi, i) || INT_TYPE_MUL_OVERFLOW(size, i) || (size*i>(INT_TYPE)(POW_RAM_LIMIT/sizeof(mp_limb_t))))
     {
        if(mpz_cmp_si(THIS, -1)<0 || mpz_cmp_si(THIS, 1)>0)
 	 SIMPLE_ARG_ERROR ("pow", 1, "Exponent too large.");
