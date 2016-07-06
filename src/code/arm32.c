f96d486547ed78ff79765c09f0bf551afcd200ef
Armisch <arm@armish>
2016-07-06 23:13:36 +0930

    ARM32: implement F_MARK_AT

--- src/code/arm32.c
+++ src/code/arm32.c
@@ -1049,8 +1049,7 @@ static void arm32_free_svalue(enum arm32_register reg, int guaranteed_ref) {
     arm32_free_svalue_off(reg, 0, guaranteed_ref);
 }
 
-static void arm32_mark(int offset) {
-  arm32_load_sp_reg();
+static void arm32_mark(enum arm32_register base, int offset) {
 
   enum arm32_register tmp = ra_alloc_any();
 
@@ -1062,15 +1061,15 @@ static void arm32_mark(int offset) {
     offset *= sizeof(struct svalue);
 
     if (offset > 0) {
-      arm32_add_reg_int(tmp2, ARM_REG_PIKE_SP, offset);
+      arm32_add_reg_int(tmp2, base, offset);
     } else {
-      arm32_sub_reg_int(tmp2, ARM_REG_PIKE_SP, -offset);
+      arm32_sub_reg_int(tmp2, base, -offset);
     }
     store_reg_imm(tmp2, tmp, 0);
 
     ra_free(tmp2);
   } else {
-    store_reg_imm(ARM_REG_PIKE_SP, tmp, 0);
+    store_reg_imm(base, tmp, 0);
   }
 
   add_reg_imm(tmp, tmp, sizeof(void*), 0);
@@ -1176,7 +1175,8 @@ static void low_ins_f_byte(unsigned int b)
       arm32_free_svalue(ARM_REG_PIKE_SP, 0);
       return;
   case F_MARK:
-      arm32_mark(0);
+      arm32_load_sp_reg();
+      arm32_mark(ARM_REG_PIKE_SP, 0);
       return;
   case F_MARK2:
       ins_f_byte(F_MARK);
@@ -1323,8 +1323,14 @@ void ins_f_byte_with_arg(unsigned int a, INT32 b)
       ins_f_byte_with_arg(F_LOCAL, b);
       return;
   case F_LOCAL:
+      arm32_debug_instr_prologue_1(a, b);
       arm32_load_locals_reg();
       arm32_push_svaluep_off(ARM_REG_PIKE_LOCALS, b);
+      return;
+  case F_MARK_AT:
+      arm32_debug_instr_prologue_1(a, b);
+      arm32_load_locals_reg();
+      arm32_mark(ARM_REG_PIKE_LOCALS, b);
       return;
   }
   arm32_mov_int(ra_alloc(ARM_REG_R0), b);
