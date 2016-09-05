4bcbf7ba65518babc9a68efc52809f2f55c35345
Arne Goedeke <el@laramies.com>
2016-09-05 21:09:19 +0200

    ARM32: added missing argument to inc/dec opcodes

--- src/code/arm32.c
+++ src/code/arm32.c
@@ -2270,7 +2270,10 @@ void ins_f_byte_with_arg(unsigned int opcode, INT32 arg1)
 
           b_imm(label_dist(&done), ARM_COND_AL);
           label_generate(&fallback);
+          ra_alloc(ARM_REG_ARG1);
+          arm32_mov_int(ARM_REG_ARG1, arg1);
           arm32_call_c_opcode_slowpath(opcode);
+          ra_free(ARM_REG_ARG1);
 
           label_generate(&done);
       }
