ddf300e1faabfdb781a4f2577f4a4f3900a8c451
Henrik Grubbström (Grubba) <grubba@grubba.org>
2016-09-07 12:36:04 +0200

    Compiler [cpp]: Fixed stringify operator on strings with quotes.

--- src/preprocessor.h
+++ src/preprocessor.h
@@ -334,10 +334,12 @@ static ptrdiff_t low_cpp(struct cpp *this,
 				string_builder_putchar(&tmp, INDEX_PCHARP(a,e));
 				if (INDEX_PCHARP(a,e) == '\\') {
 				  string_builder_putchar(&tmp, '\\');
-				  string_builder_putchar(&tmp, INDEX_PCHARP(a,++e));
-				  if (INDEX_PCHARP(a,e) == '\\') {
+				  e++;
+				  if (INDEX_PCHARP(a,e) == '\\' ||
+				      INDEX_PCHARP(a,e) == '"') {
 				    string_builder_putchar(&tmp, '\\');
 				  }
+				  string_builder_putchar(&tmp, INDEX_PCHARP(a,e));
 				}
 			      }
 			      string_builder_putchar(&tmp, '\\');
