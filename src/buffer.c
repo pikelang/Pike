05bbfbc0a154a204858fed065720e27a3428e2e7
Arne Goedeke <el@laramies.com>
2016-11-10 16:19:04 +0100

    buffer: added _nothrow variants
    This variant of buffer_ensure_space() will return 0 instead of
    throwing an exception in OOM situation. It is sometimes easier to
    handle this explicitly, than registering an exception handler.

--- src/buffer.c
+++ src/buffer.c
@@ -12,7 +12,7 @@ PMOD_EXPORT void buffer_free(struct byte_buffer *b) {
     b->dst = NULL;
 }
 
-PMOD_EXPORT void buffer_make_space(struct byte_buffer *b, size_t len) {
+PMOD_EXPORT int buffer_make_space_nothrow(struct byte_buffer *b, size_t len) {
     size_t new_length = b->length;
     const size_t content_length = buffer_content_length(b);
     size_t needed = content_length;
@@ -21,19 +21,19 @@ PMOD_EXPORT void buffer_make_space(struct byte_buffer *b, size_t len) {
 #ifdef PIKE_DEBUG
     if (b->left >= len) Pike_fatal("Misuse of buffer_make_space.\n");
 #endif
-    if (DO_SIZE_T_ADD_OVERFLOW(len, content_length, &needed)) goto OUT_OF_MEMORY;
+    if (DO_SIZE_T_ADD_OVERFLOW(len, content_length, &needed)) goto OUT_OF_MEMORY_FATAL;
 
     if (new_length < 16) new_length = 16;
 
     while (new_length < needed) {
-        if (DO_SIZE_T_MUL_OVERFLOW(new_length, 2, &new_length)) goto OUT_OF_MEMORY;
+        if (DO_SIZE_T_MUL_OVERFLOW(new_length, 2, &new_length)) goto OUT_OF_MEMORY_FATAL;
     }
 
     /* fprintf(stderr, "BUF { left=%llu; length=%llu; dst=%p; }\n", b->left, b->length, b->dst); */
 
     ptr = realloc(buffer_ptr(b), new_length);
 
-    if (!ptr) goto OUT_OF_MEMORY;
+    if (UNLIKELY(!ptr)) return 0;
 
     /* fprintf(stderr, "realloc(%p, %llu) = %p\n", buffer_ptr(b), new_length, ptr); */
     /* fprintf(stderr, "content_length: %llu\n", content_length); */
@@ -41,9 +41,13 @@ PMOD_EXPORT void buffer_make_space(struct byte_buffer *b, size_t len) {
     b->dst = (char*)ptr + content_length;
     b->length = new_length;
     b->left = new_length - content_length;
-    return;
-OUT_OF_MEMORY:
+    return 1;
+OUT_OF_MEMORY_FATAL:
     Pike_fatal(msg_out_of_mem_2, len);
+}
+
+PMOD_EXPORT void buffer_make_space(struct byte_buffer *b, size_t len) {
+    if (UNLIKELY(!buffer_make_space_nothrow(b, len))) Pike_error(msg_out_of_mem_2, len);
 }
 
 PMOD_EXPORT void* buffer_finish(struct byte_buffer *b) {
