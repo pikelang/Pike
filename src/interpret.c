15796a4faffd1c19f788444cefe98a6a8859f6c9
Arne Goedeke <el@laramies.com>
2017-02-24 17:13:49 +0100

    Interpreter: check stack size before call

--- src/interpret.c
+++ src/interpret.c
@@ -3514,6 +3514,9 @@ PMOD_EXPORT void callsite_resolve_fun(struct pike_callsite *c, struct object *o,
 
   Pike_fp = frame;
   c->frame = frame;
+
+  check_stack(256);
+  check_mark_stack(256);
 }
 
 PMOD_EXPORT void callsite_resolve_lfun(struct pike_callsite *c, struct object *o, int lfun) {
