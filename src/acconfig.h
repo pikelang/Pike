18020169e6f66319b4fbc59174589746321b31df
William Welliver <william@welliver.org>
2017-08-04 09:25:38 -0400

    smartlink: support Darwin-style rpath

--- src/acconfig.h
+++ src/acconfig.h
@@ -111,6 +111,9 @@
 /* Define this if your ld uses -rpath, but your cc wants -Wl,-rpath, */
 #undef USE_Wl
 
+/* Define this if your ld uses Darwin-style -rpath, but your cc wants -Wl,-rpath, */
+#undef USE_Wl_rpath_darwin
+
 /* Define this if your ld uses -R, but your cc wants -Wl,-R */
 #undef USE_Wl_R
 
--- src/smartlink.c
+++ src/smartlink.c
@@ -8,7 +8,7 @@
  * smartlink - A smarter linker.
  * Based on the /bin/sh script smartlink 1.23.
  *
- * Henrik Grubbström 1999-03-04
+ * Henrik Grubbstrï¿½m 1999-03-04
  */
 
 #ifdef __MINGW32__
@@ -96,6 +96,13 @@ int main(int argc, char **argv)
   char *full_rpath;
   char **new_argv;
   char *ld_lib_path;
+  
+#if defined(USE_Wl_rpath_darwin)
+  char **darwin_argv;
+  char * darwin_arg;
+  int darwin_argc = 0;
+#endif
+
   int new_argc;
   int n32 = 0;
   int linking = 1;	/* Maybe */
@@ -157,6 +164,14 @@ int main(int argc, char **argv)
   if (!(new_argv = malloc(sizeof(char *)*(argc + 150)))) {
     fatal("Out of memory (5)!\n");
   }
+  
+#if defined(USE_Wl_rpath_darwin)
+  /* 50 rpath args should be enough... */
+    if (!(darwin_argv = malloc(sizeof(char *)*(argc + 50)))) {
+    fatal("Out of memory (6)!\n");
+  }
+#endif
+
 
   new_argc = 0;
   full_rpath = rpath;
@@ -204,9 +219,27 @@ int main(int argc, char **argv)
 	  i++;
 	  if (i < argc) {
 	    rpath_in_use |= add_path(rpath, argv[i]);
+#if defined(USE_Wl_rpath_darwin)
+           if(!(darwin_arg = malloc(sizeof(char *) * (strlen(argv[i]) + 12)))) /*  path lengh plus length of -Wl,-rpath, */
+           {
+             fatal("Out of memory (7)!\n");
+           }
+           darwin_arg = strcat(darwin_arg, "-Wl,-rpath,");
+           darwin_arg = strcat(darwin_arg, argv[i]);
+           darwin_argv[darwin_argc++] = darwin_arg;
+#endif
 	  }
 	} else {
 	  rpath_in_use |= add_path(rpath, argv[i] + 2);
+#if defined(USE_Wl_rpath_darwin)
+          if(!(darwin_arg = malloc(sizeof(char *) * (strlen(argv[i] + 2) + 12)))) /*  path lengh plus length of -Wl,-rpath, */
+          {
+            fatal("Out of memory (7.5)!\n");
+          }
+          darwin_arg = strcat(darwin_arg, "-Wl,-rpath,");
+          darwin_arg = strcat(darwin_arg, argv[i] + 2);
+          darwin_argv[darwin_argc++] = darwin_arg;
+#endif
 	}
 	continue;
       } else if ((argv[i][1] == 'n') && (argv[i][2] == '3') &&
@@ -344,6 +377,9 @@ int main(int argc, char **argv)
     }
 #elif defined(USE_R) || defined(USE_Wl_R)
     new_argv[new_argc++] = full_rpath;
+#elif defined(USE_Wl_rpath_darwin)
+   for(int darwin_argc_i = 0; darwin_argc_i < darwin_argc; darwin_argc_i++)
+    new_argv[new_argc++] = darwin_argv[darwin_argc_i];
 #elif defined(USE_LD_LIBRARY_PATH)
     if (putenv(full_rpath)) {
       fatal("Out of memory (6)!");
