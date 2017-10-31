be932f508271a7d03fb7b397dbef5f0e3d99ea51
Arne Goedeke <el@laramies.com>
2017-10-31 16:04:49 +0100

    push_string(): handle OOM errors correctly
    make_shared_string() may throw an error in OOM situation. If that
    happens here, we will end up having a corrupt svalue on the stack.

--- src/interpret.c
+++ src/interpret.c
@@ -3133,18 +3133,20 @@ void low_safe_apply_handler(const char *fun,
 
 PMOD_EXPORT void push_text( const char *x )
 {
+    struct pike_string *s = make_shared_string(x);
     struct svalue *_sp_ = Pike_sp++;
     SET_SVAL_SUBTYPE(*_sp_, 0);
-    _sp_->u.string=make_shared_string(x);
+    _sp_->u.string=s
     debug_malloc_touch(_sp_->u.string);
     SET_SVAL_TYPE(*_sp_, PIKE_T_STRING);
 }
 
 PMOD_EXPORT void push_static_text( const char *x )
 {
+    struct pike_string *s = make_shared_static_string(x, strlen(x), eightbit);
     struct svalue *_sp_ = Pike_sp++;
     SET_SVAL_SUBTYPE(*_sp_, 0);
-    _sp_->u.string=make_shared_static_string(x, strlen(x), eightbit);
+    _sp_->u.string=s;
     debug_malloc_touch(_sp_->u.string);
     SET_SVAL_TYPE(*_sp_, PIKE_T_STRING);
 }
