5fb1bbc4a0ebac20d65e19ee275cc9cdd215384f
Henrik Grubbström (Grubba) <grubba@grubba.org>
2017-11-07 17:32:15 +0100

    Build: Fixed syntax error --with-dmalloc.

--- src/interpret.c
+++ src/interpret.c
@@ -3104,7 +3104,7 @@ PMOD_EXPORT void push_text( const char *x )
     struct pike_string *s = make_shared_string(x);
     struct svalue *_sp_ = Pike_sp++;
     SET_SVAL_SUBTYPE(*_sp_, 0);
-    _sp_->u.string=s
+    _sp_->u.string=s;
     debug_malloc_touch(_sp_->u.string);
     SET_SVAL_TYPE(*_sp_, PIKE_T_STRING);
 }
