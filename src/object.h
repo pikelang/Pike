352c2dbb6f19aef354275a81ed4fc58e794e511b
Arne Goedeke <el@laramies.com>
2018-02-26 20:54:29 +0100

    Internal: added API to get writable buffer space
    The original API only allowed for getting the space inside of buffer
    objects which contains data. This new API allows also for querying for
    writeable space, too. This will be used in Stdio.read() to read
    directly into a buffer.

--- src/object.h
+++ src/object.h
@@ -149,7 +149,15 @@ enum memobj_type{
     MEMOBJ_STDIO_IOBUFFER,
 };
 
+struct pike_memory_object {
+  void *ptr;
+  size_t len;
+  int shift;
+};
+
 PMOD_EXPORT enum memobj_type get_memory_object_memory( struct object *o, void **ptr, size_t *len, int *shift );
+PMOD_EXPORT enum memobj_type pike_get_memory_object( struct object *o, struct pike_memory_object *m,
+                                                     int writeable );
 
 
 unsigned gc_touch_all_objects(void);
