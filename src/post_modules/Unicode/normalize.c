015f43286472fbdbd7ce32315740063f0ab3d605
Henrik Grubbström (Grubba) <grubba@grubba.org>
2018-03-01 14:31:25 +0100

    Unicode: Optimize normalize() in NFC mode on 8-bit strings.
    Unicode.normalize() in NFS mode on 8-bit strings is a noop,
    so there's no need to scan though the string.
    
    Fixes [PIKE-79].

--- src/post_modules/Unicode/normalize.c
+++ src/post_modules/Unicode/normalize.c
@@ -1,7 +1,7 @@
 #include "global.h"
 #include "stralloc.h"
 #include "global.h"
-RCSID("$Id: normalize.c,v 1.7 2001/11/22 14:52:18 grubba Exp $");
+RCSID("$Id$");
 #include "pike_macros.h"
 #include "interpret.h"
 #include "program.h"
@@ -276,7 +276,14 @@ struct pike_string *unicode_normalize( struct pike_string *source,
     return source;
   }
   /* What, me lisp? */
-  if( how & COMPOSE_BIT )
+  if( how & COMPOSE_BIT ) {
+    if (!source->size_shift && !(how & COMPAT_BIT)) {
+      /* NB: There are 8-bit characters that are changed in
+       *     compat mode; eg NBSP (0xA0) and DIAERESIS (0xA8).
+       */
+      add_ref(source);
+      return source;
+    }
     return
       uc_buffer_to_pikestring(
 	unicode_compose_buffer(
@@ -286,6 +293,7 @@ struct pike_string *unicode_normalize( struct pike_string *source,
 	      source ),
 	    how ),
 	  how ) );
+  }
   return
     uc_buffer_to_pikestring(
       unicode_decompose_buffer(
