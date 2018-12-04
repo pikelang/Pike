4d4c4c0f504a5f52f05e74ffc0684b1502e9148a
Henrik Grubbström (Grubba) <grubba@grubba.org>
2018-12-04 13:00:21 +0100

    EFUNs: Fixed leak of arrays in annotations() on programs.

--- src/program.c
+++ src/program.c
@@ -8049,6 +8049,7 @@ struct array *program_annotations(struct program *p, int flags)
       }
     }
   }
+  do_free_array(inherit_annotations);
   f_aggregate(n);
   res = Pike_sp[-1].u.array;
   add_ref(res);
