99ab241a00ee95a7c6c033016dfe61ce74bfb6bd
Henrik Grubbström (Grubba) <grubba@grubba.org>
2019-05-04 12:09:30 +0200

    CritBit [bignum]: Added detection of broken gmp header files.
    Fixes issues with the system libgmp on some versions of Solaris.

---
+++ src/post_modules/CritBit/acconfig.h
@@ -0,0 +1,19 @@
+/* -*- c -*-
+|| This file is part of Pike. For copyright information see COPYRIGHT.
+|| Pike is distributed under GPL, LGPL and MPL. See the file COPYING
+|| for more information.
+*/
+
+#ifndef GMP_MACHINE_H
+#define GMP_MACHINE_H
+
+@TOP@
+@BOTTOM@
+
+/* Define if GMP_LIMB_BITS lies. */
+#undef PIKE_GMP_LIMB_BITS_INVALID
+
+/* Define to the actual number of numb bits if known. */
+#undef PIKE_GMP_NUMB_BITS
+
+#endif
--- src/post_modules/CritBit/critbit/key_bignum.h
+++ src/post_modules/CritBit/critbit/key_bignum.h
@@ -4,6 +4,7 @@
 #include "pike_int_types.h"
 #include <gmp.h>
 #include "bignum.h"
+#include "critbit_machine.h"
 
 typedef struct object * CB_NAME(string);
 typedef mp_limb_t CB_NAME(char);
@@ -17,6 +18,16 @@ typedef mp_limb_t CB_NAME(char);
 # undef cb_char
 #endif
 #define cb_char CB_NAME(char)
+
+#ifdef PIKE_GMP_LIMB_BITS_INVALID
+/* Attempt to repair the header file... */
+#undef GMP_LIMB_BITS
+#define GMP_LIMB_BITS (SIZEOF_MP_LIMB_T * CHAR_BIT)
+#ifdef PIKE_GMP_NUMB_BITS
+#undef GMP_NUMB_BITS
+#define GMP_NUMB_BITS PIKE_GMP_NUMB_BITS
+#endif /* PIKE_GMP_NUMB_BITS */
+#endif /* PIKE_GMP_LIMB_BITS_INVALID */
 
 static CB_INLINE unsigned INT32 gclz(mp_limb_t a) {
 #if GMP_NUMB_BITS == 64
