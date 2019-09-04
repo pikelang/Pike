82c97fd1ddda076375901fa5f760279acd54497b
Henrik Grubbström (Grubba) <grubba@grubba.org>
2019-09-04 11:14:57 +0200

    Image.Image: Support count_memory() for images.
    The memory allocated for image bitmaps is now included
    in the value reported by count_memory().
    
    Fixes [PIKE-204].

--- src/modules/Image/image.c
+++ src/modules/Image/image.c
@@ -2,7 +2,7 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: image.c,v 1.235 2007/08/10 13:54:51 grubba Exp $
+|| $Id$
 */
 
 /*
@@ -106,6 +106,7 @@
 #include "pike_macros.h"
 #include "object.h"
 #include "interpret.h"
+#include "gc.h"
 #include "svalue.h"
 #include "threads.h"
 #include "array.h"
@@ -194,6 +195,13 @@ static void exit_image_struct(struct object *obj)
 	  THIS->xsize,THIS->ysize,
 	  (THIS->xsize*THIS->ysize*sizeof(rgb_group)+sizeof(struct image))/1024.0);
 	  */
+}
+
+static void gc_recurse_image_struct(struct object *UNUSED(obj))
+{
+  if (mc_count_bytes(Pike_fp->current_object) && THIS->img) {
+    mc_counted_bytes += sizeof(rgb_group)*THIS->xsize*(long)THIS->ysize;
+  }
 }
 
 /***************** internals ***********************************/
@@ -5126,6 +5134,8 @@ void init_image_image(void)
 
    set_init_callback(init_image_struct);
    set_exit_callback(exit_image_struct);
+
+   set_gc_recurse_callback(gc_recurse_image_struct);
 
 
 #ifndef FAKE_DYNAMIC_LOAD
