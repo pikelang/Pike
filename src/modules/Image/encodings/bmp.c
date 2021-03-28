03b435dbe87861f8bf1b0621016d3675abab1218
Tobias S. Josefowitz <tobij@tobij.de>
2021-03-29 01:24:27 +0200

    Image.BMP: Error on images with less than two dimensions
    Thanks to Cezary Cerekwicki <ccerekwicki@opera.com> for the report.

--- src/modules/Image/encodings/bmp.c
+++ src/modules/Image/encodings/bmp.c
@@ -704,6 +704,10 @@ void i_img_bmp__decode(INT32 args,int header_only)
 
    push_int(xsize);
    push_int(ysize);
+
+   if (!(xsize || ysize))
+      Pike_error("Image.BMP.decode: Cannot decode image with less than two dimensions.\n");
+
    push_object(o=clone_object(image_program,2));
    img=(struct image*)get_storage(o,image_program);
    n++;
