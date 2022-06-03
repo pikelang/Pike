c8449820688fac0d855596e9c0c37c4625e0cbc5
Marcus Comstedt <marcus@mc.pp.se>
2022-06-03 21:07:08 +0200

    WhiteFish: Fix warning

--- src/modules/_WhiteFish/blob.c
+++ src/modules/_WhiteFish/blob.c
@@ -57,7 +57,7 @@ int wf_blob_next( Blob *b )
     }
     ref_push_string( b->word );
     push_int( b->docid );
-    push_int64( (INT64) b );
+    push_int64( (INT64)(ptrdiff_t) b );
     apply_svalue( b->feed, 3 );
     if( TYPEOF(sp[-1]) != T_STRING )
     {
@@ -80,7 +80,7 @@ int wf_blob_next( Blob *b )
       }
       ref_push_string( b->word );
       push_int( b->docid );
-      push_int64( (INT64) b );
+      push_int64( (INT64)(ptrdiff_t) b );
       apply_svalue( b->feed, 3 );
       if( TYPEOF(sp[-1]) != T_STRING )
       {
