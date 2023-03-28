304ea92ee26fcee274ee0ceaf1667741fe9a3607
Henrik Grubbström (Grubba) <grubba@grubba.org>
2023-03-28 16:12:33 +0200

    Image: Add Emacs indentation metadata where relevant.

--- src/modules/Image/encodings/xwd.c
+++ src/modules/Image/encodings/xwd.c
@@ -1,4 +1,4 @@
-/*
+/* -*- mode: C; c-basic-offset: 3; -*-
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
