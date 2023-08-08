666d1345ea3c1386bc9ab08db25833fc04f01969
Henrik Grubbström (Grubba) <grubba@grubba.org>
2023-08-08 10:24:04 +0200

    CritBit: Adjust error formatting string.
    Fixes some warnings from gcc.

--- src/post_modules/CritBit/tree_high.c
+++ src/post_modules/CritBit/tree_high.c
@@ -223,8 +223,8 @@ static inline size_t _low_cb_check_node(cb_node_t node,
 
     if (len + CB_HAS_VALUE(node) != node->size) {
 	/* Pike_error("Found node with wrong size. is: %p\n", node); */
-	Pike_error("%s:%d Found node with wrong size. is: 0x%08X\n",
-		   file, line, node);
+        Pike_error("%s:%d Found node with wrong size. is: 0x%08lX\n",
+                   file, line, (unsigned long)(size_t)node);
     }
 
     return node->size;
