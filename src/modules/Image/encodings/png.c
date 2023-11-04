89a48f956553408852f06adcc724b695ac8e8323
Tobias S. Josefowitz <tobij@tobij.de>
2023-11-04 16:19:33 +0100

    Image.PNG: Remove $Id$

--- src/modules/Image/encodings/png.c
+++ src/modules/Image/encodings/png.c
@@ -2,7 +2,6 @@
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
-|| $Id: png.c,v 1.100 2010/03/02 17:27:03 grubba Exp $
 */
 
 #include "global.h"
