2fdc727286714c0c889b2a12576340584c9198cb
Henrik Grubbström (Grubba) <grubba@grubba.org>
2025-03-20 08:14:24 +0100

    Image.DSI: Add encode().
    Also adjusts decode() to not lose some color bits.
    
    Adds testsuite per #10180.

--- src/modules/Image/encodings/dsi.c
+++ src/modules/Image/encodings/dsi.c
@@ -54,7 +54,7 @@ static void f__decode( INT32 args )
   struct object *i, *a;
   struct image *ip, *ap;
   rgb_group black = {0,0,0};
-  if( TYPEOF(sp[-args]) != T_STRING )
+  if( !args || TYPEOF(sp[-args]) != T_STRING )
     Pike_error("Illegal argument 1 to Image.DSI._decode\n");
   data = (unsigned char *)sp[-args].u.string->str;
   len = (size_t)sp[-args].u.string->len;
@@ -95,9 +95,9 @@ static void f__decode( INT32 args )
         r = ((px>>11) & 31);
         g = ((px>>5) & 63);
         b = ((px) & 31);
-        p.r = (r*255)/31;
-        p.g = (g*255)/63;
-        p.b = (b*255)/31;
+        p.r = (r * 0x21)>>2;	/* (r<<3) | (r>>2) */
+        p.g = (g * 0x41)>>4;	/* (g<<2) | (g>>4) */
+        p.b = (b * 0x21)>>2;	/* (b<<3) | (b>>2) */
         ip->img[ x + y*xs ] = p;
       }
     }
@@ -119,10 +119,82 @@ static void f_decode( INT32 args )
   f_index( 2 );
 }
 
+/*! @decl string(8bit) encode(Image.Image img)
+ *!
+ *!  Encode the image to DSI format.
+ *!
+ *! @note
+ *!  Pixels of color full red, no green, full blue (ie full magenta)
+ *!  will be regarded as transparent by @[decode()]. Pixels that
+ *!  would have mapped to that color (@expr{0xf81f@}) by the colorspace
+ *!  reduction will instead be mapped to full red, min green, full blue
+ *!  (@expr{0xf83f@}).
+ */
+static void f_encode(INT32 args)
+{
+  struct image *img = NULL;
+  size_t x, y;
+  rgb_group *s;
+  struct pike_string *res = NULL;
+  p_wchar0 *pos;
+
+  if (!args) {
+     SIMPLE_WRONG_NUM_ARGS_ERROR("encode", 1);
+  }
+
+  if (TYPEOF(Pike_sp[-args]) != T_OBJECT ||
+      !(img = get_storage(Pike_sp[-args].u.object, image_program))) {
+    SIMPLE_ARG_TYPE_ERROR("encode", 1, "Image.Image");
+  }
+
+  if (!img->img) {
+    PIKE_ERROR("encode", "No image.\n", sp, args);
+  }
+
+  if ((img->xsize & ~0x7fffffff) || (img->ysize & ~0x7fffffff) ||
+      INT32_MUL_OVERFLOW(img->xsize, img->ysize)) {
+    PIKE_ERROR("encode", "Invalid dimensions.\n", sp, args);
+  }
+
+  res = begin_shared_string((img->xsize * img->ysize * 2) + 8);
+  pos = STR0(res);
+
+  x = (size_t)img->xsize;
+  *pos++ = x & 0xff;
+  *pos++ = (x>>8) & 0xff;
+  *pos++ = (x>>16) & 0xff;
+  *pos++ = (x>>24) & 0xff;
+
+  y = (size_t)img->ysize;
+  *pos++ = y & 0xff;
+  *pos++ = (y>>8) & 0xff;
+  *pos++ = (y>>16) & 0xff;
+  *pos++ = (y>>24) & 0xff;
+
+  s = img->img;
+  for (y = (size_t)img->ysize; y--;) {
+    for (x = (size_t)img->xsize; x--;) {
+      unsigned char r = s->r>>3;
+      unsigned char g = s->g>>2;
+      unsigned char b = s->b>>3;
+      unsigned int c = (r << 11) | (g << 5) | b;
+      if ((c == 0xf81f) && ((s->r != 0xff) || s->g || (s->b != 0xff))) {
+        /* Pixel got transparent due to color space reduction. */
+        c = 0xf83f;
+      }
+      *pos++ = c & 0xff;
+      *pos++ = (c>>8) & 0xff;
+      s++;
+    }
+  }
+  push_string(end_shared_string(res));
+}
+
 void init_image_dsi(void)
 {
   ADD_FUNCTION("_decode", f__decode, tFunc(tStr,tMapping), 0);
   ADD_FUNCTION("decode", f_decode, tFunc(tStr,tObj), 0);
+  ADD_FUNCTION("encode", f_encode, tFunc(tObj,tStr), 0);
 }
 
 
