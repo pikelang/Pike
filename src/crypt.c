30fc41602fff2f25fc7b7a8dd10a72697d4e2b30
Henrik Grubbström (Grubba) <grubba@grubba.org>
2025-05-10 15:36:46 +0200

    Core [NT][OpenBSD]: crypt.c needs to include <stddef.h>.

--- src/crypt.c
+++ src/crypt.c
@@ -1,10 +1,11 @@
-/* -*- mode: C; c-basic-offset: 8; -*- */
+/* -*- mode: C; c-basic-offset: 8; indent-tabs-mode: t; -*- */
 /*
 || This file is part of Pike. For copyright information see COPYRIGHT.
 || Pike is distributed under GPL, LGPL and MPL. See the file COPYING
 || for more information.
 */
 
+#include <stddef.h>
 #include <errno.h>
 
 /*
