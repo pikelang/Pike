f2290d8d7a6d0b12b6f3a4b9b380d2b126faed01
Henrik Grubbström (Grubba) <grubba@grubba.org>
2025-11-12 08:03:09 +0100

    EFUNs: Consider weak flag for arrays and multisets in equal().

--- src/array.c
+++ src/array.c
@@ -1930,6 +1930,10 @@ PMOD_EXPORT int array_equal_p(struct array *a, struct array *b, struct processin
   if(a == b) return 1;
   if(!a || !b) return 0;
   if(a->size != b->size) return 0;
+  if ((a->flags ^ b->flags) & ARRAY_WEAK_FLAG) {
+    /* Weak flag differs. */
+    return 0;
+  }
   if(!a->size) return 1;
 
   if ((a->flags ^ b->flags) & ARRAY_WEAK_FLAG) return 0;
